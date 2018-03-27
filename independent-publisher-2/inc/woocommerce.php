b58060e973823c41f6c6760c0f05608e16b4e00d
Jeffrey Pearce <jeffikus@gmail.com>
2018-03-27 15:48:41 +0200

    Independent Publisher 2: Fixes Jetpack Infinite Scroll WC issues.

--- independent-publisher-2/inc/woocommerce.php
+++ independent-publisher-2/inc/woocommerce.php
@@ -87,7 +87,9 @@ add_filter( 'body_class', 'ip_woocommerce_active_body_class' );
 function ip_woocommerce_products_per_page() {
 	return 12;
 }
-add_filter( 'loop_shop_per_page', 'ip_woocommerce_products_per_page' );
+if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.3', '<' ) ) {
+	add_filter( 'loop_shop_per_page', 'ip_woocommerce_products_per_page' );
+}
 
 /**
  * Product gallery thumnbail columns.
@@ -293,21 +295,7 @@ function ip_woocommerce_is_shop_page() {
 	return $is_shop_page;
 }
 
-/**
- * Jetpack infinite scroll duplicates posts where orderby is anything other than modified or date
- * This filter offsets the products returned by however many are displayed per page
- *
- * @link https://github.com/Automattic/jetpack/issues/1135
- * @param  array $args infinite scroll args.
- * @return array       infinite scroll args.
- */
-function ip_woocommerce_jetpack_duplicate_products( $args ) {
-	if ( ( isset( $args['post_type'] ) && 'product' === $args['post_type'] ) || ( isset( $args['taxonomy'] ) && 'product_cat' === $args['taxonomy'] ) ) {
-		$args['offset'] = $args['posts_per_page'] * $args['paged'];
-	}
- 	return $args;
-}
-add_filter( 'infinite_scroll_query_args', 'ip_woocommerce_jetpack_duplicate_products', 100 );
+
 /**
  * Override number of products per page in Jetpack infinite scroll.
  *
