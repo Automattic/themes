b399ab11ab1deb19ea6a3b0da42334212ddb3853
Chris Runnells <chris.runnells@automattic.com>
2018-10-18 18:45:50 -1000

    Revert "Revert "Merge branch 'master' of https://github.com/Automattic/themes""
    This reverts commit 96c121674883a06850d4e59706c056891c98f541.

--- independent-publisher-2/inc/extras.php
+++ independent-publisher-2/inc/extras.php
@@ -67,3 +67,60 @@ function independent_publisher_2_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'independent_publisher_2_pingback_header' );
+
+/**
+ * Utility function to check if a gravatar exists for a given email or id
+ * @param int|string|object $id_or_email A user ID,  email address, or comment object
+ * @return bool if the gravatar exists or not
+ * @link https://gist.github.com/justinph/5197810
+ */
+
+function validate_gravatar( $id_or_email ) {
+	// The id or email code is borrowed from wp-includes/pluggable.php.
+	$email = '';
+	if ( is_numeric( $id_or_email ) ) {
+		$id   = (int) $id_or_email;
+		$user = get_userdata( $id );
+		if ( $user ) {
+			$email = $user->user_email;
+		}
+	} elseif ( is_object( $id_or_email ) ) {
+		// No avatar for pingbacks or trackbacks.
+		$allowed_comment_types = apply_filters( 'get_avatar_comment_types', array( 'comment' ) );
+		if ( ! empty( $id_or_email->comment_type ) && ! in_array( $id_or_email->comment_type, (array) $allowed_comment_types, true ) ) {
+			return false;
+		}
+
+		if ( ! empty( $id_or_email->user_id ) ) {
+			$id   = (int) $id_or_email->user_id;
+			$user = get_userdata( $id );
+			if ( $user ) {
+				$email = $user->user_email;
+			}
+		} elseif ( ! empty( $id_or_email->comment_author_email ) ) {
+			$email = $id_or_email->comment_author_email;
+		}
+	} else {
+		$email = $id_or_email;
+	}
+
+	$hashkey = md5( strtolower( trim( $email ) ) );
+	$uri     = 'http://www.gravatar.com/avatar/' . $hashkey . '?d=404';
+	$data    = wp_cache_get( $hashkey );
+	$expire  = 60 * 5;
+	$group   = '';
+	if ( false === $data ) {
+		$response = wp_remote_head( $uri );
+		if ( is_wp_error( $response ) ) {
+			$data = 'not200';
+		} else {
+			$data = $response['response']['code'];
+		}
+		wp_cache_set( $hashkey, $data, $group, $expire );
+	}
+	if ( 200 === $data ) {
+		return true;
+	} else {
+		return false;
+	}
+}
--- pique/inc/woocommerce.php
+++ pique/inc/woocommerce.php
@@ -112,7 +112,10 @@ add_filter( 'body_class', 'pique_woocommerce_active_body_class' );
  * @return integer number of products.
  */
 function pique_woocommerce_products_per_page() {
-	return 12;
+	return absint( apply_filters( 'pique_woocommerce_products_per_page', 12 ) );
+}
+if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.3', '<' ) ) {
+	add_filter( 'loop_shop_per_page', 'pique_woocommerce_products_per_page' );
 }
 
 // Legacy WooCommerce products per page filter.
@@ -126,7 +129,7 @@ if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.3', '<' ) ) {
  * @return integer number of columns.
  */
 function pique_woocommerce_thumbnail_columns() {
-	return 4;
+	return absint( apply_filters( 'pique_woocommerce_product_thumbnail_columns', 4 ) );
 }
 add_filter( 'woocommerce_product_thumbnails_columns', 'pique_woocommerce_thumbnail_columns' );
 
@@ -136,7 +139,7 @@ add_filter( 'woocommerce_product_thumbnails_columns', 'pique_woocommerce_thumbna
  * @return integer products per row.
  */
 function pique_woocommerce_loop_columns() {
-	return 3;
+	return absint( apply_filters( 'pique_woocommerce_loop_columns', 3 ) );
 }
 
 // Legacy WooCommerce columns filter.
@@ -151,12 +154,10 @@ if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.3', '<' ) ) {
  * @return array $args related products args.
  */
 function pique_woocommerce_related_products_args( $args ) {
-	$defaults = array(
+	$args = apply_filters( 'radcliffe_2_woocommerce_related_products_args', array(
 		'posts_per_page' => 3,
 		'columns'        => 3,
-	);
-
-	$args = wp_parse_args( $defaults, $args );
+	) );
 
 	return $args;
 }
@@ -241,18 +242,6 @@ if ( ! function_exists( 'pique_woocommerce_wrapper_after' ) ) {
 	}
 }
 add_action( 'woocommerce_after_main_content', 'pique_woocommerce_wrapper_after' );
-
-/**
- * Sample implementation of the WooCommerce Mini Cart.
- *
- * You can add the WooCommerce Mini Cart to header.php like so ...
- *
-	<?php
-		if ( function_exists( 'pique_woocommerce_header_cart' ) ) {
-			pique_woocommerce_header_cart();
-		}
-	?>
- */
 
 if ( ! function_exists( 'pique_woocommerce_cart_link_fragment' ) ) {
 	/**
@@ -410,22 +399,6 @@ function pique_woocommerce_is_shop_page() {
 	}
 	return $is_shop_page;
 }
-
-/**
- * Jetpack infinite scroll duplicates posts where orderby is anything other than modified or date
- * This filter offsets the products returned by however many are displayed per page
- *
- * @link https://github.com/Automattic/jetpack/issues/1135
- * @param  array $args infinite scroll args.
- * @return array       infinite scroll args.
- */
-function pique_woocommerce_jetpack_duplicate_products( $args ) {
-	if ( ( isset( $args['post_type'] ) && 'product' === $args['post_type'] ) || ( isset( $args['taxonomy'] ) && 'product_cat' === $args['taxonomy'] ) ) {
-		$args['offset'] = $args['posts_per_page'] * $args['paged'];
-	}
- 	return $args;
-}
-add_filter( 'infinite_scroll_query_args', 'pique_woocommerce_jetpack_duplicate_products', 100 );
 
 /**
  * Override number of products per page in Jetpack infinite scroll.
