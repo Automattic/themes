e455dc3fc85108d14561b8573ae11cfd83f69169
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-07-13 22:06:06 +0200

    Modern Business: add FSE support
    This adds required functionality to support rendering FSE header and
    footer on all site pages (static and dynamic). It's also includes the
    data population functionality, which has now been removed from the plugin
    and will be delegated to each theme for maximum flexibility.

---
+++ modern-business/footer.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+</div><!-- #content -->
+
+<?php
+// If FSE plugin is active, use Footer template part for content.
+if( defined( 'A8C_FSE_VERSION' ) ) {
+	fse_get_footer();
+}
+
+// Otherwise we'll fall back to default Twenty Nineteen footer below.
+?>
+
+<?php if( ! defined( 'A8C_FSE_VERSION' ) ) : ?>
+	<footer id="colophon" class="site-footer">
+		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
+		<div class="site-info">
+			<?php $blog_info = get_bloginfo( 'name' ); ?>
+			<?php if ( ! empty( $blog_info ) ) : ?>
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+			<?php endif; ?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
+				<?php
+				/* translators: %s: WordPress. */
+				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
+				?>
+			</a>
+			<?php
+			if ( function_exists( 'the_privacy_policy_link' ) ) {
+				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+			}
+			?>
+			<?php if ( has_nav_menu( 'footer' ) ) : ?>
+				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'twentynineteen' ); ?>">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'menu_class'     => 'footer-menu',
+							'depth'          => 1,
+						)
+					);
+					?>
+				</nav><!-- .footer-navigation -->
+			<?php endif; ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+<?php endif; ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
