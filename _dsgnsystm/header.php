fa7d7b0705c55d81ccd7d6f63e8ffdf7b2da3cac
Allan Cole <allan.cole@automattic.com>
2019-07-17 17:25:40 -0400

    _dsgnsystm: Replace drop-down arrow with open(+) and close(x) icons

--- _dsgnsystm/header.php
+++ _dsgnsystm/header.php
@@ -32,6 +32,8 @@
 					<input type="checkbox" role="button" aria-haspopup="true" id="toggle" class="hide-visually">
 					<label for="toggle" id="toggle-menu" class="button">
 						<?php _e( 'Menu', '_dsgnsystm' ); ?>
+						<span class="dropdown-icon open">+</span>
+						<span class="dropdown-icon close">&times;</span>
 						<span class="hide-visually expanded-text">expanded</span>
 						<span class="hide-visually collapsed-text">collapsed</span>
 					</label>
