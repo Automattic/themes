c2589e40b3d89cbff9eca023b9159bb282287764
sharaz <sharazg5@outlook.com>
2019-08-01 20:04:51 -0700

    dyad-2: wp_body_open() function added after body tag

--- dyad-2/header.php
+++ dyad-2/header.php
@@ -19,6 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'dyad-2' ); ?></a>
 
