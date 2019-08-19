31fa2331f81d33e5f531ba22cba77122e3d96b42
Allan Cole <allan.cole@automattic.com>
2019-08-19 11:20:38 -0400

    Varia: Remove `.responsive-max-width` class from wrappers to allow alignfull and alignwide to work properly on post, pages, search, image and archives

--- varia/archive.php
+++ varia/archive.php
@@ -13,7 +13,7 @@ get_header();
 ?>
 
 	<section id="primary" class="content-area">
-		<main id="main" class="site-main responsive-max-width">
+		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
 
