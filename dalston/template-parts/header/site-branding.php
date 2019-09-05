1a8498493aaf86312c23a6e648817afce3139d12
Allan Cole <allan.cole@automattic.com>
2019-09-05 10:44:23 -0400

    Dalston: Redesigning the header layout

--- dalston/template-parts/header/site-branding.php
+++ dalston/template-parts/header/site-branding.php
@@ -7,7 +7,7 @@
  * @since 1.0.0
  */
 ?>
-
+<div class="site-branding">
 <?php if ( has_custom_logo() ) : ?>
 	<div class="site-logo"><?php the_custom_logo(); ?></div>
 <?php endif; ?>
@@ -20,6 +20,7 @@
 		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 	<?php endif; ?>
 <?php endif; ?>
+</div>
 
 <?php
 $description = get_bloginfo( 'description', 'display' );
