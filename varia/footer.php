0b57ec545eca956062ae406861f3974350ed256e
Allan Cole <allan.cole@automattic.com>
2019-09-27 11:26:02 -0400

    Varia: code tidying.

--- varia/footer.php
+++ varia/footer.php
@@ -44,7 +44,7 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><span class="comma">,</span>
 			<?php endif; ?>
-			<?php /* translators: %1$s: WordPress link, %2$s: WordPress. */
+			<?php /* translators: 1: WordPress link, 2: WordPress. */
 			printf( '<a href="%1$s" class="imprint">proudly powered by %2$s</a>.',
 				esc_url( __( 'https://wordpress.org/', 'varia' ) ),
 				'WordPress'
