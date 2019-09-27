b8c9164a262863b22f180b1366317b57db759157
Allan Cole <allan.cole@automattic.com>
2019-09-27 11:16:50 -0400

    Varia: Improve appreance and placement of privacy policy link.

--- varia/footer.php
+++ varia/footer.php
@@ -44,14 +44,13 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><span class="comma">,</span>
 			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'varia' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress. */
-				printf( __( 'proudly powered by %s.', 'varia' ), 'WordPress' );
-				?>
-			</a>
+			<?php /* translators: %1$s: WordPress link, %2$s: WordPress. */
+			printf( '<a href="%1$s" class="imprint">proudly powered by %2$s</a>.',
+				esc_url( __( 'https://wordpress.org/', 'varia' ) ),
+				'WordPress'
+			); ?>
 			<?php if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+				the_privacy_policy_link();
 			} ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
