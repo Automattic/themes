827e79a77ed42cdf73751e3266945bd2412f6a0c
Copons <copons@users.noreply.github.com>
2019-10-15 12:15:40 +0100

    Stop using theme classes for template parts

--- varia/header.php
+++ varia/header.php
@@ -25,7 +25,7 @@
 
 	<?php if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content. ?>
 
-		<header id="masthead" class="site-header site-branding entry-content">
+		<header id="masthead" class="fse-template fse-header entry-content">
 			<?php
 				$template = new A8C\FSE\WP_Template();
 				$template->output_template_content( A8C\FSE\WP_Template::HEADER );
