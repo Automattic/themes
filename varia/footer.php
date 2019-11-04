91bba0a24c9149274613bf1e6d4cde03159e7876
Jacopo Tomasone <Copons@users.noreply.github.com>
2019-11-04 16:29:25 +0000

    Full Site Editing: Stop using theme classes for template parts (#1517)

--- varia/footer.php
+++ varia/footer.php
@@ -15,29 +15,30 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer <?php echo class_exists( 'A8C\FSE\WP_Template' ) ? 'entry-content' : 'responsive-max-width'; ?>">
-		<?php
-			if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content.
-				$template = new A8C\FSE\WP_Template();
-				$template->output_template_content( A8C\FSE\WP_Template::FOOTER );
-			else : // Otherwise we'll fallback to the default Varia footer below.
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+	<?php if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content. ?>
+	<footer class="fse-template-part fse-footer entry-content">
+	<?php
+		$template = new A8C\FSE\WP_Template();
+		$template->output_template_content( A8C\FSE\WP_Template::FOOTER );
+	else : // Otherwise we'll fallback to the default Varia footer below. ?>
+	<footer id="colophon" class="site-footer responsive-max-width">
+	<?php
+		get_template_part( 'template-parts/footer/footer', 'widgets' );
 
-				if ( has_nav_menu( 'menu-2' ) ) : ?>
-					<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'varia' ); ?>">
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'menu-2',
-								'menu_class'     => 'footer-menu',
-								'depth'          => 1,
-							)
-						);
-						?>
-					</nav><!-- .footer-navigation -->
-				<?php endif;
-			endif;
-		?>
+		if ( has_nav_menu( 'menu-2' ) ) : ?>
+			<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'varia' ); ?>">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-2',
+						'menu_class'     => 'footer-menu',
+						'depth'          => 1,
+					)
+				);
+				?>
+			</nav><!-- .footer-navigation -->
+		<?php endif;
+	endif; ?>
 
 		<div class="site-info">
 			<?php $blog_info = get_bloginfo( 'name' ); ?>
