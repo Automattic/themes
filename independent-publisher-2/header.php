9b327a8f025f4db7292a400e7828b51d9de13cf3
Niels Lange <info@nielslange.de>
2019-12-24 10:35:01 +0800

    Add ‘wp_body_open’ hook to Independent Publisher 2 theme

--- independent-publisher-2/header.php
+++ independent-publisher-2/header.php
@@ -18,7 +18,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+<?php wp_body_open(); ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'independent-publisher-2' ); ?></a>
 
