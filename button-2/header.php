9818292b86f24e5f1ba77220c807b80e8e3977b9
Niels Lange <info@nielslange.de>
2019-12-24 10:32:48 +0800

    Add ‘wp_body_open’ hook to Button 2 theme

--- button-2/header.php
+++ button-2/header.php
@@ -18,6 +18,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'button-2' ); ?></a>
 
