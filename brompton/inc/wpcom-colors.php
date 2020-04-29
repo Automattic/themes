dae026f74b9112d492ee81227e9a74df1d6088a2
Kjell Reigstad <kjell@kjellr.com>
2020-04-29 15:47:31 -0400

    Update color values and classes

--- brompton/inc/wpcom-colors.php
+++ brompton/inc/wpcom-colors.php
@@ -3,14 +3,63 @@
 
 // Background Color
 // $config-global--color-background-default
-add_color_rule( 'bg', 'white', array(
+add_color_rule( 'bg', '#E8E4DD', array(
 
 	// Background-color
-	array( '', 'background-color' ),
+	array( '.has-background-background-color[class],
+			.screen-reader-text:focus,
+			body,
+			body .widget_eu_cookie_law_widget #eu-cookie-law,
+			body .widget_eu_cookie_law_widget #eu-cookie-law.negative input.accept,
+			.main-navigation > div > ul > li > .sub-menu', 'background-color' ),
 
 	// Text-color
 	// Needs contrast against `link` (primary)
-	array( '', 'color', 'link' ),
+	array( '.a8c-posts-list-item__featured span,
+			.a8c-posts-list__view-all,
+			.a8c-posts-list__view-all:focus,
+			.a8c-posts-list__view-all:hover,
+			.button,
+			.button:focus,
+			.button:hover,
+			.has-background-color[class],
+			.has-focus.a8c-posts-list__view-all,
+			.has-focus.button,
+			.has-focus.wp-block-button__link,
+			.has-focus.wp-block-file__button,
+			.sticky-post,
+			.wp-block-button__link,
+			.wp-block-button__link:focus,
+			.wp-block-button__link:hover,
+			.wp-block-cover-image:not([class*='background-color']) .wp-block-cover-image-text,
+			.wp-block-cover-image:not([class*='background-color']) .wp-block-cover-text,
+			.wp-block-cover-image:not([class*='background-color']) .wp-block-cover__inner-container,
+			.wp-block-cover:not([class*='background-color']) .wp-block-cover-image-text,
+			.wp-block-cover:not([class*='background-color']) .wp-block-cover-text,
+			.wp-block-cover:not([class*='background-color']) .wp-block-cover__inner-container,
+			.wp-block-file .wp-block-file__button,
+			.wp-block-file a.wp-block-file__button:active,
+			.wp-block-file a.wp-block-file__button:focus,
+			.wp-block-file a.wp-block-file__button:hover,
+			.wp-block-file a.wp-block-file__button:visited,
+			.wp-block-file__button,
+			.wp-block-file__button:focus,
+			.wp-block-file__button:hover,
+			.wp-block-pullquote.is-style-solid-color,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept:focus,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept:hover,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.has-focus.accept,
+			body .widget_eu_cookie_law_widget #eu-cookie-law.negative,
+			button,
+			button.has-focus,
+			button:focus,
+			button:hover,
+			button[data-load-more-btn],
+			input.has-focus[type="submit"],
+			input:focus[type="submit"],
+			input:hover[type="submit"],
+			input[type="submit"]', 'color', 'link' ),
 
 	/**
 	 * Utility Classes
@@ -46,22 +95,95 @@ add_color_rule( 'bg', 'white', array(
 
 // Link Color
 // $config-global--color-primary-default
-add_color_rule( 'link', '#CA2017', array(
+add_color_rule( 'link', '#C04239', array(
 
 	// Background-color
-	array( '', 'background-color' ),
+	array( '.a8c-posts-list-item__featured span,
+			.a8c-posts-list__view-all:focus,
+			.a8c-posts-list__view-all:hover,
+			.button:focus,
+			.button:hover,
+			.has-focus.a8c-posts-list__view-all,
+			.has-focus.button,
+			.has-focus.wp-block-button__link,
+			.has-focus.wp-block-file__button,
+			.sticky-post,
+			.wp-block-button__link:focus,
+			.wp-block-button__link:hover,
+			.wp-block-file__button:focus,
+			.wp-block-file__button:hover,
+			.wp-block-pullquote.is-style-solid-color,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept:focus,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept:hover,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.has-focus.accept,
+			button.has-focus,
+			button:focus,
+			button:hover,
+			input.has-focus[type="submit"],
+			input:focus[type="submit"],
+			input:hover[type="submit"]', 'background-color' ),
 
 	// Text-color
 	// Needs contrast against `bg`
-	array( '', 'color', 'bg' ),
+	array( '.a8c-posts-list .a8c-posts-list-item__meta a:active,
+			.a8c-posts-list .a8c-posts-list-item__meta a:focus,
+			.a8c-posts-list .a8c-posts-list-item__meta a:hover,
+			.a8c-posts-list .a8c-posts-list-item__title a:active,
+			.a8c-posts-list .a8c-posts-list-item__title a:focus,
+			.a8c-posts-list .a8c-posts-list-item__title a:hover,
+			.comments-area .comment-meta a:active,
+			.comments-area .comment-meta a:focus,
+			.comments-area .comment-meta a:hover,
+			.comments-area .reply a:active,
+			.comments-area .reply a:focus,
+			.comments-area .reply a:hover,
+			.entry-footer a:active,
+			.entry-footer a:focus,
+			.entry-footer a:hover,
+			.page-title a:active,
+			.page-title a:focus,
+			.page-title a:hover,
+			.pagination .nav-links > *.current,
+			.pagination .nav-links > *:active,
+			.pagination .nav-links > *:focus,
+			.pagination .nav-links > *:hover,
+			.post-navigation a:active,
+			.post-navigation a:focus,
+			.post-navigation a:hover,
+			.wp-block-button.is-style-outline .wp-block-button__link.has-focus,
+			.wp-block-button.is-style-outline .wp-block-button__link:focus,
+			.wp-block-button.is-style-outline .wp-block-button__link:hover,
+			.wp-block-button.is-style-outline.has-focus,
+			.wp-block-button.is-style-outline:focus,
+			.wp-block-button.is-style-outline:hover,
+			.wp-block-newspack-blocks-homepage-articles article .entry-title a,
+			.wp-block-newspack-blocks-homepage-articles article .entry-title a:active,
+			.wp-block-newspack-blocks-homepage-articles article .entry-title a:focus,
+			.wp-block-newspack-blocks-homepage-articles article .entry-title a:hover,
+			a,
+			article .entry-header .entry-title a:active,
+			article .entry-header .entry-title a:focus,
+			article .entry-header .entry-title a:hover', 'color', 'bg' ),
 
 	// Border color left
 	// Needs contrast against `bg`
-	array( '', 'border-left-color', 'bg' ),
+	array( '.wp-block-quote', 'border-left-color', 'bg' ),
 
 	// Border color right
 	// Needs contrast against `bg`
-	array( '', 'border-right-color', 'bg' ),
+	array( '.wp-block-quote[style*="text-align: right"],
+			.wp-block-quote[style*="text-align:right"]', 'border-right-color', 'bg' ),
+
+	// Border color bottom
+	// Needs contrast against `bg`
+	array( '#colophon .site-info,
+			#masthead', 'border-bottom-color', 'bg' ),
+
+	// Border color top
+	// Needs contrast against `bg`
+	array( '#colophon .footer-navigation,
+			#colophon .site-info,
+			#masthead', 'border-top-color', 'bg' ),
 
 	/**
 	 * Utility Classes
@@ -76,15 +198,113 @@ add_color_rule( 'link', '#CA2017', array(
 
 // Text Color
 // $config-global--color-foreground-default
-add_color_rule( 'txt', '#222222', array(
+add_color_rule( 'txt', '#252E36', array(
 
 	// Text-color
 	// Needs contrast against `bg` with more contrast
-	array( '', 'color', 'bg', 7 ),
+	array( '.a8c-posts-list__item .a8c-posts-list-item__meta a:active,
+			.a8c-posts-list__item .a8c-posts-list-item__meta a:hover,
+			.comment-meta .comment-metadata,
+			.comment-meta .comment-metadata a:active,
+			.comment-meta .comment-metadata a:hover,
+			.entry-footer a:active,
+			.entry-footer a:hover,
+			.entry-meta a:active,
+			.entry-meta a:hover,
+			.main-navigation .button,
+			.pagination .nav-links > *,
+			.post-navigation .meta-nav,
+			.screen-reader-text:focus,
+			.wp-block-button.is-style-outline,
+			.wp-block-button.is-style-outline .wp-block-button__link,
+			.wp-block-button.is-style-outline .wp-block-button__link:active,
+			.wp-block-button.is-style-outline:active,
+			.wp-block-code,
+			.wp-block-code pre,
+			.wp-block-newspack-blocks-homepage-articles article .cat-links a:active,
+			.wp-block-newspack-blocks-homepage-articles article .cat-links a:hover,
+			.wp-block-newspack-blocks-homepage-articles article .entry-meta a:active,
+			.wp-block-newspack-blocks-homepage-articles article .entry-meta a:hover,
+			.wp-block-newspack-blocks-homepage-articles article .entry-title a:hover,
+			.wp-block-pullquote,
+			a:hover,
+			body,
+			body .widget_eu_cookie_law_widget #eu-cookie-law,
+			body .widget_eu_cookie_law_widget #eu-cookie-law.negative input.accept,
+			input[type="color"],
+			input[type="color"]:focus,
+			input[type="date"],
+			input[type="date"]:focus,
+			input[type="datetime"],
+			input[type="datetime"]:focus,
+			input[type="datetime-local"],
+			input[type="datetime-local"]:focus,
+			input[type="email"],
+			input[type="email"]:focus,
+			input[type="month"],
+			input[type="month"]:focus,
+			input[type="number"],
+			input[type="number"]:focus,
+			input[type="password"],
+			input[type="password"]:focus,
+			input[type="range"],
+			input[type="range"]:focus,
+			input[type="search"],
+			input[type="search"]:focus,
+			input[type="tel"],
+			input[type="tel"]:focus,
+			input[type="text"],
+			input[type="text"]:focus,
+			input[type="time"],
+			input[type="time"]:focus,
+			input[type="url"],
+			input[type="url"]:focus,
+			input[type="week"],
+			input[type="week"]:focus,
+			textarea,
+			textarea:focus', 'color', 'bg', 7 ),
 
 	// Background-color
 	// Needs contrast against `bg` with more contrast
-	array( '', 'background-color', 'bg', 7 ),
+	array( '#colophon .footer-navigation,
+			#colophon .site-info,
+			#masthead,
+			#masthead:before,
+			#page:before,
+			.a8c-posts-list__view-all,
+			.button,
+			.wp-block-button__link,
+			.wp-block-cover,
+			.wp-block-cover-image,
+			.wp-block-file .wp-block-file__button,
+			.wp-block-file__button,
+			body .widget_eu_cookie_law_widget #eu-cookie-law input.accept,
+			body .widget_eu_cookie_law_widget #eu-cookie-law.negative,
+			button,
+			button[data-load-more-btn],
+			input[type="submit"],
+			.main-navigation > div > ul > li.current-menu-item li > a,
+			.main-navigation > div > ul > li.focus li > a,
+			.main-navigation > div > ul > li:hover li > a', 'background-color', 'bg', 7 ),
+
+	// Border-color
+	// Needs contrast against `bg` with more contrast
+	array( 'input[type="color"]:focus,
+			input[type="date"]:focus,
+			input[type="datetime"]:focus,
+			input[type="datetime-local"]:focus,
+			input[type="email"]:focus,
+			input[type="month"]:focus,
+			input[type="number"]:focus,
+			input[type="password"]:focus,
+			input[type="range"]:focus,
+			input[type="search"]:focus,
+			input[type="tel"]:focus,
+			input[type="text"]:focus,
+			input[type="time"]:focus,
+			input[type="url"]:focus,
+			input[type="week"]:focus,
+			textarea:focus', 'border-color', 'bg', 7 ),
 
 	/**
 	 * Utility Classes
@@ -108,16 +328,26 @@ add_color_rule( 'txt', '#222222', array(
 
 // Accent Color
 // $config-global--color-secondary-default
-add_color_rule( 'fg1', '#007FDB', array(
+add_color_rule( 'fg1', '#FFFFFF', array(
 
 	/**
 	 * Utility Classes
 	 */
 	// Text-color
 	// Needs contrast against `bg`
-	array( '.has-secondary-color[class]', 'color', 'bg' ),
+	array( '.footer-navigation .footer-menu,
+			.has-secondary-color[class],
+			.main-navigation,
+			.main-navigation a,
+			.main-navigation a:link,
+			.main-navigation a:visited,
+			.site-branding,
+			.site-info,
+			.site-title,
+			.social-navigation a', 'color', 'bg' ),
 
 	// Background-color
-	array( '.has-secondary-background-color[class]', 'background-color' ),
+	array( '.has-secondary-background-color[class],
+			.main-navigation .button', 'background-color' ),
 
 ), __( 'Secondary Color' ) );
