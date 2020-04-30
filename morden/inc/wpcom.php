89c7633553363c369131e788d32d68d3179c4134
Kjell Reigstad <kjell@kjellr.com>
2020-04-30 16:14:57 -0400

    Morden: Add Color Annotations

---
+++ morden/inc/wpcom.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function morden_wpcom_setup() {
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => 'white', // $config-global--color-background-default
+			'border' => 'C5C5C5', // $config-global--color-border-default
+			'text'   => '303030', // $config-global--color-foreground-default
+			'link'   => 'CD2220', // $config-global--color-primary-default
+			'url'    => 'CD2220', // $config-global--color-primary-default
+		);
+	}
+}
+add_action( 'after_setup_theme', 'morden_wpcom_setup' );
