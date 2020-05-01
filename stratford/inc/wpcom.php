107e4dd443af32089458863b232f0facf1dea5c5
Kjell Reigstad <kjell@kjellr.com>
2020-05-01 09:53:57 -0400

    Stratford: Add Color Annotations

---
+++ stratford/inc/wpcom.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * Adds support for wp.com-specific theme functions.
+ *
+ * @global array $themecolors
+ */
+function stratford_wpcom_setup() {
+	global $themecolors;
+
+	// Set theme colors for third party services.
+	if ( ! isset( $themecolors ) ) {
+		$themecolors = array(
+			'bg'     => 'white', // $config-global--color-background-default
+			'border' => 'DDDDDD', // $config-global--color-border-default
+			'text'   => '74767e', // $config-global--color-foreground-default
+			'link'   => '2c313f', // $config-global--color-primary-default
+			'url'    => '2c313f', // $config-global--color-primary-default
+		);
+	}
+}
+add_action( 'after_setup_theme', 'stratford_wpcom_setup' );
