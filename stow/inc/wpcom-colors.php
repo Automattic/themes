ac859873eeb6cba0c25d91be7e5deb2b20e6e563
Kjell Reigstad <kjell@kjellr.com>
2020-05-06 14:08:09 -0400

    Remove contrast annotations

--- stow/inc/wpcom-colors.php
+++ stow/inc/wpcom-colors.php
@@ -17,7 +17,6 @@ add_color_rule( 'bg', 'white', array(
 			body:not(.fse-enabled) .main-navigation.main-navigation ul ul', 'background-color' ),
 
 	// Text-color
-	// Needs contrast against `link` (primary)
 	array( '.a8c-posts-list-item__featured span,
 			.a8c-posts-list__view-all,
 			.a8c-posts-list__view-all:focus,
@@ -68,31 +67,23 @@ add_color_rule( 'bg', 'white', array(
 			input:hover[type="submit"],
 			input[type="submit"],
 			body:not(.fse-enabled) #site-navigation.main-navigation ul li a,
-			body:not(.fse-enabled) .main-navigation.main-navigation ul li a', 'color', 'link' ),
+			body:not(.fse-enabled) .main-navigation.main-navigation ul li a', 'color' ),
 
 	/**
 	 * Utility Classes
 	 */
 	// Text-color
-	// Needs contrast against `link` (primary)
-	array( '.has-primary-background-color[class]', 'color', 'link' ),
-	// Text-color
-	// Needs contrast against `fg1` (secondary)
-	array( '.has-secondary-background-color[class]', 'color', 'fg1' ),
-
-	// Text-color
-	// Needs contrast against `txt` (foreground)
-	array( '.has-foreground-background-color[class],
+	array( '.has-primary-background-color[class],
+			.has-secondary-background-color[class],
+			.has-foreground-background-color[class],
 			.has-foreground-dark-background-color[class],
-			.has-foreground-light-background-color[class]', 'color', 'txt', 12 ),
-	// Text-color
-	// Needs contrast against `txt` (background)
-	array( '.has-background-color[class],
+			.has-foreground-light-background-color[class],
+			.has-background-color[class],
 			.has-background-dark-color[class],
 			.has-background-light-color[class],
 			.has-background-background-color[class],
 			.has-background-dark-background-color[class],
-			.has-background-light-background-color[class]', 'color', 'bg', 12 ),
+			.has-background-light-background-color[class]', 'color' ),
 	// Background-color
 	array( '.has-background-background-color[class]', 'background-color' ),
 	// Background-color darkened
@@ -113,7 +104,6 @@ add_color_rule( 'link', '#404040', array(
 			.wp-block-pullquote.is-style-solid-color', 'background-color' ),
 
 	// Text-color
-	// Needs contrast against `bg`
 	array( '.footer-navigation .footer-menu,
 			.main-navigation a,
 			.main-navigation a:link,
@@ -122,16 +112,14 @@ add_color_rule( 'link', '#404040', array(
 			.site-title,
 			.social-navigation a,
 			.wp-block-newspack-blocks-homepage-articles article .entry-title a,
-			a', 'color', 'bg' ),
+			a', 'color' ),
 
 	// Border color left
-	// Needs contrast against `bg`
-	array( '.wp-block-quote', 'border-left-color', 'bg' ),
+	array( '.wp-block-quote', 'border-left-color' ),
 
 	// Border color right
-	// Needs contrast against `bg`
 	array( '.wp-block-quote[style*="text-align: right"],
-			.wp-block-quote[style*="text-align:right"]', 'border-right-color', 'bg' ),
+			.wp-block-quote[style*="text-align:right"]', 'border-right-color' ),
 
 	/**
 	 * Utility Classes
@@ -139,8 +127,7 @@ add_color_rule( 'link', '#404040', array(
 	// Background-color
 	array( '.has-primary-background-color[class]', 'background-color' ),
 	// Text-color
-	// Needs contrast against `bg`
-	array( '.has-primary-color[class]', 'color', 'bg' ),
+	array( '.has-primary-color[class]', 'color' ),
 
 ), __( 'Link Color' ) );
 
@@ -149,7 +136,6 @@ add_color_rule( 'link', '#404040', array(
 add_color_rule( 'txt', '#444444', array(
 
 	// Text-color
-	// Needs contrast against `bg` with more contrast
 	array( '.comment-meta .comment-metadata,
 			.main-navigation,
 			.screen-reader-text:focus,
@@ -190,21 +176,19 @@ add_color_rule( 'txt', '#444444', array(
 			input[type="week"],
 			input[type="week"]:focus,
 			textarea,
-			textarea:focus', 'color', 'bg', 7 ),
+			textarea:focus', 'color' ),
 
 	// Background-color
-	// Needs contrast against `bg` with more contrast
 	array( '.has-foreground-background-color[class],
 			.wp-block-cover,
 			.wp-block-cover-image,
-			body .widget_eu_cookie_law_widget #eu-cookie-law.negative', 'background-color', 'bg', 7 ),
+			body .widget_eu_cookie_law_widget #eu-cookie-law.negative', 'background-color' ),
 
 	/**
 	 * Utility Classes
 	 */
 	// Foreground
-	// Needs contrast against `bg`
-	array( '.has-foreground-color[class]', 'color', 'bg' ),
+	array( '.has-foreground-color[class]', 'color' ),
 	array( '.has-foreground-background-color[class]', 'background-color' ),
 
 	// Text-color darkened
@@ -239,7 +223,6 @@ add_color_rule( 'fg1', '#f25f70', array(
 			input[type="submit"]', 'background-color' ),
 
 	// Text-color
-	// Needs contrast against `bg` with more contrast
 	array( '#masthead .social-navigation ul li a,
 			.a8c-posts-list__item .a8c-posts-list-item__meta a:active,
 			.a8c-posts-list__item .a8c-posts-list-item__meta a:hover,
@@ -273,10 +256,9 @@ add_color_rule( 'fg1', '#f25f70', array(
 			body:not(.fse-enabled) #site-navigation.main-navigation ul ul li a:hover,
 			body:not(.fse-enabled) #site-navigation.main-navigation ul ul li.current-menu-item a,
 			body:not(.fse-enabled) .main-navigation.main-navigation ul ul li a:hover,
-			body:not(.fse-enabled) .main-navigation.main-navigation ul ul li.current-menu-item a', 'color', 'bg', 7 ),
+			body:not(.fse-enabled) .main-navigation.main-navigation ul ul li.current-menu-item a', 'color' ),
 
 	// Border-color
-	// Needs contrast against `bg`
 	array( 'input[type="color"]:focus,
 			input[type="date"]:focus,
 			input[type="datetime"]:focus,
@@ -292,14 +274,13 @@ add_color_rule( 'fg1', '#f25f70', array(
 			input[type="time"]:focus,
 			input[type="url"]:focus,
 			input[type="week"]:focus,
-			textarea:focus', 'border-color', 'bg' ),
+			textarea:focus', 'border-color' ),
 
 	/**
 	 * Utility Classes
 	 */
 	// Text-color
-	// Needs contrast against `bg`
-	array( '.has-secondary-color[class]', 'color', 'bg' ),
+	array( '.has-secondary-color[class]', 'color' ),
 
 	// Background-color
 	array( '.has-secondary-background-color[class]', 'background-color' ),
