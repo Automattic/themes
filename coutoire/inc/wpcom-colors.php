b5a7bd6e2429349caadc456e9edfa750f49dabf7
Allan Cole <allan.cole@automattic.com>
2020-06-12 14:06:23 -0400

    Coutoire: Clean up dim support in cover block.

--- coutoire/inc/wpcom-colors.php
+++ coutoire/inc/wpcom-colors.php
@@ -36,6 +36,8 @@ add_color_rule( 'bg', '#FFFFFF', array(
 			.wp-block-cover:not([class="has-background-background-color"]) .wp-block-cover-image-text,
 			.wp-block-cover:not([class="has-background-background-color"]) .wp-block-cover-text,
 			.wp-block-cover:not([class="has-background-background-color"]) .wp-block-cover__inner-container,
+			.wp-block-cover.has-background-dim,
+			.wp-block-cover-image.has-background-dim,
 			.wp-block-file .wp-block-file__button,
 			.wp-block-file a.wp-block-file__button:active,
 			.wp-block-file a.wp-block-file__button:focus,
@@ -224,8 +226,8 @@ add_color_rule( 'txt', '#444444', array(
 			textarea:focus', 'color' ),
 
 	// Background-color
-	array( '.wp-block-cover,
-			.wp-block-cover-image,
+	array( '.wp-block-cover.has-background-dim,
+			.wp-block-cover-image.has-background-dim,
 			table.is-style-stripes tbody tr:nth-child(odd),
 			.wp-block-table.is-style-stripes tbody tr:nth-child(odd),
 			body .widget_eu_cookie_law_widget #eu-cookie-law.negative', 'background-color' ),
