a6a5ca211c5daf48bccda8681729bc7613f8c4ea
Niels Lange <info@nielslange.de>
2020-07-15 18:40:51 +0200

    #2218: Varia: Comment reply title skips heading hierarchy

--- varia/comments.php
+++ varia/comments.php
@@ -69,7 +69,12 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	comment_form(
+		array(
+			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+			'title_reply_after'  => '</h2>',
+		)
+	);
 	?>
 
 </div><!-- #comments -->
