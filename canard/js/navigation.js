b90882f2d2248d15c34c9de88ec0ff568cae7d39
Danny Dudzic <daniel.dudzic@automattic.com>
2020-07-21 01:04:06 +0200

    Canard: Fix tab navigation for mobile menu

--- canard/js/navigation.js
+++ canard/js/navigation.js
@@ -103,5 +103,34 @@
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
+	
+	
+	// Keyboard accessibility
+	const  focusableElements =
+		'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+	const modal = document.querySelector( '#site-navigation' );
+	const firstFocusableElement = modal.querySelectorAll( focusableElements )[0];
+	const focusableContent = modal.querySelectorAll( focusableElements );
+	const lastFocusableElement = focusableContent[focusableContent.length - 1];
+	
+	document.addEventListener( 'keydown', function( e ) {
+		let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+	
+		if ( !isTabPressed ) {
+			return;
+		}
+	
+		if ( e.shiftKey ) { // if shift key pressed for shift + tab combination
+			if ( document.activeElement === firstFocusableElement ) {
+				lastFocusableElement.focus(); // add focus for the last focusable element
+				e.preventDefault();
+			}
+		} else { // if tab key is pressed
+			if ( document.activeElement === lastFocusableElement ) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+				firstFocusableElement.focus(); // add focus for the first focusable element
+				e.preventDefault();
+			}
+		}
+	});
 
 } )();
