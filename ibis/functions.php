97fef311874fcdf157cd9c71a260bab83c67f5f3
Ian Stewart <ian@iandanielstewart.com>
2020-07-29 13:26:17 -0500

    New theme: Ibis
    This PR is the first commit of new, proposed, block-based theme, Ibis. Forked from Seedlet Blocks, and a variation that more closely aligns with some of the defaults the WP.com View Team has come to expect from our favourite Varia-based themes on WP.com. It's named after a bird because birds are cool.
    
    There are several things wrong, potentially wrong, or unfinished with it. Here's a few todo items off the top of my head.
    
    - The header template part needs updating.
    - The editor styles don't match the front-end.
    - The default palette is probably lame in practice.
    - The grid proportions need to be adjusted down to mobile.

---
+++ ibis/functions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Ibis functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage seedlet_blocks
+ * @since 1.0.0
+ */
+
+if ( ! function_exists( 'ibis_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function ibis_setup() {
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style-editor.css' );
+	}
+endif;
+add_action( 'after_setup_theme', 'ibis_setup' );
+
+/**
+ * Add Google webfonts
+ *
+ * http://themeshaper.com/2014/08/13/how-to-add-google-fonts-to-wordpress-themes/
+ */
+function ibis_fonts_url() {
+
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	* supported by Lora, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$poppins = esc_html_x( 'on', 'Lora font: on or off', 'ibis' );
+
+	if ( 'off' !== $poppins ) {
+		$font_families = array();
+
+		$font_families[] = 'Lora:400,400i,600,600i,700,700i';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function ibis_enqueue() {
+	wp_enqueue_style( 'ibis-styles', get_stylesheet_uri() );
+
+	// enqueue Google fonts
+	wp_enqueue_style( 'mayland-fonts', ibis_fonts_url(), array(), null );
+
+}
+add_action( 'wp_enqueue_scripts', 'ibis_enqueue' );
