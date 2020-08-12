41f1d0697c9e96bc043fe71ea822d8e89096e500
Jeff Ong <jeff.ong@automattic.com>
2020-08-12 12:30:02 -0400

    Seedlet: fix keyboard navigation (#2305)
    * Scope focus styles to the site.
    
    * Implement keyboard navigation trapping in menu modal.
    
    * Change taborder of menu buttons.
    
    * Cleanup focus selector.
    
    * Fix keyboard nav within modal.
    
    * Refactor primary menu logic to enable correct tab order.
    
    * Refactor to work with multiple navs.
    
    * Fix empty cart bug.

--- seedlet/header.php
+++ seedlet/header.php
@@ -21,18 +21,29 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'seedlet' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content" tabindex="1"><?php _e( 'Skip to content', 'seedlet' ); ?></a>
 
 		<header id="masthead" class="site-header default-max-width">
-
+			<div class="menu-button-container">
+				<?php if ( class_exists( 'WooCommerce' ) ) : ?>
+					<button id="woo-open-menu" class="button open">
+						<span class="dropdown-icon open"><?php echo seedlet_get_icon_svg( 'shopping_cart' ); ?> <?php _e( 'Cart', 'seedlet' ); ?></span>
+						<span class="hide-visually expanded-text"><?php esc_html__( 'expanded', 'seedlet' ) ?></span>
+					</button>
+				<?php endif; ?>
+				<?php if ( has_nav_menu( 'primary' ) ) : ?>
+					<button id="primary-open-menu" class="button open">
+						<span class="dropdown-icon open"><?php _e( 'Menu', 'seedlet' ); ?> <?php echo seedlet_get_icon_svg( 'menu' ) ?></span>
+						<span class="hide-visually expanded-text"><?php _e( 'expanded', 'seedlet' ); ?></span>
+					</button>
+				<?php endif; ?>
+			</div>
 			<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 
 			<?php if ( has_nav_menu( 'primary' ) ) : ?>
 				<nav id="site-navigation" class="primary-navigation" aria-label="<?php esc_attr_e( 'Primary Navigation', 'seedlet' ); ?>">
-					<button id="toggle-menu" class="button">
-						<span class="dropdown-icon open"><?php _e( 'Menu', 'seedlet' ); ?> <?php echo seedlet_get_icon_svg( 'menu' ) ?></span>
+					<button id="primary-close-menu" class="button close">
 						<span class="dropdown-icon close"><?php _e( 'Close', 'seedlet' ); ?> <?php echo seedlet_get_icon_svg( 'close' ) ?></span>
-						<span class="hide-visually expanded-text"><?php _e( 'expanded', 'seedlet' ); ?></span>
 						<span class="hide-visually collapsed-text"><?php _e( 'collapsed', 'seedlet' ); ?></span>
 					</button>
 					<?php
@@ -46,7 +57,7 @@
 						array(
 							'theme_location'  => 'primary',
 							'menu_class'      => 'menu-wrapper',
-							'container_class' => 'primary-menu-container menu-'. $menu_obj->slug .'-container',
+							'container_class' => 'primary-menu-container',
 							'items_wrap'      => '<ul id="%1$s" class="%2$s" aria-label="' . esc_attr__( 'submenu', 'seedlet' ) . '">%3$s</ul>',
 						)
 					);
@@ -57,32 +68,25 @@
 			<?php if ( class_exists( 'WooCommerce' ) ) : ?>
 				<nav class="woo-navigation" aria-label="<?php esc_attr_e( 'Woo Minicart', 'seedlet' ); ?>">
 					<?php echo( sprintf(
-						'<button id="toggle-cart" class="button">
-							<span class="dropdown-icon open">%1$s %2$s</span>
-							<span class="dropdown-icon close">%3$s %4$s</span>
-							<span class="hide-visually expanded-text">%5$s</span>
-							<span class="hide-visually collapsed-text">%6$s</span>
+						'<button id="woo-close-menu" class="button close">
+							<span class="dropdown-icon close">%1$s %2$s</span>
+							<span class="hide-visually collapsed-text">%3$s</span>
 						</button>
 						<div class="woocommerce-menu-container">
-						<ul id="woocommerce-menu" class="menu-wrapper" aria-label="%7$s">
-						<li class="menu-item woocommerce-menu-item %8$s" title="%9$s">
-							%10$s
-							<ul class="sub-menu">
-								<li class="woocommerce-cart-widget" title="%11$s">
-									%12$s
-								</li>
-							</ul>
-						</li>',
-						seedlet_get_icon_svg( 'shopping_cart' ),
+							<ul id="woocommerce-menu" class="menu-wrapper" aria-label="%4$s">
+							<li class="menu-item woocommerce-menu-item %5$s" title="%6$s">
+								<ul class="sub-menu">
+									<li class="woocommerce-cart-widget" title="%7$s">
+										%8$s
+									</li>
+								</ul>
+							</li>',
 						esc_html__( 'Cart', 'seedlet' ),
-						esc_html__( 'Close', 'seedlet' ),
 						seedlet_get_icon_svg( 'close' ),
-						esc_html__( 'expanded', 'seedlet' ),
 						esc_html__( 'collapsed', 'seedlet' ),
 						esc_attr__( 'submenu', 'seedlet' ),
 						is_cart() ? 'current-menu-item' : '',
 						esc_attr__( 'View your shopping cart', 'seedlet' ),
-						seedlet_cart_link(),
 						esc_attr__( 'View your shopping list', 'seedlet' ),
 						seedlet_cart_widget()
 					) ); ?>
