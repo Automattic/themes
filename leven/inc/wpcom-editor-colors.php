2365a4e07f76f4d450a873eb07b3025824339122
Allan Cole <allan.cole@automattic.com>
2020-08-13 13:03:06 -0400

    Leven: clean up nested link colors.

--- leven/inc/wpcom-editor-colors.php
+++ leven/inc/wpcom-editor-colors.php
@@ -252,3 +252,14 @@ add_editor_color_rule( 'fg1', '#1285ce', array(
 			#editor .editor-styles-wrapper .wp-block .has-secondary-background-color[class]', 'background-color' ),
 
 ), __( 'Secondary Color' ) );
+
+/**
+ * Custom CSS
+ */
+function leven_editor_custom_colors_extra_css() { ?>
+	#editor .editor-styles-wrapper .has-text-color a,
+	#editor .editor-styles-wrapper .has-background a {
+		color: currentColor;
+	}
+<?php }
+add_theme_support( 'custom_colors_extra_css', 'leven_editor_custom_colors_extra_css' );
