09070c7be60e4abfc11f0e0f723e23290b31c24a
Ben Dwyer <ben@scruffian.com>
2020-08-20 17:13:20 +0100

    fix PHP notices

--- seedlet/functions.php
+++ seedlet/functions.php
@@ -108,13 +108,6 @@ if ( ! function_exists( 'seedlet_setup' ) ) :
 
 		$editor_stylesheet_path = './assets/css/style-editor.css';
 
-		// Note, the is_IE global variable is defined by WordPress and is used
-		// to detect if the current browser is internet explorer.
-		global $is_IE;
-		if ( $is_IE ) {
-			$editor_stylesheet_path = './assets/css/ie-editor.css';
-		}
-
 		// Enqueue editor styles.
 		add_editor_style(
 			array(
