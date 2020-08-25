962a02674459cdf7a53e54f288d02640dc316f5c
Kjell Reigstad <kjell@kjellr.com>
2020-08-25 12:47:29 -0400

    Add a block pattern to access this feature.

--- seedlet/inc/block-patterns.php
+++ seedlet/inc/block-patterns.php
@@ -37,4 +37,12 @@ if ( function_exists( 'register_block_pattern' ) ) {
 			'content'	=> "<!-- wp:group {\"align\":\"full\",\"className\":\"is-style-overflow\",\"gradient\":\"stripe\"} -->\n<div class=\"wp-block-group alignfull is-style-overflow has-stripe-gradient-background has-background\"><div class=\"wp-block-group__inner-container\"><!-- wp:columns {\"align\":\"wide\"} -->\n<div class=\"wp-block-columns alignwide\"><!-- wp:column -->\n<div class=\"wp-block-column\"><!-- wp:image -->\n<figure class=\"wp-block-image\"><img alt=\"\"/></figure>\n<!-- /wp:image -->\n\n<!-- wp:image -->\n<figure class=\"wp-block-image\"><img alt=\"\"/></figure>\n<!-- /wp:image --></div>\n<!-- /wp:column -->\n\n<!-- wp:column {\"verticalAlignment\":\"center\"} -->\n<div class=\"wp-block-column is-vertically-aligned-center\"><!-- wp:image -->\n<figure class=\"wp-block-image\"><img alt=\"\"/></figure>\n<!-- /wp:image --></div>\n<!-- /wp:column --></div>\n<!-- /wp:columns --></div></div>\n<!-- /wp:group -->",
 		)
 	);
+	register_block_pattern(
+		'seedlet/latest-posts-alternating-grid',
+		array (
+			'title'		=> __( 'Alternating Grid of Latest Posts', 'seedlet' ),
+			'categories'=> array( 'seedlet' ),
+			'content'	=> "<!-- wp:latest-posts {\"displayPostContent\":true,\"columns\":5,\"className\":\"is-style-seedlet-alternating-grid\"} /-->",
+		)
+	);
 } 
