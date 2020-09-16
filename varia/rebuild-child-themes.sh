d7a5aba5b2079f829150aec231fac0b55c7a2361
Ben Dwyer <ben@scruffian.com>
2020-09-16 14:12:30 +0100

    update all varia children

--- varia/rebuild-child-themes.sh
+++ varia/rebuild-child-themes.sh
@@ -3,8 +3,9 @@
 declare -a ChildThemes=("alves" "balasana" "barnsbury" "brompton" "coutoire" "dalston" "exford" "hever" "leven" "mayland" "maywood" "morden" "redhill" "rivington" "rockfield" "shawburn" "stow" "stratford" )
 
 for child in ${ChildThemes[@]}; do
-	echo 'Rebulding '${child}
 	cd '../'${child}
+	echo 'Rebulding '${child}
+	perl -pi -e 's/Version: ((\d+\.)*)(\d+)(.*)$/"Version: ".$1.($3+1).$4/ge' sass/style-child-theme.scss
 	npm install
 	npm run build
 done
