4bd93049bd5ff64f8230ba673b63162cd07fd78e
Jeff Ong <jonger4@gmail.com>
2020-10-16 14:03:33 -0400

    Restore git changes after recompiling the version number.

--- seedlet/package-dotorg.sh
+++ seedlet/package-dotorg.sh
@@ -5,3 +5,4 @@ mkdir seedlet;
 rsync -rv --exclude="seedlet" --exclude=".git" --exclude=".DS_Store" --exclude="*.sh" --exclude="*.json" --exclude="postcss.config.js" --exclude="node_modules" --exclude="*wpcom*" --exclude="inc/headstart" ./ seedlet
 zip -r -X seedlet.zip seedlet
 rm -rf seedlet
+git restore ./
