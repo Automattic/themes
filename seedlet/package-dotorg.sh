375064d5495ceb01b6bc80b3412cdd7b2b5ba9fa
Jeff Ong <jonger4@gmail.com>
2020-10-16 14:07:01 -0400

    Ignore previous zip in the rsync.

--- seedlet/package-dotorg.sh
+++ seedlet/package-dotorg.sh
@@ -2,7 +2,7 @@
 find assets/sass/*.scss -type f -exec sed -i '' 's/-wpcom//g' {} \; 
 npm run build;
 mkdir seedlet;
-rsync -rv --exclude="seedlet" --exclude=".git" --exclude=".DS_Store" --exclude="*.sh" --exclude="*.json" --exclude="postcss.config.js" --exclude="node_modules" --exclude="*wpcom*" --exclude="inc/headstart" ./ seedlet
+rsync -rv --exclude="seedlet.zip" --exclude="seedlet" --exclude=".git" --exclude=".DS_Store" --exclude="*.sh" --exclude="*.json" --exclude="postcss.config.js" --exclude="node_modules" --exclude="*wpcom*" --exclude="inc/headstart" ./ seedlet
 zip -r -X seedlet.zip seedlet
 rm -rf seedlet
 git restore ./
