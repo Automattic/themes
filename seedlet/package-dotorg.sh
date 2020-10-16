1c036fd21ce60b5706ab39a1d1a436a28f42860d
Jeff Ong <jonger4@gmail.com>
2020-10-16 14:00:53 -0400

    Add script for packaging theme for submission to dotorg directory.

---
+++ seedlet/package-dotorg.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+find assets/sass/*.scss -type f -exec sed -i '' 's/-wpcom//g' {} \; 
+npm run build;
+mkdir seedlet;
+rsync -rv --exclude="seedlet" --exclude=".git" --exclude=".DS_Store" --exclude="*.sh" --exclude="*.json" --exclude="postcss.config.js" --exclude="node_modules" --exclude="*wpcom*" --exclude="inc/headstart" ./ seedlet
+zip -r -X seedlet.zip seedlet
+rm -rf seedlet
