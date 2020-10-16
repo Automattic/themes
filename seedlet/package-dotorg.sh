dfbf6a2c0f5b9f1db7ca744887d93ad339157bf9
Jeff Ong <jonger4@gmail.com>
2020-10-16 14:27:54 -0400

    Delete map files.

--- seedlet/package-dotorg.sh
+++ seedlet/package-dotorg.sh
@@ -3,7 +3,7 @@ find assets/sass/*.scss -type f -exec sed -i '' 's/-wpcom//g' {} \;
 find assets/sass/*.scss -type f -exec sed -i '' 's/auto-loading-homepage, //g' {} \; 
 npm run build;
 mkdir seedlet;
-rsync -rv --exclude="seedlet.zip" --exclude="seedlet" --exclude=".git" --exclude=".DS_Store" --exclude="*.sh" --exclude="*.map" --exclude="*.json" --exclude="postcss.config.js" --exclude="node_modules" --exclude="*wpcom*" --exclude="inc/headstart" ./ seedlet
+rsync -avz --exclude-from 'dotorg-exclude.txt' ./ seedlet
+find seedlet -type f -name '*.map' -delete # for some reason rsync won't exclude map files
 zip -r -X seedlet.zip seedlet
 rm -rf seedlet
-git restore ./
