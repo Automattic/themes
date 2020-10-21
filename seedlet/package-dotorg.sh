febf647ca2843c6c467870baafea134fa32d0823
Jeff Ong <jonger4@gmail.com>
2020-10-21 14:27:26 -0400

    git restore and ignore all zip files.

--- seedlet/package-dotorg.sh
+++ seedlet/package-dotorg.sh
@@ -7,3 +7,4 @@ rsync -avz --exclude-from 'dotorg-exclude.txt' ./ seedlet
 find seedlet -type f -name '*.map' -delete # for some reason rsync won't exclude map files
 zip -r -X seedlet.zip seedlet
 rm -rf seedlet
+git restore ./
