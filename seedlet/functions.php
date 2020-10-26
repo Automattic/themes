e67ff5c9dfd958f35fe40f32742f079ffd8e96ff
Ben Dwyer <ben@scruffian.com>
2020-10-26 16:04:24 +0000

    implement the jetpack author bio

--- seedlet/functions.php
+++ seedlet/functions.php
@@ -422,6 +422,21 @@ function seedlet_skip_link_focus_fix() {
 }
 add_action( 'wp_print_footer_scripts', 'seedlet_skip_link_focus_fix' );
 
+if ( ! function_exists( 'seedlet_author_bio' ) ) {
+	/**
+	 * Implements the Jetpack Author bio
+	 */
+	function seedlet_author_bio() {
+		if ( ! function_exists( 'jetpack_author_bio' ) ) {
+			if ( ! is_singular( 'attachment' ) ) {
+				get_template_part( 'template-parts/post/author-bio' );
+			}
+		} else {
+			jetpack_author_bio();
+		}
+	}
+}
+
 /**
  * SVG Icons class.
  */
