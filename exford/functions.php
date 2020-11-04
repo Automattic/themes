04a77c15120286c878c3dd92b5949ee4bffd6e1c
Allan Cole <allan.cole@automattic.com>
2020-11-04 17:09:36 -0500

    Exford: Reduce specificity on rules to hide extra color palettes.

--- exford/functions.php
+++ exford/functions.php
@@ -191,8 +191,8 @@ function exford_editor_styles() {
 	// Hide duplicate palette colors
 	$colors_array = get_theme_mod( 'colors_manager' );
 	if ( ! empty( $colors_array ) && $colors_array['colors']['txt'] != '#6E6E6E' ) { // $config-global--color-foreground-light-default;
-		$inline_palette_css = '.block-editor-color-gradient-control .components-circular-option-picker__option-wrapper:nth-child(5),
-			.block-editor-color-gradient-control .components-circular-option-picker__option-wrapper:nth-child(6) {
+		$inline_palette_css = '.components-circular-option-picker__option-wrapper:nth-child(5),
+			.components-circular-option-picker__option-wrapper:nth-child(6) {
 				display: none;
 			}';
 		wp_add_inline_style( 'wp-edit-blocks', $inline_palette_css );
