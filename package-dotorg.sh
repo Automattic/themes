ba94d83620f4e767ecbb5f9d27762767dc30b9ed
Jeff Ong <jonger4@gmail.com>
2020-11-06 13:37:25 -0500

    Revert "Replace with less redundant expression for now."
    This reverts commit 09bbde9e704205fadda089b1c29b138f8571f3a8.

--- package-dotorg.sh
+++ package-dotorg.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 if [[ "$1" != "" ]]; then
     THEME="$1"
-	find $THEME/assets/sass/style.scss -type f -exec sed -i '' 's/-wpcom//g' {} \;
-	find $THEME/assets/sass/style.scss -type f -exec sed -i '' 's/, auto-loading-homepage//g' {} \;
-	find $THEME/assets/sass/style.scss -type f -exec sed -i '' 's/, jetpack-global-styles//g' {} \;
+	find $THEME/assets/sass/*.scss -type f -exec sed -i '' 's/-wpcom//g' {} \; 
+	find $THEME/assets/sass/*.scss -type f -exec sed -i '' 's/, auto-loading-homepage, jetpack-global-styles//g' {} \; 
+	find $THEME/assets/sass/*.scss -type f -exec sed -i '' 's/auto-loading-homepage, //g' {} \; 
 	cd $THEME && npm run build;
 	mkdir $THEME;
 	rsync -avz --exclude $THEME --exclude-from '../dotorg-exclude.txt' ./ $THEME
