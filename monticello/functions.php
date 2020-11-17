a1c829b50ae8723e0f038aa77c7173994057a022
Jeff Ong <jonger4@gmail.com>
2020-11-17 12:00:16 -0500

    Add google fonts.

--- monticello/functions.php
+++ monticello/functions.php
@@ -22,7 +22,12 @@ if ( ! function_exists( 'monticello_support' ) ) :
 		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		add_editor_style(
+			array(
+				monticello_fonts_url(),
+				'style.css',
+			)
+		);
 	}
 	add_action( 'after_setup_theme', 'monticello_support' );
 endif;
@@ -37,5 +42,21 @@ function monticello_scripts() {
 
 	// Enqueue alignments stylesheet.
 	wp_enqueue_style( 'monticello-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+
+	// Enqueue Google fonts, if necessary.
+	wp_enqueue_style( 'monticello-fonts', monticello_fonts_url(), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'monticello_scripts' );
+
+function monticello_fonts_url() {
+	$font_families = array();
+
+	$font_families[] = 'family=Poppins:ital,wght@0,200;0,400;0,700;1,100;1,400;1,700';
+	$font_families[] = 'family=Manuale:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700';
+	$font_families[] = 'display=swap';
+
+	// Make a single request for the theme fonts.
+	$fonts_url = 'https://fonts.googleapis.com/css2?' . implode( '&', $font_families );
+
+	return esc_url_raw( $fonts_url );
+}
