b28f335abb17d631c7652c13d2d2a4f7aecab538
Ben Dwyer <ben@scruffian.com>
2020-11-18 16:03:57 +0000

    try a full width approach

--- fewer/functions.php
+++ fewer/functions.php
@@ -18,11 +18,17 @@ if ( ! function_exists( 'fewer_support' ) ) :
 		// Add support for featured image
 		add_theme_support( 'post-thumbnails' );
 
+		// Add support for custom spacing
+		add_theme_support('custom-spacing');
+
 		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+		add_editor_style( array(
+			'style.css',
+			fewer_fonts_url(),
+		) );
 	}
 	add_action( 'after_setup_theme', 'fewer_support' );
 endif;
@@ -37,5 +43,38 @@ function fewer_scripts() {
 
 	// Enqueue alignments stylesheet.
 	wp_enqueue_style( 'fewer-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+
+	// Enqueue Google fonts, if necessary.
+	wp_enqueue_style( 'fewer-fonts', fewer_fonts_url(), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'fewer_scripts' );
+
+/**
+ * Add Google webfonts, if necessary.
+ *
+ * - See: http://themeshaper.com/2014/08/13/how-to-add-google-fonts-to-wordpress-themes/
+ */
+function fewer_fonts_url() {
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	* supported by Epilogue, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$epilogue = esc_html_x( 'on', 'Epilogue font: on or off', 'fewer' );
+
+	if ( 'off' !== $epilogue ) {
+		$font_families = array();
+
+		$font_families[] = 'Epilogue:300,300i';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
