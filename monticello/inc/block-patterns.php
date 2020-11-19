d74b1554971401f2960b9bb2c7656e8fbffcf26e
Jeff Ong <jonger4@gmail.com>
2020-11-19 16:32:01 -0500

    Update the learn more block pattern to use built in type sizes.

--- monticello/inc/block-patterns.php
+++ monticello/inc/block-patterns.php
@@ -34,12 +34,14 @@ if ( function_exists( 'register_block_pattern' ) ) {
 			'categories'    => array( 'monticello' ),
 			'viewportWidth' => 1440,
 			'content'       => '<!-- wp:group {"align":"full","backgroundColor":"light-beige-3"} -->
-			<div class="wp-block-group alignfull has-light-beige-3-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:heading {"textAlign":"center"} -->
-			<h2 class="has-text-align-center">At Java the Hut, we serve the finest freshly ground beans from all over the world.</h2>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center"} -->
-			<p class="has-text-align-center">Open 7am–4pm daily<br>321 Main Street, New York, NY, 10001</p>
+			<div class="wp-block-group alignfull has-light-beige-3-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size">At Java the Hut, we serve the finest freshly ground beans from all over the world.</p>
 			<!-- /wp:paragraph -->
+			
+			<!-- wp:paragraph {"align":"center","fontSize":"regular"} -->
+			<p class="has-text-align-center has-regular-font-size">Open 7am–4pm daily<br>321 Main Street, New York, NY, 10001</p>
+			<!-- /wp:paragraph -->
+			
 			<!-- wp:buttons {"contentJustification":"center"} -->
 			<div class="wp-block-buttons is-content-justification-center"><!-- wp:button -->
 			<div class="wp-block-button"><a class="wp-block-button__link">Learn More</a></div>
