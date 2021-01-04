ee8cc94689b67097bc17d4cdcd9eb551994a0bc9
Kjell Reigstad <kjell@kjellr.com>
2021-01-04 16:04:51 -0500

    Remove comments forms from single post pages.

--- blankie/single.php
+++ blankie/single.php
@@ -21,11 +21,6 @@ get_header();
 
 				get_template_part( 'template-parts/content/content-singular' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
 			endwhile; // End of the loop.
 			?>
 
