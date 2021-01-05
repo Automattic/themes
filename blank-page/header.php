f4734badd1e1930322b65587e0bc59711d163ab2
Kjell Reigstad <kjell@kjellr.com>
2021-01-05 11:53:16 -0500

    Add logo/title/tagline to archive pages.

--- blank-page/header.php
+++ blank-page/header.php
@@ -9,6 +9,11 @@
  * @package Blank Page
  * @since 1.0
  */
+$blog_info    = get_bloginfo( 'name' );
+$description  = get_bloginfo( 'description', 'display' );
+$show_title   = ( true === get_theme_mod( 'display_title_and_tagline', true ) );
+$header_class = $show_title ? 'site-title' : 'screen-reader-text';
+
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -22,4 +27,27 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'blank-page' ); ?></a>
+
+	<header id="masthead" class="<?php echo $header_classes; ?>" role="banner">
+		<?php if ( has_custom_logo() && $show_title && ! is_singular() ) : ?>
+			<div class="site-logo"><?php the_custom_logo(); ?></div>
+		<?php endif; ?>
+
+		<div class="site-branding">
+			<?php if ( ! empty( $blog_info ) && $show_title && ! is_singular() ) : ?>
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $blog_info; ?></a></h1>
+				<?php else : ?>
+					<p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $blog_info; ?></a></p>
+				<?php endif; ?>
+			<?php endif; ?>
+
+			<?php if ( ( $description || is_customize_preview() ) && $show_title && ! is_singular() ) : ?>
+				<p class="site-description">
+					<?php echo $description; ?>
+				</p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+	</header><!-- #masthead -->
+
 	<div id="content" class="site-content">
