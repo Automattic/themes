11166eb7713cb7f0298e00600a335a6584e0b0eb
Jason Crist <jcrist@pbking.com>
2021-01-12 13:58:16 -0500

    Remove 'dark-editor-style' theme support
    This fixes the issue where some styles (such as the image caption) have
    "dark mode" support added to them.  This is fine if a user has "dark
    mode" enabled for their system.  But in the default mode it isn't a
    "dark" style and therefore those stylistic behaviors introduce problems.
    
    I did not notice any negative behavior due to this being removed.
    
    This behavior seems to have been introduced due to recent chagnes in
    Gutenberg that introduce additional editor styles for themes that
    express support for 'dark-editor-style'.

--- spearhead/functions.php
+++ spearhead/functions.php
@@ -21,7 +21,6 @@ if ( ! function_exists( 'spearhead_setup' ) ) :
 
 		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
-		add_theme_support( 'dark-editor-style' );
 
 		// Enqueue editor styles.
 		add_editor_style(
