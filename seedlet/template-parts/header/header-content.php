0e3abe3f6cd6dba4c1be414cd083ef34be254858
Kjell Reigstad <kjell@kjellr.com>
2021-01-12 09:12:35 -0500

    Only hide site header/footer if the homepage is set to a static page.

--- seedlet/template-parts/header/header-content.php
+++ seedlet/template-parts/header/header-content.php
@@ -6,7 +6,7 @@ $header_classes .= true === get_theme_mod( 'display_title_and_tagline', true ) ?
 $header_classes .= $has_primary_nav ? ' has-menu' : '';
 ?>
 
-<?php if ( ! ( true === get_theme_mod( 'hide_site_header', false ) && is_front_page() ) ) : ?>
+<?php if ( ! ( true === get_theme_mod( 'hide_site_header', false ) && is_front_page() && is_page() ) ) : ?>
 	<header id="masthead" class="<?php echo $header_classes; ?>" role="banner">
 		<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 		<?php get_template_part( 'template-parts/header/navigation' ); ?>
