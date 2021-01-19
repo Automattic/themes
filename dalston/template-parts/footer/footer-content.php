f5897b420e64fe304de49c1476c9bdaa7e03b76e
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-01-19 15:59:52 +0100

    footer dalston, margin top header too

--- dalston/template-parts/footer/footer-content.php
+++ dalston/template-parts/footer/footer-content.php
@@ -1,31 +1,21 @@
-<footer id="colophon" class="site-footer <?php echo class_exists( 'A8C\FSE\WP_Template' ) ? 'entry-content' : 'responsive-max-width'; ?>">
+<?php if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content. ?>
+	<footer class="fse-template-part fse-footer entry-content">
 	<?php
-	if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content.
 		$template = new A8C\FSE\WP_Template();
 		$template->output_template_content( A8C\FSE\WP_Template::FOOTER );
-		else : // Otherwise we'll fallback to the default Varia footer below.
-			get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-			if ( has_nav_menu( 'menu-2' ) ) :
-				?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'varia' ); ?>">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'menu-2',
-							'menu_class'     => 'footer-menu',
-							'depth'          => 1,
-						)
-					);
-					?>
-				</nav><!-- .footer-navigation -->
-				<?php
-			endif;
-		endif;
+	else : // Otherwise we'll fallback to the default Varia footer below.
 		?>
 
-	<div class="site-info">
-		<?php get_template_part( 'template-parts/footer/site-name', '' ); ?>
-		<?php get_template_part( 'template-parts/footer/privacy-policy', '' ); ?>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
+	<footer id="colophon" class="site-footer responsive-max-width">
+			<?php
+			if ( ! ( true === get_theme_mod( 'hide_site_footer', false ) && is_front_page() && is_page() ) ) : // If this is the homepage and the footer elements are set to hide, don't load this part.
+				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				get_template_part( 'template-parts/footer/footer', 'navigation' );
+			endif;
+			?>
+	<?php endif; ?>
+		<div class="site-info">
+			<?php get_template_part( 'template-parts/footer/site-name', '' ); ?>
+			<?php get_template_part( 'template-parts/footer/privacy-policy', '' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
