cff4974e2321bf0fbaccc1a00356b66c831b94ba
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-01-19 16:41:15 +0100

    rockfield header and footer

--- rockfield/template-parts/footer/footer-content.php
+++ rockfield/template-parts/footer/footer-content.php
@@ -1,22 +1,22 @@
-<footer id="colophon" class="site-footer">
-	<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
-	<?php get_template_part( 'template-parts/footer/privacy-policy', '' ); ?>
-	<?php get_template_part( 'template-parts/header/social', 'navigation' ); ?>
+<?php if ( class_exists( 'A8C\FSE\WP_Template' ) ) : // If the FSE plugin is active, use the Header template for content. ?>
+	<footer class="fse-template-part fse-footer entry-content">
+	<?php
+		$template = new A8C\FSE\WP_Template();
+		$template->output_template_content( A8C\FSE\WP_Template::FOOTER );
+	else : // Otherwise we'll fallback to the default Varia footer below.
+		?>
+	<footer id="colophon" class="site-footer">
 
-	<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
-		<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'rockfield' ); ?>">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-2',
-					'menu_class'     => 'footer-menu',
-					'depth'          => 1,
-				)
-			);
+		<?php
+			if ( ! ( true === get_theme_mod( 'hide_site_footer', false ) && is_front_page() && is_page() ) ) : // If this is the homepage and the footer elements are set to hide, don't load this part.
+				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				get_template_part( 'template-parts/footer/privacy', 'policy' );
+				get_template_part( 'template-parts/header/social', 'navigation' );echo 'ee';
+				get_template_part( 'template-parts/footer/footer', 'navigation' );echo 'ee';
+			endif;
 			?>
-		</nav><!-- .footer-navigation -->
 	<?php endif; ?>
 	<div class="site-info">
-		<?php get_template_part( 'template-parts/footer/site-name', '' ); ?>
+		<?php get_template_part( 'template-parts/footer/site', 'name' ); ?>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
