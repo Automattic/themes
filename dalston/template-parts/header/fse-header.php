4a0a3ffb4781532185685108b1ab1adba19fbf1a
Ben Dwyer <ben@scruffian.com>
2021-01-20 11:18:53 +0000

    refactor FSE header templates

---
+++ dalston/template-parts/header/fse-header.php
@@ -0,0 +1,6 @@
+<header id="masthead" class="site-header site-branding entry-content">
+	<?php
+		$template = new A8C\FSE\WP_Template();
+		$template->output_template_content( A8C\FSE\WP_Template::HEADER );
+	?>
+</header>
