35aed362a8ed3066527d0c29624608fb0530d189
Danny Dudzic <daniel.dudzic@automattic.com>
2021-02-03 17:44:29 +0100

    Balasana: Refactor color annotations to use CSS variables

--- balasana/functions.php
+++ balasana/functions.php
@@ -9,6 +9,18 @@
  * @since 1.0.0
  */
 
+if ( ! function_exists( 'varia_default_colors' ) ) {
+	function varia_default_colors() {
+		return array(
+			'background' => '#ffffff',
+			'foreground' => '#303030',
+			'primary'    => '#19744c',
+			'secondary'  => '#bc2213',
+			'tertiary'   => '#19744c',
+		);
+	}
+}
+
 if ( ! function_exists( 'balasana_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -49,56 +61,6 @@ if ( ! function_exists( 'balasana_setup' ) ) :
 					'shortName' => __( 'XL', 'balasana' ),
 					'size'      => 34.56,
 					'slug'      => 'huge',
-				),
-			)
-		);
-
-		/*
-		 * Get customizer colors and add them to the editor color palettes
-		 *
-		 * - if the customizer color is empty, use the default
-		 */
-		$colors_array     = get_theme_mod( 'colors_manager' ); // color annotations array()
-		$primary          = ! empty( $colors_array ) ? $colors_array['colors']['link'] : '#19744C'; // $config-global--color-primary-default;
-		$secondary        = ! empty( $colors_array ) ? $colors_array['colors']['fg1'] : '#BC2213';  // $config-global--color-secondary-default;
-		$background       = ! empty( $colors_array ) ? $colors_array['colors']['bg'] : '#FFFFFF';   // $config-global--color-background-default;
-		$foreground       = ! empty( $colors_array ) ? $colors_array['colors']['txt'] : '#303030';  // $config-global--color-foreground-default;
-		$foreground_light = ( ! empty( $colors_array ) && $colors_array['colors']['txt'] != '#303030' ) ? $colors_array['colors']['txt'] : '#505050';  // $config-global--color-foreground-light-default;
-		$foreground_dark  = ( ! empty( $colors_array ) && $colors_array['colors']['txt'] != '#303030' ) ? $colors_array['colors']['txt'] : '#101010';  // $config-global--color-foreground-dark-default;
-
-		// Editor color palette.
-		add_theme_support(
-			'editor-color-palette',
-			array(
-				array(
-					'name'  => __( 'Primary', 'balasana' ),
-					'slug'  => 'primary',
-					'color' => $primary,
-				),
-				array(
-					'name'  => __( 'Secondary', 'balasana' ),
-					'slug'  => 'secondary',
-					'color' => $secondary,
-				),
-				array(
-					'name'  => __( 'Background', 'balasana' ),
-					'slug'  => 'background',
-					'color' => $background,
-				),
-				array(
-					'name'  => __( 'Foreground', 'balasana' ),
-					'slug'  => 'foreground',
-					'color' => $foreground,
-				),
-				array(
-					'name'  => __( 'Foreground Light', 'balasana' ),
-					'slug'  => 'foreground-light',
-					'color' => $foreground_light,
-				),
-				array(
-					'name'  => __( 'Foreground Dark', 'balasana' ),
-					'slug'  => 'foreground-dark',
-					'color' => $foreground_dark,
 				),
 			)
 		);
@@ -196,15 +158,5 @@ function balasana_editor_styles() {
 
 	// Enqueue Google fonts in the editor
 	wp_enqueue_style( 'balasana-editor-fonts', balasana_fonts_url(), array(), null );
-
-	// Hide duplicate palette colors
-	$colors_array = get_theme_mod('colors_manager', array( 'colors' => true )); // color annotations array()
-	if ( ! empty( $colors_array ) && $colors_array['colors']['txt'] != '#394d55' ) { // $config-global--color-foreground-light-default;
-		$inline_palette_css = '.components-circular-option-picker__option-wrapper:nth-child(5),
-			.components-circular-option-picker__option-wrapper:nth-child(6) {
-				display: none;
-			}';
-		wp_add_inline_style( 'wp-edit-blocks', $inline_palette_css );
-	}
 }
 add_action( 'enqueue_block_editor_assets', 'balasana_editor_styles' );
