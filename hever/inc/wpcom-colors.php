d5e61a6f556c01c2fe2cfa8d62d264f799609f2a
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-02-09 17:48:40 +0100

    extracted the customizer js to be required directly by the children that need it

--- hever/inc/wpcom-colors.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once get_template_directory() . '/inc/wpcom-colors-v2.php';
---
+++ hever/inc/wpcom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions.
+ *
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @package Hever
+ */
+require_once get_template_directory() . '/inc/wpcom-colors-v2.php';
+/**
+ * Bind JS handlers to instantly live-preview changes.
+ */
+function hever_wpcom_color_annotations_preview_js() {
+	wp_enqueue_script( 'hever_wpcom_color_annotations_preview', get_template_directory_uri() . '/inc/color-annotations-preview.js', array( 'customize-preview' ), '1.0', true );
+}
+add_action( 'customize_preview_init', 'hever_wpcom_color_annotations_preview_js' ); 
