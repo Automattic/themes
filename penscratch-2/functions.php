bd63f2f5579957d0202c454a5b57e3903b3eb626
Ben Dwyer <ben@scruffian.com>
2021-02-16 16:24:40 +0000

    Penscratch: Update editor styles

--- penscratch-2/functions.php
+++ penscratch-2/functions.php
@@ -126,6 +126,11 @@ if ( ! function_exists( 'penscratch_2_setup' ) ) :
 			)
 		);
 
+		add_theme_support( 'editor-styles' );
+		add_editor_style( array(
+			get_theme_file_uri( '/css/editor-blocks.css' ),
+			penscratch_2_fonts_url()
+		) );
 	}
 endif; // penscratch_2_setup
 add_action( 'after_setup_theme', 'penscratch_2_setup' );
@@ -221,17 +226,6 @@ function penscratch_2_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'penscratch_2_scripts' );
-
-/**
- * Enqueue editor styles for Gutenberg
- */
-function penscratch_2_block_editor_styles() {
-	// Block styles.
-	wp_enqueue_style( 'penscratch-2-block-editor-style', get_theme_file_uri( '/css/editor-blocks.css' ) );
-	// Fonts.
-	wp_enqueue_style( 'penscratch-2-fonts-url', penscratch_2_fonts_url(), array(), null );
-}
-add_action( 'enqueue_block_editor_assets', 'penscratch_2_block_editor_styles' );
 
 /**
  * Register Google Fonts
