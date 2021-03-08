fbfecdb627e821680dd9ce1a1e7beb3e9ea829c4
Jeff Ong <jonger4@gmail.com>
2021-03-08 11:44:21 -0800

    Add padding.

--- blank-canvas-blocks/functions.php
+++ blank-canvas-blocks/functions.php
@@ -11,9 +11,6 @@ if ( ! function_exists( 'blank_canvas_blocks_support' ) ) :
 		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
 
-		// Add support for basic block styles.
-		add_theme_support( 'wp-block-styles' );
-
 		// Enqueue editor styles.
 		add_editor_style( array( 
 			'/assets/ponyfill.css'
