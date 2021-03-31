f3dbf85dec545838d23ac953129524ccb02cb664
Jason Crist <jcrist@pbking.com>
2021-03-31 08:21:55 -0400

    Adding SCSS build + watch workflow to Seedlet Blocks (#3575)
    * Adding SCSS build + watch workflow to Seedlet Blocks

--- seedlet-blocks/functions.php
+++ seedlet-blocks/functions.php
@@ -7,7 +7,7 @@ function seedlet_blocks_editor_styles() {
 	// Enqueue editor styles.
 	add_editor_style(
 		array(
-			'/style.css',
+			'/assets/theme.css',
 		)
 	);
 }
@@ -18,6 +18,6 @@ add_action( 'after_setup_theme', 'seedlet_blocks_editor_styles' );
  * Enqueue scripts and styles.
  */
 function seedlet_blocks_scripts() {
-	wp_enqueue_style( 'seedlet_blocks-styles', get_stylesheet_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'seedlet_blocks-styles', get_stylesheet_directory_uri() . '/assets/theme.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'seedlet_blocks_scripts' );
