552f48a74bf86c3bcdbe61b847c2f447a93cd248
Jason Crist <jcrist@pbking.com>
2021-04-01 15:38:34 -0400

    Style seedlet bcb latest post Block (#3597)
    * Added latest post block styling
    * Register latest post block pattern from Seedlet
    * Added latest post block styles
    Co-authored-by: Jeff Ong <jeff.ong@automattic.com>

--- seedlet-blocks/functions.php
+++ seedlet-blocks/functions.php
@@ -21,3 +21,13 @@ function seedlet_blocks_scripts() {
 	wp_enqueue_style( 'seedlet_blocks-styles', get_stylesheet_directory_uri() . '/assets/theme.css', array('blank_canvas_blocks-ponyfill'), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'seedlet_blocks_scripts' );
+
+/**
+ * Block Patterns.
+ */
+require get_stylesheet_directory() . '/inc/block-patterns.php';
+
+/**
+ * Block Styles.
+ */
+require get_stylesheet_directory() . '/inc/block-styles.php';
