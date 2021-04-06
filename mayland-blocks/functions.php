809051e840c8febef5377bb3f9ea0ee43f632731
Maggie <maggie.cabrera@automattic.com>
2021-04-06 17:49:02 +0200

    removed supports that are on the parent theme (#3594)

--- mayland-blocks/functions.php
+++ mayland-blocks/functions.php
@@ -3,21 +3,6 @@
 if ( ! function_exists( 'mayland_blocks_support' ) ) :
 	function mayland_blocks_support() {
 
-		// Alignwide and alignfull classes in the block editor.
-		add_theme_support( 'align-wide' );
-
-		// Adding support for core block visual styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Adding support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
-
-		// Add support for experimental link color control.
-		add_theme_support( 'experimental-link-color' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
 		// Enqueue editor styles.
 		add_editor_style(
 			array(
