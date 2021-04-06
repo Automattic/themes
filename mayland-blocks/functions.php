3dbecedb42aa453de0f68f04e6ef9758bdc9f2ca
Jeff Ong <jonger4@gmail.com>
2021-04-06 14:46:54 -0400

    Cherry pick changes to seedlet.

--- mayland-blocks/functions.php
+++ mayland-blocks/functions.php
@@ -1,23 +1,74 @@
 <?php
 
 if ( ! function_exists( 'mayland_blocks_support' ) ) :
-	function mayland_blocks_support() {
+	function mayland_blocks_support()  {
+
+		// Alignwide and alignfull classes in the block editor.
+		add_theme_support( 'align-wide' );
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Adding support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
+
+		// Add support for experimental link color control.
+		add_theme_support( 'experimental-link-color' );
+
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style(
-			array(
-				'style.css',
-			)
-		);
-	}
-	add_action( 'after_setup_theme', 'mayland_blocks_support' );
+		add_editor_style( array( 
+			'style.css',
+			mayland_blocks_fonts_url()
+		) );
+    }
+    add_action( 'after_setup_theme', 'mayland_blocks_support' );
 endif;
+
+/**
+ * Add Google webfonts, if necessary.
+ *
+ * - See: http://themeshaper.com/2014/08/13/how-to-add-google-fonts-to-wordpress-themes/
+ */
+function mayland_blocks_fonts_url() {
+
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	* supported by Poppins, translate this to 'off'. Do not translate
+	* into your own language.
+	*/
+	$poppins = esc_html_x( 'on', 'Poppins font: on or off', 'mayland' );
+
+	if ( 'off' !== $poppins ) {
+		$font_families = array();
+
+		$font_families[] = 'Poppins:400,400i,600,600i';
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
 
 /**
  * Enqueue scripts and styles.
  */
 function mayland_blocks_scripts() {
 	// Enqueue front-end styles.
-	wp_enqueue_style( 'mayland-blocks-styles', get_stylesheet_uri(), array( 'blank_canvas_blocks-ponyfill' ), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'mayland-blocks-styles', get_stylesheet_uri() );
+
+	// Enqueue front-end styles.
+	wp_enqueue_style( 'mayland-blocks-block-styles', get_stylesheet_directory_uri() . '/assets/alignments.css' );
+
+	// Enqueue Google fonts, if necessary.
+	wp_enqueue_style( 'mayland-blocks-fonts', mayland_blocks_fonts_url(), array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'mayland_blocks_scripts' );
