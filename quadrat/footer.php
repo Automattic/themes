7c1b27df225480596857371a55edd7e59f9fc2dd
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-04-14 12:14:03 +0200

    added header and footer templates

---
+++ quadrat/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Quadrat
+ * @since 1.0.0
+ */
+
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
