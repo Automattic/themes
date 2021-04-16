b50476ffdfefac5f29443c6339ad99750274f8dc
Jeff Ong <jeff.ong@automattic.com>
2021-04-16 08:37:48 -0700

    Add the hosts block pattern. (#3662)
    * Add the hosts block pattern.
    
    * Fix categories.
    
    * Update img refs.
    
    * Add alt text to media text pattern.
    
    * Revise block pattern names.

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -11,19 +11,64 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 
 		if ( function_exists( 'register_block_pattern_category' ) ) {
 			register_block_pattern_category(
-				'quadrat-blocks',
-				array( 'label' => __( 'Quadrat', 'quadrat-blocks' ) )
+				'quadrat',
+				array( 'label' => __( 'Quadrat', 'quadrat' ) )
 			);
 		}
 
 		if ( function_exists( 'register_block_pattern' ) ) {
 
 			register_block_pattern(
-				'quadrat-blocks/example',
+				'quadrat/headline-button',
 				array(
-					'title'      => __( 'Alternating Grid of Latest Posts', 'quadrat-blocks' ),
-					'categories' => array( 'quadrat-blocks' ),
-					'content'    => '<!-- wp:latest-posts {"displayPostContent":true,"columns":5,"className":"is-style-quadrat-alternating-grid"} /-->',
+					'title'      => __( 'Headline and button', 'quadrat' ),
+					'categories' => array( 'quadrat' ),
+					'content'    => '<!-- wp:group {"align":"wide"} -->
+					<div class="wp-block-group alignwide"><!-- wp:paragraph -->
+					<p>' . esc_html__( 'The Hosts', 'quadrat') . '</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:heading -->
+					<h2>' . esc_html__( 'Samantha and Olivia are both writers and activists.', 'quadrat') . '</h2>
+					<!-- /wp:heading -->
+					
+					<!-- wp:spacer {"height":36} -->
+					<div style="height:36px" aria-hidden="true" class="wp-block-spacer"></div>
+					<!-- /wp:spacer -->
+					
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button -->
+					<div class="wp-block-button"><a class="wp-block-button__link">' . esc_html__( 'Learn More', 'quadrat') . '</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+					<!-- /wp:group -->',
+				)
+			);
+
+			register_block_pattern(
+				'quadrat/media-text-button',
+				array(
+					'title'      => __( 'Media and text with button', 'quadrat' ),
+					'categories' => array( 'quadrat' ),
+					'content'    => '<!-- wp:media-text {"mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/04/girls-illustration.png","mediaType":"image"} -->
+					<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/04/girls-illustration.png" alt="' . esc_attr__( 'Illustration of two women.' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+					<p>' . esc_html__( 'The Hosts', 'quadrat') . '</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:heading -->
+					<h2>' . esc_html__( 'Sarah &amp; Olivia are both writers and activists.', 'quadrat') . '</h2>
+					<!-- /wp:heading -->
+					
+					<!-- wp:spacer {"height":36} -->
+					<div style="height:36px" aria-hidden="true" class="wp-block-spacer"></div>
+					<!-- /wp:spacer -->
+					
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button -->
+					<div class="wp-block-button"><a class="wp-block-button__link">' . esc_html__( 'Learn More', 'quadrat') . '</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div></div>
+					<!-- /wp:media-text -->',
 				)
 			);
 
