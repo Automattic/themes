0a1434354328bc78a6ae48fb99c49ad5f43df589
Maggie <maggie.cabrera@automattic.com>
2021-04-16 17:58:32 +0200

    Quadrat: override BCB's templates with php ones (#3667)
    * override BCB's templates with php ones

---
+++ quadrat/search.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Search template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Quadrat
+ * @since 1.0.0
+ */
+get_header();
+
+	// the header
+	echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/header.html' ) );
+
+	echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/search.html' ) );
+
+	// the footer
+	echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/footer.html' ) );
+
+get_footer();
