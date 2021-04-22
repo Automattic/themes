a49b9d3f808bbd12c63b7243d5606e239fcbf641
Maggie <maggie.cabrera@automattic.com>
2021-04-22 20:56:06 +0200

    Quadrat: Add Latest episodes block pattern (#3665)
    * added Latest episodes block pattern
    
    * translatable strings
    
    * staging url for media
    
    * rebased, updated texts and adjusted paddings
    
    * added a group with padding to the blocks
    
    * Body line-height to 1.7.
    
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

--- quadrat/header.php
+++ quadrat/header.php
@@ -7,7 +7,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordPress
- * @subpackage çQuadrat
+ * @subpackage Quadrat
  * @since 1.0.0
  */
 ?><!doctype html>
