c2300566a669c656ed333cb60171da65e1da56d6
Ben Dwyer <ben@scruffian.com>
2021-04-23 13:13:12 +0100

    Add menu locations to quadrat

--- quadrat/index.php
+++ quadrat/index.php
@@ -12,15 +12,51 @@
  * @package Quadrat
  * @since 1.0.0
  */
+
+function get_nav_menu() {
+	$menus = wp_get_nav_menus();
+
+	if ( is_wp_error( $menus ) || empty( $menus ) ) {
+		return false;
+	}
+
+	$counts = wp_list_pluck( $menus, 'count' );
+	$menus  = array_combine( $counts, $menus );
+	ksort( $menus );
+	$menus = array_reverse( $menus );
+	$menus = array_values( $menus );
+	$menu  = array_shift( $menus );
+
+	if ( empty( $menu->count ) ) {
+		return false;
+	}
+
+	return $menu;
+}
+
 get_header();
 
-	// the header
-	echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/header.html' ) );
+// the header
+echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/header.html' ) );
+
+if ( is_active_sidebar( 'navigation' ) ) {
+	dynamic_sidebar( 'navigation' );
+} else {
+	$menu = get_nav_menu();
+	the_widget(
+		'WP_Nav_Menu_Widget',
+		array(
+			'nav_menu' => $menu,
+		)
+	);
+}
+
+if ( is_active_sidebar( 'social-icons' ) ) {
+	dynamic_sidebar( 'social-icons' );
+}
 
 	// the query
 	echo do_blocks( '<!-- wp:post-content {"layout":{"inherit":true}} /-->' );
 
 	// the footer
 	echo do_blocks( file_get_contents( get_stylesheet_directory() . '/templates/footer.html' ) );
-	
-get_footer();
