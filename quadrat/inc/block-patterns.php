f1e851bf535f8c1e82b95605af2a9a6aeb58c39b
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-04-26 11:50:52 +0200

    changes to fit the design, lowered opacity on black and white versions

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -122,8 +122,8 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 				array(
 					'title'      => __( 'Cover block with heading', 'quadrat' ),
 					'categories' => array( 'quadrat' ),
-					'content'    => '<!-- wp:cover {"overlayColor":"darker-blue","align":"wide","className":"is-style-quadrat-cover-diamond"} -->
-					<div class="wp-block-cover alignwide has-darker-blue-background-color has-background-dim is-style-quadrat-cover-diamond"><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"pink"} -->
+					'content'    => '<!-- wp:cover {"overlayColor":"darker-blue","minHeight":700,"align":"wide","className":"is-style-quadrat-cover-diamond"} -->
+					<div class="wp-block-cover alignwide has-darker-blue-background-color has-background-dim is-style-quadrat-cover-diamond" style="min-height:700px"><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"pink"} -->
 					<h2 class="has-text-align-center has-pink-color has-text-color">' . esc_html__( 'Episode 3: A Cycle of Emotions', 'quadrat' ) . '</h2>
 					<!-- /wp:heading --></div></div>
 					<!-- /wp:cover -->',
