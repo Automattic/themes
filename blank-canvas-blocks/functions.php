0d7ec66c8bc7b5aa6de1de89e1b0186e5808393f
Ben Dwyer <ben@scruffian.com>
2021-04-27 10:17:30 +0100

    BCB: Fix PHP warnings

--- blank-canvas-blocks/functions.php
+++ blank-canvas-blocks/functions.php
@@ -59,8 +59,15 @@ add_action( 'wp_enqueue_scripts', 'blank_canvas_blocks_scripts' );
 
 function blank_canvas_blocks_fonts_url() {
 	$fonts_url = '';
+	if ( ! class_exists( 'WP_Theme_JSON_Resolver' ) ) {
+		return '';
+	}
 
-	$theme_data  = WP_Theme_JSON_Resolver::get_merged_data()->get_settings();
+	$theme_data = WP_Theme_JSON_Resolver::get_merged_data()->get_settings();
+	if ( empty( $theme_data ) || empty( $theme_data['defaults'] ) || empty( $theme_data['defaults']['custom'] ) ) {
+		return '';
+	}
+
 	$custom_data = $theme_data['defaults']['custom'];
 	if ( array_key_exists( 'fontsToLoadFromGoogle', $custom_data ) ) {
 		$font_families = $theme_data['defaults']['custom']['fontsToLoadFromGoogle'];
