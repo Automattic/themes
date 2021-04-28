81bb9f99ac060073e521d3a0516848b735cbbe40
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-04-28 12:10:57 +0200

    changed the url of the image, added alt content and em tag

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -123,12 +123,13 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 					'title'      => __( 'Episode', 'quadrat' ),
 					'categories' => array( 'quadrat' ),
 
-					'content'    => '<!-- wp:media-text {"mediaId":1559,"mediaLink":"http://localhost:4759/?attachment_id=1559","mediaType":"image","mediaWidth":40,"backgroundColor":"darker-blue","textColor":"pink"} -->
-					<div class="wp-block-media-text alignwide is-stacked-on-mobile has-pink-color has-darker-blue-background-color has-text-color has-background" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="http://localhost:4759/wp-content/uploads/2021/04/Introspection-scaled-1-1024x914.jpeg" alt="" class="wp-image-1559 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
+					'content'    => '<!-- wp:media-text {"mediaId":1497,"mediaLink":"http://freethemes.local/?attachment_id=1497","mediaType":"image","mediaWidth":40,"mediaSizeSlug":"large","imageFill":false,"className":"has-pink-color has-darker-blue-background-color has-text-color has-background is-style-default"} -->
+					<div class="wp-block-media-text alignwide is-stacked-on-mobile has-pink-color has-darker-blue-background-color has-text-color has-background is-style-default" style="grid-template-columns:40% auto"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/04/Introspection-scaled.jpg" alt="' . esc_attr__( 'Illustration of an introspective woman.', 'quadrat') . '" class="wp-image-1497 size-large"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3} -->
 					<h3>' . esc_html__( 'Episode 3: A Cycle of Emotions', 'quadrat' ) . '</h3>
 					<!-- /wp:heading -->
+					
 					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size">' . esc_html__( '“Do we need periods if they make us crazy?”, Sarah asks. We reached out to our friend Diana Roth, an endocrinologist specialized in women’s hormonal health to know how menstrual cycles impact women’s brains and emotions. Hint: yes, we do.', 'quadrat' ) . '</p>
+					<p class="has-small-font-size">' . wp_kses_post( __( '<em>“Do we need periods if they make us crazy?”</em>, Sarah asks. We reached out to our friend Diana Roth, an endocrinologist specialized in women’s hormonal health to know how menstrual cycles impact women’s brains and emotions. Hint: yes, we do.', 'quadrat' ) ) . '</p>
 					<!-- /wp:paragraph --></div></div>
 					<!-- /wp:media-text -->',
 				)
