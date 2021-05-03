7f18b1188a7b93bc93feb12dc3bff104c8a58b6b
Jeff Ong <jonger4@gmail.com>
2021-05-03 14:27:31 -0400

    Restore header wrapper that was accidentally removed in rebase.

--- quadrat/template-parts/header.php
+++ quadrat/template-parts/header.php
@@ -11,11 +11,13 @@
 require get_stylesheet_directory() . '/inc/render-navigation-block.php';
 ?>
 
-<?php
+<header class="site-header" role="banner">
+	<?php
 	echo do_blocks(
-		'<!-- wp:group {"className":"site-header","style":{"spacing":{"padding":{"right":"35px","left":"35px"}}}} --><div class="wp-block-group site-header" style="padding-right:35px;padding-left:35px">
+		'<!-- wp:group {"style":{"spacing":{"padding":{"right":"35px","left":"35px"}}}} --><div class="wp-block-group" style="padding-right:35px;padding-left:35px">
 		<!-- wp:site-title /-->' .
 		render_navigation_block( 'primary' ) .
 		'</div><!-- /wp:group -->'
 	);
-
+	?>
+</header>
