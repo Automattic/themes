26d128917e66bd010cc3232e84bf62e1aca3ea7a
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-05-04 16:58:50 +0200

    added new class to all the group blocks

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -177,8 +177,8 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 					<!-- /wp:buttons --></div></div>
 					<!-- /wp:media-text --></div>
 					<!-- /wp:group -->
-					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}},"border":{"style":"none"}},"borderColor":"pink"} -->
-					<div class="wp-block-group alignwide has-border-color has-pink-border-color" style="border-style:none;padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaPosition":"right","mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Pink-squared-shapes-combination._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
+					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}}},"className":"headlines-pattern"} -->
+					<div class="wp-block-group alignwide headlines-pattern" style="padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaPosition":"right","mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Pink-squared-shapes-combination._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
 					<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:auto 52%"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Pink-squared-shapes-combination._.png" alt="' . esc_attr__( 'Pink squared shapes combination', 'quadrat' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"tiny"} -->
 					<p class="has-tiny-font-size">Coaching</p>
 					<!-- /wp:paragraph -->
@@ -198,8 +198,8 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 					<!-- /wp:buttons --></div></div>
 					<!-- /wp:media-text --></div>
 					<!-- /wp:group -->
-					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}},"border":{"style":"none"}},"borderColor":"pink"} -->
-					<div class="wp-block-group alignwide has-border-color has-pink-border-color" style="border-style:none;padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Orange-triangle-in-purple-background._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
+					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}}},"className":"headlines-pattern"} -->
+					<div class="wp-block-group alignwide headlines-pattern" style="padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Orange-triangle-in-purple-background._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
 					<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:52% auto"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Orange-triangle-in-purple-background._.png" alt="' . esc_attr__( 'Orange triangle in purple background', 'quadrat' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"tiny"} -->
 					<p class="has-tiny-font-size">Coaching</p>
 					<!-- /wp:paragraph -->
