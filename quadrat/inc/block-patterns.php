8781ff30e83be8a8a9970ebafc5ee748d83f56a4
Maggie <maggie.cabrera@automattic.com>
2021-05-04 19:12:20 +0200

    Quadrat: Headlines and Buttons Block Pattern (#3744)
    * created block pattern
    
    * padding rules for mobile
    
    * added new class to all the group blocks
    
    * Remove all padding from text content on mobile.
    
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -151,6 +151,77 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 				)
 			);
 
+			register_block_pattern(
+				'quadrat/headlines-and-buttons',
+				array(
+					'title'      => __( 'Headlines and buttons', 'quadrat' ),
+					'categories' => array( 'quadrat' ),
+					'content'    => '<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}}},"className":"headlines-pattern"} -->
+					<div class="wp-block-group alignwide headlines-pattern" style="padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Group-of-squares-displayed-diagonally._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
+					<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:52% auto"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Group-of-squares-displayed-diagonally._.png" alt="' . esc_attr__( 'Group of squares displayed diagonally', 'quadrat' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"tiny"} -->
+					<p class="has-tiny-font-size">Coaching</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:heading {"level":3} -->
+					<h3>' . esc_html__( 'Career Coaching 101', 'quadrat' ) . '</h3>
+					<!-- /wp:heading -->
+					
+					<!-- wp:paragraph -->
+					<p>' . esc_html__( 'Starting at $ 99', 'quadrat' ) . '</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"pink"} -->
+					<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background">' . esc_html__( 'Learn more', 'quadrat' ) . '</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div></div>
+					<!-- /wp:media-text --></div>
+					<!-- /wp:group -->
+					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}}},"className":"headlines-pattern"} -->
+					<div class="wp-block-group alignwide headlines-pattern" style="padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaPosition":"right","mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Pink-squared-shapes-combination._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
+					<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:auto 52%"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Pink-squared-shapes-combination._.png" alt="' . esc_attr__( 'Pink squared shapes combination', 'quadrat' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"tiny"} -->
+					<p class="has-tiny-font-size">Coaching</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:heading {"level":3} -->
+					<h3>' . esc_html__( 'Business Coaching', 'quadrat' ) . '</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph -->
+					<p>' . esc_html__( 'Starting at $ 99', 'quadrat' ) . '</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"pink"} -->
+					<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background">' . esc_html__( 'Learn more', 'quadrat' ) . '</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div></div>
+					<!-- /wp:media-text --></div>
+					<!-- /wp:group -->
+					<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"70px","top":"70px"}}},"className":"headlines-pattern"} -->
+					<div class="wp-block-group alignwide headlines-pattern" style="padding-top:70px;padding-bottom:70px"><!-- wp:media-text {"mediaLink":"https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Orange-triangle-in-purple-background._.png","mediaType":"image","mediaWidth":52,"verticalAlignment":"top"} -->
+					<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:52% auto"><figure class="wp-block-media-text__media"><img src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/Orange-triangle-in-purple-background._.png" alt="' . esc_attr__( 'Orange triangle in purple background', 'quadrat' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"tiny"} -->
+					<p class="has-tiny-font-size">Coaching</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:heading {"level":3} -->
+					<h3>' . esc_html__( 'Life Coaching', 'quadrat' ) . '</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph -->
+					<p>' . esc_html__( 'Starting at $ 99', 'quadrat' ) . '</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"pink"} -->
+					<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background">' . esc_html__( 'Learn more', 'quadrat' ) . '</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div></div>
+					<!-- /wp:media-text --></div>
+					<!-- /wp:group -->',
+				)
+			);
+
 		}
 	}
 endif;
