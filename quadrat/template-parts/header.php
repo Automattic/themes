3e0947cf6ffb95233cd8a50921f5aa31978b371c
Jeff Ong <jonger4@gmail.com>
2021-05-05 12:17:34 -0400

    Move site logo above site title.

--- quadrat/template-parts/header.php
+++ quadrat/template-parts/header.php
@@ -14,13 +14,13 @@ require get_stylesheet_directory() . '/inc/render-navigation-block.php';
 <header class="site-header" role="banner">
 	<?php
 	echo do_blocks(
-		'<!-- wp:group {"style":{"spacing":{"padding":{"right":"35px","left":"35px"}}}} --><div class="wp-block-group" style="padding-right:35px;padding-left:35px">
+		'<!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","right":"35px","left":"35px"}}}} --><div class="wp-block-group" style="padding-top:35px;padding-right:35px;padding-left:35px">
 		<!-- wp:group --><div class="wp-block-group">
+		<!-- wp:site-logo {"width":100} /-->
 		<!-- wp:site-title /-->
 		</div><!-- /wp:group -->' .
 		render_navigation_block( 'primary' ) .
-		'</div><!-- /wp:group -->
-		<!-- wp:site-logo {"width":100} /-->'
+		'</div><!-- /wp:group -->'
 	);
 	?>
 </header>
