c81be2845f7b2f351d13cb1a794ebdcb10f038b5
Maggie Cabrera <maggie.cabrera@automattic.com>
2021-05-06 11:25:02 +0200

    refactor listen to the podcast

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -25,6 +25,7 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 				'episode',
 				'cover-with-heading',
 				'headlines-and-buttons',
+				'listen-to-the-podcast',
 			);
 
 			foreach ( $block_patterns as $block_pattern ) {
@@ -34,24 +35,6 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 				);
 			}
 
-			register_block_pattern(
-				'quadrat/listen-to-the-podcast',
-				array(
-					'title'      => __( 'Listen to the podcast', 'quadrat' ),
-					'categories' => array( 'quadrat' ),
-					'content'    => '<!-- wp:heading {"textAlign":"center","level":4} -->
-						<h4 class="has-text-align-center">' . esc_html__( 'Listen to the podcast' ) . '</h4>
-						<!-- /wp:heading -->
-
-						<!-- wp:audio {"id":102} -->
-						<figure class="wp-block-audio"><audio controls src="https://quadrat.mystagingwebsite.com/wp-content/uploads/2021/05/quadrat.mp3"></audio></figure>
-						<!-- /wp:audio -->
-
-						<!-- wp:paragraph {"align":"center","fontSize":"tiny"} -->
-						<p class="has-text-align-center has-tiny-font-size">' . sprintf( wp_kses( __( 'Listen on <a href="%1$s">Apple Podcasts</a>, <a href="%2$s">Spotify</a>.' ), array( 'a' => array( 'href' => array() ) ) ), 'https://podcasts.apple.com', 'https://spotify.com' ) . '</p>
-						<!-- /wp:paragraph -->',
-				)
-			);
 		}
 	}
 endif;
