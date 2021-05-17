b6f25bde99c7728f25e9709fbadd0711cc18a22c
Ben Dwyer <ben@scruffian.com>
2021-05-17 16:19:36 +0100

    Save/retrive the colors in the CPT

--- quadrat/inc/customization.php
+++ quadrat/inc/customization.php
@@ -131,15 +131,18 @@ class CustomizerBridge {
 	}
 
 	function register_color_control( $custom_option, $section_key, $wp_customize ) {
-		$setting_key = 'customizer-bridge-' . $custom_option->slug;
-
-		$wp_customize->add_setting(
+		$setting_key           = 'customizer-bridge-' . $custom_option->slug;
+		$global_styles_setting = new WP_Customize_Global_Styles_Setting(
+			$wp_customize,
 			$setting_key,
 			array(
 				'default'           => esc_html( $custom_option->default ),
 				'sanitize_callback' => 'sanitize_hex_color',
+				'slug'              => $custom_option->slug,
 			)
 		);
+
+		$wp_customize->add_setting( $global_styles_setting );
 
 		$wp_customize->add_control(
 			new WP_Customize_Color_Control(
@@ -154,3 +157,160 @@ class CustomizerBridge {
 	}
 }
 new CustomizerBridge;
+
+if ( class_exists( 'WP_Customize_Setting' ) ) {
+	/**
+	 * Customize API: WP_Customize_Global_Styles_Setting class
+	 *
+	 * This handles validation, sanitization and saving of the value.
+	 *
+	 * @package WordPress
+	 * @subpackage Customize
+	 * @since 4.7.0
+	 */
+
+	/**
+	 * Custom Setting to handle WP Global_Styles.
+	 *
+	 * @since 4.7.0
+	 *
+	 * @see WP_Customize_Setting
+	 */
+	final class WP_Customize_Global_Styles_Setting extends WP_Customize_Setting {
+
+		/**
+		 * The setting type.
+		 *
+		 * @since 4.7.0
+		 * @var string
+		 */
+		public $type = 'global_styles';
+
+		/**
+		 * Setting Transport
+		 *
+		 * @since 4.7.0
+		 * @var string
+		 */
+		public $transport = 'refresh'; // This is the default
+
+		/**
+		 * Capability required to edit this setting.
+		 *
+		 * @since 4.7.0
+		 * @var string
+		 */
+		public $capability = 'edit_css';
+
+		/**
+		 * Slug
+		 *
+		 * @var string
+		 */
+		public $slug = '';
+
+		/**
+		 * WP_Customize_Global_Styles_Setting constructor.
+		 *
+		 * @since 4.7.0
+		 *
+		 * @throws Exception If the setting ID does not match the pattern `custom_css[$stylesheet]`.
+		 *
+		 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
+		 * @param string               $id      A specific ID of the setting.
+		 *                                      Can be a theme mod or option name.
+		 * @param array                $args    Setting arguments.
+		 */
+		public function __construct( $manager, $id, $args = array() ) {
+			parent::__construct( $manager, $id, $args );
+			/*if ( 'customizer-bridge-foreground' !== $this->id_data['base'] ) {
+			throw new Exception( 'Expected customizer-bridge-foreground id_base.' );
+			}
+			if ( 1 !== count( $this->id_data['keys'] ) || empty( $this->id_data['keys'][0] ) ) {
+			throw new Exception( 'Expected customizer-bridge-foreground key.' );
+			}
+			$this->stylesheet = $this->id_data['keys'][0];*/
+		}
+
+		/**
+		 * Add filter to preview post value.
+		 *
+		 * @since 4.7.9
+		 *
+		 * @return bool False when preview short-circuits due no change needing to be previewed.
+		 */
+		public function preview() {
+			if ( $this->is_previewed ) {
+				return false;
+			}
+			$this->is_previewed = true;
+			return true;
+		}
+
+		/**
+		 * Fetch the value of the setting. Will return the previewed value when `preview()` is called.
+		 *
+		 * @since 4.7.0
+		 *
+		 * @see WP_Customize_Setting::value()
+		 *
+		 * @return string
+		 */
+		public function value() {
+			if ( $this->is_previewed ) {
+				$post_value = $this->post_value( null );
+				if ( null !== $post_value ) {
+					return $post_value;
+				}
+			}
+			$id_base = $this->id_data['base'];
+			$value   = '';
+
+			$user_custom_post_type_id = WP_Theme_JSON_Resolver::get_user_custom_post_type_id();
+			$user_theme_json_post     = get_post( $user_custom_post_type_id );
+
+			if ( $user_theme_json_post ) {
+				$post_content = $user_theme_json_post->post_content;
+			}
+			if ( empty( $post_content ) ) {
+				$value = $this->default;
+			}
+
+			$post_json = json_decode( $post_content );
+			$value     = $post_json->styles->color->{$this->slug};
+
+			/** This filter is documented in wp-includes/class-wp-customize-setting.php */
+			$value = apply_filters( "customize_value_{$id_base}", $value, $this );
+
+			return $value;
+		}
+
+		/**
+		 * Store the color in the Global Styles custom post
+		 *
+		 * @since 4.7.0
+		 *
+		 * @param string $color The input color.
+		 * @return int|false The post ID or false if the value could not be saved.
+		 */
+		public function update( $color ) {
+			if ( empty( $color ) ) {
+				$color = '';
+			}
+
+			$user_custom_post_type_id                                   = WP_Theme_JSON_Resolver::get_user_custom_post_type_id();
+			$user_theme_json_post                                       = get_post( $user_custom_post_type_id );
+			$user_theme_json_post_content                               = json_decode( $user_theme_json_post->post_content );
+			$user_theme_json_post_content->styles->color->{$this->slug} = $color;
+
+			$user_theme_json_post->post_content = json_encode( $user_theme_json_post_content );
+			$result                             = wp_update_post( $user_theme_json_post );
+
+			if ( $r instanceof WP_Error ) {
+				return false;
+			}
+
+			return $r->ID;
+		}
+	}
+}
