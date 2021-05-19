c74c3d9cd0714277def7ce114d273fc9a03f61a3
Jason Crist <jcrist@pbking.com>
2021-05-19 13:58:29 -0400

    removed npm install from the build run

--- blank-canvas-blocks/rebuild-child-themes.sh
+++ blank-canvas-blocks/rebuild-child-themes.sh
@@ -5,7 +5,6 @@ declare -a ChildThemes=( "mayland-blocks" "seedlet-blocks" "quadrat" )
 for child in ${ChildThemes[@]}; do
 	cd '../'${child}
 	echo 'Rebulding '${child}
-	npm install
 	npm run build
 done
 
