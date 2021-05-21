f28b725e624c9ca232a739be2bbadb80514e7cb6
Jordan <jordan@jordesign.com>
2021-05-21 12:25:26 +1000

    Simplify output of link

--- seedlet/template-parts/content/content-none.php
+++ seedlet/template-parts/content/content-none.php
@@ -22,23 +22,12 @@
 			<p>
 			<?php _e( "Your site is set to show the the most recent posts on your homepage - but you don't have any Posts published.", 'seedlet' ); ?></p>
 
-			<?php printf(
-				'<p>' 
-				. wp_kses('<a href="%1$s" class="button">',
-					array(
-						'a' => array(
-							'href' => array(),
-							'class' => 'button'
-						)
-					)					
-				) ,
-				esc_url( admin_url( 'edit.php' ) )
-			);
-			
-			/* translators: 1: link to WP admin new post page. 2: class for anchor */
+			<p>
+				<a href="<?php  echo esc_url( admin_url( 'edit.php' ) ); ?>" class="button">
+			<?php /* translators: 1: link to WP admin new post page. */
 			 _e( 'Add or publish Posts', 'seedlet' ); ?>
-			
-			</a></p>
+				</a>
+			</p>
 
 		<?php elseif ( is_search() ) :
 			?>
