1e2f0a163ae154fc19173849dd45a199465d5794
Jeff Ong <jonger4@gmail.com>
2021-05-25 13:36:10 -0400

    Empty core required template files.

---
+++ blockbase/comments.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Empty comments file to avoid theme check errors
+ * 
+ * @package Blockbase
+ * @since 1.0
+ * 
+ */
+
+ return;
---
+++ blockbase/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Boilerplate header for initial themecheck.
+ * 
+ * @package Blockbase
+ * @since 1.0
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
