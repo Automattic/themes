efd9b80774616aa43967cb80abf0450a17b1923c
Daniel Dudzic <daniel.dudzic@automattic.com>
2021-06-30 20:17:16 +0200

    Blockbase: Hide WP.com stats smiley (#4137)
    * Blockbase: Hide WP.com stats smiley
    Co-authored-by: Jason Crist <jcrist@pbking.com>

---
+++ blockbase/inc/wpcom.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * WordPress.com-specific functions and definitions.
+ *
+ * This file is centrally included from `wp-content/mu-plugins/wpcom-theme-compat.php`.
+ *
+ * @package Blockbase
+ */
+
+
+/**
+ * Enqueue our WP.com styles for front-end.
+ * Loads after theme styles so we can add overrides.
+ */
+function blockbase_wpcom_scripts() {
+	wp_enqueue_style( 'blockbase-wpcom-style', get_template_directory_uri() . '/inc/wpcom-style.css', array( 'blockbase-ponyfill' ) );
+}
+add_action( 'wp_enqueue_scripts', 'blockbase_wpcom_scripts' );
+
