75fcbc8301e08c258f41df86a9f8647971d6bae7
Ben Dwyer <ben@scruffian.com>
2021-07-06 15:47:38 +0100

    i18n: Search Results pattern

--- quadrat/inc/block-patterns.php
+++ quadrat/inc/block-patterns.php
@@ -26,6 +26,7 @@ if ( ! function_exists( 'quadrat_register_block_patterns' ) ) :
 				'latest-episodes',
 				'listen-to-the-podcast',
 				'media-text-button',
+				'search-results',
 			);
 
 			if ( class_exists( 'WP_Block_Type_Registry' ) && \WP_Block_Type_Registry::get_instance()->is_registered( 'jetpack/subscriptions' ) ) {
