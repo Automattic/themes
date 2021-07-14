dd4f57dbfe51f4a86649bfc21ab72e87ce15f31f
Jason Crist <jcrist@pbking.com>
2021-07-14 14:19:23 -0400

    Fix/quadrat localization domain (#4228)
    * Added missing localization domain strings
    
    * Changed esc_attr to esc_html per theme-check warning
    
    * refactored calls to _x to __

--- quadrat/inc/patterns/listen-to-the-podcast.php
+++ quadrat/inc/patterns/listen-to-the-podcast.php
@@ -9,7 +9,7 @@ return array(
 	'title'      => __( 'Listen to the podcast', 'quadrat' ),
 	'categories' => array( 'quadrat' ),
 	'content'    => '<!-- wp:heading {"textAlign":"center","level":4} -->
-		<h4 class="has-text-align-center">' . esc_html__( 'Listen to the podcast' ) . '</h4>
+		<h4 class="has-text-align-center">' . esc_html__( 'Listen to the podcast', 'quadrat' ) . '</h4>
 		<!-- /wp:heading -->
 
 		<!-- wp:audio {"id":102} -->
@@ -17,6 +17,6 @@ return array(
 		<!-- /wp:audio -->
 
 		<!-- wp:paragraph {"align":"center","fontSize":"tiny"} -->
-		<p class="has-text-align-center has-tiny-font-size">' . sprintf( wp_kses( __( 'Listen on <a href="%1$s">Apple Podcasts</a>, <a href="%2$s">Spotify</a>.' ), array( 'a' => array( 'href' => array() ) ) ), 'https://podcasts.apple.com', 'https://spotify.com' ) . '</p>
+		<p class="has-text-align-center has-tiny-font-size">' . sprintf( wp_kses( __( 'Listen on <a href="%1$s">Apple Podcasts</a>, <a href="%2$s">Spotify</a>.', 'quadrat' ), array( 'a' => array( 'href' => array() ) ) ), 'https://podcasts.apple.com', 'https://spotify.com' ) . '</p>
 		<!-- /wp:paragraph -->',
 );
