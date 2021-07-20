95d49679febc2a96c1204375414808ea991426f1
Matt Wiebe <matt@somadesign.ca>
2021-07-20 14:01:12 -0500

    Revert "Blockbase: Disable the Site Editor on WPCOM (#4206)"
    This reverts commit ae2d449610b4a58f0d36c7c70a25481b39a73518.

--- blockbase/inc/wpcom.php
+++ blockbase/inc/wpcom.php
@@ -7,6 +7,7 @@
  * @package Blockbase
  */
 
+
 /**
  * Enqueue our WP.com styles for front-end.
  * Loads after theme styles so we can add overrides.
@@ -15,3 +16,4 @@ function blockbase_wpcom_scripts() {
 	wp_enqueue_style( 'blockbase-wpcom-style', get_template_directory_uri() . '/inc/wpcom-style.css', array( 'blockbase-ponyfill' ) );
 }
 add_action( 'wp_enqueue_scripts', 'blockbase_wpcom_scripts' );
+
