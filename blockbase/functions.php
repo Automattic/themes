cbfecf8a57b38c4f4ffe24dc5704abd89f0b6487
Matt Wiebe <wiebe@automattic.com>
2021-07-29 14:27:42 -0500

    Restore the Customizer

--- blockbase/functions.php
+++ blockbase/functions.php
@@ -96,6 +96,16 @@ function blockbase_fonts_url() {
 }
 
 /**
+ * Restores the Customizer since we still rely on it.
+ */
+function blockbase_restore_customizer() {
+	remove_action( 'admin_menu', 'gutenberg_remove_legacy_pages' );
+}
+add_action( 'init', 'blockbase_restore_customizer' );
+// For WPcom REST API requests to work properly.
+add_action( 'restapi_theme_init', 'blockbase_restore_customizer', 11 );
+
+/**
  * Customize Global Styles
  */
 require get_template_directory() . '/inc/customizer/wp-customize-colors.php';
