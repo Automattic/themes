9b011ae59e93274d6b716921947982a3e20ac5b8
Maggie <maggie.cabrera@automattic.com>
2021-08-05 18:05:40 +0200

    created block pattern file

---
+++ skatepark/inc/patterns/full-width-image-with-aside-caption.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Full width image with aside caption.
+ *
+ * @package Skatepark
+ */
+
+return array(
+	'title'      => __( 'Full width image with aside caption', 'skatepark' ),
+	'categories' => array( 'skatepark' ),
+	'content'    => '',
+);
