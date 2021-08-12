e9287679e9ca65e9cd23aee6934a6b54c1a8d4d1
Ben Dwyer <ben@scruffian.com>
2021-08-12 17:36:53 +0100

    Blockbase: Fix 404 template (#4402)
    * Blockbase: Fix the 404 template
    
    * Blockbase: Fix the 404 template
    
    * Add a 404 for skatepark
    
    * update footer tag
    
    * fix textdomain

--- blockbase/404.php
+++ blockbase/404.php
@@ -5,20 +5,14 @@
  * @package Blockbase
  * @since 1.1.1
  */
-wp_head();
-
-echo gutenberg_block_template_part( 'header' );
+get_header();
 ?>
+	<main class="container-404">
+		<h1 class="has-text-align-center has-large-font-size"><?php _e( 'Oops! That page can&rsquo;t be found.', 'blockbase' ); ?></h1>
 
-<main class="container-404">
-	<h1 class="has-text-align-center has-large-font-size"><?php _e( 'Oops! That page can&rsquo;t be found.', 'blockbase' ); ?></h1>
+		<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'blockbase' ); ?></p>
 
-	<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'blockbase' ); ?></p>
-
-	<?php echo do_blocks('<!-- wp:search {"label":""} /-->'); ?>
-</main>
-
+		<?php echo do_blocks('<!-- wp:search {"label":""} /-->'); ?>
+	</main>
 <?php
-echo gutenberg_block_template_part( 'footer' );
-
-wp_footer();
+get_footer();
