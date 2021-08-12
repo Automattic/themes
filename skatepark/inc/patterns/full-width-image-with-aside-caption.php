c840c418c7d17626a00985be989ac7a4680091cd
Maggie <maggie.cabrera@automattic.com>
2021-08-12 10:52:16 +0200

    created block style, changed approach to use caption instead of columns

--- skatepark/inc/patterns/full-width-image-with-aside-caption.php
+++ skatepark/inc/patterns/full-width-image-with-aside-caption.php
@@ -8,23 +8,7 @@
 return array(
 	'title'      => __( 'Full width image with aside caption', 'skatepark' ),
 	'categories' => array( 'skatepark' ),
-	'content'    => '<!-- wp:image {"align":"full","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":["#000","#BFF5A5"]},"border":{"radius":"0px"}}} -->
-	<figure class="wp-block-image alignfull size-large" style="border-radius:0px"><img src="https://skateparkdemo.files.wordpress.com/2021/08/stocksnap_5otfjyvyse.jpg" alt=""/></figure>
-	<!-- /wp:image -->
-	
-	<!-- wp:columns {"align":"wide","className":"aside-caption"} -->
-	<div class="wp-block-columns alignwide aside-caption"><!-- wp:column -->
-	<div class="wp-block-column"></div>
-	<!-- /wp:column -->
-	
-	<!-- wp:column {"width":"455px"} -->
-	<div class="wp-block-column" style="flex-basis:455px"><!-- wp:separator {"className":"is-style-wide"} -->
-	<hr class="wp-block-separator is-style-wide"/>
-	<!-- /wp:separator -->
-	
-	<!-- wp:paragraph {"fontSize":"small"} -->
-	<p class="has-small-font-size">Learn the basics of skating along with a group of your peers. More advanced at skating? Our skateboarding coaches will work with you 1:1 to advance your technique.</p>
-	<!-- /wp:paragraph --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns -->',
+	'content'    => '<!-- wp:image {"align":"full","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":["#000","#BFF5A5"]},"border":{"radius":"0px"}},"className":"is-style-skatepark-aside-caption"} -->
+	<figure class="wp-block-image alignfull size-large is-style-skatepark-aside-caption" style="border-radius:0px"><img src="https://skateparkdemo.files.wordpress.com/2021/08/stocksnap_5otfjyvyse.jpg" alt=""/><figcaption><strong>Learn the basics of skating along with a group of your peers. More advanced at skating? Our skateboarding coaches will work with you 1:1 to advance your technique.</strong></figcaption></figure>
+	<!-- /wp:image -->',
 );
