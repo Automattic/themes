577064a0e758a60914d16f1f466a1f7b54600949
Ben Dwyer <ben@scruffian.com>
2021-08-12 17:21:25 +0100

    Blockbase: Fix the 404 template

---
+++ blockbase/footer.php
@@ -0,0 +1,11 @@
+<div class="wp-block-template-part site-footer-container">
+		<?php
+			echo gutenberg_block_template_part( 'footer' );
+		?>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
