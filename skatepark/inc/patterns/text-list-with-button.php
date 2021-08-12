72b73c8249655a7ba1e6180bdae204b1acf3f0f1
Jeff Ong <jonger4@gmail.com>
2021-08-12 11:57:58 -0400

    Use columns instead of a group.

--- skatepark/inc/patterns/text-list-with-button.php
+++ skatepark/inc/patterns/text-list-with-button.php
@@ -10,8 +10,9 @@
 return array(
 	'title'      => __( 'Text List with Button', 'skatepark' ),
 	'categories' => array( 'skatepark' ),
-	'content'    => '<!-- wp:group -->
-		<div class="wp-block-group"><!-- wp:heading {"level":4,"className":"is-style-skatepark-heading-border"} -->
+	'content'    => '<!-- wp:columns {"align":"wide"} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"75vw"} -->
+		<div class="wp-block-column" style="flex-basis:75vw"><!-- wp:heading {"level":4,"className":"is-style-skatepark-heading-border"} -->
 		<h4 class="is-style-skatepark-heading-border">' . esc_html__( 'Visiting Coaches', 'skatepark' ) . '</h4>
 		<!-- /wp:heading -->
 		<!-- wp:heading --><h2>' . esc_html__( 'Rodney Mullen. Paul Rodriguez. Bucky Lasek. Bob Burnquist. Tony Hawk. Ryan Sheckler. Eric Koston. Bam Margera. Chris Cole. Andrew Reynolds.', 'skatepark' ) . '</h2>
@@ -21,5 +22,9 @@ return array(
 		<div class="wp-block-button is-style-outline"><a class="wp-block-button__link">' . esc_html__( 'See All Faculty', 'skatepark' ) . '</a></div>
 		<!-- /wp:button --></div>
 		<!-- /wp:buttons --></div>
-		<!-- /wp:group -->'
+		<!-- /wp:column -->
+		<!-- wp:column {"width":"25vw"} -->
+		<div class="wp-block-column" style="flex-basis:25vw"></div>
+		<!-- /wp:column --></div>
+		<!-- /wp:columns -->'
 );
