085edb419606ee3a78895d85d3904c5109c36b7a
Jeff Ong <jonger4@gmail.com>
2021-08-16 16:17:32 -0400

    Add and refine mixed media in container pattern.

--- skatepark/inc/patterns/mixed-media-in-container.php
+++ skatepark/inc/patterns/mixed-media-in-container.php
@@ -8,17 +8,21 @@
 return array(
 	'title'      => __( 'Mixed Media in Container', 'skatepark' ),
 	'categories' => array( 'skatepark' ),
-	'content'    => '<!-- wp:group {"align":"wide","style":{"border":{"style":"solid"},"spacing":{"padding":{"top":"0%","right":"2.5%","bottom":"2.5%","left":"2.5%"}}}} -->
-	<div class="wp-block-group alignwide" style="border-style:solid;padding-top:0%;padding-right:2.5%;padding-bottom:2.5%;padding-left:2.5%"><!-- wp:columns {"align":"wide"} -->
+	'content'    => '<!-- wp:spacer {"height":40} -->
+	<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	
+	<!-- wp:group {"align":"wide","style":{"border":{"style":"solid"},"spacing":{"padding":{"top":"1%","right":"2.5%","bottom":"2.5%","left":"2.5%"}}},"className":"skatepark-mixed-media-in-container"} -->
+	<div class="wp-block-group alignwide skatepark-mixed-media-in-container" style="border-style:solid;padding-top:1%;padding-right:2.5%;padding-bottom:2.5%;padding-left:2.5%"><!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.33%"} -->
 	<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:heading {"level":3} -->
-	<h3>Why Camp?</h3>
+	<h3>' . esc_html__( 'Why Camp?', 'skatepark' ) . '</h3>
 	<!-- /wp:heading --></div>
 	<!-- /wp:column -->
 	
 	<!-- wp:column {"width":"66.66%"} -->
 	<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph {"className":"is-style-indented-paragraph"} -->
-	<p class="is-style-indented-paragraph">Skate ipsum dolor sit amet, poseur nollie casper pop shove-it. Kickturn noseblunt slide tailslide lien air Bonite. Rocket air poseur snake shoveit Chico Brenes. Crooked grind locals casper slide bank. Disaster frigid air bail aerial. Hang ten smith grind tailslide sick.</p>
+	<p class="is-style-indented-paragraph">' . esc_html__( 'Skateboarding can teach people discipline, patience, and resiliency. It’s a great sport for kids with a lot of energy! We’ll help build up some structure and support in your life, all while you meet new people and have a ton of fun learning how to skateboard. Just think of all the cool tricks you’ll learn!', 'skatepark' ) . '</p>
 	<!-- /wp:paragraph -->
 	
 	<!-- wp:spacer {"height":40} -->
@@ -26,7 +30,7 @@ return array(
 	<!-- /wp:spacer -->
 	
 	<!-- wp:image {"sizeSlug":"large","style":{"color":{"duotone":["#000","#BFF5A5"]}}} -->
-	<figure class="wp-block-image size-large"><img src="https://cdn.stocksnap.io/img-thumbs/960w/skateboard-sideways_USRYO9E14Y.jpg" alt=""/></figure>
+	<figure class="wp-block-image size-large"><img src="' . get_stylesheet_directory_uri() . '/assets/images/skateboard-sideways.jpg" " alt="' . esc_attr__( 'A skateboard laying on its side on top of concrete.', 'skatepark' ) . '"/></figure>
 	<!-- /wp:image -->
 	
 	<!-- wp:spacer {"height":40} -->
@@ -38,11 +42,11 @@ return array(
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide"><!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:heading {"level":4} -->
-	<h4>Discipline</h4>
+	<h4>' . esc_html__( 'Discipline', 'skatepark' ) . '</h4>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
-	<p>Skate ipsum dolor sit amet, poseur nollie casper pop shove-it. Kickturn noseblunt slide tailslide lien air Bonite. Rocket air poseur snake shoveit Chico Brenes. Crooked grind locals casper slide bank.</p>
+	<p>' . esc_html__( 'Skateboarding can teach people discipline, patience, and resiliency. It’s a great sport for kids with a lot of energy! We’ll help build up some structure and support in your life, and you’ll have fun.', 'skatepark' ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column -->
 	
@@ -52,11 +56,11 @@ return array(
 	
 	<!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:heading {"level":4} -->
-	<h4>Friendship</h4>
+	<h4>' . esc_html( 'Friendship', 'skatepark' ) . '</h4>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
-	<p>Skate ipsum dolor sit amet, poseur nollie casper pop shove-it. Kickturn noseblunt slide tailslide lien air Bonite. Rocket air poseur snake shoveit Chico Brenes. Crooked grind locals casper slide bank.</p>
+	<p>' . esc_html( 'Skateboarding is a social sport! Come hang out and meet some new friends to cheer you on while you skate. We have a strict no-bullying policy and maintain a supportive environment.', 'skatepark' ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns -->
@@ -70,11 +74,11 @@ return array(
 	
 	<!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:heading {"level":4} -->
-	<h4>Mentoring</h4>
+	<h4>' . esc_html( 'Mentoring', 'skatepark' ) . '</h4>
 	<!-- /wp:heading -->
 	
 	<!-- wp:paragraph -->
-	<p>Skate ipsum dolor sit amet, poseur nollie casper pop shove-it. Kickturn noseblunt slide tailslide lien air Bonite. Rocket air poseur snake shoveit Chico Brenes. Crooked grind locals casper slide bank.</p>
+	<p>' . esc_html( 'Work 1:1 with our team of coaches to learn how to skate. They’ll be your mentors and guides while you’re at camp. Plus, many of our older campers will mentor the younger skaters.', 'skatepark' ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column -->
 	
