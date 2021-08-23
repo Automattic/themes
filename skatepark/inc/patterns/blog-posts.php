452d5bccccc5e2fe79ce446a18f37562a69c529b
Maggie <maggie.cabrera@automattic.com>
2021-08-23 11:23:54 +0200

    fixed spacing issues on the query block

--- skatepark/inc/patterns/blog-posts.php
+++ skatepark/inc/patterns/blog-posts.php
@@ -10,8 +10,8 @@ return array(
 	'categories' => array( 'skatepark' ),
 	'content'    => '<!-- wp:query {"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"displayLayout":{"type":"flex","columns":3},"align":"wide"} -->
 	<div class="wp-block-query alignwide"><!-- wp:post-template -->
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"layout":{"inherit":false}} -->
-	<div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:separator {"className":"is-style-wide"} -->
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","left":"30px"}}},"layout":{"inherit":false}} -->
+	<div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-left:30px"><!-- wp:separator {"className":"is-style-wide"} -->
 	<hr class="wp-block-separator is-style-wide"/>
 	<!-- /wp:separator -->
 	
@@ -19,7 +19,7 @@ return array(
 	
 	<!-- wp:post-title {"isLink":true,"fontSize":"normal"} /-->
 	
-	<!-- wp:post-excerpt {"className":"is-style-indented-post-excerpt"} /-->
+	<!-- wp:post-excerpt {"moreText":"Continue reading","className":"is-style-indented-post-excerpt"} /-->
 	
 	<!-- wp:post-date {"style":{"typography":{"fontWeight":"500"}},"fontSize":"tiny"} /--></div>
 	<!-- /wp:group -->
