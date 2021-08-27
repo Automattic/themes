3071cdd46827a82bcfd219120601dfed7e666ea0
Sarah Norris <sarah@sekai.co.uk>
2021-08-27 13:54:54 +0100

    Skatepark: Add class to body if featured image exists

--- skatepark/functions.php
+++ skatepark/functions.php
@@ -57,3 +57,15 @@ require get_stylesheet_directory() . '/inc/block-patterns.php';
  * Block Styles.
  */
 require get_stylesheet_directory() . '/inc/block-styles.php';
+
+/**
+ * Add class to body if post/page has a featured image.
+ */
+function add_featured_image_class( $classes ) {    
+	global $post;
+	if ( isset ( $post->ID ) && get_the_post_thumbnail( $post->ID ) ) {
+		$classes[] = 'has-featured-image';
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'add_featured_image_class' );
