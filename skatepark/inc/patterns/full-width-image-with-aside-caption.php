a92c9048b297b4e3a61d0e945974ad64c5c0ddea
Maggie <maggie.cabrera@automattic.com>
2021-08-30 12:54:24 +0200

    added images to files and credited them

--- skatepark/inc/patterns/full-width-image-with-aside-caption.php
+++ skatepark/inc/patterns/full-width-image-with-aside-caption.php
@@ -9,6 +9,6 @@ return array(
 	'title'      => __( 'Full width image with aside caption', 'skatepark' ),
 	'categories' => array( 'skatepark' ),
 	'content'    => '<!-- wp:image {"align":"full","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":["#000","#BFF5A5"]},"border":{"radius":"0px"}},"className":"is-style-skatepark-aside-caption"} -->
-	<figure class="wp-block-image alignfull size-large is-style-skatepark-aside-caption" style="border-radius:0px"><img src="https://skateparkdemo.files.wordpress.com/2021/08/stocksnap_5otfjyvyse.jpg" alt="' . esc_attr__( "A skateboarder does a grab trick in a bowl-shaped skate park. In the background is a watching crowd, palm trees, and the ocean.", 'skatepark' ) . '"/><figcaption>' . esc_html__( "Learn the basics of skating along with a group of your peers. More advanced at skating? Our skateboarding coaches will work with you 1:1 to advance your technique.", 'skatepark' ) . '</figcaption></figure>
+	<figure class="wp-block-image alignfull size-large is-style-skatepark-aside-caption" style="border-radius:0px"><img src="' . get_stylesheet_directory_uri() . 'assets/images/skatepark.jpg" alt="' . esc_attr__( "A skateboarder does a grab trick in a bowl-shaped skate park. In the background is a watching crowd, palm trees, and the ocean.", 'skatepark' ) . '"/><figcaption>' . esc_html__( "Learn the basics of skating along with a group of your peers. More advanced at skating? Our skateboarding coaches will work with you 1:1 to advance your technique.", 'skatepark' ) . '</figcaption></figure>
 	<!-- /wp:image -->',
 );
--- skatepark/readme.txt
+++ skatepark/readme.txt
@@ -2,7 +2,7 @@
 Contributors: Automattic
 Requires at least: 5.7
 Tested up to: 5.7.2
-Requires PHP: 5.6.2
+Requires PHP: 5.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -33,5 +33,7 @@ Skatepark uses the following images:
 
 License: CC0
 https://stocksnap.io/photo/skateboard-sideways-USRYO9E14Y by Messala Ciulla (https://stocksnap.io/author/messala)
+https://stocksnap.io/photo/skateboarder-shoes-BTZE6FHZIV by Burst (https://stocksnap.io/author/burstshopify)
+https://stocksnap.io/photo/skateboard-skater-5OTFJYVYSE by Joe Cooke (https://stocksnap.io/author/14659)
 
 Used in block patterns.
