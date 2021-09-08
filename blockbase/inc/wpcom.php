d8948a3a9eb8242d1dfa5c53d259a786e1fea0e9
Ben Dwyer <ben@scruffian.com>
2021-09-08 13:50:59 +0100

    Revert "Blockbase: Remove wpcom specific CSS file (#4548)" (#4549)
    This reverts commit 463aafcee74d2dc5153549fb6e8fc154cfd58eb4.

--- blockbase/inc/wpcom.php
+++ blockbase/inc/wpcom.php
@@ -8,6 +8,15 @@
  */
 
 /**
+ * Enqueue our WP.com styles for front-end.
+ * Loads after theme styles so we can add overrides.
+ */
+function blockbase_wpcom_scripts() {
+	wp_enqueue_style( 'blockbase-wpcom-style', get_template_directory_uri() . '/inc/wpcom-style.css', array( 'blockbase-ponyfill' ) );
+}
+add_action( 'wp_enqueue_scripts', 'blockbase_wpcom_scripts' );
+
+/**
  * Restores the Customizer since we still rely on it.
  * (For WPcom REST API requests to work properly.)
  */
