056a24a6d63e31bc363de148de10aad5c89dce7b
Maggie <maggie.cabrera@automattic.com>
2021-09-08 09:53:20 +0200

    change google url for Fira

--- blockbase/inc/customizer/wp-customize-fonts.php
+++ blockbase/inc/customizer/wp-customize-fonts.php
@@ -12,6 +12,8 @@ class GlobalStylesFontsCustomizer {
 	private $font_control_default_body;
 	private $font_control_default_heading;
 
+	//Not all fonts support v2 of the API that allows for the shorter URls
+	//list of supported fonts: https://fonts.google.com/variablefonts
 	private $fonts = array(
 		'system-font'       => array(
 			'fontFamily' => '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
@@ -70,7 +72,7 @@ class GlobalStylesFontsCustomizer {
 			'fontFamily' => '"Fira Sans", sans-serif',
 			'slug'       => 'fira-sans',
 			'name'       => 'Fira Sans',
-			'google'     => 'family=Fira+Sans:ital,wght@0,100..900;1,100..900',
+			'google'     => 'family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900',
 		),
 		'inter'             => array(
 			'fontFamily' => '"Inter", sans-serif',
