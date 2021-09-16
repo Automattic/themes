2fd99f825670d1f76cdf8e67facb68702eff9de6
Ben Dwyer <ben@scruffian.com>
2021-09-16 15:37:04 +0100

    Update styles for block patterns

--- geologist/inc/patterns/quote.php
+++ geologist/inc/patterns/quote.php
@@ -8,8 +8,8 @@
 return array(
 	'title'      => __( 'Quote', 'geologist' ),
 	'categories' => array( 'geologist' ),
-	'blockTypes' => array( 'core/quote' ),
-	'content'    => '<!-- wp:quote -->
-	<blockquote class="wp-block-quote"><p>' . esc_html__( 'Amazonite, Ametrine, Beryl, Dioptase, Howlite, Jade, and Ruby.', 'geologist' ) . '”</p><cite>' . __( 'Sarah Edwards' ) . '</cite></blockquote>
-	<!-- /wp:quote -->',
+	'blockTypes' => array( 'core/pullquote' ),
+	'content'    => '<!-- wp:pullquote {"backgroundColor":"primary","textColor":"background"} -->
+	<figure class="wp-block-pullquote has-background-color has-primary-background-color has-text-color has-background"><blockquote><p>“' . esc_html( 'Amazonite, Ametrine, Beryl, Dioptase, Howlite, Jade, and Ruby.', 'geologist' ) . '”</p><cite>Sarah Edwards</cite></blockquote></figure>
+	<!-- /wp:pullquote -->',
 );
