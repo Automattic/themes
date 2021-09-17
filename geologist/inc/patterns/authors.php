220b663f1929cd5ea976c9e9ab17f4d4d00f1bfe
Ben Dwyer <ben@scruffian.com>
2021-09-17 20:44:01 +0100

    Update content for block patterns

--- geologist/inc/patterns/authors.php
+++ geologist/inc/patterns/authors.php
@@ -14,12 +14,36 @@ return array(
 	<!-- /wp:paragraph -->
 
 	<!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:post-author {"showAvatar":false,"showBio":true,"fontSize":"extra-large"} /--></div>
-	<!-- /wp:column -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading -->
+<h2>' . esc_html__( 'Sarah Edwards' ) . '</h2>
+<!-- /wp:heading -->
 
-	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:post-author {"showAvatar":false,"showBio":true,"fontSize":"extra-large"} /--></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns -->',
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Sarah is a geologist at Anthem State University. Her love for all things underground was sparked in Kindergarten when she unearthed a toy that someone left buried in the sandbox.' ) . '</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"primary","iconColorValue":"#F5F0E5","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"wordpress.com","service":"wordpress"} /-->
+
+<!-- wp:social-link {"url":"instagram.com","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading -->
+<h2>' . esc_html__( 'Angelo Ortiz' ) . '</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>' . esc_html__( 'Angelo is an independent researcher and hobbyist. His collection of gemstones rivals that of some museums. Angelo is always on the lookout for a new discovery, and carries his toolkit everywhere.' ) . '</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"primary","iconColorValue":"#F5F0E5","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"instagram.com","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"twitter.com","service":"twitter"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->',
 );
--- geologist/inc/patterns/image-feature.php
+++ geologist/inc/patterns/image-feature.php
@@ -9,20 +9,20 @@ return array(
 	'title'      => __( 'Image feature', 'geologist' ),
 	'categories' => array( 'geologist' ),
 	'blockTypes' => array( 'core/image', 'core/columns' ),
-	'content'    => '<!-- wp:image {"sizeSlug":"large"} -->
-	<figure class="wp-block-image size-large"><img src="https://geologistdemo.files.wordpress.com/2021/09/gem-02.jpg?w=2048" alt=""/></figure>
+	'content'    => '<!-- wp:image {"align":"wide","sizeSlug":"large"} -->
+	<figure class="wp-block-image alignwide size-large"><img src="https://geologistdemo.files.wordpress.com/2021/09/gem-02.jpg?w=2048" alt=""/></figure>
 	<!-- /wp:image -->
 
-	<!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column -->
+	<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column -->
 	<div class="wp-block-column"><!-- wp:heading {"level":3} -->
-	<h3>' . esc_html__( 'Geologist' ) .'</h3>
+	<h3>' . esc_html__( 'Geologist' ) . '</h3>
 	<!-- /wp:heading --></div>
 	<!-- /wp:column -->
 
 	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:paragraph {"fontSize":"tiny"} -->
-	<p class="has-tiny-font-size">' . esc_html__( "Geologist is a blog dedicated to everything buried under the earth's surface." ) . '</p>
+	<div class="wp-block-column"><!-- wp:paragraph -->
+	<p>' . esc_html__( "Geologist is a blog dedicated to everything buried under the earth's surface." ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns -->',
