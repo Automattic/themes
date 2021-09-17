c0772c8a18815f5e719d1c7154b12a5d33856268
Ben Dwyer <ben@scruffian.com>
2021-09-17 21:04:30 +0100

    fix quote markup

--- geologist/inc/patterns/quote.php
+++ geologist/inc/patterns/quote.php
@@ -9,7 +9,7 @@ return array(
 	'title'      => __( 'Quote', 'geologist' ),
 	'categories' => array( 'geologist' ),
 	'blockTypes' => array( 'core/pullquote' ),
-	'content'    => '<!-- wp:pullquote {"backgroundColor":"primary","textColor":"background"} -->
-	<figure class="wp-block-pullquote has-background-color has-primary-background-color has-text-color has-background"><blockquote><p>“' . esc_html( 'Amazonite, Ametrine, Beryl, Dioptase, Howlite, Jade, and Ruby.', 'geologist' ) . '”</p><cite>Sarah Edwards</cite></blockquote></figure>
-	<!-- /wp:pullquote -->',
+	'content'    => '<!-- wp:pullquote {"textColor":"background","className":"has-background-color has-primary-background-color has-text-color has-background"} -->
+	<figure class="wp-block-pullquote has-background-color has-primary-background-color has-text-color has-background"><blockquote class="has-text-color has-background-color"><p>“Amazonite, Ametrine, Beryl, Dioptase, Howlite, Jade, and Ruby.”</p><cite>Sarah Edwards</cite></blockquote></figure>
+	<!-- /wp:pullquote -->'
 );
