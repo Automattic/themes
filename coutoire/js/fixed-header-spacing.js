e0c44f4a788e0e8e2738f88c638ec32ad3eb40b4
Aurorum <torres126@clubpenguinmountains.com>
2021-09-18 11:13:24 +0100

    Fix issues with scroll

--- coutoire/js/fixed-header-spacing.js
+++ coutoire/js/fixed-header-spacing.js
@@ -40,7 +40,7 @@
 	 * Get page header height and use it for top-margin on
 	 * site-content when above mobile breakpoint
 	 */
-	function pageHeaderHeight() {
+	function pageHeaderHeight( adjustLogoHeight ) {
 		if ( document.documentElement.clientWidth <= 640 ) {
 
 			document.getElementById("primary").style.marginTop = 0;
@@ -59,24 +59,44 @@
 				content.style.marginTop = header.offsetHeight + "px";
 			}
 
-			logoHeight();
+			if ( adjustLogoHeight ) {
+				logoWidth();
+			}
 		}
 	};
-	
-	function logoHeight() {
+
+	let initialLogoWidth = 0;
+	function logoWidth() {
 		var firstTitle    = document.body.querySelector( '.entry-title' );
 		var content       = document.getElementById( 'primary' );
 		var siteLogo      = document.body.querySelector( '.site-logo' );
-		
-		if ( 
-			! document.body.classList.contains( 'sticky-menu-enabled' ) &&
-			siteLogo &&
-			siteLogo.offsetHeight + 64 > content.getBoundingClientRect().top 
-		)	{
-				// Subtract 64px based on the fact that the padding is already 32px, plus to give some extra space. 
-				siteLogo.style.maxWidth = firstTitle.getBoundingClientRect().left - 64 + 'px';
-			}
+
+		if ( document.body.classList.contains( 'sticky-menu-enabled' ) || ! siteLogo ) {
+			return;
 		}
+				
+		if ( siteLogo.offsetHeight + 64 > content.getBoundingClientRect().top )	{
+			// Subtract 64px based on the fact that the padding is already 32px, plus to give some extra space. 
+			siteLogo.style.width = firstTitle.getBoundingClientRect().left - 64 + 'px';
+			pageHeaderHeight( false );
+		}
+		
+		if ( window.scrollY === 0 ) {
+			siteLogo.style.width = initialLogoWidth + 'px';
+			pageHeaderHeight( false );
+
+			let intervalCount = 0;
+			let adjustingWidthInterval = setInterval( function() {
+				intervalCount++;
+				pageHeaderHeight( false );
+				
+				// 25 because the transition is 0.25s.
+				if ( intervalCount === 25 ) {
+					clearInterval( adjustingWidthInterval );
+				}
+			}, 1 );
+		}
+	}
 
 	/**
 	 * Run our function every time the window resizes
@@ -96,7 +116,9 @@
 		} )
 	);
 	
-	window.addEventListener( 'scroll', logoHeight );
+	initialLogoWidth = document.body.querySelector( '.site-logo img' ).width;
+	
+	window.addEventListener( 'scroll', logoWidth );
 
 	/**
 	 * Run our page header height function
