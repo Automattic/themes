2da3cc4b8e3991f5a3972616d884f297dfe06dee
Ben Dwyer <ben@scruffian.com>
2021-09-21 10:29:07 +0100

    remove white space

--- videomaker/functions.php
+++ videomaker/functions.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /**
@@ -22,4 +21,3 @@ function newtheme_scripts() {
 	wp_enqueue_style( 'newtheme-styles', get_stylesheet_directory_uri() . '/assets/theme.css', array('blockbase-ponyfill'), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'newtheme_scripts' );
-
