1d7ac7123356320a1714929a39cfbed2d6c6e9cc
Jason Crist <jcrist@pbking.com>
2021-09-27 22:19:31 -0400

    Got a successful push-button deploy

--- theme-utils.mjs
+++ theme-utils.mjs
@@ -1,7 +1,7 @@
-import { spawn } from 'child_process';
-import open from 'open';
 import { Octokit } from "@octokit/rest";
+import { spawn } from 'child_process';
 import fs from 'fs';
+import open from 'open';
 
 const remoteSSH = 'wpcom-sandbox';
 const sandboxPublicThemesFolder = '/home/wpdev/public_html/wp-content/themes/pub';
@@ -9,7 +9,7 @@ const isWin = process.platform === 'win32';
 
 let octokit;
 
-(function start() {
+(async function start() {
 	let args = process.argv.slice(2);
 	let command = args?.[0];
 	switch (command) {
@@ -40,33 +40,132 @@ function showHelp(){
  Create a phabricator diff (which gets opened in your browser)
 */
 async function pushButtonDeployGit() {
+
 	//TODO: If this branch isn't current with trunk exit and require a rebase
 
-	//TODO: If this results in version bumps (it should) commit that change to the branch (and push?)
-	await versionBumpThemes();
+	//Getting the hash now, to use later, because it will change and we need the old one later, not the new one
+	let hash = await getLastDeployedHash();
+
+	await versionBumpThemes({
+		commit: true
+	});
 
 	//TODO: Can these be automagically uploaded?
-	await buildChangedOrgZips();
-	await cleanSandbox();
+	//await buildChangedOrgZips();
+
+	//await cleanSandbox();
 	await pushChangesToSandbox();
-	let diffURL = await createGitPhabricatorDiff();
-	await createGithubPR(diffURL);
-	//TODO: Tag this release?
+	await updateLastDeployedHash();
+	let diffURL = await createGitPhabricatorDiff({
+		hash: hash
+	});
+
+	await tagDeployment({
+		hash: hash,
+		diffURL: diffURL
+	});
 }
 
 function initGithubApi() {
 	try {
-		const githubToken = fs.readFileSync('.github_token');
+		const githubToken = fs.readFileSync('.github_token').toString();
 		octokit = new Octokit({ auth: githubToken });
+		return octokit;
 	} catch (err) {
 		console.log('Octokit (Github API) not initialized: ', err);
+		console.log('Make sure your github token is stored in the file: .github_token');
+		return null;
 	}
 }
 
-async function versionBumpThemes() {
+async function getLastDeployedHash() {
+	let result = await executeOnSandbox(`
+		cat ${sandboxPublicThemesFolder}/.pub-git-hash
+	`);
+	return result;
+}
+
+async function updateLastDeployedHash() {
+	let hash = await executeCommand(`git rev-parse HEAD`);
+	await executeOnSandbox(`
+		echo '${hash}' > ${sandboxPublicThemesFolder}/.pub-git-hash
+	`);
+}
+
+async function versionBumpThemes(options) {
 	console.log("Version Bumping");
-	let result = await executeCommand(`./theme-batch-utils.sh version-bump`);
-	console.log(result);
+
+	let themes = await getActionableThemes();
+	let hash = await getLastDeployedHash();
+	let versionBumpCount = 0;
+
+	for (let theme of themes) {
+		let hasChanges = await checkThemeForChanges(theme, hash);
+		if( ! hasChanges){
+			// console.log(`${theme} has no changes`);
+			continue;
+		}
+
+		versionBumpCount++;
+
+		let hasVersionBump = await checkThemeForVersionBump(theme, hash);
+		if( hasVersionBump ){
+			// console.log(`${theme} has already been version bumpped`);
+			continue;
+		}
+
+		await versionBumpTheme(theme);
+	}
+
+	let rootHasVersionBump = await checkThemeForVersionBump('.', hash);
+	if ( versionBumpCount > 0 && ! rootHasVersionBump ) {
+		//version bump the root project if there were changes to any of the themes
+		await executeCommand(`npm version patch --no-git-tag-version`);
+	}
+
+	if (options?.commit && versionBumpCount > 0 && !rootHasVersionBump) {
+		console.log('commiting version-bump');
+		let commitResult = await executeCommand(`
+			git commit -a -m "Version Bump";
+			git push
+		`);
+		console.log(commitResult);
+	}
+}
+
+async function versionBumpTheme(theme){
+	console.log(`${theme} needs a version bump`);
+	await executeCommand(`npm --prefix ${theme} version patch --no-git-tag-version`);
+	let currentPackage = JSON.parse(fs.readFileSync(`${theme}/package.json`))
+	let currentVersion = currentPackage.version;
+	let filesToUpdate = await executeCommand(`find ${theme} -name style.css -o -name style.scss -o -name style-child-theme.scss -maxdepth 2`);
+	filesToUpdate = filesToUpdate.split('\n');
+	for ( let file of filesToUpdate ) {
+		console.log('updating file', file, currentVersion);
+		//TODO: I'm sure we can use something other than perl for this but this was prior art...
+		await executeCommand(`perl -pi -e 's/Version: (.*)$/"Version: '${currentVersion}'"/ge' ${file}`);
+	}
+}
+
+async function checkThemeForVersionBump(theme, hash){
+	let previousPackageString = await executeCommand(`
+		git show ${hash}:${theme}/package.json 2>/dev/null
+	`);
+	let previousPackage = JSON.parse(previousPackageString);
+	let currentPackage = JSON.parse(fs.readFileSync(`${theme}/package.json`))
+	return previousPackage.version != currentPackage.version;	
+}
+
+async function checkThemeForChanges(theme, hash){
+	let uncomittedChanges = await executeCommand(`git diff-index --name-only HEAD -- ${theme}`);
+	let comittedChanges = await executeCommand(`git diff --name-only ${hash} HEAD -- ${theme}`);
+	return uncomittedChanges != '' || comittedChanges != '';
+}
+
+async function getActionableThemes() {
+	//TODO: This could probably be done more effeciently
+	let result = await executeCommand(`find . -depth 2 -name package.json -print0 | xargs -0 -n1 dirname | sort --unique`);
+	return result.split('\n');
 }
 
 async function buildChangedOrgZips() {
@@ -86,7 +185,7 @@ async function cleanSandbox() {
 		cd ${sandboxPublicThemesFolder};
 		git reset --hard HEAD;
 		git clean -fd;
-		git checkout trunk;
+		git checkout develop;
 		git pull;
 		echo
 	`)
@@ -120,7 +219,7 @@ async function pushChangesToSandbox() {
 
 	updateRemote();
 	let branchName = await getBranchName();
-	let hash = await getHashAtDivergence(branchName);
+	let hash = await getLastDeployedHash();
 
 	let deletedFiles = await getDeletedFilesSince(hash);
 	let changedFiles = await getComittedChangesSinceHash(hash, branchName);
@@ -142,9 +241,6 @@ async function pushChangesToSandbox() {
 
 	if(changedFiles.length > 0) {
 		console.log('pushing changed files to sandbox:', changedFiles);
-		console.log(`
-			rsync -avR --no-p --no-times --exclude-from='.sandbox-ignore' ${changedFiles.join(' ')} wpcom-sandbox:${sandboxPublicThemesFolder}/
-		`)
 		let pushResponse = await executeCommand(`
 			rsync -avR --no-p --no-times --exclude-from='.sandbox-ignore' ${changedFiles.join(' ')} wpcom-sandbox:${sandboxPublicThemesFolder}/
 		`);
@@ -184,10 +280,6 @@ function getBranchName() {
 	return executeCommand(`git branch --show-current`);
 }
 
-function getHashAtDivergence(branch) {
-	return executeCommand(`git merge-base origin/prod ${branch}`);
-}
-
 function updateRemote() {
 	executeCommand(`git remote update > /dev/null`);
 }
@@ -195,7 +287,7 @@ function updateRemote() {
 async function buildPhabricatorCommitMessageSince(hash){
 
 	let projectVersion = await executeCommand(`node -p "require('./package.json').version"`);
-	let logs = await executeCommand(`git log --pretty=format:%s ${hash}..HEAD`);
+	let logs = await executeCommand(`git log --reverse --pretty=format:%s ${hash}..HEAD`);
 	return `Deploy Themes ${projectVersion} to wpcom
    
 Summary:
@@ -209,11 +301,13 @@ Subscribers:
 `;
 }
 
-async function createGitPhabricatorDiff() {
+async function createGitPhabricatorDiff(options={}) {
+
+	console.log('creating Phabricator Diff');
 
 	updateRemote();
-	let branchName = await getBranchName();
-	let hash = await getHashAtDivergence(branchName);
+
+	let hash = options.hash || await getLastDeployedHash();
 
 	let commitMessage = await buildPhabricatorCommitMessageSince(hash);
 
@@ -228,9 +322,12 @@ async function createGitPhabricatorDiff() {
 
 	let phabricatorUrl = getPhabricatorUrlFromResponse(result);
 
+	console.log('Diff Created at: ', phabricatorUrl);
+
 	if(phabricatorUrl) {
 		open(phabricatorUrl);
 	}
+
 	return phabricatorUrl;
 }
 
@@ -241,6 +338,64 @@ function getPhabricatorUrlFromResponse(response){
 			return item.includes('Revision URI: ');
 		})
 		?.split("Revision URI: ")[1];
+}
+
+async function tagDeployment(options={}) {
+
+	let hash = options.hash || await getLastDeployedHash();
+
+	let workInTheOpenPhabricatorUrl = '';
+	if (options.diffUrl) {
+		workInTheOpenPhabricatorUrl = `Phabricator: ${options.diffUrl.split('a8c.com/')[1]}-code`;
+	}
+	let projectVersion = await executeCommand(`node -p "require('./package.json').version"`);
+	let logs = await executeCommand(`git log --reverse --pretty=format:%s ${hash}..HEAD`);
+	let tag = `v${projectVersion}`;
+	let message = `Deploy Themes ${tag} to wpcom. \n\n${logs} \n\n${workInTheOpenPhabricatorUrl}`;
+
+	await executeCommand(`
+		git tag -a ${tag} -m "${message}"
+		git push origin ${tag}
+	`);
+}
+
+async function createGithubPR(phabricatorUrl) {
+
+	console.log("Creating Github PR");
+
+	if ( ! initGithubApi() ) {
+		return;
+	}
+
+	updateRemote();
+
+	let branchName = await getBranchName();
+	let hash = await getLastDeployedHash();
+	let workInTheOpenPhabricatorUrl = '';
+	if (phabricatorUrl) {
+		workInTheOpenPhabricatorUrl = `\n\n Phabricator: ${phabricatorUrl.split('a8c.com/')[1]}-code`;
+	}
+	let projectVersion = await executeCommand(`node -p "require('./package.json').version"`);
+	let logs = await executeCommand(`git log --reverse --pretty=format:%s ${hash}..HEAD`);
+	let options = {
+  		owner: 'Automattic',
+  		repo: 'themes',
+  		head: branchName,
+  		base: 'prod',
+		title: `Deploy Themes ${projectVersion} to wpcom`,
+		body: logs + workInTheOpenPhabricatorUrl
+	};
+
+	try {
+		let response = await octokit.request('POST /repos/Automattic/themes/pulls', options);
+		console.log("Github PR opened: ", response.html_url);
+		open(response.html_url);
+		return response.html_url;
+	} 
+	catch (err) {
+		console.log(err);
+	}
+
 }
 
 function executeOnSandbox(command){
@@ -280,38 +435,4 @@ async function executeCommand(command) {
 			resolove(response.trim());
 		});
 	});
-}
-
-async function createGithubPR(phabricatorUrl) {
-
-	if ( ! initGithubApi() ) {
-		return;
-	}
-
-	updateRemote();
-
-	let branchName = await getBranchName();
-	let hash = await getHashAtDivergence(branchName);
-	let workInTheOpenPhabricatorUrl = '';
-	if (phabricatorUrl) {
-		workInTheOpenPhabricatorUrl = `\n\n Phabricator: ${phabricatorUrl.split('/')[1]}-code`;
-	}
-	let projectVersion = await executeCommand(`node -p "require('./package.json').version"`);
-	let logs = await executeCommand(`git log --pretty=format:%s ${hash}..HEAD`);
-	let options = {
-  		owner: 'Automattic',
-  		repo: 'themes',
-  		head: branchName,
-  		base: 'prod',
-		title: `Deploy Themes ${projectVersion} to wpcom`,
-		body: logs + workInTheOpenPhabricatorUrl
-	};
-
-	try {
-		let response = await octokit.request('POST /repos/Automattic/themes/pulls', options);
-		open(response.url);
-	} catch (err) {
-		console.log(err);
-	}
-
 }
