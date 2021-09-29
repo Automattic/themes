52b6bd9d7b510524d78ccaa61a10b2119443f672
Ben Dwyer <ben@scruffian.com>
2021-09-29 12:06:18 +0100

    fix third post

--- quadrat/inc/patterns/query-episodes.php
+++ quadrat/inc/patterns/query-episodes.php
@@ -24,12 +24,21 @@ return array(
 	<!-- /wp:query -->
 	<!-- wp:query {"queryId":34,"query":{"perPage":"1","pages":0,"offset":"2","postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"align":"wide"} -->
 	<div class="wp-block-query alignwide"><!-- wp:post-template -->
-	<!-- wp:cover {"overlayColor":"tertiary","minHeight":360,"className":"mt-0","style":{"spacing":{"padding":{"top":"30px","right":"40px","bottom":"30px","left":"40px"}}}} -->
-	<div class="wp-block-cover has-tertiary-background-color has-background-dim mt-0" style="padding-top:30px;padding-right:40px;padding-bottom:30px;padding-left:40px;min-height:360px"><div class="wp-block-cover__inner-container">
-	<!-- wp:post-featured-image {"align":"left","className":"image-no-margin mb-0"} /-->
+
+	<!-- wp:group {"className":"horizontal-query-pattern","layout":{"type":"flex"}} -->
+	<div class="wp-block-group horizontal-query-pattern">
+
+	<!-- wp:post-featured-image {"className":"image-no-margin mb-0"} /-->
+
+	<!-- wp:cover {"className":"horizontal-query-pattern","overlayColor":"tertiary","minHeight":360,"className":"mt-0","style":{"spacing":{"padding":{"top":"30px","right":"40px","bottom":"30px","left":"40px"}}}} -->
+	<div class="wp-block-cover has-tertiary-background-color has-background-dim mt-0 horizontal-query-pattern" style="padding-top:30px;padding-right:40px;padding-bottom:30px;padding-left:40px;min-height:360px"><div class="wp-block-cover__inner-container">
 	<!-- wp:post-title {"isLink":true,"linkTarget":"_blank","textColor":"primary"} /-->
 	<!-- wp:post-excerpt {"textColor":"primary","fontSize":"small"} /--></div></div>
 	<!-- /wp:cover -->
+
+	</div>
+	<!-- /wp:group -->
+
 	<!-- /wp:post-template --></div>
 	<!-- /wp:query -->',
 );
