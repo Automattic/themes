272adbc3fd1a0166fc9a4b51756493d3c5248ef4
Maggie <maggie.cabrera@automattic.com>
2021-10-08 09:35:06 +0200

    remove unnecesary queryId attribute

--- geologist/inc/patterns/two-featured-posts.php
+++ geologist/inc/patterns/two-featured-posts.php
@@ -9,7 +9,7 @@ return array(
 	'title'      => __( 'Two featured posts', 'geologist' ),
 	'categories' => array( 'geologist' ),
 	'blockTypes' => array( 'core/query' ),
-	'content'    => '<!-- wp:query {"queryId":14,"query":{"perPage":2,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"displayLayout":{"type":"flex","columns":2}} -->
+	'content'    => '<!-- wp:query {"query":{"perPage":2,"pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false},"displayLayout":{"type":"flex","columns":2}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
 	<!-- wp:post-featured-image {"isLink":true} /-->
 
