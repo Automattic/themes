676738bd2b236f541485f07a27150aaf1305b7ab
Ben Dwyer <ben@scruffian.com>
2021-10-12 14:19:45 +0100

    Update the structure of the social links when appended to match the way that the navigation block works

--- blockbase/inc/social-navigation.php
+++ blockbase/inc/social-navigation.php
@@ -48,10 +48,11 @@ function append_social_links_block( $parent_content, $social_links_block ) {
 		return $social_links_block;
 	}
 	$dom = new domDocument;
+	$domXPath = new DomXPath( $dom );
 	// Since the nav block uses HTML5 element names, we need to suppress the warnings it sends when we loadHTML with HTML5 elements.
 	libxml_use_internal_errors( true );
 	$dom->loadHTML( $parent_content );
-	$wp_block_navigation__container = $dom->getElementsByTagName('ul')->item( 0 );
+	$wp_block_navigation__container = $dom->getElementsByTagName('ul')->item( 0 )->parentNode;
 	$social_links_node = $dom->createDocumentFragment();
 	$social_links_node->appendXML( $social_links_block );
 	$wp_block_navigation__container->appendChild( $social_links_node );
