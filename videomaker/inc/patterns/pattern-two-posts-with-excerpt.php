f33d548de14fedb7c95b3af88b9c680a740a258b
Ben Dwyer <ben@scruffian.com>
2021-10-14 12:22:12 +0100

    Videomaker: Add block patterns (#4701)
    * Videomaker: Add block patterns
    
    * Remove autoplay, loop and muted
    
    * Match post meta to the comps
    
    * Add more translations to the patterns
    
    * Set the site title CSS in the theme.json
    
    * Reset the header
    
    * Update patterns - add video controls and update the source video

---
+++ videomaker/inc/patterns/pattern-two-posts-with-excerpt.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Pattern: Two posts with excerpt
+ *
+ * @package Videomaker
+ */
+
+return array(
+	'title'      => __( 'Two posts with excerpt', 'videomaker' ),
+	'categories' => array( 'videomaker' ),
+	'content'    => '<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"80%"} -->
+	<div class="wp-block-column" style="flex-basis:80%">
+	<!-- wp:video {"src":"' . get_stylesheet_directory_uri() . '/assets/videos/digging.mp4"} -->
+	<figure class="wp-block-video"><video controls src="' . get_stylesheet_directory_uri() . '/assets/videos/digging.mp4"></video></figure>
+	<!-- /wp:video -->
+
+	<!-- wp:columns -->
+	<div class="wp-block-columns"><!-- wp:column -->
+	<div class="wp-block-column">
+
+	<!-- wp:heading {"level":4} -->
+	<h4>' . esc_html__( 'Man Working at Dawn', 'videomaker' ) . '</h4>
+	<!-- /wp:heading -->
+
+	<!-- wp:template-part {"slug":"post-meta","theme":"themes/videomaker"} /-->
+
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"}},"fontSize":"small"} -->
+	<p class="has-small-font-size" style="line-height:1.5">' . esc_html__( 'As nations under the present system are driven to compete with one another for the markets of the world, so also have the workers to compete with each other—for livelihood.', 'videomaker' ) . '</p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"33.33%"} -->
+	<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:spacer -->
+	<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns -->
+
+	<!-- wp:spacer -->
+	<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+
+	<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"20%"} -->
+	<div class="wp-block-column" style="flex-basis:20%"><!-- wp:spacer -->
+	<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"80%"} -->
+	<div class="wp-block-column" style="flex-basis:80%">
+
+	<!-- wp:video {"src":"' . get_stylesheet_directory_uri() . '/assets/videos/boats.mp4"} -->
+	<figure class="wp-block-video"><video controls src="' . get_stylesheet_directory_uri() . '/assets/videos/boats.mp4"></video></figure>
+	<!-- /wp:video -->
+
+	<!-- wp:columns -->
+	<div class="wp-block-columns"><!-- wp:column -->
+	<div class="wp-block-column">
+
+	<!-- wp:heading {"level":4} -->
+	<h4>' . esc_html__( 'Boats', 'videomaker' ) . '</h4>
+	<!-- /wp:heading -->
+
+	<!-- wp:template-part {"slug":"post-meta","theme":"themes/videomaker"} /-->
+
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"}},"fontSize":"small"} -->
+	<p class="has-small-font-size" style="line-height:1.5">' . esc_html__( 'As nations under the present system are driven to compete with one another for the markets of the world, so also have the workers to compete with each other—for livelihood.', 'videomaker' ) . '</p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns -->',
+);
