3b5856b67b5c7930538f0e91b4a84ec9da5e9264
Ben Dwyer <ben@scruffian.com>
2021-10-14 13:32:19 +0100

    Zoologist: Update spacing from wpcom (#4834)

--- zoologist/functions.php
+++ zoologist/functions.php
@@ -3,7 +3,7 @@
 /**
  * Add Editor Styles
  */
-function newtheme_editor_styles() {
+function zoologist_editor_styles() {
 	// Enqueue editor styles.
 	add_editor_style(
 		array(
@@ -11,13 +11,13 @@ function newtheme_editor_styles() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'newtheme_editor_styles' );
+add_action( 'after_setup_theme', 'zoologist_editor_styles' );
 
 /**
  *
  * Enqueue scripts and styles.
  */
-function newtheme_scripts() {
-	wp_enqueue_style( 'newtheme-styles', get_stylesheet_directory_uri() . '/assets/theme.css', array('blockbase-ponyfill'), wp_get_theme()->get( 'Version' ) );
+function zoologist_scripts() {
+	wp_enqueue_style( 'zoologist-styles', get_stylesheet_directory_uri() . '/assets/theme.css', array('blockbase-ponyfill'), wp_get_theme()->get( 'Version' ) );
 }
-add_action( 'wp_enqueue_scripts', 'newtheme_scripts' );
+add_action( 'wp_enqueue_scripts', 'zoologist_scripts' );
