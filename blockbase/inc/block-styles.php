1cc839babcaaaa3e7ee6b9d88b15e7081cc5eab3
Ben Dwyer <ben@scruffian.com>
2021-10-16 00:15:07 +0100

    Blockbase: Try some block styles

--- blockbase/inc/block-styles.php
+++ blockbase/inc/block-styles.php
@@ -2,7 +2,7 @@
 /**
  * Blockbase Theme: Block Styles
  *
- * @package Blockbase 
+ * @package Blockbase
  * @since 1.2.8
  */
 
@@ -24,6 +24,59 @@ if ( ! function_exists( 'blockbase_register_block_styles' ) ) :
 				)
 			);
 
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'retro',
+					'label'        => __('Retro', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-retro',
+				)
+			);
+
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'neon',
+					'label'        => __('Neon', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-neon',
+				)
+			);
+
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'inset',
+					'label'        => __('Inset', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-inset',
+				)
+			);
+
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => '3d',
+					'label'        => __('3D', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-3d',
+				)
+			);
+
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'burning',
+					'label'        => __('Burning', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-burning',
+				)
+			);
+
+			register_block_style(
+				'core/heading',
+				array(
+					'name'         => 'stacked',
+					'label'        => __('Stacked', 'blockbase' ),
+					'style_handle' => 'blockbase-heading-stacked',
+				)
+			);
 		}
 	}
 endif;
