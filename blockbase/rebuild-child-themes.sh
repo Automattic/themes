0ec6a9ad5bdd2fb606ffb8196eedff90bcd15d3e
Jason Crist <jcrist@pbking.com>
2021-10-18 14:34:28 -0400

    converted build tool to a 'replacement tool'

--- blockbase/rebuild-child-themes.sh
+++ blockbase/rebuild-child-themes.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "Woah!  Careful.  On THIS branch when you do this the child-theme.json will REPLACE theme.json and then will be no more.  You have been warned. \n\n";
+read SOMEANSWER
+
 declare -a ChildThemes=( "geologist" "kerr" "mayland-blocks" "payton" "quadrat" "russell" "seedlet-blocks" "skatepark" "videomaker" "zoologist" )
 
 for child in ${ChildThemes[@]}; do
 	cd '../'${child}
-	echo 'Rebulding '${child}
-	npm i
-	npm run build
+	rm theme.json
+	mv child-theme.json theme.json
 done
