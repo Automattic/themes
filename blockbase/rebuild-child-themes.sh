49d2bffa9897c12e867f562c47915327dd783c51
Ben Dwyer <ben@scruffian.com>
2021-10-19 11:11:50 +0100

    add payton as a child of blockbase

--- blockbase/rebuild-child-themes.sh
+++ blockbase/rebuild-child-themes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a ChildThemes=( "geologist" "mayland-blocks" "quadrat" "russell" "seedlet-blocks" "skatepark" "videomaker" "zoologist" )
+declare -a ChildThemes=( "geologist" "mayland-blocks" "payton" "quadrat" "russell" "seedlet-blocks" "skatepark" "videomaker" "zoologist" )
 
 for child in ${ChildThemes[@]}; do
 	cd '../'${child}
