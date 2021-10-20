6b82343a8de2f5b3e5854a39ad24fe40b0c7a031
Sarah Norris <sarah@sekai.co.uk>
2021-10-20 11:00:25 +0100

    Videomaker: make all pattern navs uppercase

--- videomaker/inc/patterns/full-width-homepage.php
+++ videomaker/inc/patterns/full-width-homepage.php
@@ -15,7 +15,7 @@ return array(
 	<!-- /wp:column -->
 
 	<!-- wp:column {"verticalAlignment":"center"} -->
-	<div class="wp-block-column is-vertically-aligned-center"><!-- wp:navigation {"itemsJustification":"right","isResponsive":true,"__unstableLocation":"primary"} /--></div>
+	<div class="wp-block-column is-vertically-aligned-center"><!-- wp:navigation {"itemsJustification":"right","isResponsive":true,"__unstableLocation":"primary","style":{"typography":{"textTransform":"uppercase"}}} /--></div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns -->
 
