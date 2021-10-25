1873f0451e025dcf5002aa1d1a6565884495f1b2
Ben Dwyer <ben@scruffian.com>
2021-10-25 16:42:41 +0100

    Videomaker: Replace references to post-meta template with the markup

--- videomaker/inc/patterns/pattern-grid.php
+++ videomaker/inc/patterns/pattern-grid.php
@@ -29,12 +29,6 @@ return array(
 	</div>
 	<!-- /wp:group -->
 
-	<!-- wp:group {"className":"post-tags-container"} -->
-	<div class="wp-block-group post-tags-container">
-		<!-- wp:post-terms {"term": "post_tag","style":{"typography":{"fontSize":"var(--wp--custom--font-sizes--tiny)"}}} /-->
-	</div>
-	<!-- /wp:group -->
-
 	<!-- wp:spacer {"height":20} -->
 	<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
