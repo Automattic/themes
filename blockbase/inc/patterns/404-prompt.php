6d791ad6da077b40d3c1e85269c505f612a2b9d1
Jason Crist <jcrist@pbking.com>
2021-11-16 10:36:09 -0500

    corrected 404 page layout styles

--- blockbase/inc/patterns/404-prompt.php
+++ blockbase/inc/patterns/404-prompt.php
@@ -8,7 +8,7 @@
 return array(
 	'title'      => __( '404-prompt', 'blockbase' ),
 	'categories' => array( 'blockbase' ),
-	'content'    => '<!-- wp:group {"tagName":"main","className":"container-404"} -->
+	'content'    => '<!-- wp:group {"tagName":"main","className":"container-404","layout":{"inherit":true}} -->
 <main class="wp-block-group container-404"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
 <h1 class="has-text-align-center has-medium-font-size" id="oops-that-page-can-t-be-found">' . esc_html__( "Oops! That page can&rsquo;t be found.", "blockbase" ) . '</h1>
 <!-- /wp:heading -->
