d66ae50b5c5f1a526d318ab7a7890c41de31d48d
Ben Dwyer <ben@scruffian.com>
2021-11-18 15:43:34 +0000

    Only use the copy in the block

--- blockbase/inc/patterns/404-prompt.php
+++ blockbase/inc/patterns/404-prompt.php
@@ -9,15 +9,11 @@ return array(
 	'title'      => __( '404-prompt', 'blockbase' ),
 	'categories' => array( 'blockbase' ),
 	'inserter'   => false,
-	'content'    => '<!-- wp:group {"tagName":"main","className":"container-404","layout":{"inherit":true}} -->
-<main class="wp-block-group container-404"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
+	'content'    => '<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
 <h1 class="has-text-align-center has-medium-font-size" id="oops-that-page-can-t-be-found">' . esc_html__( "Oops! That page can&rsquo;t be found.", "blockbase" ) . '</h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
 <p>' . esc_html__( "It looks like nothing was found at this location. Maybe try a search?", "blockbase" ) . '</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:search {"label":""} /--></main>
-<!-- /wp:group -->',
+<!-- /wp:paragraph -->',
 );
