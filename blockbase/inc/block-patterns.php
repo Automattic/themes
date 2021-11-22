8cfaa36188533c67167689d158e2a8865f12bf3a
Jason Crist <jcrist@pbking.com>
2021-11-22 08:21:35 -0500

    Paternize 404 Page (#5039)
    * Refactor Blockbase 404 page to block pattern instead of PHP page
    
    * Adjustments to get 404 to render in child themes.
    
    * Adjustments to style Skatepark's 404 based on original PHP file.
    
    * corrected 404 page layout styles
    
    * Refactored videomaker 404 to use Blockbase pattern but match original PHP file styling.
    
    * Refactored videomaker 404 to use Blockbase pattern but match original PHP file styling.
    
    * Added 'blank' message to index.php
    
    * formatted PHP comment
    
    * Hide the pattern in the inserter
    
    * Only use the copy in the block
    
    * adding 404 templates
    
    * Removed unused class
    
    * rename the prompt
    
    * added spacer to align with what other child themes are doing
    
    Co-authored-by: Ben Dwyer <ben@scruffian.com>
    Co-authored-by: Maggie <maggie.cabrera@automattic.com>

---
+++ blockbase/inc/block-patterns.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Blockbase Theme: Block Patterns
+ *
+ * @package Blockbase
+ */
+if ( ! function_exists( 'blockbase_register_block_patterns' ) ) :
+
+	function blockbase_register_block_patterns() {
+
+		if ( function_exists( 'register_block_pattern_category' ) ) {
+			register_block_pattern_category(
+				'blockbase',
+				array( 'label' => __( 'Blockbase', 'blockbase' ) )
+			);
+		}
+
+		if ( function_exists( 'register_block_pattern' ) ) {
+			$block_patterns = array(
+				'404'
+			);
+
+			foreach ( $block_patterns as $block_pattern ) {
+				register_block_pattern(
+					'blockbase/' . $block_pattern,
+					require __DIR__ . '/patterns/' . $block_pattern . '.php'
+				);
+			}
+		}
+	}
+endif;
+
+add_action( 'init', 'blockbase_register_block_patterns', 9 );
--- blockbase/index.php
+++ blockbase/index.php
@@ -1,24 +1,2 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Blockbase
- * @since 1.2.9
- */
-get_header();
-
-// the query
-if ( function_exists( 'block_template_part' ) ) {
-	echo block_template_part( 'index' );
-} else {
-	echo gutenberg_block_template_part( 'index' );
-}
-
-get_footer();
+	# This page intentionally left blank
