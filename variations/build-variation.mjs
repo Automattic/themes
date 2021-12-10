fdd75bd3c2c72fb2a3af5f18a74268910d9aa1fe
Ben Dwyer <ben@scruffian.com>
2021-12-10 13:12:26 +0000

    add some initial code to copy theme variants

--- variations/build-variation.mjs
+++ variations/build-variation.mjs
@@ -1,3 +1,4 @@
+import fs from 'fs-extra';
 
 (async function start() {
 	let args = process.argv.slice(2);
@@ -5,7 +6,7 @@
 	let variation = args?.[1];
 	if ( source && variation ) {
 		return buildVariation(source, variation);
-	}	
+	}
 	return buildAllVariations();
 })();
 
@@ -16,5 +17,30 @@ function buildAllVariations(){
 }
 
 function buildVariation(source, variation) {
+	const srcDir = '../' + source;
+	const destDir = '../' + variation;
 
+	// First empty the old directory.
+	fs.emptyDir( destDir )
+		.then( () => {
+			// Then copy the source directory.
+			fs.copy( srcDir, destDir )
+				.then( () => {
+					console.log( 'Copied the source.' );
+
+					fs.readdir( source + '/' + variation, ( err, files ) => {
+						files.forEach( file => {
+							console.log( file );
+							const fileToCopy = source + '/' + variation + '/' + file;
+							fs.copy( fileToCopy, destDir + '/' + file )
+								.then(() => console.log( 'Copied ' + fileToCopy ) )
+								.catch(err => console.error(err) );
+						});
+					});
+				 } )
+				.catch( err => console.error( err ) );
+	  	} )
+		.catch( err => {
+			console.error( err )
+		} );
 }
