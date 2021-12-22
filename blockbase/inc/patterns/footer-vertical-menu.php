0afada4bfe67d0be35f111e1871dd75b1309c7f0
Maggie <maggie.cabrera@automattic.com>
2021-12-22 13:23:23 +0100

    added spacer to videomaker

--- blockbase/inc/patterns/footer-vertical-menu.php
+++ blockbase/inc/patterns/footer-vertical-menu.php
@@ -11,8 +11,8 @@ return array(
 	'blockTypes' => array( 'core/template-part/footer' ),
 	'content'    => '<!-- wp:group {"layout":{"inherit":"true"}} -->
 	<div class="wp-block-group">
-		<!-- wp:group {"align":"wide","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"space-between"},"style":{"spacing":{"padding":{"top":"170px","bottom":"min(30px, 5vw)"}}}} -->
-		<div class="wp-block-group alignwide" style="padding-top: 170px;padding-bottom: min(30px, 5vw)">	<!-- wp:group -->
+		<!-- wp:group {"align":"wide","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"space-between"},"style":{"spacing":{"padding":{"top":"30px","bottom":"min(30px, 5vw)"}}}} -->
+		<div class="wp-block-group alignwide" style="padding-top: 30px;padding-bottom: min(30px, 5vw)">	<!-- wp:group -->
 			<div class="wp-block-group">
 				<!-- wp:navigation {"overlayMenu":"never","__unstableLocation":"footer","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"vertical"},"style":{"typography":{"textTransform":"uppercase"},"spacing":{"blockGap":"0px"}}} /-->
 			</div>
