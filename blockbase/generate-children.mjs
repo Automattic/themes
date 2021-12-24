19efaf5bbe61e5b2c96739cde21ebead2427eb49
Ben Dwyer <ben@scruffian.com>
2021-12-24 16:10:18 +0000

    tidy up

--- blockbase/generate-children.mjs
+++ blockbase/generate-children.mjs
@@ -19,16 +19,19 @@ async function generateChildren() {
 	const children = [ {
 		"name": 'Skatepark',
 		"description": "Skatepark is a bold and exciting WordPress theme designed for modern events and organizations. With its simple but vivid color default scheme, duotone support, and playful grid, Skatepark is a theme designed for work and play. Customize its colors or try out different font pairings to create your own unique look and feel.",
-		"slug": 'skatepark',
-		"version": '1.0.26'
+		"slug": 'skatepark'
 	} ];
 
 	const packageJson = await getPackageJson( '.' );
-	children.forEach( childTheme => {
-		childTheme.bugs = packageJson.bugs;
-		childTheme.bugs.url = packageJson.bugs.url.replace( 'Blockbase', childTheme.name );
-		childTheme.homepage = packageJson.homepage.replace( packageJson.name, childTheme.slug );
-		const newPackageJson = Object.assign( {}, packageJson, childTheme );
-		console.log( newPackageJson );
+	children.forEach( async childTheme => {
+		const themePackageJson = await getPackageJson( '../' + childTheme.slug );
+		const newPackageJson = {};
+		newPackageJson.name = childTheme.slug;
+		newPackageJson.description = childTheme.description;
+		newPackageJson.version = themePackageJson.version;
+		newPackageJson.bugs = packageJson.bugs;
+		newPackageJson.bugs.url = packageJson.bugs.url.replace( 'Blockbase', childTheme.name );
+		newPackageJson.homepage = packageJson.homepage.replace( packageJson.name, childTheme.slug );
+		console.log( Object.assign( {}, packageJson, newPackageJson ) );
 	} );
 }
