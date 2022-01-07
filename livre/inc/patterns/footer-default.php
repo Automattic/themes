df7253432ea271260ea5e80d801271316ef9e42b
Kjell Reigstad <kjell@kjellr.com>
2022-01-07 08:35:27 -0500

    Fix markup.

--- livre/inc/patterns/footer-default.php
+++ livre/inc/patterns/footer-default.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Default header block pattern
+ * Default footer block pattern
  */
 return array(
-	'title'      => __( 'Default header', 'livre' ),
+	'title'      => __( 'Default footer', 'livre' ),
 	'categories' => array( 'header' ),
-	'blockTypes' => array( 'core/template-part/header' ),
-	'content'    => '<!-- wp:group {"align":"full","layout":{"inherit":true}} --><!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)"}}},"layout":{"inherit":true}} -->
-					<div class="wp-block-group" style="padding-top:var(--wp--custom--spacing--large, 8rem)">
+	'blockTypes' => array( 'core/template-part/footer' ),
+	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)","bottom":"var(--wp--custom--spacing--small, 1.25rem)"}}}} -->
+					<div class="wp-block-group alignfull" style="padding-top:var(--wp--custom--spacing--large, 8rem);padding-bottom:var(--wp--custom--spacing--small, 1.25rem)">
 					<!-- wp:paragraph {"style":{"fontSize":"small"} -->
 					<p class="has-small-font-size">' .
 					sprintf(
@@ -16,6 +16,5 @@ return array(
 						'<a href="' . esc_url( __( 'https://wordpress.org', 'livre' ) ) . '" rel="nofollow">WordPress</a>'
 					) . '</p>
 					<!-- /wp:paragraph --></div>
-					<!-- /wp:group --></div>
 					<!-- /wp:group -->',
 );
