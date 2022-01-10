73eb3d19c26c364981c4550877e315c9363b4f91
Jason Crist <jcrist@pbking.com>
2022-01-10 12:31:01 -0500

    Refactored headers to be both patterns and parts

--- blockbase/inc/block-patterns.php
+++ blockbase/inc/block-patterns.php
@@ -13,6 +13,12 @@ if ( ! function_exists( 'blockbase_register_block_patterns' ) ) :
 				'blockbase',
 				array( 'label' => __( 'Blockbase', 'blockbase' ) )
 			);
+
+			register_block_pattern_category(
+				'blockbase-headers',
+				array( 'label' => __( 'Blockbase Headers', 'blockbase-headers' ) )
+			);
+	
 		}
 
 		if ( function_exists( 'register_block_pattern' ) ) {
@@ -24,6 +30,28 @@ if ( ! function_exists( 'blockbase_register_block_patterns' ) ) :
 				register_block_pattern(
 					'blockbase/' . $block_pattern,
 					require __DIR__ . '/patterns/' . $block_pattern . '.php'
+				);
+			}
+
+			//register header templates also as patterns
+			$header_patterns = array(
+				'centered',
+				'default',
+				'lineaer',
+				'minimal',
+				'rounded-logo',
+				'wide'
+
+			);
+
+			foreach ( $header_patterns as $header_pattern ) {
+				register_block_pattern(
+					'blockbase/header-' . $header_pattern,
+					array(
+						'title'      => __( 'Blockbase Header (' . $header_pattern . ')', 'blockbase' ),
+						'categories' => array( 'blockbase', 'headers' ),
+						'content'    => file_get_contents (get_template_directory() . '/block-template-parts/header-' . $header_pattern . '.html'),
+					)
 				);
 			}
 		}
