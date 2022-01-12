73bb6fd1791694693eca72d7c7beeb0b09d8ee6b
Jason Crist <jcrist@pbking.com>
2022-01-12 09:37:41 -0500

    ACTUALLY wRemove customizer restoration as it is no longer being removed.

--- blockbase/functions.php
+++ blockbase/functions.php
@@ -150,14 +150,6 @@ function blockbase_fonts_url() {
 }
 
 /**
- * Restores the Customizer since we still rely on it.
- */
-// function blockbase_restore_customizer() {
-// 	remove_action( 'admin_menu', 'gutenberg_remove_legacy_pages' );
-// }
-// add_action( 'init', 'blockbase_restore_customizer' );
-
-/**
  * Customize Global Styles
  */
 if ( class_exists( 'WP_Theme_JSON_Resolver_Gutenberg' ) ) {
