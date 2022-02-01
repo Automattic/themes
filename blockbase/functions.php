d0e39980f759f3d6751573f60e8aa11e3179c35c
Ben Dwyer <ben@scruffian.com>
2022-02-01 13:22:53 +0000

    Blockbase: Remove the fallback for the navigation block

--- blockbase/functions.php
+++ blockbase/functions.php
@@ -175,6 +175,14 @@ add_action(
 );
 
 /**
+ * Disable the fallback for the core/navigation block.
+ */
+function blockbase_core_navigation_render_fallback() {
+	return null;
+}
+add_filter( 'block_core_navigation_render_fallback', 'blockbase_core_navigation_render_fallback' );
+
+/**
  * Block Styles.
  */
 require get_template_directory() . '/inc/block-styles.php';
