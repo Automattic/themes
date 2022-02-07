8d029ac97d04078b0828c34a37809642b09017e4
Sarah Norris <sarah@sekai.co.uk>
2022-02-07 15:47:11 +0000

    Add dynamic spacers

--- archeo/inc/patterns/layered-images-with-headline.php
+++ archeo/inc/patterns/layered-images-with-headline.php
@@ -8,8 +8,8 @@ return array(
 	'content'    => '<!-- wp:cover {"url":"' . esc_url( get_template_directory_uri() ) . '/assets/images/palais-du-cirque.jpg","dimRatio":85,"overlayColor":"background","contentPosition":"center center","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0vw","left":"7vw"}}}} -->
 	<div class="wp-block-cover alignfull is-light" style="padding-top:0px;padding-right:0px;padding-bottom:0vw;padding-left:7vw"><span aria-hidden="true" class="has-background-background-color has-background-dim-90 wp-block-cover__gradient-background has-background-dim"></span><img class="wp-block-cover__image-background" alt="' . esc_attr__( 'Photo of Palais Du Cirque', 'archeo' ) . '" src="' . esc_url( get_template_directory_uri() ) . '/assets/images/palais-du-cirque.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
 	<div class="wp-block-columns are-vertically-aligned-top" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"verticalAlignment":"top"} -->
-	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:spacer {"height":"200px"} -->
-	<div style="height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:spacer {"height":"14vw"} -->
+	<div style="height:14vw" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
 	
 	<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"fontSize":"huge"} -->
@@ -18,8 +18,8 @@ return array(
 	<!-- /wp:column -->
 	
 	<!-- wp:column {"verticalAlignment":"top"} -->
-	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:spacer -->
-	<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+	<div class="wp-block-column is-vertically-aligned-top"><!-- wp:spacer {"height":"8vw"} -->
+	<div style="height:8vw" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
 	
 	<!-- wp:image {"align":"right","sizeSlug":"full","linkDestination":"none"} -->
