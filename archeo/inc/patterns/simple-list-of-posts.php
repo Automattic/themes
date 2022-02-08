0ab8160fd823302237baf2d4f3f88778880e8f79
Sarah Norris <sarah@sekai.co.uk>
2022-02-08 17:40:00 +0000

    Add custom CSS for alignment

--- archeo/inc/patterns/simple-list-of-posts.php
+++ archeo/inc/patterns/simple-list-of-posts.php
@@ -8,8 +8,8 @@ return array(
 	'content'    => '<!-- wp:group {"align":"full"} -->
 	<div class="wp-block-group alignfull"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
-	<!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
-	<div class="wp-block-group"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"300"}},"fontSize":"huge"} /-->
+	<!-- wp:group {"className":"simple-list-of-posts","layout":{"type":"flex","allowOrientation":false}} -->
+	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"300"}},"fontSize":"huge"} /-->
 	
 	<!-- wp:post-author-name {"fontSize":"medium"} /--></div>
 	<!-- /wp:group -->
