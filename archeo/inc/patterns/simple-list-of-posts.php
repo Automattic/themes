5f28db059b8e3b2e306d44c964dbc118adefda98
Sarah Norris <sarah@sekai.co.uk>
2022-02-08 17:39:25 +0000

    Add pattern

---
+++ archeo/inc/patterns/simple-list-of-posts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Simple list of posts
+ */
+return array(
+	'title'      => __( 'Simple list of posts', 'archeo' ),
+	'categories' => array( 'pages' ),
+	'content'    => '<!-- wp:group {"align":"full"} -->
+	<div class="wp-block-group alignfull"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
+	<div class="wp-block-query"><!-- wp:post-template -->
+	<!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
+	<div class="wp-block-group"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"300"}},"fontSize":"huge"} /-->
+	
+	<!-- wp:post-author-name {"fontSize":"medium"} /--></div>
+	<!-- /wp:group -->
+	<!-- /wp:post-template --></div>
+	<!-- /wp:query --></div>
+	<!-- /wp:group -->',
+);
