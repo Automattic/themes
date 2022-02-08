d122b0354a361a166602b0b141fa7074e7826303
Maggie <maggie.cabrera@automattic.com>
2022-02-08 11:09:23 +0100

    refactored paragraph with quote pattern

--- skatepark/inc/patterns/paragraph-with-quote.php
+++ skatepark/inc/patterns/paragraph-with-quote.php
@@ -9,12 +9,12 @@ return array(
 	'title'      => __( 'Paragraph with quote', 'skatepark' ),
 	'categories' => array( 'skatepark' ),
 	'content'    => '<!-- wp:columns {"align":"wide","className":"paragraph-with-quote"} -->
-	<div class="wp-block-columns alignwide paragraph-with-quote"><!-- wp:column {"width":"25%"} -->
-	<div class="wp-block-column" style="flex-basis:25%"></div>
+	<div class="wp-block-columns alignwide paragraph-with-quote"><!-- wp:column {"width":"22%"} -->
+	<div class="wp-block-column" style="flex-basis:22%"></div>
 	<!-- /wp:column -->
 
-	<!-- wp:column {"width":"50%"} -->
-	<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"level":4} -->
+	<!-- wp:column {"width":"56%","style":{"spacing":{"padding":{"right":"10%"}}}} -->
+	<div class="wp-block-column" style="padding-right:10%;flex-basis:56%"><!-- wp:heading {"level":4} -->
 	<h4>' . esc_html__( 'Why come to Skatepark?', 'skatepark' ) . '</h4>
 	<!-- /wp:heading -->
 
@@ -27,10 +27,22 @@ return array(
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column -->
 
-	<!-- wp:column {"width":"25%"} -->
-	<div class="wp-block-column" style="flex-basis:25%"><!-- wp:quote {"className":"is-style-side-quote"} -->
-	<blockquote class="wp-block-quote is-style-side-quote"><p>' . esc_html__( 'I learned how to do a kick flip and made a bunch of new friends. Can’t wait to go back!', 'skatepark' ) . '</p><cite>' . esc_html__( '– Jane Doe, 11', 'skatepark' ) . '</cite></blockquote>
-	<!-- /wp:quote --></div>
+	<!-- wp:column {"width":"22%","style":{"spacing":{"padding":{"top":"4.1em"}}}} -->
+	<div class="wp-block-column" style="padding-top:4.1em;flex-basis:22%">
+
+	<!-- wp:separator {"className":"is-style-wide"} -->
+	<hr class="wp-block-separator is-style-wide"/>
+	<!-- /wp:separator -->
+	
+	<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.4}},"fontSize":"medium"} -->
+	<p class="has-medium-font-size" style="line-height:1.4">' . esc_html__( 'I learned how to do a kick flip and made a bunch of new friends. Can’t wait to go back!', 'skatepark' ) . '</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+	<p style="font-style:normal;font-weight:600">' . esc_html__( '– Jane Doe, 11', 'skatepark' ) . '</p>
+	<!-- /wp:paragraph -->
+
+	</div>
 	<!-- /wp:column --></div>
 	<!-- /wp:columns -->',
 );
