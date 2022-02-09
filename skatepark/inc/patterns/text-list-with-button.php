42cf804814d16294db0cc42b64364c9f356cb91a
Maggie <maggie.cabrera@automattic.com>
2022-02-09 11:08:52 +0100

    removed headings styles refactoring pattern

--- skatepark/inc/patterns/text-list-with-button.php
+++ skatepark/inc/patterns/text-list-with-button.php
@@ -12,29 +12,18 @@ return array(
 	'categories' => array( 'skatepark' ),
 	'content'    => '<!-- wp:columns {"align":"wide"} -->
 		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"75%"} -->
-		<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":4,"className":"is-style-skatepark-heading-border"} -->
-		<h4 class="is-style-skatepark-heading-border">' . esc_html__( 'Visiting Coaches', 'skatepark' ) . '</h4>
-		<!-- /wp:heading -->
-		<!-- wp:heading --><h2>' . esc_html__( 'Steph Harper. Buddy Sellers. Mehmet Farrow. Bob Lopez. Tammy Habich. Tye Barclay. Kaden Price. Lily Rivera. Chris Wright. Sammy Sanchez.', 'skatepark' ) . '</h2>
-		<!-- /wp:heading -->
-		<!-- wp:buttons -->
-		<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
-		<div class="wp-block-button is-style-outline"><a class="wp-block-button__link">' . esc_html__( 'See All Faculty', 'skatepark' ) . '</a></div>
-		<!-- /wp:button --></div>
-		<!-- /wp:buttons --></div>
-		<!-- /wp:column -->
-		<!-- wp:column {"width":"25%"} -->
-		<div class="wp-block-column" style="flex-basis:25%"></div>
-		<!-- /wp:column --></div>
-		<!-- /wp:columns -->
+		<div class="wp-block-column" style="flex-basis:75%">
+		<!-- wp:group {"layout":{"type":"flex","allowOrientation":false}} -->
+		<div class="wp-block-group"><!-- wp:group -->
+		<div class="wp-block-group"><!-- wp:separator {"className":"is-style-wide"} -->
+		<hr class="wp-block-separator is-style-wide"/>
+		<!-- /wp:separator -->
 		
-		
-		
-		<!-- wp:columns {"align":"wide"} -->
-		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"75%"} -->
-		<div class="wp-block-column" style="flex-basis:75%"><!-- wp:heading {"level":4} -->
-		<h4>' . esc_html__( 'Visiting Coaches', 'skatepark' ) . '</h4>
-		<!-- /wp:heading -->
+		<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"1em"}}}} -->
+		<h4 id="visiting-coaches" style="margin-top:1em">' . esc_html__( 'Visiting Coaches', 'skatepark' ) . '</h4>
+		<!-- /wp:heading --></div>
+		<!-- /wp:group --></div>
+		<!-- /wp:group -->
 		<!-- wp:heading --><h2>' . esc_html__( 'Steph Harper. Buddy Sellers. Mehmet Farrow. Bob Lopez. Tammy Habich. Tye Barclay. Kaden Price. Lily Rivera. Chris Wright. Sammy Sanchez.', 'skatepark' ) . '</h2>
 		<!-- /wp:heading -->
 		<!-- wp:buttons -->
