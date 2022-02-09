628651d223aaf511e692b82183f80e00f7c7715d
Sarah Norris <sarah@sekai.co.uk>
2022-02-09 12:15:47 +0000

    Add another spacer & adjust sizes

--- archeo/inc/patterns/layout-with-two-images-and-text.php
+++ archeo/inc/patterns/layout-with-two-images-and-text.php
@@ -8,7 +8,11 @@ return array(
 	'content'    => '<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaLink":"' . esc_url( get_template_directory_uri() ) . '/assets/images/uxmal2.jpg","mediaType":"image","imageFill":true,"focalPoint":{"x":"0.50","y":"0.90"},"backgroundColor":"foreground","textColor":"background"} -->
 	<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile is-image-fill has-background-color has-foreground-background-color has-text-color has-background"><figure class="wp-block-media-text__media" style="background-image:url(' . esc_url( get_template_directory_uri() ) . '/assets/images/uxmal2.jpg);background-position:50% 90%"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/uxmal2.jpg" alt="' . __( 'Photo of the Governors Palace at Uxmal', 'archeo' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:columns -->
 	<div class="wp-block-columns"><!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"100","lineHeight":"1.2"}},"fontSize":"medium"} -->
+	<div class="wp-block-column"><!-- wp:spacer {"height":"min(10rem, 10vw)"} -->
+	<div style="height:min(10rem, 10vw)" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer --><
+	
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"100","lineHeight":"1.2"}},"fontSize":"medium"} -->
 	<p class="has-medium-font-size" style="font-style:normal;font-weight:100;line-height:1.2">' . wp_kses_post( __( 'Palais Du Gouverneur, <br>à Uxmal', 'archeo' ) ) . '</p>
 	<!-- /wp:paragraph -->
 	
@@ -18,8 +22,8 @@ return array(
 	<!-- /wp:column -->
 	
 	<!-- wp:column -->
-	<div class="wp-block-column"><!-- wp:spacer {"height":"min(34rem, 34vw)"} -->
-	<div style="height:min(34rem, 34vw)" aria-hidden="true" class="wp-block-spacer"></div>
+	<div class="wp-block-column"><!-- wp:spacer {"height":"min(43rem, 43vw)"} -->
+	<div style="height:min(43rem, 43vw)" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
 	
 	<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"}},"fontSize":"small"} -->
