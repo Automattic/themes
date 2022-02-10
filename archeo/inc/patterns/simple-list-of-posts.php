9594d39126553f3933c282ce79519db434dc8d6f
Sarah Norris <sarah@sekai.co.uk>
2022-02-10 16:24:55 +0000

    Change to alignwide

--- archeo/inc/patterns/simple-list-of-posts.php
+++ archeo/inc/patterns/simple-list-of-posts.php
@@ -5,8 +5,8 @@
 return array(
 	'title'      => __( 'Simple list of posts', 'archeo' ),
 	'categories' => array( 'pages' ),
-	'content'    => '<!-- wp:group {"align":"full"} -->
-	<div class="wp-block-group alignfull"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
+	'content'    => '<!-- wp:group {"align":"wide"} -->
+	<div class="wp-block-group alignwide"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
 	<!-- wp:group {"className":"simple-list-of-posts","layout":{"type":"flex","allowOrientation":false}} -->
 	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
