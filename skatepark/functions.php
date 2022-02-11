f24b34ac2e50c70418db7d99d9395a2000e4bee2
Maggie <maggie.cabrera@automattic.com>
2022-02-11 10:55:11 +0100

    removed parent theme, added functions file

--- skatepark/functions.php
+++ skatepark/functions.php
@@ -1,6 +1,14 @@
 <?php
-
 /**
+ * Skatepark functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Skatepark
+ * @since Skatepark 1.0
+ */
+
+ /**
  * Add class to body if post/page has a featured image.
  */
 function add_featured_image_class( $classes ) {
@@ -11,3 +19,174 @@ function add_featured_image_class( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'add_featured_image_class' );
+
+if ( ! function_exists( 'skatepark_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Skatepark 1.0
+	 *
+	 * @return void
+	 */
+	function skatepark_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'skatepark_support' );
+
+if ( ! function_exists( 'skatepark_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Skatepark 1.0
+	 *
+	 * @return void
+	 */
+	function skatepark_styles() {
+
+		// Register theme stylesheet.
+		wp_register_style(
+			'skatepark-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			wp_get_theme()->get( 'Version' )
+		);
+
+		// Add styles inline.
+		wp_add_inline_style( 'skatepark-style', skatepark_get_font_face_styles() );
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'skatepark-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'skatepark_styles' );
+
+if ( ! function_exists( 'skatepark_editor_styles' ) ) :
+
+	/**
+	 * Enqueue editor styles.
+	 *
+	 * @since Skatepark 1.0
+	 *
+	 * @return void
+	 */
+	function skatepark_editor_styles() {
+
+		// Add styles inline.
+		wp_add_inline_style( 'wp-block-library', skatepark_get_font_face_styles() );
+
+	}
+
+endif;
+
+add_action( 'admin_init', 'skatepark_editor_styles' );
+
+
+if ( ! function_exists( 'skatepark_get_font_face_styles' ) ) :
+
+	/**
+	 * Get font face styles.
+	 * Called by functions skatepark_styles() and skatepark_editor_styles() above.
+	 *
+	 * @since Skatepark 1.0
+	 *
+	 * @return string
+	 */
+	function skatepark_get_font_face_styles() {
+
+		return "
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 100;
+			font-style: normal;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-Thin.woff2' ) . "') format('woff2');
+		}
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 400;
+			font-style: normal;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-Regular.woff2' ) . "') format('woff2');
+		}
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 700;
+			font-style: normal;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-Bold.woff2' ) . "') format('woff2');
+		}
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 100;
+			font-style: italic;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-ThinItalic.woff2' ) . "') format('woff2');
+		}
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 400;
+			font-style: italic;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-Italic.woff2' ) . "') format('woff2');
+		}
+		@font-face{
+			font-family: 'Chivo';
+			font-weight: 700;
+			font-style: italic;
+			font-stretch: normal;
+			font-display: swap;
+			src: url('" . get_theme_file_uri( 'assets/fonts/Chivo-BoldItalic.woff2' ) . "') format('woff2');
+		}
+		";
+
+	}
+
+endif;
+
+if ( ! function_exists( 'skatepark_preload_webfonts' ) ) :
+
+	/**
+	 * Preloads the main web font to improve performance.
+	 *
+	 * Only the main web font (font-weight: 100,400, font-style: normal) is preloaded here since that font is always relevant. 
+	 * The other fonts are only needed if the user changed style or weight of the fonts,
+	 * and therefore preloading it would in most cases regress performance when that font would otherwise not be loaded
+	 * at all.
+	 *
+	 * @since Skatepark 1.0
+	 *
+	 * @return void
+	 */
+	function skatepark_preload_webfonts() {
+		?>
+		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/Chivo-Thin.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
+		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/Chivo-Regular.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
+		<?php
+	}
+
+endif;
+
+add_action( 'wp_head', 'skatepark_preload_webfonts' );
+
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
+
