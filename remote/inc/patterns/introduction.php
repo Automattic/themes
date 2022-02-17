29939b05a792928e1ded9b09b3303bbe8fe73932
Sarah Norris <sarah@sekai.co.uk>
2022-02-17 12:40:46 +0000

    Add Introduction pattern

---
+++ remote/inc/patterns/introduction.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Introduction
+ */
+return array(
+	'title'      => __( 'Introduction', 'remote' ),
+	'categories' => array( 'pages' ),
+	'content'    => '<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.17"}},"fontSize":"huge"} -->
+    <p class="has-huge-font-size" style="line-height:1.17">' . __( 'Hi, I\'m Tiffany! After years of long commutes and crowded offices, I\'m now a happy advocate for remote work.', 'remote' ) . '</p><!-- /wp:paragraph -->
+
+    <!-- wp:spacer -->
+    <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+    <!-- /wp:spacer -->',
+);
