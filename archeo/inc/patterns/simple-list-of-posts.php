f2318efca0547190e85d2d8548f6170cf7ecf5a7
Sarah Norris <sarah@sekai.co.uk>
2022-02-21 10:10:25 +0000

    Add links to post titles in list of posts

--- archeo/inc/patterns/simple-list-of-posts.php
+++ archeo/inc/patterns/simple-list-of-posts.php
@@ -9,7 +9,7 @@ return array(
 	<div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--medium);padding-bottom:var(--wp--custom--spacing--medium);margin-top:0px"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
 	<!-- wp:group {"className":"simple-list-of-posts","layout":{"type":"flex","allowOrientation":false}} -->
-	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
+	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"isLink":true,"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
 	
 	<!-- wp:post-author-name {"style":{"typography":{"fontStyle":"normal","fontWeight":"100","lineHeight":"1.2"}},"fontSize":"medium"} /--></div>
 	<!-- /wp:group -->
