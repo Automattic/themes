566ff1208dc211e2cc9c34df190405213234b94f
Sarah Norris <sarah@sekai.co.uk>
2022-02-21 21:03:44 +0000

    Archeo: Add home template (#5564)
    * Add simple list of posts with background pattern
    
    * Add home template
    
    * Remove layout inherit setting
    
    * Adjust spacing in Simple list of posts
    
    * Adjust spacing in Layered images with headline
    
    * Change paragraph to heading
    
    * Remove custom CSS, not needed!
    
    * Add empty line at end of style.css
    
    * Change header bottom padding to outer
    
    * Remove top padding from Image with headline and description
    
    * Add links to post titles in list of posts
    
    * Add pagination to list of posts patterns
    
    * restore english text
    
    Co-authored-by: Ben Dwyer <ben@scruffian.com>

--- archeo/inc/patterns/simple-list-of-posts.php
+++ archeo/inc/patterns/simple-list-of-posts.php
@@ -5,15 +5,22 @@
 return array(
 	'title'      => __( 'Simple list of posts', 'archeo' ),
 	'categories' => array( 'pages' ),
-	'content'    => '<!-- wp:group {"align":"wide"} -->
-	<div class="wp-block-group alignwide"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
+	'content'    => '<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--medium)","bottom":"var(--wp--custom--spacing--medium)"},"margin":{"top":"0px"}}}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--custom--spacing--medium);padding-bottom:var(--wp--custom--spacing--medium);margin-top:0px"><!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
 	<div class="wp-block-query"><!-- wp:post-template -->
 	<!-- wp:group {"className":"simple-list-of-posts","layout":{"type":"flex","allowOrientation":false}} -->
-	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
+	<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"isLink":true,"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
 	
 	<!-- wp:post-author-name {"style":{"typography":{"fontStyle":"normal","fontWeight":"100","lineHeight":"1.2"}},"fontSize":"medium"} /--></div>
 	<!-- /wp:group -->
-	<!-- /wp:post-template --></div>
+	<!-- /wp:post-template -->
+	<!-- wp:spacer {"height":"40px"} -->
+	<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+	<!-- /wp:spacer -->
+	<!-- wp:query-pagination {"paginationArrow":"chevron","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<!-- wp:query-pagination-previous /-->
+	<!-- wp:query-pagination-next /-->
+	<!-- /wp:query-pagination --></div>
 	<!-- /wp:query --></div>
 	<!-- /wp:group -->',
 );
