02f3850729ce5e5d780d92c8fc4e96a8c2f58040
Jason Crist <jcrist@pbking.com>
2022-02-21 12:36:14 -0500

    Moved wide setting from internal group to top-level.

--- remote/inc/patterns/hero-text.php
+++ remote/inc/patterns/hero-text.php
@@ -5,16 +5,14 @@
 return array(
 	'title'      => __( 'Hero text', 'remote' ),
 	'categories' => array( 'pages' ),
-	'content'    => '<!-- wp:group {"layout":{"inherit":true}} -->
-    <div class="wp-block-group"><!-- wp:spacer {"height":"32px"} -->
+	'content'    => '<!-- wp:group {"align":"wide"} -->
+    <div class="wp-block-group alignwide"><!-- wp:spacer {"height":"32px"} -->
     <div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
     <!-- /wp:spacer -->
     
-    <!-- wp:group {"align":"wide"} -->
-    <div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.17"}},"fontSize":"huge"} -->
+    <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.17"}},"fontSize":"huge"} -->
     <p class="has-huge-font-size" style="line-height:1.17">' . esc_html__( 'Hi, I’m Tiffany! After years of long commutes and crowded offices, I’m now a happy advocate for remote work.', 'remote' ) . '</p>
-    <!-- /wp:paragraph --></div>
-    <!-- /wp:group -->
+    <!-- /wp:paragraph -->
     
     <!-- wp:spacer -->
     <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
