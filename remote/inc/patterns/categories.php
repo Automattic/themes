9725bca0d1f0503f383535c6e196daad9e49cac3
Jeff Ong <jonger4@gmail.com>
2022-02-25 14:15:08 -0500

    Remote: add footer and tags pattern (#5543)
    * Add tags pattern and register it.
    
    * Revise footer.
    
    * categories pattern, used different blocks, added to the footer
    
    * Corrected a couple of comments
    
    * Remove CSS that is in gutenberg now.
    
    * Make tags uppercase.
    
    * Remove text tranform from tag cloud.
    
    Co-authored-by: Maggie <maggie.cabrera@automattic.com>
    Co-authored-by: Jason Crist <jcrist@pbking.com>

---
+++ remote/inc/patterns/categories.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Categories Pattern
+ */
+return array(
+	'title'      => __( 'Categories', 'remote' ),
+	'categories' => array( 'text' ),
+	'content'    => '<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} -->
+		<p class="has-small-font-size" style="text-transform:uppercase">' . esc_html__( 'Categories', 'remote' ) . '</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:tag-cloud {"className":"is-style-outline","taxonomy":"category"} /-->',
+);
