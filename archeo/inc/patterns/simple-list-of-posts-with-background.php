19f895f6fefa6c7f7de26bdcac4795c67addddd0
Sarah Norris <sarah@sekai.co.uk>
2022-03-08 17:06:22 +0000

    Add top margin to post title

--- archeo/inc/patterns/simple-list-of-posts-with-background.php
+++ archeo/inc/patterns/simple-list-of-posts-with-background.php
@@ -14,7 +14,7 @@ return array(
 		<!-- wp:query {"queryId":3,"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"perPage":3}} -->
 		<div class="wp-block-query"><!-- wp:post-template -->
 		<!-- wp:group {"className":"simple-list-of-posts","layout":{"type":"flex","allowOrientation":false}} -->
-		<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"isLink":true,"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"}},"fontSize":"huge"} /-->
+		<div class="wp-block-group simple-list-of-posts"><!-- wp:post-title {"isLink":true,"style":{"typography":{"lineHeight":"1.2","fontStyle":"normal","fontWeight":"100"},"spacing":{"margin":{"top":"1.25rem"}}},"fontSize":"huge"} /-->
 		
 		<!-- wp:post-author-name {"style":{"typography":{"fontStyle":"normal","fontWeight":"100","lineHeight":"1.2"},"spacing":{"margin":{"top":"0px"}}},"fontSize":"medium"} /--></div>
 		<!-- /wp:group -->
