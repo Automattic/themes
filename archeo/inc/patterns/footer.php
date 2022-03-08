d6a3f63a15352330290032b06fc1af1b53e77c4d
Sarah Norris <sarah@sekai.co.uk>
2022-03-08 17:29:40 +0000

    Left align footer items

--- archeo/inc/patterns/footer.php
+++ archeo/inc/patterns/footer.php
@@ -8,10 +8,10 @@ return array(
 	'blockTypes' => array( 'core/template-part/footer' ),
 	'content'    => '<!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between"},"style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--medium)","top":"var(--wp--custom--spacing--medium)"}}}} -->
 	<div class="wp-block-group" style="padding-top: var(--wp--custom--spacing--medium); padding-bottom: var(--wp--custom--spacing--medium);">
-		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center"},"overlayMenu":"never","style":{"typography":{"fontStyle":"normal"},"spacing":{"blockGap":"50px"}},"fontSize":"small"} /-->
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"},"overlayMenu":"never","style":{"typography":{"fontStyle":"normal"},"spacing":{"blockGap":"50px"}},"fontSize":"small"} /-->
 	
-		<!-- wp:paragraph {"align":"center","fontSize":"small","style":{"spacing":{"margin":{"top":0}}}} -->
-		<p class="has-text-align-center has-small-font-size" style="margin-top: 0;">' .
+		<!-- wp:paragraph {"align":"left","fontSize":"small","style":{"spacing":{"margin":{"top":0}}}} -->
+		<p class="has-small-font-size" style="margin-top: 0;">' .
 		sprintf(
 			/* Translators: WordPress link. */
 			esc_html__( 'Proudly powered by %s', 'archeo' ),
