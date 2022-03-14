db02288001d13abb79d564d02a2ad210953fc975
Maggie <maggie.cabrera@automattic.com>
2022-03-14 12:13:50 +0100

    refactored image with headline pattern

--- archeo/inc/patterns/image-with-headline-description.php
+++ archeo/inc/patterns/image-with-headline-description.php
@@ -5,15 +5,21 @@
 return array(
 	'title'      => __( 'Image with headline and description', 'archeo' ),
 	'categories' => array( 'featured', 'images' ),
-	'content'    => '<!-- wp:group {"align":"wide"} -->
-	<div class="wp-block-group alignwide"><!-- wp:media-text {"mediaLink":"' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif","mediaType":"image","mediaWidth":64,"verticalAlignment":"top"} -->
-	<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:64% auto"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif" alt="' . esc_attr__( 'Chahk: rain deity', 'archeo' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textAlign":"right","style":{"typography":{"fontSize":"clamp(64px, 6vw, 100px)","lineHeight":"1","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"48px"}}}} -->
-	<h2 class="has-text-align-right" id="chahk-raindeity" style="font-size:clamp(64px, 6vw, 100px);line-height:1;margin-bottom:48px;text-transform:uppercase">' . wp_kses_post( __( 'Chahk:<br>Rain<br>deity', 'archeo' ) ) . '</h2>
+	'content'    => '<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"64%"} -->
+	<div class="wp-block-column" style="flex-basis:64%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+	<figure class="wp-block-image size-full"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif" alt="' . esc_attr__( 'Chahk: rain deity', 'archeo' ) . '"/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:heading {"textAlign":"right","style":{"typography":{"fontSize":"clamp(64px, 6vw, 100px)","lineHeight":"1","textTransform":"uppercase"}}} -->
+	<h2 class="has-text-align-right" id="chahk-raindeity" style="font-size:clamp(64px, 6vw, 100px);line-height:1;text-transform:uppercase">' . wp_kses_post( __( 'Chahk:<br>Rain<br>deity', 'archeo' ) ) . '</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column {"width":"15%"} -->
-	<div class="wp-block-column" style="flex-basis:15%"></div>
+	<div class="wp-block-columns"><!-- wp:column {"width":"26%"} -->
+	<div class="wp-block-column" style="flex-basis:26%"></div>
 	<!-- /wp:column -->
 
 	<!-- wp:column {"width":""} -->
@@ -21,7 +27,7 @@ return array(
 	<p class="has-text-align-right has-small-font-size">' . esc_html__( 'Chaahk is shown here clutching his axe, ready to strike rain and thunder from the clouds. This sculpture was likely created in the Puuc hills of Yucatan and Campeche.', 'archeo' ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div></div>
-	<!-- /wp:media-text --></div>
-	<!-- /wp:group -->',
+	<!-- /wp:columns --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns -->',
 );
