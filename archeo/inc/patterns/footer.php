1958eef9b1a33046c15aaefa0cfb569964d58938
Maggie <maggie.cabrera@automattic.com>
2022-03-15 15:25:05 +0100

    Archeo: home page and header wide aligned (#5623)
    * make everything wide aligned
    
    * make footer wide aligned
    
    * fix markup error
    
    * Fix merge conflict
    
    * Add CSS to remove media/text padding
    
    * removed padding class
    
    * refactored image with headline pattern
    
    * removed image margin bottom
    
    * Add padding to column.
    
    * Add left-right padding to cover instead of column.
    
    Co-authored-by: Sarah Norris <sarah@sekai.co.uk>
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

--- archeo/inc/patterns/footer.php
+++ archeo/inc/patterns/footer.php
@@ -6,8 +6,9 @@ return array(
 	'title'      => __( 'Default footer', 'archeo' ),
 	'categories' => array( 'footer' ),
 	'blockTypes' => array( 'core/template-part/footer' ),
-	'content'    => '<!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between"},"style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--medium)","top":"var(--wp--custom--spacing--medium)"}}}} -->
-	<div class="wp-block-group" style="padding-top: var(--wp--custom--spacing--medium); padding-bottom: var(--wp--custom--spacing--medium);">
+	'content'    => '<!-- wp:group {"layout":{"inherit":"true"}} -->
+	<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"},"style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--medium)","top":"var(--wp--custom--spacing--medium)"}}}} -->
+	<div class="wp-block-group alignwide" style="padding-top: var(--wp--custom--spacing--medium); padding-bottom: var(--wp--custom--spacing--medium);">
 		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"},"overlayMenu":"never","className":"site-footer","style":{"typography":{"fontStyle":"normal"},"spacing":{"blockGap":"var(--wp--custom--spacing--small)"}},"fontSize":"small"} /-->
 	
 		<!-- wp:paragraph {"align":"left","fontSize":"small","style":{"spacing":{"margin":{"top":0}}}} -->
@@ -18,5 +19,6 @@ return array(
 			'<a href="' . esc_url( __( 'https://wordpress.org', 'archeo' ) ) . '" rel="nofollow">WordPress</a>'
 		) . '</p>
 		<!-- /wp:paragraph --></div>
+		<!-- /wp:group --></div>
 		<!-- /wp:group -->',
 );
--- archeo/inc/patterns/image-with-headline-description.php
+++ archeo/inc/patterns/image-with-headline-description.php
@@ -5,15 +5,23 @@
 return array(
 	'title'      => __( 'Image with headline and description', 'archeo' ),
 	'categories' => array( 'featured', 'images' ),
-	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","right":"5vw","bottom":"0","left":"5vw"}}}} -->
-	<div class="wp-block-group alignfull" style="padding-top:0;padding-right:5vw;padding-bottom:0;padding-left:5vw"><!-- wp:media-text {"mediaLink":"' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif","mediaType":"image","mediaWidth":64,"verticalAlignment":"top"} -->
-	<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top" style="grid-template-columns:64% auto"><figure class="wp-block-media-text__media"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif" alt="' . esc_attr__( 'Chahk: rain deity', 'archeo' ) . '"/></figure><div class="wp-block-media-text__content"><!-- wp:heading {"textAlign":"right","style":{"typography":{"fontSize":"clamp(64px, 6vw, 100px)","lineHeight":"1","textTransform":"uppercase"},"spacing":{"margin":{"bottom":"clamp(5px, 2vw, 20px)"}}}} -->
-	<h2 class="has-text-align-right" id="chahk-raindeity" style="font-size:clamp(64px, 6vw, 100px);line-height:1;margin-bottom:clamp(5px, 2vw, 20px);text-transform:uppercase">' . wp_kses_post( __( 'Chahk:<br>Rain<br>deity', 'archeo' ) ) . '</h2>
+	'content'    => '<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"width":"64%"} -->
+	<div class="wp-block-column" style="flex-basis:64%"><!-- wp:group -->
+	<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+	<figure class="wp-block-image size-full"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/chahk.gif" alt="' . esc_attr__( 'Chahk: rain deity', 'archeo' ) . '"/></figure>
+	<!-- /wp:image --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column -->
+	<div class="wp-block-column"><!-- wp:heading {"textAlign":"right","style":{"typography":{"fontSize":"clamp(64px, 6vw, 100px)","lineHeight":"1","textTransform":"uppercase"}}} -->
+	<h2 class="has-text-align-right" id="chahk-raindeity" style="font-size:clamp(64px, 6vw, 100px);line-height:1;text-transform:uppercase">' . wp_kses_post( __( 'Chahk:<br>Rain<br>deity', 'archeo' ) ) . '</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:columns -->
-	<div class="wp-block-columns"><!-- wp:column {"width":"15%"} -->
-	<div class="wp-block-column" style="flex-basis:15%"></div>
+	<div class="wp-block-columns"><!-- wp:column {"width":"26%"} -->
+	<div class="wp-block-column" style="flex-basis:26%"></div>
 	<!-- /wp:column -->
 
 	<!-- wp:column {"width":""} -->
@@ -21,7 +29,7 @@ return array(
 	<p class="has-text-align-right has-small-font-size">' . esc_html__( 'Chaahk is shown here clutching his axe, ready to strike rain and thunder from the clouds. This sculpture was likely created in the Puuc hills of Yucatan and Campeche.', 'archeo' ) . '</p>
 	<!-- /wp:paragraph --></div>
 	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div></div>
-	<!-- /wp:media-text --></div>
-	<!-- /wp:group -->',
+	<!-- /wp:columns --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns -->',
 );
