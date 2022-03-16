5c0b4f58d5475b13c65526097ed6acda17dcbad4
Jason Crist <jcrist@pbking.com>
2022-03-16 14:07:53 -0400

    Cloned 'Block Canvas' and adjusted the palette colors as a starting point

---
+++ pendant/functions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Block Canvas functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Block Canvas
+ * @since Block Canvas 1.0
+ */
+
+
+if ( ! function_exists( 'pendant_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Block Canvas 1.0
+	 *
+	 * @return void
+	 */
+	function pendant_support() {
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'pendant_support' );
+
+if ( ! function_exists( 'pendant_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Block Canvas 1.0
+	 *
+	 * @return void
+	 */
+	function pendant_styles() {
+
+		// Register theme stylesheet.
+		wp_register_style(
+			'pendant-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			wp_get_theme()->get( 'Version' )
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'pendant-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'pendant_styles' );
