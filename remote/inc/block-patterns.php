e97bc457519a83abe90e6dbb492cfed93e92b5a3
Maggie <maggie.cabrera@automattic.com>
2022-03-16 13:18:10 +0100

    re register pattern categories

--- remote/inc/block-patterns.php
+++ remote/inc/block-patterns.php
@@ -14,6 +14,36 @@
  */
 function remote_register_block_patterns() {
 
+	$block_pattern_categories = array(
+		'featured' => array( 'label' => __( 'Featured', 'remote' ) ),
+		'columns'  => array( 'label' => __( 'Columns', 'remote' ) ),
+		'text'     => array( 'label' => __( 'Text', 'remote' ) ),
+		'query'    => array( 'label' => __( 'Query', 'remote' ) ),
+	);
+
+	/**
+	 * Filters the theme block pattern categories.
+	 *
+	 * @since remote 1.0
+	 *
+	 * @param array[] $block_pattern_categories {
+	 *     An associative array of block pattern categories, keyed by category name.
+	 *
+	 *     @type array[] $properties {
+	 *         An array of block category properties.
+	 *
+	 *         @type string $label A human-readable label for the pattern category.
+	 *     }
+	 * }
+	 */
+	$block_pattern_categories = apply_filters( 'remote_block_pattern_categories', $block_pattern_categories );
+
+	foreach ( $block_pattern_categories as $name => $properties ) {
+		if ( ! WP_Block_Pattern_Categories_Registry::get_instance()->is_registered( $name ) ) {
+			register_block_pattern_category( $name, $properties );
+		}
+	}
+
 	$block_patterns = array(
 		'hero-text',
 		'hidden-404-content',
