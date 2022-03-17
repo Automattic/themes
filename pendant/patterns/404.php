d4929cbefb646e294a40658848cabcd25683dd0c
Jason Crist <jcrist@pbking.com>
2022-03-17 09:04:05 -0400

    Create Pendant Base Theme (#5694)
    * Cloned 'Block Canvas' and adjusted the palette colors as a starting point
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

---
+++ pendant/patterns/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Pattern Name: A 404 page
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
+<h1 class="has-text-align-center has-medium-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'pendant' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'pendant' ); ?></p>
+<!-- /wp:paragraph -->
