204a35ebaf1ac5d0f15144ab4ba3e153c66cf25b
Maggie <maggie.cabrera@automattic.com>
2022-03-17 11:18:33 +0100

    Try: Blockbase standalone theme (#5660)
    * first commit with initial files
    
    * templates
    
    * styles
    
    * template parts
    
    * Removed errant metadata
    
    * Added create tool
    
    To use `php create.php` and answer the prompts.
    This will create a folder alongside this theme with the provided
    metadata as well as namespaced attributes refactored.
    
    * Removed a create script.  (It shouldn't be here...)
    
    * 404 block pattern
    
    * remove fontSlug
    
    * removed block styles support
    
    * change slug of block pattern
    
    * added namespace to pattern
    
    * ignore this theme when pushing to the sandbox
    
    Co-authored-by: Jason Crist <jcrist@pbking.com>

---
+++ block-canvas/patterns/404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Pattern Name: A 404 page
+ * Namespace: block-canvas
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
+<h1 class="has-text-align-center has-medium-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'block-canvas' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'block-canvas' ); ?></p>
+<!-- /wp:paragraph -->
