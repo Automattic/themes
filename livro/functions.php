38d6bc4dcfc924b3722a1fe3e5674e6276f90277
Ben Dwyer <ben@scruffian.com>
2022-03-17 18:03:07 +0000

    Archeo: Add fonts to theme.json (#5609)
    * Archeo: Add fonts to theme.json
    
    * Add archeo_preload_webfonts function back in
    
    * Also replace the fonts in Stewart
    
    * Use theme.json fonts for Livro
    
    * Add fonts for Remote to theme.json
    
    * Remove preload fuctions and use  on the font files
    
    Co-authored-by: Sarah Norris <sarah@sekai.co.uk>

--- livro/functions.php
+++ livro/functions.php
@@ -51,100 +51,16 @@ if ( ! function_exists( 'livro_styles' ) ) :
 			wp_get_theme()->get( 'Version' )
 		);
 
-		// Add styles inline.
-		wp_add_inline_style( 'livro-style', livro_get_font_face_styles() );
-
 		// Enqueue theme stylesheet.
 		wp_enqueue_style( 'livro-style' );
-
 	}
 
 endif;
 
 add_action( 'wp_enqueue_scripts', 'livro_styles' );
 
-if ( ! function_exists( 'livro_editor_styles' ) ) :
-
-	/**
-	 * Enqueue editor styles.
-	 *
-	 * @since Livro 1.0
-	 *
-	 * @return void
-	 */
-	function livro_editor_styles() {
-
-		// Add styles inline.
-		wp_add_inline_style( 'wp-block-library', livro_get_font_face_styles() );
-
-	}
-
-endif;
 
 add_action( 'admin_init', 'livro_editor_styles' );
 
-
-if ( ! function_exists( 'livro_get_font_face_styles' ) ) :
-
-	/**
-	 * Get font face styles.
-	 * Called by functions livro_styles() and livro_editor_styles() above.
-	 *
-	 * @since Livro 1.0
-	 *
-	 * @return string
-	 */
-	function livro_get_font_face_styles() {
-
-		return "
-		@font-face{
-			font-family: 'Newsreader';
-			font-weight: 200 800;
-			font-style: normal;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/Newsreader.woff2' ) . "') format('woff2');
-		}
-
-		@font-face{
-			font-family: 'Newsreader';
-			font-weight: 200 800;
-			font-style: italic;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/Newsreader-italic.woff2' ) . "') format('woff2');
-		}
-		";
-
-	}
-
-endif;
-
-if ( ! function_exists( 'livro_preload_webfonts' ) ) :
-
-	/**
-	 * Preloads the main web font to improve performance.
-	 *
-	 * Only the main web font (font-style: normal) is preloaded here since that font is always relevant (it is used
-	 * on every heading, for example). The other font is only needed if there is any applicable content in italic style,
-	 * and therefore preloading it would in most cases regress performance when that font would otherwise not be loaded
-	 * at all.
-	 *
-	 * @since Livro 1.0
-	 *
-	 * @return void
-	 */
-	function livro_preload_webfonts() {
-		?>
-		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/Newsreader.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
-		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/Newsreader-italic.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
-		<?php
-	}
-
-endif;
-
-add_action( 'wp_head', 'livro_preload_webfonts' );
-
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';
-
--- remote/functions.php
+++ remote/functions.php
@@ -51,9 +51,6 @@ if ( ! function_exists( 'remote_styles' ) ) :
 			wp_get_theme()->get( 'Version' )
 		);
 
-		// Add styles inline.
-		wp_add_inline_style( 'remote-style', remote_get_font_face_styles() );
-
 		// Enqueue theme stylesheet.
 		wp_enqueue_style( 'remote-style' );
 
@@ -62,102 +59,6 @@ if ( ! function_exists( 'remote_styles' ) ) :
 endif;
 
 add_action( 'wp_enqueue_scripts', 'remote_styles' );
-
-if ( ! function_exists( 'remote_editor_styles' ) ) :
-
-	/**
-	 * Enqueue editor styles.
-	 *
-	 * @since Remote 1.0
-	 *
-	 * @return void
-	 */
-	function remote_editor_styles() {
-
-		// Add styles inline.
-		wp_add_inline_style( 'wp-block-library', remote_get_font_face_styles() );
-
-	}
-
-endif;
-
-add_action( 'admin_init', 'remote_editor_styles' );
-
-
-if ( ! function_exists( 'remote_get_font_face_styles' ) ) :
-
-	/**
-	 * Get font face styles.
-	 * Called by functions remote_styles() and remote_editor_styles() above.
-	 *
-	 * @since Remote 1.0
-	 *
-	 * @return string
-	 */
-	function remote_get_font_face_styles() {
-
-		return "
-		@font-face{
-			font-family: 'DM Sans';
-			font-weight: 400;
-			font-style: normal;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/DMSans-Regular.woff2' ) . "') format('woff2');
-		}
-		@font-face{
-			font-family: 'DM Sans';
-			font-weight: 700;
-			font-style: normal;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/DMSans-Bold.woff2' ) . "') format('woff2');
-		}
-		@font-face{
-			font-family: 'DM Sans';
-			font-weight: 400;
-			font-style: italic;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/DMSans-Italic.woff2' ) . "') format('woff2');
-		}
-		@font-face{
-			font-family: 'DM Sans';
-			font-weight: 700;
-			font-style: italic;
-			font-stretch: normal;
-			font-display: swap;
-			src: url('" . get_theme_file_uri( 'assets/fonts/DMSans-BoldItalic.woff2' ) . "') format('woff2');
-		}
-		";
-
-	}
-
-endif;
-
-if ( ! function_exists( 'remote_preload_webfonts' ) ) :
-
-	/**
-	 * Preloads the main web font to improve performance.
-	 *
-	 * Only the main web font (font-weight: 100,400, font-style: normal) is preloaded here since that font is always relevant.
-	 * The other fonts are only needed if the user changed style or weight of the fonts,
-	 * and therefore preloading it would in most cases regress performance when that font would otherwise not be loaded
-	 * at all.
-	 *
-	 * @since Remote 1.0
-	 *
-	 * @return void
-	 */
-	function remote_preload_webfonts() {
-		?>
-		<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/DMSans-Regular.woff2' ) ); ?>" as="font" type="font/woff2" crossorigin>
-		<?php
-	}
-
-endif;
-
-add_action( 'wp_head', 'remote_preload_webfonts' );
 
 /**
  * Block Patterns.
