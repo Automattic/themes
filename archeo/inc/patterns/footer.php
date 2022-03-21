6702d7b4a6dd17d43f8bcec5cf4532bdcd3394fe
Sarah Norris <sarah@sekai.co.uk>
2022-03-21 17:16:43 +0000

    Change blockgap from variable to 2.5rem (#5671)

--- archeo/inc/patterns/footer.php
+++ archeo/inc/patterns/footer.php
@@ -9,7 +9,7 @@ return array(
 	'content'    => '<!-- wp:group {"layout":{"inherit":"true"}} -->
 	<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"},"style":{"spacing":{"padding":{"bottom":"var(--wp--custom--spacing--medium)","top":"var(--wp--custom--spacing--medium)"}}}} -->
 	<div class="wp-block-group alignwide" style="padding-top: var(--wp--custom--spacing--medium); padding-bottom: var(--wp--custom--spacing--medium);">
-		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"},"overlayMenu":"never","className":"site-footer","style":{"typography":{"fontStyle":"normal"},"spacing":{"blockGap":"var(--wp--custom--spacing--small)"}},"fontSize":"small"} /-->
+		<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left"},"overlayMenu":"never","className":"site-footer","style":{"typography":{"fontStyle":"normal"},"spacing":{"blockGap":"2.5rem"}},"fontSize":"small"} /-->
 	
 		<!-- wp:paragraph {"align":"left","fontSize":"small","style":{"spacing":{"margin":{"top":0}}}} -->
 		<p class="has-small-font-size" style="margin-top: 0;">' .
