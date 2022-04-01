4fd5c6149d3d5830c2536dbc7545e5fb841b2557
Jason Crist <jcrist@pbking.com>
2022-04-01 06:19:43 -0400

    Added Gutenberg check to Blockbase themes (#5735)
    * Added Gutenberg check to Blockbase themes
    
    * tucked the  check into the if statement where it belongs
    
    * Lint autofix.
    
    * Update translation text domain
    
    Co-authored-by: Jeff Ong <jonger4@gmail.com>
    Co-authored-by: Sarah Norris <sarah@sekai.co.uk>

---
+++ blockbase/inc/gutenberg-dependency-check.php
@@ -0,0 +1,60 @@
+<?php
+
+add_action( 'admin_notices', 'show_admin_messages' );
+
+function print_admin_message( $version ) {
+	echo '<div id="message" class="error"><p><strong>';
+	printf( __( 'The installed theme requires <a href="https://wordpress.org/plugins/gutenberg/">Gutenberg</a> version %s or higher.', 'blockbase' ), $version );
+	echo '</strong></p></div>';
+}
+
+function show_admin_messages() {
+
+	if ( is_child_theme() ) {
+		$metadata = file_get_contents( get_template_directory() . '/style.css' );
+		preg_match( '/(?<=Requires Gutenberg:).+/', $metadata, $match );
+
+		if ( sizeof( $match ) > 0 ) {
+			$version = trim( $match[0] );
+		}
+	}
+
+	$metadata = file_get_contents( get_stylesheet_directory() . '/style.css' );
+	preg_match( '/(?<=Requires Gutenberg:).+/', $metadata, $match );
+
+	if ( ! isset( $version ) || ( sizeof( $match ) > 0 && version_compare( $version, trim( $match[0] ) ) < 0 ) ) {
+		$version = trim( $match[0] );
+	}
+
+	if ( ! isset( $version ) ) {
+		return; // Gutenberg is not required
+	}
+
+	if ( ! defined( 'IS_GUTENBERG_PLUGIN' ) ) {
+		print_admin_message( $version ); // Gutenberg is not activated
+		return;
+	}
+
+	// Determine Gutenberg version from defined constant
+	if ( defined( 'GUTENBERG_VERSION' ) ) {
+		if ( version_compare( GUTENBERG_VERSION, $version ) < 0 ) {
+			print_admin_message( $version );
+		}
+		return;
+	}
+
+	// We have confirmed that Gutenberg is installed and activated, however we cannot use the GUTENBERG_VERSION constant
+	// (probably because we are in development mode)
+	// we'll use the metadata from get_plugins() to determine the version of Gutenberg
+	$plugins = get_plugins();
+	foreach ( $plugins as $plugin ) {
+		if ( 'Gutenberg' === $plugin['Name'] ) {
+			if ( version_compare( trim( $plugin['Version'] ), $version ) < 0 ) {
+				print_admin_message( $version );
+			}
+			return;
+		}
+	}
+
+	// We weren't able to confirm the version, however we do know that it's installed and activated so we'll just hope for the best!
+}
