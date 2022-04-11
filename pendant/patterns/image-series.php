819871552bd350fd5b281884e190ff1690f8079d
Jason Crist <jcrist@pbking.com>
2022-04-11 11:52:56 -0400

    Tweaks to image series pattern to make mobile layout closer to design

--- pendant/patterns/image-series.php
+++ pendant/patterns/image-series.php
@@ -6,12 +6,12 @@
 ?>
 
 <!-- wp:group {"backgroundColor":"tertiary","layout":{"inherit":true}} -->
-<div class="wp-block-group has-tertiary-background-color has-background"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"60px","bottom":"60px"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide" style="padding-top:60px;padding-bottom:60px"><!-- wp:heading {"level":4} -->
-<h4><?php echo esc_html__( 'Our Picks', 'pendant' ); ?></h4>
+<div class="wp-block-group has-tertiary-background-color has-background"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"60px","bottom":"60px"},"blockGap":"100px"}},"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide" style="padding-top:60px;padding-bottom:60px"><!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="has-text-align-center"><?php echo esc_html__( 'Our Picks', 'pendant' ); ?></h4>
 <!-- /wp:heading -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"90px"}},"layout":{"type":"flex","justifyContent":"center"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"100px"}},"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/jewelery-1.png'; ?>" alt=""/></figure>
 <!-- /wp:image -->
