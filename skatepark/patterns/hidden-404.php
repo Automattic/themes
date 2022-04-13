f9387d04ed2c4cb1f0a0448c0a2ab81d2dd6de23
madhusudhand <madhusudhan.dollu@gmail.com>
2022-04-13 17:30:26 +0530

    Skatepark: refactor patterns

---
+++ skatepark/patterns/hidden-404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: 404 page
+ * Slug: skatepark/hidden-404
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"medium"} -->
+<h1 class="has-text-align-center has-medium-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( "Oops! That page can&rsquo;t be found.", "skatepark" ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( "It looks like nothing was found at this location. Maybe try a search?", "skatepark" ); ?></p>
+<!-- /wp:paragraph -->
