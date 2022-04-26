84dd6938ac3880493f859a79fd82705ea118f540
Jason Crist <jcrist@pbking.com>
2022-04-26 10:17:47 -0400

    Added padding to columns and heading

--- pendant/patterns/image-series.php
+++ pendant/patterns/image-series.php
@@ -5,27 +5,59 @@
  */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"20px","padding":{"top":"50px","bottom":"50px"}}},"backgroundColor":"tertiary","layout":{"inherit":true}} -->
-<div class="wp-block-group alignfull has-tertiary-background-color has-background" style="padding-top:50px;padding-bottom:50px"><!-- wp:heading {"textAlign":"center","level":4} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"40px","padding":{"top":"60px","bottom":"60px"}}},"backgroundColor":"tertiary","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-tertiary-background-color has-background" style="padding-top:60px;padding-bottom:60px"><!-- wp:heading {"textAlign":"center","level":4} -->
 <h4 class="has-text-align-center"><?php echo esc_html__( 'Our Picks', 'pendant' ); ?></h4>
 <!-- /wp:heading -->
 
 <!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center">
+	
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/jewelery-1.png'; ?>" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/jewelery-2.png'; ?>" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/jewelery-3.png'; ?>" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+
+
+
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"40px","padding":{"top":"60px","bottom":"60px"}}},"backgroundColor":"tertiary","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-tertiary-background-color has-background" style="padding-top:60px;padding-bottom:60px"><!-- wp:heading {"textAlign":"center","level":4} -->
+<h4 class="has-text-align-center">Our Picks</h4>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="http://localhost:4759/wp-content/themes/themes/pendant/assets/images/jewelery-1.png" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="http://localhost:4759/wp-content/themes/themes/pendant/assets/images/jewelery-2.png" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"40px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="http://localhost:4759/wp-content/themes/themes/pendant/assets/images/jewelery-3.png" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
