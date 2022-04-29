9d478bd3f2265e7e00ba2f87a416967e3df5f10c
Matias Benedetto <matias.benedetto@gmail.com>
2022-04-29 13:57:49 -0300

    updating missing separator

--- skatepark/patterns/mixed-media-in-container.php
+++ skatepark/patterns/mixed-media-in-container.php
@@ -67,9 +67,10 @@
 
 <!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:separator {"color":"primary","className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-background has-primary-background-color has-primary-color is-style-wide"/>
-<!-- /wp:separator --></div>
+<div class="wp-block-column">
+<!-- wp:separator {"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
 <!-- /wp:column -->
 
 <!-- wp:column -->
