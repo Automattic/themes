3d02d533fb7257ba9bb576f3b72c10326c253a92
madhusudhand <madhusudhan.dollu@gmail.com>
2022-05-11 13:19:14 +0530

    add archive template and posts-grid pattern

---
+++ curator/patterns/hidden-post-terms-and-date.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Tags and date
+ * Slug: curator/post-terms-and-date
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+<!-- wp:post-terms {"term":"post_tag"} /-->
+<!-- wp:paragraph -->
+<p>·</p>
+<!-- /wp:paragraph -->
+<!-- wp:post-date {"isLink":true} /-->
+</div>
+<!-- /wp:group -->
