49cc34af581dc50708ac2f516a214ffc5abbe8b7
Jeremy Yip <jeremy.yip@automattic.com>
2022-05-16 01:32:41 -0700

    Add customizer font deprecation notice when site is migrated

--- blockbase/inc/customizer/wp-customize-fonts.php
+++ blockbase/inc/customizer/wp-customize-fonts.php
@@ -331,11 +331,48 @@ class GlobalStylesFontsCustomizer {
 	);
 
 	function __construct() {
+		if ( get_option( 'blockbase_legacy_font_settings' )  ) {
+			add_action( 'customize_register', array( $this, 'init_deprecation_notice' ) );
+			return;
+		}
+
 		add_action( 'customize_register', array( $this, 'initialize' ) );
 		add_action( 'customize_preview_init', array( $this, 'handle_customize_preview_init' ) );
 		add_action( 'customize_register', array( $this, 'enqueue_google_fonts' ) );
 		add_action( 'customize_save_after', array( $this, 'handle_customize_save_after' ) );
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'customize_control_js' ) );
+	}
+
+	function init_deprecation_notice( $wp_customize ) {
+		$wp_customize->add_section(
+			$this->section_key,
+			array(
+				'capability'  => 'edit_theme_options',
+				'title'       => __( 'Fonts', 'blockbase' ),
+			)
+		);
+
+		$wp_customize->add_control(
+			$this->section_key . '-v1-blockbase-font-deprecation-notice',
+			array(
+				'type'        => 'hidden',
+				'description' => '<div class="notice notice-info">
+				<p>' . __( 'Updating fonts for this theme is now even easier! Use the site editor to select and preview different font families.', 'blockbase' ) . '</p>
+				</div>',
+				'settings'    => array(),
+				'section'     => $this->section_key,
+			)
+		);
+
+		$wp_customize->add_control(
+			$this->section_key . '-site-editor-button',
+			array(
+				'type'        => 'hidden',
+				'description' => sprintf( '<a class="button button-primary" href=%s style="font-style: normal;" >Use Site Editor</a>', esc_url( admin_url( 'site-editor.php' ) ) ),
+				'settings'    => array(),
+				'section'     => $this->section_key,
+			)
+		);
 	}
 
 	function handle_customize_preview_init( $wp_customize ) {
