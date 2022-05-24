e4ac1570ad5378eae5d674fabd9eb751b7813c44
Madhu Dollu <madhusudhan.dollu@gmail.com>
2022-05-24 10:49:07 +0530

    Update post-terms to category
    Co-authored-by: Jeff Ong <jonger4@gmail.com>

--- curator/patterns/hidden-post-terms-and-date.php
+++ curator/patterns/hidden-post-terms-and-date.php
@@ -10,6 +10,6 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group">
 <!-- wp:post-date {"isLink":true} /-->
-<!-- wp:post-terms {"term":"post_tag"} /-->
+<!-- wp:post-terms {"term":"category"} /-->
 </div>
 <!-- /wp:group -->
