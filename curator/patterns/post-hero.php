5a784142b0716da0c2602b17d9d5d5fa6772df20
Jeff Ong <jonger4@gmail.com>
2022-05-26 13:07:10 -0400

    Use cover within a cover hack, update opacity and height of the cover.

--- curator/patterns/post-hero.php
+++ curator/patterns/post-hero.php
@@ -7,12 +7,14 @@
 
 ?>
 
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":80,"minHeight":80,"minHeightUnit":"vh"} -->
-<div class="wp-block-cover" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:post-terms {"term":"post_tag","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"background","className":"curator-post-tags","fontSize":"medium"} /-->
+<!-- wp:cover {"overlayColor":"foreground","minHeightUnit":"vh","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-cover" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":40,"overlayColor":"foreground","minHeight":440,"minHeightUnit":"px"} -->
+<div class="wp-block-cover" style="min-height:440px"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-40 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"category","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontSize":"medium"} /-->
 
 <!-- wp:post-date {"textColor":"background","fontSize":"medium"} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:post-title {"textAlign":"center","level":1} /--></div></div>
+<!-- wp:post-title {"textAlign":"center","level":1,"textColor":"background"} /--></div></div>
+<!-- /wp:cover --></div></div>
 <!-- /wp:cover -->
