a9b4fc61ec4c3878599c237be3b660e1511320c6
Jeff Ong <jonger4@gmail.com>
2022-07-06 10:12:27 -0400

    Fix search button border color issue.

--- curator/patterns/hidden-search-form.php
+++ curator/patterns/hidden-search-form.php
@@ -10,4 +10,4 @@
 <!-- wp:heading {"textAlign":"center","level":1,"style":{"spacing":{"margin":{"bottom":"4rem"}}}} -->
 <h1 class="has-text-align-center" style="margin-bottom:4rem"><?php echo esc_html__( 'Search', 'curator' ); ?></h1>
 <!-- /wp:heading -->
-<!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html__( 'Search', 'curator' ); ?>","buttonUseIcon":true,"borderColor":"foreground"} /-->
+<!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html__( 'Search', 'curator' ); ?>","buttonUseIcon":true} /-->
