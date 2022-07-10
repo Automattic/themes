da530720760c6b4612ff72ae128c17096554c998
Dennis Snell <dennis.snell@automattic.com>
2022-07-10 17:47:38 +0200

    Add 404 template

---
+++ rainfall/patterns/hidden-404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: 404 content
+ * Slug: rainfall/hidden-404
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"align":"full","level":1,"backgroundColor":"light-orange"} -->
+<h1 class="alignfull has-light-orange-background-color has-background"><?php esc_html_e( 'Missing page', 'rainfall' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'The page you requested cannot be found; poor luck, eh?', 'rainfall' ); ?></p>
+<!-- /wp:paragraph -->
