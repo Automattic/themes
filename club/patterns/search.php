fcd50de3b0b268ed0073aa975c7e594409a969fa
Matias Benedetto <matias.benedetto@gmail.com>
2022-08-09 11:06:27 +0200

    Club:  move search block to a pattern

---
+++ club/patterns/search.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Search block
+ * Slug: club/search
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html__( 'Search...', 'club' ); ?>","buttonText":"<?php echo esc_html__( 'WHAT R U WAITING FOR?', 'club' ); ?>"} /-->
