0e6721d55c770ac49f8404b1d90c26f095179d34
alaczek <ola@bodera.com>
2022-08-16 17:17:03 +1000

    Lynx: Add a Link in Bio pattern with media on the right, and associated assets.

---
+++ lynx/patterns/link-in-bio-media-right.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Link in Bio with media on the right
+ * Slug: lynx/link-in-bio-media-right
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaId":5817,"mediaLink":"<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/man-holding-leaves.jpg'; ?>","mediaType":"image","isStackedOnMobile":false,"imageFill":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"black","textColor":"white"} -->
+<div class="wp-block-media-text alignfull has-media-on-the-right is-image-fill has-white-color has-black-background-color has-text-color has-background has-link-color"><div class="wp-block-media-text__content"><!-- wp:group {"style":{"spacing":{"blockGap":"30px","padding":{"top":"5vw","right":"4vw","bottom":"5vw","left":"4vw"}}}} -->
+<div class="wp-block-group" style="padding-top:5vw;padding-right:4vw;padding-bottom:5vw;padding-left:4vw">
+	<!-- wp:heading {"style":{"typography":{"lineHeight":"1","fontSize":"11rem","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"textColor":"background"} -->
+	<h2 class="has-background-color has-text-color" style="font-size:11rem;font-style:normal;font-weight:700;line-height:1;text-transform:uppercase"><?php echo  esc_html__( 'Biba', 'lynx' ); ?></h2>
+	<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'Hey! Thanks for stopping by. Check out my links if you like. You know, the future is now!', 'lynx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"black","width":100,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(34,193,195) 0%,rgb(224,253,45) 100%)"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(34,193,195) 0%,rgb(224,253,45) 100%)"><strong><?php echo  esc_html__( 'Soundcloud', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(255,231,102) 0%,rgb(255,94,98) 100%)","text":"#960c0d"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(255,231,102) 0%,rgb(255,94,98) 100%);color:#960c0d"><strong><?php echo  esc_html__( 'Bandcamp', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(12,235,235) 0%,rgb(32,227,178) 49%,rgb(41,255,198) 100%)","text":"#0b5340"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(12,235,235) 0%,rgb(32,227,178) 49%,rgb(41,255,198) 100%);color:#0b5340"><strong><?php echo  esc_html__( 'Hey! Thanks for stopping by.', 'lynx' ); ?>Spotify</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(100,207,241) 0%,rgb(122,103,241) 100%)","text":"#142547"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(100,207,241) 0%,rgb(122,103,241) 100%);color:#142547"><strong><?php echo  esc_html__( 'YouTube', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(175,109,252) 0%,rgb(238,140,198) 51%,rgb(252,229,161) 100%)","text":"#1c006d"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(175,109,252) 0%,rgb(238,140,198) 51%,rgb(252,229,161) 100%);color:#1c006d"><strong><?php echo  esc_html__( 'Instagram', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div><figure class="wp-block-media-text__media" style="background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/man-holding-leaves.jpg'; ?>);background-position:50% 50%"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/man-holding-leaves.jpg'; ?>" alt="" class="wp-image-5817 size-full"/></figure></div>
+<!-- /wp:media-text -->
