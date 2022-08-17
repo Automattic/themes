0e868cd1b4e2f8d9c5e4d21e52d019f01446f4ed
alaczek <ola@bodera.com>
2022-08-17 10:55:48 +1000

    Lynx: Add a Link in Bio pattern with background image, and associated assets.

---
+++ lynx/patterns/link-in-bio-background-image.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Link in Bio with background image
+ * Slug: lynx/link-in-bio-background-image
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/mountain-sunrise.jpg'; ?>","id":5826,"hasParallax":true,"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light has-parallax" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background wp-image-5826 has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/mountain-sunrise.jpg'; ?>)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"border":{"radius":"8px"}},"layout":{"inherit":false,"contentSize":"440px"}} -->
+<div class="wp-block-group" style="border-radius:8px"><!-- wp:group {"style":{"border":{"radius":"8px"},"color":{"text":"#082167"},"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}}},"backgroundColor":"white"} -->
+<div class="wp-block-group has-white-background-color has-text-color has-background" style="border-radius:8px;color:#082167;padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px"><!-- wp:image {"align":"center","width":120,"height":120,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-large is-resized is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/woman-with-red-hair.jpg'; ?>" alt="" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<h2 class="has-text-align-center" style="font-style:normal;font-weight:700"><?php echo  esc_html__( 'Emily Jennings', 'lynx' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php echo  esc_html__( 'Leadership coaching for leaders.', 'lynx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"customIconBackgroundColor":"#082167","iconBackgroundColorValue":"#082167","className":"is-style-default","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":{"top":"10px","left":"10px"}}}} -->
+<ul class="wp-block-social-links has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"tiktok"} /-->
+
+<!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"youtube"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
