5c1ca2127b683c372d2c13bb54bdb18cc2ba990d
alaczek <ola@bodera.com>
2022-08-17 13:03:38 +1000

    Lynx: Add a Link in Bio pattern with mesh gradient background, and associated assets.

---
+++ lynx/patterns/link-in-bio-gradient.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Title: Link in Bio with a gradient background
+ * Slug: lynx/link-in-bio-gradient
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/mesh_gradient.jpg'; ?>","hasParallax":true,"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light has-parallax" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/mesh_gradient.jpg'; ?>)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"20px"}}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","width":120,"height":120,"sizeSlug":"large","linkDestination":"none","style":{"border":{"width":"2px"}},"className":"has-custom-border is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-large is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/colorful-flower.jpg'; ?>" alt="" style="border-width:2px" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"1.125rem"}}} -->
+<h2 class="has-text-align-center" style="font-size:1.125rem;font-style:normal;font-weight:700"><?php echo  esc_html__( 'Color Burst', 'lynx' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><?php echo  esc_html__( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'lynx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"background","textColor":"black","width":100,"style":{"border":{"radius":"100px"},"typography":{"textTransform":"uppercase"},"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}}},"className":"is-style-outline","fontSize":"small"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size is-style-outline has-small-font-size" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-background-background-color has-text-color has-background wp-element-button" style="border-radius:100px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><strong><?php echo  esc_html__( 'Read the Blog', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"background","textColor":"black","width":100,"style":{"border":{"radius":"100px"},"typography":{"textTransform":"uppercase"},"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}}},"className":"is-style-outline","fontSize":"small"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size is-style-outline has-small-font-size" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-background-background-color has-text-color has-background wp-element-button" style="border-radius:100px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><strong><?php echo  esc_html__( 'YouTube Channel', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"background","textColor":"black","width":100,"style":{"border":{"radius":"100px"},"typography":{"textTransform":"uppercase"},"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}}},"className":"is-style-outline","fontSize":"small"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size is-style-outline has-small-font-size" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-background-background-color has-text-color has-background wp-element-button" style="border-radius:100px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><strong><?php echo  esc_html__( 'Latest Podcast Episode', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"background","textColor":"black","width":100,"style":{"border":{"radius":"100px"},"typography":{"textTransform":"uppercase"},"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}}},"className":"is-style-outline","fontSize":"small"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 has-custom-font-size is-style-outline has-small-font-size" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-background-background-color has-text-color has-background wp-element-button" style="border-radius:100px;padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px"><strong><?php echo  esc_html__( 'Merch store', 'lynx' ); ?></strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","iconBackgroundColorValue":"#ffffff","className":"is-style-default","layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+
+<!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
