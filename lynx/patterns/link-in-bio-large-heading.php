ef68122e43e05b4312dc1119acf0068a23d6c01d
alaczek <ola@bodera.com>
2022-08-17 12:35:54 +1000

    Lynx: Add a Link in Bio pattern with a large heading.

---
+++ lynx/patterns/link-in-bio-large-heading.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Title: Link in Bio with a large heading
+ * Slug: lynx/link-in-bio-large-heading
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:cover {"customOverlayColor":"#502b7f","minHeight":100,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#502b7f"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false,"contentSize":"850px"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"color":{"text":"#ed6e69"},"typography":{"fontStyle":"normal","fontWeight":"900","fontSize":"128px"}}} -->
+<p class="has-text-align-left has-text-color" style="color:#ed6e69;font-size:128px;font-style:normal;font-weight:900"><?php echo  esc_html__( 'Ella D.', 'lynx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"style":{"color":{"text":"#ffffffbf"}},"fontSize":"small"} -->
+<p class="has-text-color has-small-font-size" style="color:#ffffffbf"><?php echo  esc_html__( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.', 'lynx' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'buy merch', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'support on patreon', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'buy me coffee', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'instagram', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'twitter', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"small"} -->
+<p class="has-link-color has-small-font-size"><a href="#">→ <?php echo  esc_html__( 'youtube', 'lynx' ); ?></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
