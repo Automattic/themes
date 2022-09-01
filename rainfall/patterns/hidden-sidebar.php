88cc468074d67e7b2cc0278fdcb70fd2f4fc32b8
Sarah Norris <sarah@sekai.co.uk>
2022-09-01 13:46:07 +0100

    Make sidebar translatable

---
+++ rainfall/patterns/hidden-sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Sidebar content
+ * Slug: rainfall/hidden-sidebar
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:search {"label":"<?php esc_html_e( 'Search for a topic', 'rainfall' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'rainfall' ); ?>"} /-->
+
+<!-- wp:heading {"level":6} -->
+<h6><?php esc_html_e( 'Submissions', 'rainfall' ); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'Would you like to contribute as an editor or a writer to our blog? Let us know all the details about yourself and send us a message.', 'rainfall' ); ?></p>
+<!-- /wp:paragraph -->
