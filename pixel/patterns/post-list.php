c48daa6b85598caae60b42a25c1f40659b275d66
Jeff Ong <jonger4@gmail.com>
2022-09-06 11:45:20 -0400

    Use spacing preset for padding.

--- pixel/patterns/post-list.php
+++ pixel/patterns/post-list.php
@@ -9,8 +9,8 @@
 
 <!-- wp:query {"query":{"perPage":"10","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"tagName":"main","displayLayout":{"type":"list","columns":3},"layout":{"type":"constrained"}} -->
 <main class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:group {"layout":{"type":"constrained"},"style":{"border":{"width":"2px"},"spacing":{"padding":{"top":"33px","right":"33px","bottom":"33px","left":"33px"}}},"borderColor":"primary","className":"pixel-shadow"} -->
-<div class="wp-block-group pixel-shadow has-border-color has-primary-border-color" style="border-width:2px;padding-top:33px;padding-right:33px;padding-bottom:33px;padding-left:33px"><!-- wp:post-date {"style":{"typography":{"textTransform":"uppercase","fontSize":"12px"}}} /-->
+<!-- wp:group {"layout":{"type":"constrained"},"style":{"border":{"width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"borderColor":"primary","className":"pixel-shadow"} -->
+<div class="wp-block-group pixel-shadow has-border-color has-primary-border-color" style="border-width:2px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:post-date {"style":{"typography":{"textTransform":"uppercase","fontSize":"12px"}}} /-->
 
 <!-- wp:post-title {"isLink":true} /-->
 
