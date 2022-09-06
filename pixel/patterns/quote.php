a06d736d7bd5f328d335173e3cbca49980956d1a
Jeff Ong <jonger4@gmail.com>
2022-09-06 13:46:38 -0400

    add quote pattern.

---
+++ pixel/patterns/quote.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: A Pixel Quote
+ * Slug: pixel/quote
+ * Categories: Featured, Text
+ */
+
+?>
+
+<!-- wp:pullquote {"style":{"border":{"width":"2px"},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.4","letterSpacing":"-0.04em","fontSize":"1.25rem"}},"borderColor":"primary","className":"pixel-shadow"} -->
+<figure class="wp-block-pullquote pixel-shadow has-border-color has-primary-border-color" style="border-width:2px;font-size:1.25rem;font-style:normal;font-weight:400;letter-spacing:-0.04em;line-height:1.4"><blockquote><p><?php echo esc_html__( '"The pain itself is love, the main storage system. Aenean dapibus nisi quis dolor scelerisque egestas. Members of the mass need to feel comfortable and easy. It is important that the lion should invest."', 'pixel' ) ?></p><cite></cite></blockquote></figure>
+<!-- /wp:pullquote -->
