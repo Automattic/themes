fc204eb3c1a20e5c7fadbdfa96ec3cfea35c3935
alaczek <ola@bodera.com>
2022-09-07 14:51:11 +1000

    Lynx: Update design of the media on the right pattern.

---
+++ lynx/patterns/link-in-bio-gradient-buttons.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: Link in Bio with gradient buttons
+ * Slug: lynx/link-in-bio-gradient-buttons
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:cover {"overlayColor":"black","minHeight":100,"minHeightUnit":"vh","align":"full","style":{"spacing":{"padding":{"top":"4vw","right":"4vw","bottom":"4vw","left":"4vw"}}}} -->
+<div class="wp-block-cover alignfull" style="padding-top:4vw;padding-right:4vw;padding-bottom:4vw;padding-left:4vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"20px","margin":{"bottom":"0"}}}} -->
+<div class="wp-block-group" style="margin-bottom:0"><!-- wp:site-logo {"width":130,"isLink":false,"align":"center","className":"is-style-rounded"} /-->
+
+<!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"textTransform":"uppercase","fontSize":"69px","lineHeight":"1","fontStyle":"normal","fontWeight":"700"}},"textColor":"background"} /-->
+
+<!-- wp:site-tagline {"textAlign":"center","textColor":"background","fontSize":"x-small"} /-->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"vertical"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"black","width":75,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(34,193,195) 0%,rgb(224,253,45) 100%)"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75" style="text-transform:uppercase"><a class="wp-block-button__link has-black-color has-text-color has-background wp-element-button" style="border-radius:100px;background:linear-gradient(135deg,rgb(34,193,195) 0%,rgb(224,253,45) 100%);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><strong>Soundcloud</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":75,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(255,231,102) 0%,rgb(255,94,98) 100%)","text":"#960c0d"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;color:#960c0d;background:linear-gradient(135deg,rgb(255,231,102) 0%,rgb(255,94,98) 100%);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><strong>Bandcamp</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":75,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(12,235,235) 0%,rgb(32,227,178) 49%,rgb(41,255,198) 100%)","text":"#0b5340"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;color:#0b5340;background:linear-gradient(135deg,rgb(12,235,235) 0%,rgb(32,227,178) 49%,rgb(41,255,198) 100%);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><strong>Spotify</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":75,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(100,207,241) 0%,rgb(122,103,241) 100%)","text":"#142547"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;color:#142547;background:linear-gradient(135deg,rgb(100,207,241) 0%,rgb(122,103,241) 100%);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><strong>YouTube</strong></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":75,"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"100px"},"color":{"gradient":"linear-gradient(135deg,rgb(175,109,252) 0%,rgb(238,140,198) 51%,rgb(252,229,161) 100%)","text":"#1c006d"},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-75" style="text-transform:uppercase"><a class="wp-block-button__link has-text-color has-background wp-element-button" style="border-radius:100px;color:#1c006d;background:linear-gradient(135deg,rgb(175,109,252) 0%,rgb(238,140,198) 51%,rgb(252,229,161) 100%);padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><strong>Instagram</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
