b9fc0271f4cf312e7c8b930861df4609ca5fcc16
Jeff Ong <jonger4@gmail.com>
2022-09-09 03:17:02 -0400

    Rename theme to Pixl. (#6525)

--- pixl/functions.php
+++ pixl/functions.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Pixel functions and definitions
+ * Pixl functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Pixel
- * @since Pixel 1.0
+ * @package Pixl
+ * @since Pixl 1.0
  */
 
 
-if ( ! function_exists( 'pixel_support' ) ) :
+if ( ! function_exists( 'pixl_support' ) ) :
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @since Pixel 1.0
+	 * @since Pixl 1.0
 	 *
 	 * @return void
 	 */
-	function pixel_support() {
+	function pixl_support() {
 
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
@@ -27,32 +27,32 @@ if ( ! function_exists( 'pixel_support' ) ) :
 
 endif;
 
-add_action( 'after_setup_theme', 'pixel_support' );
+add_action( 'after_setup_theme', 'pixl_support' );
 
-if ( ! function_exists( 'pixel_styles' ) ) :
+if ( ! function_exists( 'pixl_styles' ) ) :
 
 	/**
 	 * Enqueue styles.
 	 *
-	 * @since Pixel 1.0
+	 * @since Pixl 1.0
 	 *
 	 * @return void
 	 */
-	function pixel_styles() {
+	function pixl_styles() {
 
 		// Register theme stylesheet.
 		wp_register_style(
-			'pixel-style',
+			'pixl-style',
 			get_template_directory_uri() . '/style.css',
 			array(),
 			wp_get_theme()->get( 'Version' )
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'pixel-style' );
+		wp_enqueue_style( 'pixl-style' );
 
 	}
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'pixel_styles' );
+add_action( 'wp_enqueue_scripts', 'pixl_styles' );
---
+++ pixl/patterns/quote.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: A Pixl Quote
+ * Slug: pixl/quote
+ * Categories: featured, text
+ */
+
+?>
+
+<!-- wp:pullquote {"style":{"border":{"width":"2px"},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.4","letterSpacing":"-0.02em","fontSize":"1.25rem"}},"borderColor":"primary","className":"pixl-shadow"} -->
+<figure class="wp-block-pullquote pixl-shadow has-border-color has-primary-border-color" style="border-width:2px;font-size:1.25rem;font-style:normal;font-weight:400;letter-spacing:-0.02em;line-height:1.4"><blockquote><p><?php echo esc_html__( '"The pain itself is love, the main storage system. Aenean dapibus nisi quis dolor scelerisque egestas. Members of the mass need to feel comfortable and easy. It is important that the lion should invest."', 'pixl' ) ?></p><cite><?php echo esc_html__( 'Unknown', 'pixl' ) ?></cite></blockquote></figure>
+<!-- /wp:pullquote -->
