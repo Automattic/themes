38d4fda596367822f6a016055c11518a970437d1
Jason Crist <jcrist@pbking.com>
2022-09-13 11:25:55 -0400

    Create/decibel (#6542)
    * Update Block Canvas settings to be a more generic starting point. Eliminated custom gap values (replaced with --preset--spacing), eliminated redundant (and some opinionated) block settings
    
    * Added Decibel as a (for NOW) child theme of Block Canvas with (only a few) style opinions added

---
+++ decibel/functions.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Decibel functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Decibel
+ * @since Decibel 1.0
+ */
+
+
+if ( ! function_exists( 'decibel_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Decibel 1.0
+	 *
+	 * @return void
+	 */
+	function decibel_support() {
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'decibel_support' );
+
+if ( ! function_exists( 'decibel_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Decibel 1.0
+	 *
+	 * @return void
+	 */
+	function decibel_styles() {
+
+		// Register theme stylesheet.
+		wp_register_style(
+			'decibel-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			wp_get_theme()->get( 'Version' )
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'decibel-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'decibel_styles' );
