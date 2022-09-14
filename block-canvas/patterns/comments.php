867931eeb86802832f7d083b25eeab0b769ce7fc
Jeff Ong <jonger4@gmail.com>
2022-09-14 09:04:29 -0700

    Use em values instead of px.

--- block-canvas/patterns/comments.php
+++ block-canvas/patterns/comments.php
@@ -12,8 +12,8 @@
 
 <!-- wp:comment-template -->
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"10px"}}} -->
-<div class="wp-block-group"><!-- wp:avatar {"size":40,"style":{"spacing":{"margin":{"top":"10px"}}}} /-->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"},"style":{"spacing":{"blockGap":"0.5em"}}} -->
+<div class="wp-block-group"><!-- wp:avatar {"size":40,"style":{"spacing":{"margin":{"top":"0.5em"}}}} /-->
 
 <!-- wp:group -->
 <div class="wp-block-group"><!-- wp:comment-author-name /-->
