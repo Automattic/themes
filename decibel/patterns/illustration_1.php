a8521fad8e0b52b197a70c95e9cea741cc14094e
Jason Crist <jcrist@pbking.com>
2022-09-19 21:22:50 -0400

    Switched from SVG to jpg assets as SVG isn't allowed in this way.  Jpg assets forthcoming (#6595)

--- decibel/patterns/illustration_1.php
+++ decibel/patterns/illustration_1.php
@@ -7,6 +7,6 @@
 ?>
 <!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full">
-	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration_1.svg'; ?>" />
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration_1.jpg'; ?>" />
 </figure>
 <!-- /wp:image -->
