6a68324647ba4c48d25286417c729b0131265986
alaczek <ola@bodera.com>
2022-09-20 13:04:19 +1000

    Lynx: Adding 'Gigantic' font size and updating patterns to use it instead of a custom font size.

--- lynx/patterns/link-in-bio-large-heading-columns.php
+++ lynx/patterns/link-in-bio-large-heading-columns.php
@@ -11,7 +11,7 @@
 <div class="wp-block-cover alignfull is-light has-parallax" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div role="img" class="wp-block-cover__image-background has-parallax" style="background-position:50% 50%;background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/topology_bg.jpg'; ?>)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":false,"contentSize":"940px","type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
-<div class="wp-block-column" style="flex-basis:100%"><!-- wp:site-title {"isLink":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"11rem","fontStyle":"normal","fontWeight":"300","lineHeight":"0.9"}},"textColor":"white"} /--></div>
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:site-title {"isLink":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"0.9"}},"textColor":"white","fontSize":"xxx-large"} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
--- lynx/patterns/link-in-bio-large-heading.php
+++ lynx/patterns/link-in-bio-large-heading.php
@@ -7,10 +7,13 @@
  */
 ?>
 
-
 <!-- wp:cover {"customOverlayColor":"#502b7f","minHeight":100,"minHeightUnit":"vh","align":"full"} -->
-<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#502b7f"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":false,"contentSize":"850px","type":"constrained"},"style":{"spacing":{"blockGap":"0px"}}} -->
-<div class="wp-block-group"><!-- wp:site-title {"isLink":false,"style":{"color":{"text":"#ed6e69"},"typography":{"fontSize":"128px","fontStyle":"normal","fontWeight":"900","lineHeight":"1"}}} /-->
+<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#502b7f"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"inherit":false,"contentSize":"940px","type":"constrained"},"style":{"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:site-title {"isLink":false,"style":{"color":{"text":"#ed6e69"},"typography":{"fontStyle":"normal","fontWeight":"900","lineHeight":"1"}},"fontSize":"xxx-large"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
 
 <!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
 <div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"width":"50%","style":{"spacing":{"blockGap":"10px"}}} -->
