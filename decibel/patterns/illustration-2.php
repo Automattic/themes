e935cbbe4e9b56c79ebd24d37934862f44b027d6
Jason Crist <jcrist@pbking.com>
2022-09-19 21:31:19 -0400

    Fixed filenames of resources that were not up to standards.

---
+++ decibel/patterns/illustration-2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Illustration 2
+ * Slug: decibel/illustration_b2
+ * Categories: illustrations
+ */
+?>
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration_2.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
