4d1897d2a5ae1fd0269b6b959f6ec1ce82a6b0f9
alaczek <ola@bodera.com>
2022-09-21 17:41:12 +1000

    Lynx: Add a basic link in bio pattern.

---
+++ lynx/patterns/link-in-bio-basic.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: Basic Link in Bio pattern
+ * Slug: lynx/link-in-bio-basic
+ * Inserter: yes
+ * Categories: featured, link-in-bio
+ */
+?>
+
+<!-- wp:cover {"overlayColor":"background","minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"blockGap":"var:preset|spacing|20"}}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"align":"center","className":"is-style-rounded"} /-->
+
+<!-- wp:site-title {"textAlign":"center","isLink":false,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"x-large"} /-->
+
+<!-- wp:site-tagline {"textAlign":"center","fontSize":"small"} /-->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button"><?php echo  esc_html__( 'Must read: The Place That Made Me', 'lynx' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button"><?php echo  esc_html__( 'Rain Journal by Richard Johnson', 'lynx' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button"><?php echo  esc_html__( '10th Day of Rain', 'lynx' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button"><?php echo  esc_html__( 'Blurring the Lines Between Architecture and Nature', 'lynx' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button"><?php echo  esc_html__( 'Instant Classic: A story of Persistence', 'lynx' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
