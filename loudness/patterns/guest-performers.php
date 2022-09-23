89eef77da3bfb18dbdfe6f90055a5e33a2ba2745
Jason Crist <jcrist@pbking.com>
2022-09-23 09:55:25 -0400

    Adding guest performers pattern

---
+++ loudness/patterns/guest-performers.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Guest Performers
+ * Slug: loudness/guest-performers
+ * Categories: featured
+ */
+?>
+<!-- wp:heading -->
+<h2><?php echo esc_html__( 'Guest Performers', 'loudness' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+	
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large">
+		<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-1.jpg'; ?>" alt="" />
+		<figcaption class="wp-element-caption">Spicy Little Wings</figcaption>
+	</figure>
+	<!-- /wp:image -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
+	<figure class="wp-block-image size-large">
+		<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-2.jpg'; ?>" alt="" />
+		<figcaption class="wp-element-caption">DJ Vert</figcaption>
+	</figure>
+	<!-- /wp:image -->
+</div>
+<!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
