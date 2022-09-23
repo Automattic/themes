2d1caf9a7955764717fc91e1907199e5ef6b605d
Jason Crist <jcrist@pbking.com>
2022-09-23 09:31:51 -0400

    Corrected the location of loudness' svg illustration assets (#6618)

--- loudness/patterns/illustration-1.php
+++ loudness/patterns/illustration-1.php
@@ -7,6 +7,6 @@
 ?>
 <!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full">
-	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration-1.jpg'; ?>" />
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration-1.svg'; ?>" />
 </figure>
 <!-- /wp:image -->
