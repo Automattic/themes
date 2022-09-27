c113326bdec98ecce983f94ef8d868cf3583906f
Jason Crist <jcrist@pbking.com>
2022-09-27 15:15:53 -0400

    Adding the instructors pattern (#6637)

---
+++ loudness/patterns/instructors.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Title: Instructors
+ * Slug: loudness/instructors
+ * Categories: featured
+ */
+?>
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"var:preset|spacing|60","left":"0"}}}} -->
+<h2 style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--60);margin-left:0">
+	<?php echo esc_html__( 'Instructors', 'loudness' ); ?>
+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-rounded"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-rounded">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-1.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Craig Finley</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Artist</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-default"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-default">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-2.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Damien Sloan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Artist and Producer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-default"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-default">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-3.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Nico Gardner</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Audio Engineer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-default"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-default">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-4.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Axexis Irvine</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Synth Designer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-rounded"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-rounded">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-5.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Elliot Boyd</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Artist</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-default"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-default">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-6.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Maddy Grey</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Artist and Producer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-rounded"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-rounded">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-7.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Ahmad Arce</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Marketer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"5px"}}} -->
+<div class="wp-block-column"><!-- wp:columns {"isStackedOnMobile":false} -->
+<div class="wp-block-columns is-not-stacked-on-mobile"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"5px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":31,"sizeSlug":"large","linkDestination":"none","className":"is-style-skatepark-aside-caption is-style-default"} -->
+<figure class="wp-block-image size-large is-style-skatepark-aside-caption is-style-default">
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/instructor-8.jpg'; ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase","letterSpacing":"2px"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-style:normal;font-weight:700;letter-spacing:2px;text-transform:uppercase">Mac Pace</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Producer</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
