ea6b28a23cc7c5fc0dbe9efb790683d0df4d9f0a
Takashi Irie <takashi@automattic.com>
2022-10-06 18:11:59 +0100

    Masu: Initial commit

---
+++ masu/patterns/404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: A 404 page
+ * Slug: masu/404
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"level":1,"fontSize":"normal"} -->
+<h1 class="has-normal-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'masu' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'masu' ); ?></p>
+<!-- /wp:paragraph -->
