ea4911db775188ecf565fae439a30d361a026d66
Jason Crist <jcrist@pbking.com>
2022-10-17 11:27:42 -0400

    Added load_theme_textdomain call to block canvas

--- block-canvas/functions.php
+++ block-canvas/functions.php
@@ -23,6 +23,8 @@ if ( ! function_exists( 'block_canvas_support' ) ) :
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
 
+		// Make theme available for translation.
+		load_theme_textdomain( 'block-canvas' );
 	}
 
 endif;
