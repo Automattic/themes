cb04056f1f8f0be12e85c4a335299107d107ab2c
Jeff Ong <jonger4@gmail.com>
2022-10-26 11:32:52 -0400

    Add alt text and escape text.

--- loudness/patterns/guest-performers.php
+++ loudness/patterns/guest-performers.php
@@ -16,16 +16,16 @@
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large">
-	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-1.jpg'; ?>" alt="" />
-	<figcaption class="wp-element-caption">Spicy Little Wings</figcaption></figure>
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-1.jpg'; ?>" alt="<?php echo esc_attr__( 'Headshot of woman wearing a green top and sunglasses, looking in the distance.', 'loudness' ); ?>"/>
+	<figcaption class="wp-element-caption"><?php echo esc_html__( 'Spicy Little Wings', 'loudness' ); ?></figcaption></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:image {"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large">
-	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-2.jpg'; ?>" alt="" />
-	<figcaption class="wp-element-caption">DJ Vert</figcaption></figure>
+	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/guest-2.jpg'; ?>" alt="<?php echo esc_attr__( 'Headshot of man wearing black sunglasses looking into the camera.', 'loudness' ); ?>"/>
+	<figcaption class="wp-element-caption"><?php echo esc_html__( 'DJ Vert', 'loudness' ); ?></figcaption></figure>
 <!-- /wp:image -->
 
 </div>
