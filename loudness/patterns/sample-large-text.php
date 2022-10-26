d483fe9608ec8f14078ddc68a25a2e3537d76dc1
Jeff Ong <jonger4@gmail.com>
2022-10-26 11:29:22 -0400

    Escape text.

--- loudness/patterns/sample-large-text.php
+++ loudness/patterns/sample-large-text.php
@@ -10,7 +10,7 @@
 <div class="wp-block-group alignfull">
 
 <!-- wp:paragraph {"fontSize":"x-large"} -->
-<p class="has-x-large-font-size">On September 25th, we’re hosting a day-long workshop for beginner musicians, existing artists transitioning into electronic music, or anyone curious about how to begin with electronic music production.</p>
+<p class="has-x-large-font-size"><?php echo esc_html__( 'On September 25th, we\'re hosting a day-long workshop for beginner musicians, existing artists transitioning into electronic music, or anyone curious about how to begin with electronic music production.', 'loudness' ); ?></p>
 <!-- /wp:paragraph -->
 
 </div>
