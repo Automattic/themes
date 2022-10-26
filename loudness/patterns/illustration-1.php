d9ad12bda81ced73df435211833187daf39ca502
Jason Crist <jcrist@pbking.com>
2022-10-26 11:14:31 -0400

    Add home template with content from design and refine patterns used in home template.

--- loudness/patterns/illustration-1.php
+++ loudness/patterns/illustration-1.php
@@ -5,8 +5,8 @@
  * Categories: illustrations
  */
 ?>
-<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full">
+<!-- wp:image {"align":"full","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image alignfull size-full">
 	<img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/illustrations/illustration-1.svg'; ?>" />
 </figure>
 <!-- /wp:image -->
---
+++ loudness/patterns/sample-large-text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Sample Large Text
+ * Slug: loudness/sample_large_text
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull">
+
+<!-- wp:paragraph {"fontSize":"x-large"} -->
+<p class="has-x-large-font-size">On September 25th, we’re hosting a day-long workshop for beginner musicians, existing artists transitioning into electronic music, or anyone curious about how to begin with electronic music production.</p>
+<!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group -->
