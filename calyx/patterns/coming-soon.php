facf50a71f02b38ad6988dde3981fef21e286b25
alaczek <ola@bodera.com>
2022-10-28 10:56:33 +1100

    Calyx: Initial commit.

---
+++ calyx/patterns/coming-soon.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Coming Soon
+ * Slug: calyx/coming-soon
+ * Inserter: yes
+ */
+?>
+
+
+<!-- wp:cover {"url":"https://dotcompatterns.files.wordpress.com/2021/04/pattern-food.jpeg","id":3761,"dimRatio":0,"overlayColor":"primary","focalPoint":{"x":"1.00","y":"0.51"},"minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-3761" alt="" src="https://dotcompatterns.files.wordpress.com/2021/04/pattern-food.jpeg" style="object-position:100% 51%" data-object-fit="cover" data-object-position="100% 51%"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"65%"} -->
+<div class="wp-block-column" style="flex-basis:65%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"35%","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-link-color" style="flex-basis:35%"><!-- wp:paragraph -->
+<p><strong><?php echo esc_html__( 'OAK & ANCHOR', 'calyx' ); ?></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"250px"} -->
+<div style="height:250px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"64px","lineHeight":"1.1"}}} -->
+<h2 style="font-size:64px;line-height:1.1"><strong><?php echo esc_html__( 'Coming Soon', 'calyx' ); ?></strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'A new restaurant featuring farm-to-table dining.', 'calyx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><?php echo esc_html__( '1234 S Main Street', 'calyx' ); ?><br><?php echo esc_html__( 'Townsville, MA, 01010', 'calyx' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><a href="#"><?php echo esc_html__( 'info@example.com', 'calyx' ); ?><br></a>(123) 456-7890</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#fdfdfd","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"yelp"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
---
+++ calyx/patterns/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: calyx/footer
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
+	<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px">
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center has-small-font-size">
+			<?php
+			printf(
+				/* Translators: WordPress.com link. */
+				esc_html__( 'Proudly Powered by %s', 'calyx' ),
+				'<a href="' . esc_url( __( 'https://wordpress.com', 'calyx' ) ) . '" rel="nofollow">WordPress.com</a>'
+				);
+			?>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
