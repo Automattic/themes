a0c640a3b9ea9a766aef64e16d1f776297a1cc7e
Jason Crist <jcrist@pbking.com>
2022-10-28 11:42:51 -0400

    Removed unrelated videomaker changes

--- videomaker/functions.php
+++ videomaker/functions.php
@@ -3,14 +3,14 @@
 /**
  * Add Editor Styles
  */
-function videomaker_support() {
+function videomaker_editor_styles() {
 	// Primary navigation is used on the header and the footer pattern
 	register_nav_menus(
 		array(
 			'primary' => __( 'Primary Navigation', 'videomaker' ),
-			'social'  => __( 'Social Navigation', 'videomaker' ),
-			'footer'  => __( 'Footer Navigation', 'videomaker' ),
+			'social' => __( 'Social Navigation', 'videomaker' ),
+			'footer' => __( 'Footer Navigation', 'videomaker' )
 		)
 	);
 }
-add_action( 'after_setup_theme', 'videomaker_support' );
+add_action( 'after_setup_theme', 'videomaker_editor_styles' );
