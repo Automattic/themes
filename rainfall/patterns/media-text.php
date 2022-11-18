4deb4065f4c3c718a8e25d406f7e28f5ff1a3bd3
Sarah Norris <sarah@sekai.co.uk>
2022-11-18 13:45:49 +0000

    Update media and text pattern

--- rainfall/patterns/media-text.php
+++ rainfall/patterns/media-text.php
@@ -10,7 +10,7 @@
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:cover {"overlayColor":"light-orange","minHeight":80,"minHeightUnit":"vh","contentPosition":"bottom left","isDark":false,"style":{"spacing":{"padding":{"right":"30%","left":"5%"}}}} -->
 <div class="wp-block-cover is-light has-custom-content-position is-position-bottom-left" style="padding-right:30%;padding-left:5%;min-height:80vh"><span aria-hidden="true" class="wp-block-cover__background has-light-orange-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"level":4} -->
-<h4><?php echo esc_html__( '"Professedly he had not seen them since the beginning of his hostage; but every year about the end of the rains and before the strength of the sun had come upon us from the south."', 'rainfall' ); ?></h4>
+<h4><?php echo esc_html__( '"Every year about the end of the rains and before the strength of the sun had come upon us, we\'d notice the city glow."', 'rainfall' ); ?></h4>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
