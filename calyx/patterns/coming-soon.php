ba073a2c4bdc4d065f887660f77e32b6fe524cf4
alaczek <ola@bodera.com>
2022-11-28 09:50:48 +1100

    Calyx: Initial commit. (#6704)
    * Calyx: Initial commit.
    
    * Calyx: Add image assets.
    
    * Calyx: Update homepage pattern to use included image assets.
    
    * Calyx: Remove unused code.
    
    * Calyx: Update header spacing.
    
    * Calyx: Removing a bunch of templates, as this is meant to be a simple single-page, single-purpose theme.
    
    * Calyx: Add readme.txt file.
    
    * Calyx: Update index template to display full post/page content.

---
+++ calyx/patterns/coming-soon.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Title: Coming Soon
+ * Slug: calyx/coming-soon
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/pattern-food.jpeg","dimRatio":0,"overlayColor":"primary","focalPoint":{"x":"1.00","y":"0.51"},"minHeight":100,"minHeightUnit":"vh","align":"full"} -->
+<div class="wp-block-cover alignfull" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/pattern-food.jpeg" style="object-position:100% 51%" data-object-fit="cover" data-object-position="100% 51%"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"65%"} -->
+<div class="wp-block-column" style="flex-basis:65%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"35%","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-link-color" style="flex-basis:35%"><!-- wp:paragraph -->
+<p><strong><?php echo esc_html__( 'OAK & ANCHOR', 'calyx' ); ?></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"250px"} -->
+<div style="height:250px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"64px","lineHeight":"1.1"}}} -->
+<h2 style="font-size:64px;line-height:1.1"><strong><?php echo esc_html__( 'Coming Soon', 'calyx' ); ?></strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'A new restaurant featuring farm-to-table dining.', 'calyx' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><?php echo esc_html__( '1234 S Main Street', 'calyx' ); ?><br><?php echo esc_html__( 'Townsville, MA, 01010', 'calyx' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><a href="#"><?php echo esc_html__( 'info@example.com', 'calyx' ); ?><br></a>(123) 456-7890</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#fdfdfd","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"yelp"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
