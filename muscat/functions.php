2d5948d4e2da7efd7ded16600a6fd67a8bec4652
Jeff Ong <jonger4@gmail.com>
2022-11-29 11:31:17 -0500

    Add back hyphen.

--- muscat/functions.php
+++ muscat/functions.php
@@ -42,14 +42,14 @@ if ( ! function_exists( 'muscat_styles' ) ) :
 
 		// Register theme stylesheet.
 		wp_register_style(
-			'muscatstyle',
+			'muscat-style',
 			get_stylesheet_directory_uri() . '/style.css',
 			array(),
 			wp_get_theme()->get( 'Version' )
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'muscatstyle' );
+		wp_enqueue_style( 'muscat-style' );
 
 	}
 
