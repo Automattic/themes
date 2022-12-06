1bdcbe9ed314f572f7ddb1f08cfc277b27fd67a7
Jeff Ong <jonger4@gmail.com>
2022-12-06 14:39:04 -0500

    Move text to patterns, update templates.

---
+++ vetro/patterns/designed-with-wordpress.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Title: A layout for a creative practice description
+ * Slug: vetro/about
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"textAlign":"left","level":1,"align":"full","style":{"typography":{"lineHeight":"1.1","fontStyle":"normal","fontWeight":"600","letterSpacing":"-3px","fontSize":"4rem"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<h1 class="alignfull has-text-align-left" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;font-size:4rem;font-style:normal;font-weight:600;letter-spacing:-3px;line-height:1.1"><?php echo esc_html__( 'United Architects Collab is a studio focused on sustainable design, interiors and landscapes.', 'vetro' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"45px"} -->
+<div style="height:45px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}}} -->
+<div class="wp-block-column" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4"><?php echo esc_html__( 'Working as a full-service firm, UA — C delivers contemporary projects, from our client-oriented creative process to the latest building methodologies and construction techniques applied to building sites. And we are proud to achieve that through close collaboration within team members, consultants, contractors and trades.', 'vetro' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"45px","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:45px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":"20px"}}} -->
+<div class="wp-block-buttons" style="margin-top:0px;margin-bottom:0px"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase","fontSize":"0.8rem","lineHeight":1.4,"fontStyle":"normal","fontWeight":"600"},"spacing":{"padding":{"right":"40px","left":"40px","top":"16px","bottom":"16px"}},"border":{"radius":"4px"}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:0.8rem;font-style:normal;font-weight:600;line-height:1.4;text-transform:uppercase"><a class="wp-block-button__link wp-element-button" style="border-radius:4px;padding-top:16px;padding-right:40px;padding-bottom:16px;padding-left:40px"><?php echo esc_html__( 'Get started', 'vetro' ); ?></a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"style":{"typography":{"textTransform":"uppercase","fontSize":"0.8rem","lineHeight":1.4,"fontStyle":"normal","fontWeight":"600"},"spacing":{"padding":{"right":"40px","left":"40px","top":"15px","bottom":"15px"}},"border":{"radius":"4px"}},"className":"is-style-outline"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:0.8rem;font-style:normal;font-weight:600;line-height:1.4;text-transform:uppercase"><a class="wp-block-button__link wp-element-button" style="border-radius:4px;padding-top:15px;padding-right:40px;padding-bottom:15px;padding-left:40px"><?php echo esc_html__( 'Dismiss', 'vetro' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"90px"} -->
+<div style="height:90px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --><?php
+/**
+ * Title: Designed with WordPress 
+ * Slug: vetro/designed-with
+ */
+?>
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"normal","fontWeight":"500"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:500;line-height:1"><?php echo esc_html__( 'Designed with', 'vetro' ); ?><a rel="noreferrer noopener" href="http://wordpress.org" data-type="URL" data-id="wordpress.org" target="_blank"><?php echo esc_html__( 'WordPress.', 'vetro' ); ?></a></p>
+<!-- /wp:paragraph --><?php
+/**
+ * Title: A 404 page
+ * Slug: vetro/404
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"160px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:160px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0px","padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-group alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"textAlign":"left","align":"wide","style":{"typography":{"fontWeight":"600","lineHeight":"1","fontStyle":"normal","fontSize":"8rem"}},"className":"has-text-align-center"} -->
+<h2 class="alignwide has-text-align-left has-text-align-center" style="font-size:8rem;font-style:normal;font-weight:600;line-height:1"><?php echo esc_html__( 'Oops...', 'vetro' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"90px"} -->
+<div style="height:90px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"20px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"align":"left","style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":1.4}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4"><?php echo esc_html__( 'This page could not be found.', 'vetro' ); ?><br><?php echo esc_html__( 'Maybe try a search?', 'vetro' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"label":"<?php echo esc_html__( 'Search', 'vetro' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php echo esc_html__( 'Search', 'vetro' ); ?>","buttonUseIcon":true,"align":"center","style":{"border":{"radius":"4px","width":"1px"}},"borderColor":"foreground","backgroundColor":"foreground","textColor":"foreground"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"90px"} -->
+<div style="height:90px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --><?php
+/**
+ * Title: Small About Text 
+ * Slug: vetro/small-about
+ */
+?>
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3","fontSize":"0.8rem","fontStyle":"normal","fontWeight":"500"}}} -->
+<p style="font-size:0.8rem;font-style:normal;font-weight:500;line-height:1.3"><?php echo esc_html__( 'We\'re open for new collaborations.', 'vetro' ); ?><br><?php echo esc_html__( 'News to be updated weekly.', 'vetro' ); ?></p>
+<!-- /wp:paragraph -->
