98e4c25176362971544eb30b1c616c9ec954ddf0
Mikey Arce <mikeyarce@gmail.com>
2022-12-06 14:06:11 -0800

    Add Course Theme

---
+++ course/patterns/footer.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Title: Default footer
+ * Slug: course/footer
+ * Inserter: no
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"80px","bottom":"0px","left":"0"},"blockGap":"4rem"},"border":{"top":{"width":"1px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="border-top-width:1px;padding-top:80px;padding-bottom:0px;padding-left:0">
+  <!-- wp:group {"layout":{"type":"constrained","contentSize":"1000px"}} -->
+  <div class="wp-block-group">
+    <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"40px","left":"40px"}}}} -->
+    <div class="wp-block-columns">
+      <!-- wp:column {"width":"80%","style":{"typography":{"lineHeight":"1.3"}}} -->
+      <div class="wp-block-column" style="line-height:1.3;flex-basis:80%">
+        <!-- wp:columns {"style":{"border":{"left":{"width":"1px"}},"spacing":{"padding":{"left":"20px"},"blockGap":{"top":"10px","left":"20px"}}},"className":"course-footer-links-vertical-space"} -->
+        <div class="wp-block-columns course-footer-links-vertical-space" style="border-left-width:1px;padding-left:20px">
+          <!-- wp:column {"verticalAlignment":"center","width":"128px"} -->
+          <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:128px">
+            <!-- wp:heading {"level":3,"style":{"typography":{"textTransform":"uppercase","lineHeight":"1","fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium","fontFamily":"secondary"} -->
+            <h3 class="has-secondary-font-family has-medium-font-size" style="font-style:normal;font-weight:700;line-height:1;text-transform:uppercase"><?php echo esc_html__('Social', 'course'); ?></h3>
+            <!-- /wp:heading -->
+          </div>
+          <!-- /wp:column -->
+
+          <!-- wp:column {"verticalAlignment":"center","width":"50%","layout":{"type":"default"}} -->
+          <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"},"fontFamily":"primary"} -->
+            <div class="wp-block-group has-primary-font-family">
+              <!-- wp:paragraph -->
+              <p><a href="https://www.instagram.com/">Instagram</a></p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p><a href="https://youtube.com">YouTube</a></p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p><a href="https://spotify.com">Spotify</a></p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+
+        <!-- wp:columns {"style":{"border":{"left":{"width":"1px"}},"spacing":{"padding":{"left":"20px"},"margin":{"bottom":"20px"},"blockGap":{"top":"10px","left":"20px"}}}} -->
+        <div class="wp-block-columns" style="border-left-width:1px;margin-bottom:20px;padding-left:20px">
+          <!-- wp:column {"verticalAlignment":"center","width":"128px"} -->
+          <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:128px">
+            <!-- wp:heading {"level":3,"style":{"typography":{"textTransform":"uppercase","lineHeight":"1","fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium","fontFamily":"secondary"} -->
+            <h3 class="has-secondary-font-family has-medium-font-size" style="font-style:normal;font-weight:700;line-height:1;text-transform:uppercase"><?php echo esc_html__('Pages', 'course'); ?></h3>
+            <!-- /wp:heading -->
+          </div>
+          <!-- /wp:column -->
+
+          <!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+          <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%">
+            <!-- wp:group {"style":{"border":{"width":"0px","style":"none"},"spacing":{"blockGap":"20px"}},"layout":{"type":"flex","flexWrap":"wrap"},"fontFamily":"primary"} -->
+            <div class="wp-block-group has-primary-font-family" style="border-style:none;border-width:0px">
+              <!-- wp:paragraph -->
+              <p><a href="#"><?php echo esc_html__('About', 'course'); ?></a></p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p><a href="#"><?php echo esc_html__('Video', 'course'); ?></a></p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p><a href="#"><?php echo esc_html__('Episodes', 'course'); ?></a></p>
+              <!-- /wp:paragraph -->
+
+              <!-- wp:paragraph -->
+              <p><a href="#"><?php echo esc_html__('All Posts', 'course'); ?></a></p>
+              <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+          </div>
+          <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+      </div>
+      <!-- /wp:column -->
+      <!-- wp:column {"width":"20%","layout":{"type":"constrained"}} -->
+      <div class="wp-block-column" style="flex-basis:20%">
+        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+        <div class="wp-block-group">
+          <!-- wp:paragraph -->
+          <p><a href="#"><?php echo esc_html__('Terms', 'course'); ?></a></p>
+          <!-- /wp:paragraph -->
+
+          <!-- wp:paragraph -->
+          <p><a href="#"><?php echo esc_html__('Privacy', 'course'); ?></a></p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"1.3rem"}},"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group alignfull">
+    <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group alignfull">
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+      <div class="wp-block-group">
+        <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained","contentSize":"1000px","justifyContent":"center"}} -->
+        <div class="wp-block-group">
+          <!-- wp:site-title {"textAlign":"left","isLink":false,"style":{"typography":{"letterSpacing":"0.02em","fontSize":"100px","lineHeight":"1"},"spacing":{"margin":{"top":"0px"},"padding":{"top":"0px"}}},"className":"footer-title","fontFamily":"sorts-mill-goudy"} /-->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0","padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"2.5rem"}}},"className":"separator","layout":{"type":"default"}} -->
+      <div class="wp-block-group alignfull separator" style="margin-top:2.5rem;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+        <!-- wp:separator {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-default"} -->
+        <hr class="wp-block-separator alignfull has-alpha-channel-opacity is-style-default" style="margin-top:0;margin-bottom:0" />
+        <!-- /wp:separator -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0"}}},"layout":{"type":"constrained","contentSize":""}} -->
+    <div class="wp-block-group alignfull" style="padding-top:0">
+      <!-- wp:paragraph {"align":"center","style":{"typography":{"letterSpacing":"0.02em"},"spacing":{"padding":{"bottom":"5rem","top":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|70"}}},"fontSize":"x-small","fontFamily":"system"} -->
+      <p class="has-text-align-center has-system-font-family has-x-small-font-size" style="margin-top:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--50);padding-bottom:5rem;letter-spacing:0.02em">
+        <?php
+        echo sprintf(
+          wp_kses(
+            // translators: %1$s is https://senseilms.com, %2$s is https://wordpress.org.
+            __('Course Theme by <a href="%1$s" rel="nofollow"><span style="text-decoration: underline;">Sensei</span></a>, Powered by <a href="%2$s" rel="nofollow"><span style="text-decoration: underline;">WordPress</span></a>.', 'course'),
+            [
+              'a' => [
+                'href' => [],
+                'rel' => []
+              ],
+              'span' => [
+                'style' => []
+              ]
+            ]
+          ),
+          'https://senseilms.com',
+          'https://wordpress.org'
+        );
+        ?>
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
