c02a7a296df17610a02f4f525359c69f465f2216
Sarah Norris <sarah@sekai.co.uk>
2022-12-09 21:23:48 +0000

    Adventurer initial commit

---
+++ adventurer/patterns/about.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: About
+ * Slug: adventurer/about
+ * Inserter: yes
+ */
+
+?>
+
+<!-- wp:cover {"url":"https://adventurertheme.mystagingwebsite.com/wp-content/uploads/2022/11/Adventurer_cover.png","id":145,"dimRatio":50,"overlayColor":"primary","minHeight":630,"minHeightUnit":"px","isDark":false,"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|70","top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70);min-height:630px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim"></span><img class="wp-block-cover__image-background wp-image-145" alt="" src="https://adventurertheme.mystagingwebsite.com/wp-content/uploads/2022/11/Adventurer_cover.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"40%","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"backgroundColor":"primary","textColor":"background"} -->
+<div class="wp-block-column has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60);flex-basis:40%"><!-- wp:heading -->
+<h2 class="wp-block-heading">About Me</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Hi, I’m Austin. I love exploring and writing about the world and getting lost with my camera. This blog is where I write about the things I find, and I hope it gives you some inspiration to go and have your own adventures.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"background","textColor":"primary"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-primary-color has-background-background-color has-text-color has-background wp-element-button" href="https://adventurertheme.mystagingwebsite.com/about/">Read Full Bio</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
