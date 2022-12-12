4bf7368828b5fee20a90893b7e7cf71470796880
Sarah Norris <sarah@sekai.co.uk>
2022-12-12 12:27:47 +0000

    Patternise 404

--- adventurer/patterns/404.php
+++ adventurer/patterns/404.php
@@ -7,10 +7,16 @@
 
 ?>
 
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
-<h1 class="has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'adventurer' ); ?></h1>
-<!-- /wp:heading -->
+<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"top":"16vh","bottom":"16vh"}}},"layout":{"type":"constrained","contentSize":"1100px"}} -->
+<main class="wp-block-group" style="margin-top:16vh;margin-bottom:16vh">
+    <!-- wp:heading {"textAlign":"left","level":1,"align":"wide","style":{"typography":{"fontSize":"4.4rem"}}} -->
+    <h1 class="wp-block-heading alignwide has-text-align-left" id="oops-that-page-can-t-be-found" style="font-size:4.4rem"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'adventurer' ); ?></h1>
+    <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'adventurer' ); ?></p>
-<!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'adventurer' ); ?></p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:search {"label":"","showLabel":false,"placeholder":"E.g. \u0022Botswana\u0022"} /-->
+</main>
+<!-- /wp:group -->
