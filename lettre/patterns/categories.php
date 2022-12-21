0c6fb8faaca984f969e4ca0e8c501172d55ccee7
Sarah Norris <sarah@sekai.co.uk>
2022-12-21 13:14:11 +0000

    Update categories

--- lettre/patterns/categories.php
+++ lettre/patterns/categories.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Category buttons
+ * Title: Category buttons
+ * Slug: lettre/categories
+ * Categories: featured
  */
-return array(
-	'title'      => __( 'Category buttons', 'lettre' ),
-	'categories' => array( 'featured' ),
-	'content'    => '<!-- wp:paragraph {"fontSize":"small","style":{"typography":{"textTransform":"uppercase"}}} -->
-					<p class="has-small-font-size" style="text-transform:uppercase">' . esc_html__( 'Categories', 'lettre' ) . '</p>
-					<!-- /wp:paragraph -->
-					
-					<!-- wp:tag-cloud {"taxonomy":"category","className":"is-style-outline"} /-->',
-);
+?>
+
+<!-- wp:paragraph {"fontSize":"small","style":{"typography":{"textTransform":"uppercase"}}} -->
+<p class="has-small-font-size" style="text-transform:uppercase"><?php esc_html__( 'Categories', 'lettre' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:tag-cloud {"taxonomy":"category","className":"is-style-outline"} /-->
