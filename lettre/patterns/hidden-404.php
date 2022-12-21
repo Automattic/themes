05a38a81b1ffd5c7d1de89a8133bc1ff3e01a2d0
Sarah Norris <sarah@sekai.co.uk>
2022-12-21 13:16:25 +0000

    Update hidden 404

--- lettre/patterns/hidden-404.php
+++ lettre/patterns/hidden-404.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * 404 content.
+ * Title: 404 content
+ * Slug: lettre/hidden-404
+ * Inserter: no
  */
-return array(
-	'title'    => __( '404 content', 'lettre' ),
-	'inserter' => false,
-	'content'  => '<!-- wp:group {"layout":{"inherit":true}} -->
-  <div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontWeight":"700","lineHeight":"1","fontStyle":"normal"}},"className":"has-text-align-center","fontSize":"large"} -->
-<h2 class="has-text-align-center has-large-font-size" style="font-style:normal;font-weight:700;line-height:1">' . esc_html__( 'This page could not be found.', 'lettre') . '</h2><!-- /wp:heading -->
+?>
+
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontWeight":"700","lineHeight":"1","fontStyle":"normal"}},"className":"has-text-align-center","fontSize":"large"} -->
+<h2 class="has-text-align-center has-large-font-size" style="font-style:normal;font-weight:700;line-height:1"><?php esc_html__( 'This page could not be found.', 'lettre'); ?></h2><!-- /wp:heading -->
 <!-- wp:paragraph {"align":"center","fontSize":"small"} -->
-<p class="has-text-align-center has-small-font-size">' . esc_html__( ' Maybe try a search?', 'lettre' ) . '</p>
+<p class="has-text-align-center has-small-font-size"><?php esc_html__( ' Maybe try a search?', 'lettre' ); ?></p>
 <!-- /wp:paragraph -->
-<!-- wp:search {"label":"' . esc_html__( 'Search', 'lettre' ) . '","showLabel":false,"width":100,"widthUnit":"%","buttonText":"' . esc_html__( 'Search', 'lettre' ) . '","align":"center","style":{"border":{"radius":"100px"}},"borderColor":"primary","backgroundColor":"primary"} /--></div>
-<!-- /wp:group -->',
-);
+<!-- wp:search {"label":"<?php esc_html__( 'Search', 'lettre' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_html__( 'Search', 'lettre' ); ?>","align":"center","style":{"border":{"radius":"100px"}},"borderColor":"primary","backgroundColor":"primary"} /--></div>
+<!-- /wp:group -->'
