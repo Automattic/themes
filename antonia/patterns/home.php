8392f904fb8310a39c42b97c3306ed068acf1fce
Jason Crist <jcrist@pbking.com>
2023-01-25 11:07:37 -0500

    Replace Antonia with Antonia-2 and put Headstart content in home.html template

---
+++ antonia/patterns/home.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Title: Home Pattern
+ * slug: antonia/home
+ * inserter: no
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"contentSize":"1200px"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"contentSize":"450px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1,"textColor":"primary"} -->
+<h1 class="has-primary-color has-text-color"><strong>Antonia Chair. Engineered Comfort</strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Antonia chair is a comfort focused office guest chair. A great member of the Antonia office furniture family.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:jetpack/recurring-payments {"url":"https://antoniademo.wordpress.com/?recurring_payments=undefined","uniqueId":"recurring-payments-undefined"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-undefined","passthroughAttributes":{"uniqueId":"uniqueId","url":"url"},"text":"Buy Now","url":"https://antoniademo.wordpress.com/?recurring_payments=undefined","textColor":"primary","backgroundColor":"secondary","width":"240px"} /--></div>
+<!-- /wp:jetpack/recurring-payments --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary"} -->
+<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:600;">$79.50</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:image {"id":9,"width":642,"height":722,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/chair.jpg'; ?>" alt="" class="wp-image-9" width="642" height="722" /></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","layout":{"contentSize":"1200px"}} -->
+<div class="wp-block-group alignfull"><!-- wp:columns {"isStackedOnMobile":false,"style":{"border":{"bottom":{"color":"#eaeaec","width":"1px"}},"spacing":{"padding":{"bottom":"60px"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="border-bottom-color:#eaeaec;border-bottom-width:1px;padding-bottom:60px;"><!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"textTransform":"uppercase","fontSize":"14px"}}} -->
+<h6 class="has-text-align-center" style="font-size:14px;text-transform:uppercase;">Frame</h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Pine Wood</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"textTransform":"uppercase","fontSize":"14px"}}} -->
+<h6 class="has-text-align-center" style="font-size:14px;text-transform:uppercase;">Frame</h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Pine Wood</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"textTransform":"uppercase","fontSize":"14px"}}} -->
+<h6 class="has-text-align-center" style="font-size:14px;text-transform:uppercase;">Frame</h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Pine Wood</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"10px"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":6,"style":{"typography":{"textTransform":"uppercase","fontSize":"14px"}}} -->
+<h6 class="has-text-align-center" style="font-size:14px;text-transform:uppercase;">Frame</h6>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Pine Wood</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"padding":{"top":"0px","bottom":"0px"}}},"layout":{"contentSize":"1200px"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;"><!-- wp:columns {"style":{"spacing":{"blockGap":"3vw","margin":{"top":"0px","bottom":"0px"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-columns" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;"><!-- wp:column {"style":{"spacing":{"padding":{"top":"80px","bottom":"40px"},"blockGap":"30px"}},"layout":{"contentSize":"350px"}} -->
+<div class="wp-block-column" style="padding-top:80px;padding-bottom:40px;"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":1.4}}} -->
+<h3 style="font-style:normal;font-weight:600;line-height:1.4;">Part of the Antonia Office Family</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>As part of the Antonia office set, the backrest chair is a perfect compliment to the family. The gently curved lines accentuated by sewn details are kind to your body and pleasant to look at.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"380px","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:380px;"><!-- wp:image {"id":50,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/chairs-1.jpg'; ?>" alt="" class="wp-image-50" /></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","style":{"spacing":{"padding":{"bottom":"80px","top":"40px"},"blockGap":"30px"}},"layout":{"contentSize":"350px"}} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="padding-top:40px;padding-bottom:80px;"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":1.4}}} -->
+<h3 style="font-style:normal;font-weight:600;line-height:1.4;">Pure Comfort</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>The gently curved lines accentuated by sewn details are kind to your body and pleasant to look at. Also, your back gets support and extra relief from the built-in lumbar support.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"border":{"top":{"color":"#eaeaec","width":"1px"}},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="border-top-color:#eaeaec;border-top-width:1px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":53,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/chairbigger.jpg'; ?>" alt="" class="wp-image-53" /></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","layout":{"contentSize":"480px"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":1.4}}} -->
+<h3 style="font-style:normal;font-weight:600;line-height:1.4;">The Perfect<br>Balance</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>You can lean back with perfect balance, as the tension mechanism is easy to adjust with an Allen key to suit your weight and movements.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"contentSize":""}} -->
+<div class="wp-block-group"><!-- wp:jetpack/recurring-payments {"url":"https://antoniademo.wordpress.com/?recurring_payments=undefined","uniqueId":"recurring-payments-undefined"} -->
+<div class="wp-block-jetpack-recurring-payments"><!-- wp:jetpack/button {"element":"a","uniqueId":"recurring-payments-undefined","passthroughAttributes":{"uniqueId":"uniqueId","url":"url"},"text":"Buy Now","url":"https://antoniademo.wordpress.com/?recurring_payments=undefined","textColor":"primary","backgroundColor":"secondary","width":"240px"} /--></div>
+<!-- /wp:jetpack/recurring-payments --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"primary"} -->
+<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:600;">$79.50</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"50px","padding":{"top":"60px","bottom":"60px"}}}} -->
+<div class="wp-block-group alignwide" style="padding-top:60px;padding-bottom:60px;"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.2"}}} -->
+<h2 class="has-text-align-center" style="line-height:1.2;">Thousands of<br>Happy Customers</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"80px"}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"layout":{"contentSize":"540px"}} -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":66,"width":50,"height":50,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/user2.png'; ?>" alt="" class="wp-image-66" width="50" height="50" /></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Great chair and came earlier than promised. This did come with assembly directions. My teenager assembled it with zero assistance in (far) less than ten minutes.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Anton Delgado</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"layout":{"contentSize":"540px"}} -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":70,"width":50,"height":50,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/user1.png'; ?>" alt="" class="wp-image-70" width="50" height="50" /></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>The chair is exactly as pictured and even exceeded my expectations in quality! Assembly was easy and only took about 10 minutes to put together. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Jane Delgado</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
