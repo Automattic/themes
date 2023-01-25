b8be614916a254b269ed5d55d7ed60f103a8095e
Jason Crist <jcrist@pbking.com>
2023-01-25 10:30:01 -0500

    Replace Barnett with Barnett-2 moving headstart content to home.html

---
+++ barnett-2/patterns/home.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Home Pattern 
+ * slug: barnett/home
+ * inserter: no
+ */
+
+?>
+
+<!-- wp:cover {"dimRatio":0,"overlayColor":"background","minHeight":90,"minHeightUnit":"vh","isDark":false,"align":"full"} -->
+<div class="wp-block-cover alignfull is-light" style="min-height:90vh;"><span aria-hidden="true" class="wp-block-cover__background has-background-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"contentSize":"620px"}} -->
+<div class="wp-block-group"><!-- wp:image {"align":"center","id":11,"width":142,"height":142,"sizeSlug":"large","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-large is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/assets/images/image-from-rawpixel-id-2257682-jpeg.jpg'; ?>" alt="" width="142" height="142" /></figure>
+
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="has-text-align-center" id="antonio-barnett"><strong>Antonio Barnett</strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"vertical"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"foreground","width":100,"style":{"border":{"radius":"100px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" style="border-radius:100px;">Watch my tutorials</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"foreground","width":100,"style":{"border":{"radius":"100px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" style="border-radius:100px;">Buy prints of my work</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"foreground","width":100,"style":{"border":{"radius":"100px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" style="border-radius:100px;">My wish list</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"foreground","width":100,"style":{"border":{"radius":"100px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" style="border-radius:100px;">Instagram</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"backgroundColor":"foreground","width":100,"style":{"border":{"radius":"100px"}}} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" style="border-radius:100px;">Twitter</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
