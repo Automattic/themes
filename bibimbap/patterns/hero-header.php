b169e8d968eced1b7cda2e0c2ce9bc161f8662a1
Madhu Dollu <madhusudhan.dollu@gmail.com>
2023-01-30 10:52:15 +0530

    Bibimbap: optimise images and add credits (#6818)
    * optimize images
    
    * add image credits
    
    * replace bibimbap image

--- bibimbap/patterns/hero-header.php
+++ bibimbap/patterns/hero-header.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cover-image.png","dimRatio":50,"minHeight":1017,"minHeightUnit":"px","isDark":false} -->
-<div class="wp-block-cover is-light" style="min-height:1017px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/cover-image.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/bibimbap-image.png","dimRatio":50,"minHeight":1017,"minHeightUnit":"px","isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:1017px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/bibimbap-image.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40","right":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:group {"layout":{"type":"flex"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
