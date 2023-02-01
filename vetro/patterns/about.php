62e85f63188bdf6ebda64b25c0e2046a9a1a9459
Jeff Ong <jonger4@gmail.com>
2023-02-01 12:28:26 -0500

    Alignment fixes to make everything wide.

--- vetro/patterns/about.php
+++ vetro/patterns/about.php
@@ -6,18 +6,19 @@
  */
 ?>
 
-<!-- wp:heading {"textAlign":"left","level":1,"align":"full","style":{"typography":{"lineHeight":"1.1","fontStyle":"normal","fontWeight":"600","letterSpacing":"-3px","fontSize":"4rem"},"spacing":{"padding":{"top":"0px","bottom":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
-<h1 class="alignfull has-text-align-left" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-bottom:0px;font-size:4rem;font-style:normal;font-weight:600;letter-spacing:-3px;line-height:1.1"><?php echo esc_html__( 'United Architects Collab is a studio focused on sustainable design, interiors and landscapes.', 'vetro' ); ?></h1>
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"lineHeight":"1.1","fontStyle":"normal","fontWeight":"600","letterSpacing":"-3px","fontSize":"4rem"},"spacing":{"padding":{"top":"0px","bottom":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<h1 class="wp-block-heading has-text-align-left" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-bottom:0px;font-size:4rem;font-style:normal;font-weight:600;letter-spacing:-3px;line-height:1.1"><?php echo esc_html__( 'United Architects Collab is a studio focused on sustainable design, interiors and landscapes.', 'vetro' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:spacer {"height":"45px"} -->
-<div style="height:45px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:spacer {"height":"32px"} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}}} -->
-<div class="wp-block-columns alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-bottom:0px"><!-- wp:column {"style":{"spacing":{"padding":{"top":"0px","bottom":"0px"},"blockGap":"0px"}}} -->
-<div class="wp-block-column" style="padding-top:0px;padding-bottom:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","bottom":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-bottom:0px"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"small"} -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"small"} -->
 <p class="has-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4"><?php echo esc_html__( 'Working as a full-service firm, UA — C delivers contemporary projects, from our client-oriented creative process to the latest building methodologies and construction techniques applied to building sites. And we are proud to achieve that through close collaboration within team members, consultants, contractors and trades.', 'vetro' ); ?></p>
 <!-- /wp:paragraph -->
 
@@ -37,10 +38,11 @@
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"></div>
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns -->
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
 
 <!-- wp:spacer {"height":"90px"} -->
 <div style="height:90px" aria-hidden="true" class="wp-block-spacer"></div>
