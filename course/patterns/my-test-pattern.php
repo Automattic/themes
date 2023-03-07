96635244e20c15426365e59ce1bea8a2fee322a6
Jeff Ong <jonger4@gmail.com>
2023-03-07 15:41:56 +0100

    My test commit

---
+++ course/patterns/my-test-pattern.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: My Test Pattern
+ * Slug: my-test-pattern
+ * Description: 
+ * Categories: 
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: wp_template, wp_template_part
+ * Inserter: true
+ */
+
+?>
+<!-- wp:paragraph -->
+<p>My test pattern to go inside a template</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":544,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/patterns/images/white-and-orange-squares-on-light-purple-background-3-edited-1024x640.png" alt="" class="wp-image-544"/></figure>
+<!-- /wp:image -->
