28b464a8d42a13352b213fb859dec8eafd03ad7d
madhusudhand <madhusudhan.dollu@gmail.com>
2023-03-17 14:36:09 +0530

    fix templates

---
+++ aalto/patterns/search-results.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Search pattern
+ * Slug: aalto/search-results
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'aalto' ); ?></p>
+<!-- /wp:paragraph -->
