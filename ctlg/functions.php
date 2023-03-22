9f5d74e6cdc0d735bc689bfe59bea64304757577
Matias Benedetto <matias.benedetto@gmail.com>
2023-03-22 12:40:20 -0300

    Renaming Catalog to CTLG (#6939)

--- ctlg/functions.php
+++ ctlg/functions.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * Catalog functions and definitions
+ * CTLG functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Catalog
- * @since Catalog 1.0
+ * @package CTLG
+ * @since CTLG 1.0
  */
 
 
-if ( ! function_exists( 'catalog_support' ) ) :
+if ( ! function_exists( 'ctlg_support' ) ) :
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @since Catalog 1.0
+	 * @since CTLG 1.0
 	 *
 	 * @return void
 	 */
-	function catalog_support() {
+	function ctlg_support() {
 
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
@@ -27,32 +27,32 @@ if ( ! function_exists( 'catalog_support' ) ) :
 
 endif;
 
-add_action( 'after_setup_theme', 'catalog_support' );
+add_action( 'after_setup_theme', 'ctlg_support' );
 
-if ( ! function_exists( 'catalog_styles' ) ) :
+if ( ! function_exists( 'ctlg_styles' ) ) :
 
 	/**
 	 * Enqueue styles.
 	 *
-	 * @since Catalog 1.0
+	 * @since CTLG 1.0
 	 *
 	 * @return void
 	 */
-	function catalog_styles() {
+	function ctlg_styles() {
 
 		// Register theme stylesheet.
 		wp_register_style(
-			'catalog-style',
+			'ctlg-style',
 			get_stylesheet_directory_uri() . '/style.css',
 			array(),
 			wp_get_theme()->get( 'Version' )
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'catalog-style' );
+		wp_enqueue_style( 'ctlg-style' );
 
 	}
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'catalog_styles' );
+add_action( 'wp_enqueue_scripts', 'ctlg_styles' );
--- ctlg/patterns/footer.php
+++ ctlg/patterns/footer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Default footer
- * Slug: catalog/footer
+ * Slug: ctlg/footer
  * Block Types: core/template-part/footer
  * inserter: no
  */
@@ -14,19 +14,19 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"bottom"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"textDecoration":"none"}},"fontSize":"medium"} -->
-<p class="has-medium-font-size" style="text-decoration:none"><em><a href=""><?php echo esc_html__( 'Instagram', 'catalog' ); ?></a></em></p>
+<p class="has-medium-font-size" style="text-decoration:none"><em><a href=""><?php echo esc_html__( 'Instagram', 'ctlg' ); ?></a></em></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"fontSize":"medium"} -->
-<p class="has-medium-font-size"><em><a href=""><?php echo esc_html__( 'Twitter', 'catalog' ); ?></a></em></p>
+<p class="has-medium-font-size"><em><a href=""><?php echo esc_html__( 'Twitter', 'ctlg' ); ?></a></em></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"fontSize":"medium"} -->
-<p class="has-medium-font-size"><em><a href=""><?php echo esc_html__( 'Facebook', 'catalog' ); ?></a></em></p>
+<p class="has-medium-font-size"><em><a href=""><?php echo esc_html__( 'Facebook', 'ctlg' ); ?></a></em></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:paragraph {"align":"left","fontSize":"small"} -->
-<p class="has-text-align-left has-small-font-size"><?php echo esc_html__( 'Designed with', 'catalog' ); ?><a rel="nofollow" href="https://wordpress.org"><?php echo esc_html__( 'WordPress', 'catalog' ); ?></a></p>
+<p class="has-text-align-left has-small-font-size"><?php echo esc_html__( 'Designed with', 'ctlg' ); ?><a rel="nofollow" href="https://wordpress.org"><?php echo esc_html__( 'WordPress', 'ctlg' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
