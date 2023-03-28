e0d21d9fe3bf90c5da250f5cd4f556efcacc007b
Sarah Norris <sarah@sekai.co.uk>
2023-03-28 17:26:16 +0100

    Patternise no results section

---
+++ verso/patterns/no-results.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: No Results content
+ * Slug: verso/no-results
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:query-no-results -->
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'verso' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results -->
