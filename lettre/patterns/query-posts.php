5affcb33dec87c4a6f300cb0cd3c7b41954a9919
Matias Ventura <mv@matiasventura.com>
2023-04-03 09:53:45 +0200

    Lettre: fix display of date
    Theme was using `M n` which renders the month as a number, not the actual day.

--- lettre/patterns/query-posts.php
+++ lettre/patterns/query-posts.php
@@ -15,7 +15,7 @@
 <!-- wp:cover {"customOverlayColor":"rgba(0,0,0,0)","minHeight":178,"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
 <div class="wp-block-cover" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;min-height:178px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:rgba(0,0,0,0)"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"var( --wp--style--block-gap )","bottom":"0.5em"}},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"textColor":"foreground"} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center has-foreground-color has-text-color has-link-color" style="margin-top:var( --wp--style--block-gap );margin-bottom:0.5em;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:column {"verticalAlignment":"center","width":"10%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:10%"><!-- wp:post-date {"format":"M n","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /--></div>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:10%"><!-- wp:post-date {"format":"M j","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"small"} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"70%","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
