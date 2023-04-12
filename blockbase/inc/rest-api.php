b2f858d5e7b15bcd61d045ae60173b11f6ee0e77
Jason Crist <jcrist@pbking.com>
2023-04-12 13:46:02 -0400

    Move opt out check inside of api filter and added default value

--- blockbase/inc/rest-api.php
+++ blockbase/inc/rest-api.php
@@ -15,24 +15,18 @@ function blockbase_remove_style_variations_from_child_themes( $response, $handle
 
 	$handler_class  = isset( $handler['callback'][0] ) ? $handler['callback'][0] : null;
 	$handler_method = isset( $handler['callback'][1] ) ? $handler['callback'][1] : null;
+	$opt_out        = wp_get_global_settings( array( 'custom', 'optOutOfParentStyleVariations' ) );
 
 	/*
 	 * Prevents Blockbase child themes from being considered child themes in the
 	 * `wp/v2/global-styles/themes/:theme/variations` API endpoint, so they don't
 	 * inherit the style variations from the parent theme.
 	 */
-	if ( is_a( $handler_class, 'WP_REST_Global_Styles_Controller' ) && 'get_theme_items' === $handler_method ) {
+	if ( $opt_out && is_a( $handler_class, 'WP_REST_Global_Styles_Controller' ) && 'get_theme_items' === $handler_method ) {
 		add_filter( 'template_directory', 'get_stylesheet_directory' );
 	}
 
 	return $response;
 }
 
-function blockbase_check_for_opt_out_parent_variations() {
-	$opt_out = wp_get_global_settings( array( 'custom', 'optOutOfParentStyleVariations' ) );
-	if ( $opt_out ) {
-		add_filter( 'rest_request_before_callbacks', 'blockbase_remove_style_variations_from_child_themes', 10, 2 );
-	}
-}
-
-add_action( 'init', 'blockbase_check_for_opt_out_parent_variations', 99 );
+add_filter( 'rest_request_before_callbacks', 'blockbase_remove_style_variations_from_child_themes', 10, 2 );
