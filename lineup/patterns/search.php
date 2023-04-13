4f08bfa8ba991242ce1278673f9433cd99dc3f3a
Matias Benedetto <matias.benedetto@gmail.com>
2023-04-13 11:55:59 -0300

    Linuep: add theme for review

---
+++ lineup/patterns/search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: lineup/search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder text in a search field', 'lineup' ); ?>","buttonText":"<?php echo esc_html_x( 'Search', 'This is what a search button says', 'bsoj' ); ?>","buttonUseIcon":true} /-->
