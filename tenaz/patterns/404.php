a1aafd1194bbd6c3b244a7489cd9daf51cf350f5
Takashi Irie <takashi@automattic.com>
2023-05-28 23:35:40 +0100

    Tenaz: add theme

---
+++ tenaz/patterns/404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: A 404 page
+ * Slug: tenaz/404
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"textAlign":"center","level":1,"align":"wide","fontSize":"xxx-large","anchor":"oops-that-page-can-t-be-found"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center has-xxx-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can’t be found.', 'tenaz' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'tenaz' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"label":"","showLabel":false,"buttonPosition":"button-inside","buttonUseIcon":true} /-->
