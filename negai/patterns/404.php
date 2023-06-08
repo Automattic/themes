600993c14bb7d1f76d10d38f76f078c266d25a1f
Takashi Irie <takashi@automattic.com>
2023-06-08 19:41:55 +0100

    Negai: Add theme

---
+++ negai/patterns/404.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: A 404 page
+ * Slug: negai/404
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"level":1,"fontSize":"huge","anchor":"oops-that-page-can-t-be-found"} -->
+<h1 class="wp-block-heading has-huge-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Nothing found. Maybe try a search?', 'negai' ); ?></h1>
+<!-- /wp:heading -->
---
+++ negai/patterns/no-results-content.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: negai/no-results-content
+ * Inserter: no
+ */
+?>
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"0"}}},"fontSize":"huge"} -->
+<p class="has-huge-font-size" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:0"><?php echo esc_html_x( 'Nothing found. Try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'negai' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|60"} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
