d4ce80eedb8d08077efa7fa930ed5d464b18e6df
Jeff Ong <jonger4@gmail.com>
2023-06-26 10:57:56 -0400

    Escape text.

--- loic/patterns/404.php
+++ loic/patterns/404.php
@@ -7,10 +7,16 @@
 
 ?>
 
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
-<h1 class="has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'loic' ); ?></h1>
+<!-- wp:cover {"overlayColor":"background","minHeight":100,"minHeightUnit":"vh","textColor":"primary","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-primary-color has-text-color" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1,"fontSize":"large"} -->
+<h1 class="wp-block-heading has-large-font-size"><?php echo esc_html__( 'The page you\'re looking for could not be found.', 'loic' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'loic' ); ?></p>
-<!-- /wp:paragraph -->
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/"><?php echo  esc_html__( 'Return to homepage', 'loic' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
