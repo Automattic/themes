84306277c90a586346b72a127f39a4fbd3c4d4b9
Jason Crist <jcrist@pbking.com>
2023-06-26 17:45:56 -0400

    Removed 'ref' from navigation block

--- optimismo/patterns/footer.php
+++ optimismo/patterns/footer.php
@@ -9,7 +9,7 @@
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
-	<!-- wp:navigation {"ref":449,"overlayMenu":"never","align":"wide","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"0.69rem"}}} /-->
+	<!-- wp:navigation {"overlayMenu":"never","align":"wide","layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"0.69rem"}}} /-->
 
 	<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}},"typography":{"fontSize":"0.69rem"}}} -->
 	<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--40);font-size:0.69rem"><?php
