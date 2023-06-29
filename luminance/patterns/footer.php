5c94cb54713ca6d79feea3d193acaeacbf53ff5f
Takashi Irie <takashi@automattic.com>
2023-06-29 17:18:45 +0100

    Luminance: Add theme

---
+++ luminance/patterns/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Default footer
+ * Slug: luminance/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"layout":{"selfStretch":"fit","flexSize":null},"border":{"width":"1px"}},"borderColor":"contrast","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-border-color has-contrast-border-color" style="border-width:1px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:buttons -->
+	<div class="wp-block-buttons">
+		<!-- wp:button {"className":"is-style-outline"} -->
+		<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'Instagram', 'luminance' ); ?></a></div>
+		<!-- /wp:button -->
+
+		<!-- wp:button {"className":"is-style-outline"} -->
+		<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'TikTok', 'luminance' ); ?></a></div>
+		<!-- /wp:button -->
+
+		<!-- wp:button {"className":"is-style-outline"} -->
+		<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'Tumblr', 'luminance' ); ?></a></div>
+		<!-- /wp:button -->
+	</div>
+	<!-- /wp:buttons -->
+
+	<!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"x-small"} -->
+	<p class="has-text-align-right has-primary-color has-text-color has-link-color has-x-small-font-size"><?php
+		/* Translators: WordPress link. */
+		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'luminance' ) ) . '" rel="nofollow">WordPress</a>';
+		echo sprintf(
+			esc_html__( 'Designed with %1$s', 'luminance' ),
+			$wordpress_link
+		);
+		?></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
---
+++ luminance/patterns/search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: luminance/search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder text in a search field', 'luminance' ); ?>","buttonUseIcon":true,"style":{"border":{"radius":"999px"}}} /-->
