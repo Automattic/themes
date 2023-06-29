cd09ea807b4ed6a2ed0e417ffc3d5a08267e2622
madhusudhand <madhusudhan.dollu@gmail.com>
2023-06-29 16:07:45 +0800

    move cover template part to pattern

--- poesis/patterns/footer.php
+++ poesis/patterns/footer.php
@@ -7,27 +7,21 @@
  */
 ?>
 
-<!-- wp:spacer {"height":"var(--wp--preset--spacing--80)"} -->
+<!-- wp:spacer {"height":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d80)"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center">
-			<?php
-				$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'poesis' ) ) . '" rel="nofollow">WordPress</a>';
-				echo sprintf(
-					/* Translators: WordPress link. */
-					esc_html__( 'Designed with %1$s', 'poesis' ),
-					$wordpress_link
-				);
-				?>
-		</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"0.8rem"},"color":{"text":"#cbcbcb"}}} -->
+<p class="has-text-align-left has-text-color has-link-color" style="color:#cbcbcb;font-size:0.8rem">
+	<?php
+		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'poesis' ) ) . '" rel="nofollow">WordPress</a>';
+		echo sprintf(
+			/* Translators: WordPress link. */
+			esc_html__( 'Designed with %1$s', 'poesis' ),
+			$wordpress_link
+		);
+	?>
+</p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
---
+++ poesis/patterns/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Default header
+ * Slug: poesis/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:site-title /-->
+
+<!-- wp:site-tagline /-->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:navigation {"ref":67,"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|20"}}} /--></div>
+<!-- /wp:group -->
