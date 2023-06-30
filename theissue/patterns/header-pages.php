6f059590610079344f92ba36b2fdcfeb5c668061
madhusudhand <madhusudhan.dollu@gmail.com>
2023-06-30 16:54:45 +0700

    fix site logo

--- theissue/patterns/header-pages.php
+++ theissue/patterns/header-pages.php
@@ -15,9 +15,7 @@
 <div class="wp-block-group" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"0px","top":"0px","right":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","justifyContent":"space-between","verticalAlignment":"top"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:navigation {"textColor":"primary","overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"spacing":{"blockGap":"0rem"}},"fontSize":"large"} /-->
 
-<!-- wp:image {"id":341,"width":48,"sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="https://theissue.mystagingwebsite.com/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/theissue_brand-b.png" alt="" class="wp-image-341" width="48"/></a></figure>
-<!-- /wp:image --></div>
+<!-- wp:site-logo {"width":48,"shouldSyncIcon":false} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
