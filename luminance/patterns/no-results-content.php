ffe30a384690a316b90efe7f4190df06c3ac0139
Sarah Norris <sarah@sekai.co.uk>
2023-07-04 18:15:24 +0100

    Initial commit

---
+++ luminance/patterns/no-results-content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: luminance/no-results-content
+ * Inserter: no
+ */
+?>
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
+<p style="margin-bottom:0px"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'luminance' ); ?></p>
+<!-- /wp:paragraph -->
