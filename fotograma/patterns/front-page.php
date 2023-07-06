8eac9725bdd97d7c158f154a0bdd0cddf724f94e
Takashi Irie <takashi@automattic.com>
2023-07-06 19:51:37 +0100

    Fotograma: Add theme (#7128)
    * Fotograma: Add theme
    
    * Bump version on stylesheet
    
    * Remove theme slug from templates
    
    * Avoid using template parts from patterns
    
    ---------
    
    Co-authored-by: Vicente Canales <vicente.canales@automattic.com>

---
+++ fotograma/patterns/front-page.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: fotograma/front-page
+ * Categories: featured
+ */
+?>
+<!-- wp:pattern {"slug":"fotograma/stack-photo"} /-->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"right":[],"bottom":{"color":"var:preset|color|contrast","width":"1px"},"left":[]}}} -->
+	<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:pattern {"slug":"fotograma/header"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%"} -->
+		<div class="wp-block-column" style="flex-basis:25%"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Jun. 1972: Teenager, Second Ward, El Paso', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Photographer: Danny Lyon', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'In 1972, El Paso teenagers were shaped by U.S. cultural, political upheaval - the Vietnam War, civil rights, and Watergate. The city&rsquo;s border location and rich, diverse history significantly influenced their daily experiences.', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1972-jun-a-teenager-in-the-second-ward-chicano-neighborhood-el-paso.jpg","dimRatio":0,"focalPoint":{"x":0.5,"y":0},"isDark":false,"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1972-jun-a-teenager-in-the-second-ward-chicano-neighborhood-el-paso.jpg" style="object-position:50% 0%" data-object-fit="cover" data-object-position="50% 0%"/>
+		<div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size"></p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"right":[],"bottom":{"color":"var:preset|color|contrast","width":"1px"},"left":[]}}} -->
+	<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:pattern {"slug":"fotograma/header"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%"} -->
+		<div class="wp-block-column" style="flex-basis:25%"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Jul. 1974: Four young girls and an open fire hydrant, Brooklyn', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Photographer: Dick Swanson', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'In 1974, Brooklyn, housing 2.5M+ people, was a NYC hub with diversity. From the upscale Brooklyn Heights brownstones to the working-class districts of Williamsburg and Greenpoint, it showcased vibrant community life.', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-4fWUuPi4h5w-unsplash.jpg","dimRatio":0,"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-4fWUuPi4h5w-unsplash.jpg" data-object-fit="cover"/>
+		<div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size"></p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"right":[],"bottom":{"color":"var:preset|color|contrast","width":"1px"},"left":[]}}} -->
+	<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:pattern {"slug":"fotograma/header"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%"} -->
+		<div class="wp-block-column" style="flex-basis:25%"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Jun. 1974: Manhattan Bridge Tower, Brooklyn', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Photographer: Dick Swanson', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Brooklyn, a quintessential 19th-century American city, boasts great bridges linking it to Manhattan, bustling streets, and a mix of habitation and aesthetics. Its &lsquo;worst&rsquo; neighbourhoods preserve some of America&rsquo;s finest architecture, resisting demolition.', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-QhH_dDe5plY-unsplash.jpg","dimRatio":0,"focalPoint":{"x":0.5,"y":0},"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-QhH_dDe5plY-unsplash.jpg" style="object-position:50% 0%" data-object-fit="cover" data-object-position="50% 0%"/><div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size"></p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"right":[],"bottom":{"color":"var:preset|color|contrast","width":"1px"},"left":[]}}} -->
+	<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:pattern {"slug":"fotograma/header"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%"} -->
+		<div class="wp-block-column" style="flex-basis:25%"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Aug. 1973: Taxis on 5th Ave. near 48th Street, Manhattan', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Photographer: Dan McCoy', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'In 1973, 5th Ave near 48th St. in Manhattan was a bustling hub, with high-end shops, iconic landmarks like St. Patrick&rsquo;s Cathedral, and nearby, the allure of Rockefeller Center. It was the heart of NYC, pulsating with the energy of the metropolis.', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1973-aug-taxis-at-48th-and-fifth-manhattan.jpg","dimRatio":0,"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/1973-aug-taxis-at-48th-and-fifth-manhattan.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size"></p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:pattern {"slug":"fotograma/stack-contact"} /-->
---
+++ fotograma/patterns/stack-photo.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Stack Photo
+ * Slug: fotograma/stack-photo
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="min-height:100vh;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"color":"var:preset|color|contrast","width":"1px"},"bottom":{"color":"var:preset|color|contrast","width":"1px"},"right":[],"left":[]}}} -->
+	<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--contrast);border-top-width:1px;border-bottom-color:var(--wp--preset--color--contrast);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:pattern {"slug":"fotograma/header"} /-->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%"} -->
+		<div class="wp-block-column" style="flex-basis:25%"></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'August 1973: North Philadelphia Children', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'Photographer: Dick Swanson', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-column" style="flex-basis:25%">
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size"><?php echo esc_html__( 'In the 1970s, North Philadelphia, a largely African American, working-class area, boasted rich history and lively culture. With row houses, apartments, small businesses, it nurtured a close-knit community with robust pride and belonging.', 'fotograma' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-vOxIhoEXPvc-unsplash.jpg","dimRatio":0,"focalPoint":{"x":0.5,"y":0.8},"style":{"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/documerica-vOxIhoEXPvc-unsplash.jpg" style="object-position:50% 80%" data-object-fit="cover" data-object-position="50% 80%"/>
+		<div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size"></p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
