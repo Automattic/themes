a583de3c252a5bbf61bf2b37f442a2e415d0ef1f
Jeff Ong <jonger4@gmail.com>
2023-07-07 12:04:37 -0400

    Escape and clean up.

--- grammerone/patterns/header-2.php
+++ grammerone/patterns/header-2.php
@@ -6,16 +6,17 @@
  * Inserter: no
  */
 ?>
+
 <!-- wp:cover {"dimRatio":0,"overlayColor":"primary","minHeight":8,"minHeightUnit":"rem","align":"full","style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"},"padding":{"top":"1rem","bottom":"0rem","left":"2rem","right":"2rem"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover alignfull" style="margin-top:0rem;margin-bottom:0rem;padding-top:1rem;padding-right:2rem;padding-bottom:0rem;padding-left:2rem;min-height:8rem"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"1.5rem","padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"stretch"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:image {"id":1221,"width":80,"height":80,"sizeSlug":"full","linkDestination":"custom","style":{"border":{"radius":"4px","width":"1px","color":"#222329"},"color":{"duotone":["#222329","#a5a4b2"]}},"className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-resized has-custom-border is-style-default"><a href="https://grammerone.mystagingwebsite.com/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/hand-light-photography-photographer-green-lens-595221-pxhere.com_-1.jpg" alt="" class="has-border-color wp-image-1221" style="border-color:#222329;border-width:1px;border-radius:4px" width="80" height="80"/></a></figure>
+<figure class="wp-block-image size-full is-resized has-custom-border is-style-default"><a href="/"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hand-light-photography-photographer-green-lens-595221-pxhere.com_-1.jpg" alt="" class="has-border-color wp-image-1221" style="border-color:#222329;border-width:1px;border-radius:4px;width:80px;height:80px" width="80" height="80"/></a></figure>
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"stretch"}} -->
 <div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1"},"spacing":{"padding":{"top":"0px","bottom":"0px"},"margin":{"top":"0px","bottom":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontSize":"small"} -->
-<h1 class="wp-block-heading has-background-color has-text-color has-link-color has-small-font-size" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-bottom:0px;line-height:1"><a href="https://grammerone.mystagingwebsite.com/" data-type="URL" data-id="https://grammerone.mystagingwebsite.com/"><?php echo esc_html__( 'Jacob Gola', 'grammerone' ); ?></a></h1>
+<h1 class="wp-block-heading has-background-color has-text-color has-link-color has-small-font-size" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-bottom:0px;line-height:1"><a href="/" data-type="URL" data-id="/"><?php echo esc_html__( 'Jacob Gola', 'grammerone' ); ?></a></h1>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0rem"},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"textColor":"foreground","layout":{"type":"flex","orientation":"vertical"}} -->
