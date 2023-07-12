c159f51d3d5449e3d5e96f2ceaef068a953023bb
Jeff Ong <jonger4@gmail.com>
2023-07-12 14:19:29 -0400

    Escape.

--- nested/patterns/404.php
+++ nested/patterns/404.php
@@ -8,9 +8,9 @@
 ?>
 
 <!-- wp:heading {"textAlign":"left","level":1,"anchor":"oops-that-page-can-t-be-found"} -->
-<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found">Oops! That page can’t be found.</h1>
+<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can’t be found.', 'nested' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"1.5rem"}}} -->
-<p style="font-size:1.5rem">It looks like nothing was found at this location. Maybe try a search?</p>
+<p style="font-size:1.5rem"><?php echo esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'nested' ); ?></p>
 <!-- /wp:paragraph -->
