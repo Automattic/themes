665e759123ce4db644e901e92c9d3fddc9a1696d
Jason Crist <jcrist@pbking.com>
2023-07-13 11:35:34 -0400

    Adding theme Didone from @beafialho (#7239)
    * Adding theme Didone from @beafialho
    
    * Slight adjustments to patterns; Removed unused comments pattern, started using (and made ajustments to match templates) footer and no results patterns

---
+++ didone/patterns/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Default footer
+ * Slug: didone/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"},"blockGap":"8px"}}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:site-title {"textAlign":"center"} /-->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.9rem"}}} -->
+<p class="has-text-align-center" style="font-size:0.9rem">
+<?php
+                /* Translators: WordPress link. */
+                $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'didone' ) ) . '" rel="nofollow">WordPress</a>';
+                echo sprintf(
+                    esc_html__( 'Designed with %1$s', 'didone' ),
+                    $wordpress_link
+                );
+            ?>
+</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ didone/patterns/hidden-no-results-content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Hidden No Results Content
+ * Slug: didone/hidden-no-results-content
+ * Inserter: no
+ */
+?>
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center pickles"><?php echo esc_html_x( 'Sorry, but no thing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'didone' ); ?></p>
+<!-- /wp:paragraph -->
+
