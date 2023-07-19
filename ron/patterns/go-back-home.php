dc930fbcc585f5f308d02416cdc51431165a05b7
Vicente Canales <vicente.canales@automattic.com>
2023-07-19 12:36:19 -0400

    Add 'Go back' pattern
    Needed in order to get the actual Home page URL using PHP.

---
+++ ron/patterns/go-back-home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Go back home
+ * Slug: ron/go-back-home
+ * Categories: go-back-home
+ */
+?>
+
+<!-- wp:paragraph -->
+<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Go back</a></p>
+<!-- /wp:paragraph -->
