750e466b521a7984b4dc01154f05a39260e605b4
Sarah Norris <sarah@sekai.co.uk>
2023-07-20 16:30:53 +0100

    Make no results content translatable

--- lativ/patterns/hidden-no-results-content.php
+++ lativ/patterns/hidden-no-results-content.php
@@ -5,6 +5,6 @@
  * Inserter: no
  */
 ?>
-<!-- wp:paragraph -->
-<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'lativ' ); ?></p>
+<!-- wp:paragraph {"align":"center","placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p class="has-text-align-center"><?php echo esc_html_x( 'No posts were found for your search term.', 'Message explaining that there are no results returned from a search', 'lativ' ); ?></p>
 <!-- /wp:paragraph -->
