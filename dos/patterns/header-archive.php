fdab419bcef1bc1bc9a9724eac95c5d3f6679ffe
madhusudhand <madhusudhan.dollu@gmail.com>
2023-07-26 11:56:42 +0530

    DOS: Add theme

---
+++ dos/patterns/header-archive.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: header-archive
+ * Slug: dos/header-archive
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"4rem","left":"4rem"}}},"backgroundColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-right:4rem;padding-left:4rem"><!-- wp:group {"align":"full","style":{"border":{"color":null,"style":null,"width":null,"top":[],"right":[],"bottom":{"color":"var:preset|color|primary","style":"dashed","width":"1px"},"left":[]},"spacing":{"padding":{"bottom":"1.4rem","top":"1.4rem"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignfull" style="border-bottom-color:var(--wp--preset--color--primary);border-bottom-style:dashed;border-bottom-width:1px;padding-top:1.4rem;padding-bottom:1.4rem"><!-- wp:group {"style":{"spacing":{"blockGap":"2rem","margin":{"top":"0px","bottom":"0px"}},"layout":{"selfStretch":"fixed","flexSize":"75%"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:image {"id":252,"width":12,"height":25,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/blink-block_default.gif" alt="" class="wp-image-252" width="12" height="25"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:site-tagline {"style":{"typography":{"fontSize":"1.2rem"}}} /-->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontSize":"1.2rem","fontStyle":"normal","fontWeight":"400"}}} /-->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.2rem"}}} -->
+<p style="font-size:1.2rem">/</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.2rem"}}} -->
+<p style="font-size:1.2rem">Archive</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"1.2rem"}}} -->
+<p style="font-size:1.2rem">/</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:query-title {"type":"archive"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"2rem","margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:image {"id":254,"width":19,"height":25,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/blink-menu_default.gif" alt="" class="wp-image-254" width="19" height="25"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:navigation {"ref":13,"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"vertical"},"style":{"typography":{"fontSize":"1.2rem","textTransform":"uppercase"},"spacing":{"blockGap":"0px"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ dos/readme.txt
@@ -0,0 +1,32 @@
+=== DOS ===
+Contributors: the WordPress team
+Requires at least: 5.8
+Tested up to: 6.2
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+DOS is a blog theme designed for the nostalgic ones, a tribute to the folks that invented computing as we know it today. Let us blog as if we were back to green (or amber) phosphor back again.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial release
+
+== Copyright ==
+
+DOS WordPress Theme, (C) 2023 the WordPress team
+DOS is distributed under the terms of the GNU GPL.
+DOS is based on Block Canvas (https://github.com/Automattic/themes/tree/trunk/block-canvas), (C) Automattic, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
