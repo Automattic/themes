d6368e20d2de17edb37e5204da364cb50bbcdbce
madhusudhand <madhusudhan.dollu@gmail.com>
2023-07-26 10:02:52 +0530

    fix patterns

--- screenplay/patterns/hidden-no-results-content.php
+++ screenplay/patterns/hidden-no-results-content.php
@@ -5,6 +5,13 @@
  * Inserter: no
  */
 ?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1rem"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":1,"style":{"typography":{"textTransform":"uppercase"}}} -->
+<h1 class="wp-block-heading" style="text-transform:uppercase"><?php echo esc_html__( 'Sorry, but nothing matched your search terms.', 'screenplay' ); ?></h1>
+<!-- /wp:heading -->
+
 <!-- wp:paragraph -->
-<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'screenplay' ); ?></p>
-<!-- /wp:paragraph -->
+<p><?php echo esc_html__( 'Please try again with some different keywords.', 'screenplay' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
