7650e9993d8322995bfecf06246ca88e5bf77d2f
Sarah Norris <sarah@sekai.co.uk>
2023-07-31 17:58:59 +0100

    Move footer content to pattern

--- overlaid/patterns/footer.php
+++ overlaid/patterns/footer.php
@@ -7,27 +7,21 @@
  */
 ?>
 
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px","padding":{"top":"2rem","right":"0px","bottom":"4rem","left":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:2rem;padding-right:0px;padding-bottom:4rem;padding-left:0px"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"fontSize":"1.1rem","textTransform":"uppercase","letterSpacing":"0.05rem"}},"textColor":"foreground"} -->
+<p class="has-text-align-center has-foreground-color has-text-color has-link-color" style="font-size:1.1rem;letter-spacing:0.05rem;text-transform:uppercase">
+	<?php
+		/* Translators: WordPress link. */
+		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'overlaid' ) ) . '" rel="nofollow">WordPress</a>';
+		echo sprintf(
+			esc_html__( 'Designed with %1$s', 'overlaid' ),
+			$wordpress_link
+		);
+		?>
+</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
 <!-- wp:spacer {"height":"var(--wp--preset--spacing--80)"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
-
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center">
-			<?php
-				/* Translators: WordPress link. */
-				$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'overlaid' ) ) . '" rel="nofollow">WordPress</a>';
-				echo sprintf(
-					esc_html__( 'Designed with %1$s', 'overlaid' ),
-					$wordpress_link
-				);
-				?>
-		</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
-<!-- /wp:group -->
