9fa3ffe2cdbcc738c65ad83186f9a942468ebd0c
Jason Crist <jcrist@pbking.com>
2023-07-31 15:12:37 -0400

    Adding site title to header-front pattern per design

--- snd/patterns/header-front.php
+++ snd/patterns/header-front.php
@@ -9,6 +9,7 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
 	<!-- wp:site-logo {"width":140,"shouldSyncIcon":false,"className":"is-style-rounded"} /-->
+	<!-- wp:site-title {"fontSize":"xxx-large"} /-->
 </div>
 <!-- /wp:group -->
 
