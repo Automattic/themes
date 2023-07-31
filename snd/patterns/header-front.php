ed08da81884b360b8257a8e5e5582aa84b267713
alaczek <ola@bodera.com>
2023-08-01 05:14:22 +1000

    SND: Add theme (#7270)
    Co-authored-by: Jason Crist <jcrist@pbking.com>

---
+++ snd/patterns/header-front.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Header Front
+ * Slug: snd/header-front
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:site-logo {"width":140,"shouldSyncIcon":false,"className":"is-style-rounded"} /-->
+	<!-- wp:site-title {"fontSize":"xxx-large"} /-->
+</div>
+<!-- /wp:group -->
+
