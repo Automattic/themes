ee7062ab50c06864961896176e745332c2364d3e
Jeff Ong <jonger4@gmail.com>
2023-08-04 15:23:04 -0400

    Escape patterns.

--- organizer/patterns/404.php
+++ organizer/patterns/404.php
@@ -12,10 +12,10 @@
 <div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"4rem","top":"4rem"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"left","level":1,"align":"wide","style":{"typography":{"fontSize":"3rem","letterSpacing":"-0.02rem","lineHeight":"1.2"}},"fontFamily":"libre-baskerville","anchor":"oops-that-page-can-t-be-found"} -->
-<h1 class="wp-block-heading alignwide has-text-align-left has-libre-baskerville-font-family" id="oops-that-page-can-t-be-found" style="font-size:3rem;letter-spacing:-0.02rem;line-height:1.2">Oops!<br>That page can’t be found.</h1>
+<h1 class="wp-block-heading alignwide has-text-align-left has-libre-baskerville-font-family" id="oops-that-page-can-t-be-found" style="font-size:3rem;letter-spacing:-0.02rem;line-height:1.2"><?php echo esc_html__( 'Oops!', 'organizer' ); ?><br><?php echo esc_html__( 'That page can’t be found.', 'organizer' ); ?></h1>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
-<p>It looks like nothing was found at this location. Maybe try a search?</p>
+<p><?php echo esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'organizer' ); ?></p>
 <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group --></div>
@@ -23,7 +23,7 @@
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"1rem"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-bottom:1rem"><!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"letterSpacing":"-0.02rem","textTransform":"uppercase","fontSize":"0.9rem","fontStyle":"normal","fontWeight":"400","lineHeight":"1.1"}}} -->
-<h1 class="wp-block-heading alignwide" style="font-size:0.9rem;font-style:normal;font-weight:400;letter-spacing:-0.02rem;line-height:1.1;text-transform:uppercase">Search</h1>
+<h1 class="wp-block-heading alignwide" style="font-size:0.9rem;font-style:normal;font-weight:400;letter-spacing:-0.02rem;line-height:1.1;text-transform:uppercase"><?php echo esc_html__( 'Search', 'organizer' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"2rem"}}},"layout":{"type":"default"}} -->
