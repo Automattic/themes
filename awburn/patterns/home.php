aa9fd4d844f976b417e80f9c0ebab4422d8b63b7
Jeff Ong <jonger4@gmail.com>
2023-08-30 21:29:14 +0200

    Awburn: add theme (#7310)
    * Initial commit and remove theme attribute.
    
    * Rename to awburn.
    
    * Add home template.
    
    * Remove comments.
    
    * Escape patterns.
    
    * Update to reference patterns.
    
    * Version bump.
    
    * Consolidate footer patterns.
    
    * Fix home.
    
    * Rename images.
    
    * Convert to webp and compress.
    
    * Add font license info.
    
    * Add layout constrain to post content.
    
    * Image license credits and missing image.
    
    * Bump version.

---
+++ awburn/patterns/home.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Title: home
+ * Slug: awburn/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"left":"40px","right":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:0px;padding-left:40px"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/awburn_cover.webp","id":12,"dimRatio":0,"isDark":false,"align":"wide","style":{"spacing":{"padding":{"left":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignwide is-light" style="padding-left:0px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-12" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/awburn_cover.webp" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"<?php echo esc_attr__( 'Write title…', 'awburn' ); ?>","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"4rem","padding":{"top":"80px","bottom":"80px","left":"40px","right":"40px"}}},"layout":{"type":"constrained","wideSize":"1200px"}} -->
+<div class="wp-block-group" style="padding-top:80px;padding-right:40px;padding-bottom:80px;padding-left:40px"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"1.4rem"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"align":"wide"} -->
+<h2 class="wp-block-heading alignwide"><?php echo esc_html__( 'More than your average agency', 'awburn' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'At Awburn we deliver outstanding results across 3 key business areas: branding, web design, and digital marketing.', 'awburn' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html__( 'See Our Work', 'awburn' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":73,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo_1.png" alt="" class="wp-image-73"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":74,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo_2.png" alt="" class="wp-image-74"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":75,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo_3.png" alt="" class="wp-image-75"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":76,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo_4.png" alt="" class="wp-image-76"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":77,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo_5.png" alt="" class="wp-image-77"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"60px","bottom":"60px","right":"120px","left":"120px"}},"border":{"radius":"2px","top":{"style":"none","width":"0px"},"right":{"style":"none","width":"0px"},"bottom":{"style":"none","width":"0px"},"left":{"style":"none","width":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"secondary","textColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background-color has-secondary-background-color has-text-color has-background has-link-color" style="border-radius:2px;border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:60px;padding-right:120px;padding-bottom:60px;padding-left:120px"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"70px","left":"70px"}}}} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:20%"><!-- wp:image {"id":38,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/awburn_image_2.webp" alt="" class="wp-image-38"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"lineHeight":1.3,"fontSize":"1.6rem"}}} -->
+<p style="font-size:1.6rem;line-height:1.3"><?php echo esc_html__( '“Awburn took care of our online marketing with some amazing results. We are now the first result in every search engine.”', 'awburn' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.8rem"}}} -->
+<p style="font-size:0.8rem"><?php echo esc_html__( 'Mike Wallace, Wayne Enterprises', 'awburn' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ awburn/readme.txt
@@ -0,0 +1,62 @@
+Contributors: Automattic
+Requires at least: 5.8
+Tested up to: 6.3
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Awburn is the ideal choice for creating an online presence for your business. As a block theme, Awburn allows you to choose any of the available blocks to create a wide range of content for your site. It comes with a set of color variations and templates to choose from.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial release
+
+== Copyright ==
+
+Awburn WordPress Theme, (C) 2023 Automattic
+Awburn is distributed under the terms of the GNU GPL.
+Awburn is based on Block Canvas (https://github.com/Automattic/themes/tree/trunk/block-canvas), (C) Automattic, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+Inter from Google Fonts, Open Font License (https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL)
+https://fonts.google.com/specimen/Inter
+
+Literata
+License: SIL Open Font License, 1.1
+Source: https://fonts.google.com/specimen/Literata
+
+awburn_image_2.webp
+https://stocksnap.io/photo/google-analytics-89AZTB8E5H
+CC0 Public Domain
+-
+webdesign-1024x683.webp
+https://stocksnap.io/photo/mobile-wireframe-QYCEJ7W1PG
+CC0 Public Domain
+-
+marketing-1024x683.webp
+https://pxhere.com/pt/photo/706575
+CC0 Public Domain
+-
+branding-1024x683.webp
+https://www.rawpixel.com/image/3337336/free-photo-image-wooden-desk-table
+CC0 Public Domain
+-
+awburn_cover.webp
+https://pxhere.com/pt/photo/1590
+CC0 Public Domain
+-
+awburn_testimonial.webp
+https://stocksnap.io/photo/woman-business-8AF1UG3C6N
+CC0 Public Domain
