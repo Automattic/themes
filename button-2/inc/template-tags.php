0d46c404ef31f58b5d800967a0a8827658ac6250
Matthew Reishus <mreishus@users.noreply.github.com>
2023-09-25 11:45:23 -0500

    Public Themes B: PHP Rector fixes

--- button-2/inc/template-tags.php
+++ button-2/inc/template-tags.php
@@ -100,7 +100,7 @@ function button_2_categorized_blog() {
 		) );
 
 		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+		$all_the_cool_cats = is_countable( $all_the_cool_cats ) ? count( $all_the_cool_cats ) : 0;
 
 		set_transient( 'button_2_categories', $all_the_cool_cats );
 	}
