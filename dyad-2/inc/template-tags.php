464be6edb970df39223ff486a87e854ab1ceee6b
Matthew Reishus <mreishus@users.noreply.github.com>
2023-09-25 15:28:25 -0500

    Public Themes D: PHP Rector fixes (#7385)

--- dyad-2/inc/template-tags.php
+++ dyad-2/inc/template-tags.php
@@ -95,7 +95,7 @@ function dyad_2_categorized_blog() {
 		) );
 
 		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+		$all_the_cool_cats = is_countable( $all_the_cool_cats ) ? count( $all_the_cool_cats ) : 0;
 
 		set_transient( 'dyad_2_categories', $all_the_cool_cats );
 	}
