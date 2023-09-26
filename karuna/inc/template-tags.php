6d53bddb07f7bc89f703fd020576c0bd5503c21e
Matthew Reishus <mreishus@users.noreply.github.com>
2023-09-26 16:23:26 -0500

    Public Themes J, K, L: PHP Rector fixes

--- karuna/inc/template-tags.php
+++ karuna/inc/template-tags.php
@@ -92,7 +92,7 @@ function karuna_categorized_blog() {
 		) );
 
 		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+		$all_the_cool_cats = is_countable( $all_the_cool_cats ) ? count( $all_the_cool_cats ) : 0;
 
 		set_transient( 'karuna_categories', $all_the_cool_cats );
 	}
