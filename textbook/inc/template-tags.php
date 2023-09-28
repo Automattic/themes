6c3040608472a5c58b904b8fa7cb121a612ddeaa
Matthew Reishus <mreishus@users.noreply.github.com>
2023-09-28 11:10:25 -0500

    Public Themes T1: PHP Rector fixes

--- textbook/inc/template-tags.php
+++ textbook/inc/template-tags.php
@@ -117,7 +117,7 @@ function textbook_categorized_blog() {
 		) );
 
 		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+		$all_the_cool_cats = is_countable( $all_the_cool_cats ) ? count( $all_the_cool_cats ) : 0;
 
 		set_transient( 'textbook_categories', $all_the_cool_cats );
 	}
