b48ac5f4374770a94b9ce0b3f875865ed7852ae4
Vicente Canales <vicente.canales@automattic.com>
2023-10-05 19:12:12 -0300

    Add big header pattern

--- programme/patterns/big-header.php
+++ programme/patterns/big-header.php
@@ -9,10 +9,10 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
 <div class="wp-block-group">
 <!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"0.9"}}} -->
-<h1 class="wp-block-heading" style="line-height:0.9"><a href="<?php echo home_url(); ?>">Little Theatre</a></h1>
+<h1 class="wp-block-heading" style="line-height:0.9"><a href="<?php echo home_url(); ?>"><?php echo esc_html__( 'Little Theatre', 'programme' ); ?></a></h1>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"0.9"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} -->
-<h1 class="wp-block-heading has-primary-color has-text-color has-link-color" style="line-height:0.9"><a href="<?php echo home_url(); ?>">of Mainland</a></h1>
+<h1 class="wp-block-heading has-primary-color has-text-color has-link-color" style="line-height:0.9"><a href="<?php echo home_url(); ?>"><?php echo esc_html__( 'of Mainland', 'programme' ); ?></a></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
