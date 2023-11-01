01c6ae7ccc38567bd9b0a829bf6e1c90edc9723c
Jeff Ong <jonger4@gmail.com>
2023-11-01 16:29:32 +0100

    Magalog: add theme (#7453)
    * Initial commit from zip.
    
    * Remove incorrect theme attribute.
    
    * Fix refs to malog.
    
    * Lots of bug fixes.
    
    * Add credits and bump.

---
+++ magalog/patterns/archive.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Title: archive
+ * Slug: magalog/archive
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"},"blockGap":{"top":"0rem","left":"0rem"}},"border":{"color":null,"style":null,"width":null,"top":{"color":"var:preset|color|secondary","style":"solid","width":"4rem"},"right":[],"bottom":[],"left":[]}}} -->
+<div class="wp-block-columns alignfull" style="border-top-color:var(--wp--preset--color--secondary);border-top-style:solid;border-top-width:4rem;margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"width":"22.5%","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"blockGap":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-column" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;flex-basis:22.5%"><!-- wp:template-part {"slug":"sidebar"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"77.5%","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"blockGap":"0px"}},"backgroundColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-column has-white-background-color has-background" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;flex-basis:77.5%"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","right":"4rem","bottom":"0rem","left":"4rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:4rem;padding-bottom:0rem;padding-left:4rem"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","right":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"2rem"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|custom-coral-dream-1"}}}},"textColor":"custom-coral-dream-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-custom-coral-dream-1-color has-text-color has-link-color" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:spacer {"height":"8rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:query-title {"type":"archive","style":{"spacing":{"margin":{"bottom":"0px","top":"0px","right":"0px","left":"0px"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"typography":{"letterSpacing":"0px"}},"textColor":"custom-coral-dream-1"} /-->
+
+<!-- wp:spacer {"height":"4rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":100,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"tagName":"main","align":"full","layout":{"type":"constrained"}} -->
+<main class="wp-block-query alignfull"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:post-template {"align":"full"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0rem"}},"layout":{"type":"constrained","contentSize":"100%","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0rem"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:spacer {"height":"2rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:post-title {"textAlign":"left","isLink":true} /-->
+
+<!-- wp:spacer {"height":"64px","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:64px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1rem"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:post-date {"style":{"typography":{"textTransform":"uppercase","lineHeight":1,"fontSize":"1rem","letterSpacing":"1px"},"spacing":{"blockGap":"1rem"},"elements":{"link":{"color":{"text":"var:preset|color|custom-coral-dream-1"}}}},"textColor":"custom-coral-dream-1","fontFamily":"rubik"} /-->
+
+<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","lineHeight":1,"fontSize":"1rem","letterSpacing":"1px"},"spacing":{"blockGap":"1rem"}},"fontFamily":"rubik"} -->
+<p class="has-rubik-font-family" style="font-size:1rem;letter-spacing:1px;line-height:1;text-transform:uppercase"><?php echo esc_html__( '·', 'magalog' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-terms {"term":"category","style":{"typography":{"textTransform":"uppercase","lineHeight":1,"fontSize":"1rem","letterSpacing":"1px"},"spacing":{"blockGap":"1rem"}},"fontFamily":"rubik"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"2rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":100,"style":{"typography":{"fontSize":"1.4rem"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:spacer {"height":"2rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":360,"width":32,"height":32,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/vector_arrow-1.png" alt="" class="wp-image-360" width="32" height="32"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"8rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:group -->
+
+<!-- wp:query-pagination {"align":"full","style":{"typography":{"fontSize":"3.6rem","lineHeight":2}}} -->
+<!-- wp:query-pagination-previous {"label":"<?php echo esc_html__( 'Previous', 'magalog' ); ?>"} /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next {"label":"<?php echo esc_html__( 'Next', 'magalog' ); ?>"} /-->
+<!-- /wp:query-pagination --></main>
+<!-- /wp:query -->
+
+<!-- wp:spacer {"height":"8rem","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div style="margin-top:0px;margin-bottom:0px;height:8rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","align":"full"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ magalog/readme.txt
@@ -0,0 +1,42 @@
+=== Magalog ===
+Contributors: the WordPress team
+Requires at least: 5.8
+Tested up to: 6.3
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+A beautifully designed blog theme displaying large typography and customizable color palettes. With a clean and modern design, Magalog offers a unique and engaging way to showcase your blog content.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Magalog WordPress Theme, (C) 2023 the WordPress team
+Magalog is distributed under the terms of the GNU GPL.
+Magalog is based on Magalog (https://github.com/wordpress/magalog/), (C) the WordPress team, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+This theme bundles the following third-party resources:
+
+Rubik Font, Copyright Philipp Hubert and Sebastian Fischer at Hubert & Fischer 
+as part of the Chrome Cube Lab project
+Licensed under the terms of the SIL Open Font License (v1.1)
+Source: https://github.com/googlefonts/Rubik
+
+Linik Sans is developed from Inter, version 3.3, and Manix Sans HL fonts. The changes are: truly tabular figures, and tailed 'l'.
+Linik Sans is licensed under the SIL Open Font License (OFL)
