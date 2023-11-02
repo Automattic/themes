e4993aaad50f53703ea92a02ca3920164b74d21c
Jeff Ong <jonger4@gmail.com>
2023-11-02 11:29:57 -0400

    Escape patterns.

--- stage/patterns/404.php
+++ stage/patterns/404.php
@@ -13,11 +13,11 @@
 <div class="wp-block-columns alignwide"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem","padding":{"top":"1rem","right":"1rem","bottom":"1rem","left":"1rem"},"margin":{"top":"0px","bottom":"0px"}},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between","flexWrap":"wrap"}} -->
 <div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1rem"><!-- wp:heading {"textAlign":"left","level":1,"anchor":"oops-that-page-can-t-be-found"} -->
-<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found">Oops...</h1>
+<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops...', 'stage' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading">404, Sorry.</h1>
+<h1 class="wp-block-heading"><?php echo esc_html__( '404, Sorry.', 'stage' ); ?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -25,7 +25,7 @@
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"","padding":{"top":"1rem","right":"1rem","bottom":"1rem","left":"1rem"},"margin":{"top":"0px","bottom":"0px"}},"layout":{"selfStretch":"fill","flexSize":null},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
 <div class="wp-block-group" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1rem"><!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1rem"}}} -->
-<p style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">It seems like nothing was found at this location.<br>Maybe you should try a search?</p>
+<p style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><?php echo esc_html__( 'It seems like nothing was found at this location.', 'stage' ); ?><br><?php echo esc_html__( 'Maybe you should try a search?', 'stage' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"typography":{"fontSize":"0.8rem","letterSpacing":"0.02rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
