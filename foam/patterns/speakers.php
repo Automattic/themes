c414ecdf4e90a879cc8f315d899b559949ae14e6
Jeff Ong <jonger4@gmail.com>
2023-11-03 10:26:54 -0400

    Initial zip.

---
+++ foam/patterns/speakers.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Title: speakers
+ * Slug: foam/speakers
+ * Categories: Text
+ * Inserter: yes
+ */
+?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":56,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_1.png" alt="" class="wp-image-56" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Mark Stu</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>An influential voice in cybersecurity, Mark Stu will address the evolving challenges of online security and the need for inclusive strategies to safeguard digital infrastructure, protecting individuals and organizations from cyber threats.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":60,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_6.png" alt="" class="wp-image-60" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Rita Verma</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>A leading expert in digital marketing and consumer behavior, Verma will explore the power of inclusive marketing strategies and how businesses can effectively engage diverse audiences in the digital era.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":61,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_3.png" alt="" class="wp-image-61" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Kendall Ramirez</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>A respected expert in human-computer interaction, Kendall Ramirez will explore the principles of inclusive design, emphasizing the need to create digital experiences that are accessible and user-friendly for individuals of all abilities.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":57,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_4.png" alt="" class="wp-image-57" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Dr. Isabelle Khan</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>A trailblazer in the field of virtual reality, Dr. Khan will delve into the ways VR can be harnessed for educational purposes, focusing on inclusivity in VR experiences and the democratization of knowledge.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":58,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_2.png" alt="" class="wp-image-58" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Alex Wong</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>A prominent figure in the world of venture capital, Alex Wong will shed light on the importance of diversity and inclusion in investment decisions, promoting equity and providing opportunities for underrepresented founders.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":59,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
+<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="https://foam.mystagingwebsite.com/wp-content/uploads/2023/06/speaker_5.png" alt="" class="wp-image-59" width="120" height="120"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Robert Nguyen</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>A pioneer in the field of augmented reality, Robert Nguyen will showcase the potential of AR technologies in creating inclusive and immersive digital experiences, revolutionizing industries such as education, healthcare, and entertainment.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ foam/readme.txt
@@ -0,0 +1,65 @@
+=== Foam ===
+Contributors: Automattic
+Requires at least: 6.0
+Tested up to: 6.0
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Foam is a WordPress event theme with a vibrant, straightforward style. Entirely customisable, Foam allows you to present an event in a visually appealing way. Foam features a set of useful event patterns and essential templates.
+
+== Changelog ==
+
+= 0.0.27 =
+* Add link styles for post-terms (#7058)
+
+= 0.0.26 =
+* Remove table CSS from block canvas theme as it has been addressed in Gutenberg (#6954)
+
+= 0.0.24 =
+* Foam: Add no results content to search template (#6860)
+
+= 0.0.23 =
+* Remove font-smoothing CSS (#6816)
+
+= 0.0.22 =
+* Add home.html template to Foam (#6784)
+
+= 0.0.21 =
+* Foam: Add self-closing tag to comments pattern in page template (#6764)
+
+= 0.0.20 =
+* Foam: small updates (#6705)
+
+= 0.0.19 =
+* Make theme available for translation as much as possible (#6484)
+
+= 0.0.18 =
+* Update block canvas styles (#6640)
+
+= 0.0.17 =
+* Block canvas: clean up templates (#6620)
+
+== Copyright ==
+
+Foam WordPress Theme, (C) 2022 Automattic
+Foam is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+Roboto Mono Font
+Copyright 2015 The Roboto Mono Project Authors (https://github.com/googlefonts/robotomono) 
+Licensed under the Apache License, Version 2.0 
+License URL: http://www.apache.org/licenses/LICENSE-2.0 
+Source: Google.com
+-- End of Roboto Mono Font credits --
