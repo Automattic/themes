ef35935d05effbb7cd7a4c3f6ea2d3dc9ab12e70
Jeff Ong <jonger4@gmail.com>
2023-11-03 10:04:16 -0400

    Initial zip.

---
+++ sunderland/patterns/home.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: home
+ * Slug: sunderland/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/summertime_image.png","id":22,"dimRatio":10,"overlayColor":"base","minHeight":100,"minHeightUnit":"vh","style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"textColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-contrast-color has-text-color" style="padding-right:5vw;padding-left:5vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-10 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-22" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/summertime_image.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"right":"0vw","left":"0vw"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group alignfull" style="min-height:100vh;padding-right:0vw;padding-left:0vw"><!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"0px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"139px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0.2em"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-title /-->
+
+<!-- wp:site-tagline /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"19px","style":{"layout":{"flexSize":"19px","selfStretch":"fixed"}}} -->
+<div style="height:19px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontSize":"0.8rem"}}} -->
+<p style="font-size:0.8rem;text-transform:uppercase"><strong>in stores June 26th<br></strong><br>Release Party and exclusive pre-show cocktail reception<br>Eden Garden Amphitheater on June 25th<br><a href="https://momentum.mystagingwebsite.com/sample-page/">more info here</a><br></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#ffd17b","size":"has-small-icon-size","style":{"spacing":{"blockGap":{"top":"1rem","left":"1rem"}}},"className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://momentum.mystagingwebsite.com","service":"soundcloud"} /-->
+
+<!-- wp:social-link {"url":"https://momentum.mystagingwebsite.com","service":"bandcamp"} /-->
+
+<!-- wp:social-link {"url":"https://momentum.mystagingwebsite.com","service":"spotify"} /-->
+
+<!-- wp:social-link {"url":"https://momentum.mystagingwebsite.com","service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"60px","style":{"layout":{"flexSize":"60px","selfStretch":"fixed"}}} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
---
+++ sunderland/readme.txt
@@ -0,0 +1,63 @@
+=== Sunderland ===
+Contributors: Automattic
+Requires at least: 6.0
+Tested up to: 6.0
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Sunderland is a simple WordPress event theme designed for announcing upcoming events in a visually captivating way. Sunderland's character lies in its immersive landing page, with an easily switchable full width background image. Sunderland is the ideal choice for bands, musicians, artists or event organizers who want a sleek and eye-catching website to inform and engage their audience.
+
+== Changelog ==
+
+= 0.0.27 =
+* Add link styles for post-terms (#7058)
+
+= 0.0.26 =
+* Remove table CSS from block canvas theme as it has been addressed in Gutenberg (#6954)
+
+= 0.0.24 =
+* Sunderland: Add no results content to search template (#6860)
+
+= 0.0.23 =
+* Remove font-smoothing CSS (#6816)
+
+= 0.0.22 =
+* Add home.html template to Sunderland (#6784)
+
+= 0.0.21 =
+* Sunderland: Add self-closing tag to comments pattern in page template (#6764)
+
+= 0.0.20 =
+* Sunderland: small updates (#6705)
+
+= 0.0.19 =
+* Make theme available for translation as much as possible (#6484)
+
+= 0.0.18 =
+* Update block canvas styles (#6640)
+
+= 0.0.17 =
+* Block canvas: clean up templates (#6620)
+
+== Copyright ==
+
+Sunderland WordPress Theme, (C) 2022 Automattic
+Sunderland is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+Helvetica Font
+© 1990-2006 Apple Computer Inc. © 1981 Linotype AG © 1990-91 Type Solutions Inc. 
+  
+-- End of Helvetica Font credits --
