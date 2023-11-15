80a5c31a4b7961f42b49f341ea7ff4336e523148
Takashi Irie <takashi@automattic.com>
2023-11-15 23:33:13 +0000

    Archivo: Update heading type sizes

--- archivo/patterns/hidden-404.php
+++ archivo/patterns/hidden-404.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.666666667}},"fontSize":"medium"} -->
-<h1 class="wp-block-heading has-text-align-left has-medium-font-size" id="oops-that-page-can-t-be-found" style="font-style:normal;font-weight:700;line-height:1.666666667;">
+<!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.388888889}},"fontSize":"medium"} -->
+<h1 class="wp-block-heading has-text-align-left has-medium-font-size" id="oops-that-page-can-t-be-found" style="font-style:normal;font-weight:700;line-height:1.388888889;">
 	<?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'archivo' ); ?>
 </h1>
 <!-- /wp:heading -->
