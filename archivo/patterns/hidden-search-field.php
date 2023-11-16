10704194950cad4771db3381a264f28ce0d29972
Takashi Irie <takashi@automattic.com>
2023-11-16 16:59:23 +0100

    Prepare Archivo for dotorg submission (#7489)
    * Archivo: Prepare for the dotorg submission
    
    * Archivo: Added new templates and patterns for Woo
    
    * Archivo: Custom Template declaration
    
    * Archivo: Update heading type sizes
    
    * Archivo Update the min version

--- archivo/patterns/hidden-search-field.php
+++ archivo/patterns/hidden-search-field.php
@@ -5,4 +5,4 @@
  * Inserter: no
  */
 ?>
-<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'archivo' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'archivo' ); ?>"} /-->
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder for search field', 'archivo' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'archivo' ); ?>"} /-->
