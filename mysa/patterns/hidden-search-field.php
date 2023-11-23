9964e94cede63295e56f72982105f73a85cd40fd
Takashi Irie <takashi@automattic.com>
2023-11-23 18:29:48 +0000

    Mysa: Add theme

---
+++ mysa/patterns/hidden-search-field.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Title: Hidden Search Field
+ * Slug: mysa/hidden-search-field
+ * Inserter: no
+ */
+?>
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder for search field', 'mysa' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'mysa' ); ?>"} /-->
