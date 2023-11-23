3e54d0f04513382609d3980048445f6d7393daf8
Takashi Irie <takashi@automattic.com>
2023-11-23 18:32:49 +0000

    Pomme: Add theme

---
+++ pomme/patterns/hidden-search-field.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Title: Hidden Search Field
+ * Slug: pomme/hidden-search-field
+ * Inserter: no
+ */
+?>
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder for search field', 'pomme' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'pomme' ); ?>"} /-->
