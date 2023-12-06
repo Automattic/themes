6f8c8cf89b182f455704ca4619a69404ba6e2776
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-12-06 13:58:24 +0700

    Fix esc_html_x => esc_html_e

--- assembler/patterns/byline-date.php
+++ assembler/patterns/byline-date.php
@@ -7,7 +7,7 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.26rem","padding":{"bottom":"20px"}},"typography":{"fontSize":"x-small"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"center"}} -->
 <div class="wp-block-group" style="padding-bottom:20px;font-size:x-small"><!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size"><?php echo esc_html_x( 'Published by', 'assembler' ); ?></p>
+<p class="has-x-small-font-size"><?php echo esc_html_e( 'Published by', 'assembler' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:post-author {"showAvatar":false,"showBio":false,"byline":"","fontSize":"x-small"} /-->
