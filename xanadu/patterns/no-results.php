32f96af92ae917b62aef1d04795b35fa5bceccad
Takashi Irie <takashi@automattic.com>
2023-12-06 22:43:48 +0000

    Xanadu: Add theme

---
+++ xanadu/patterns/no-results.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: xanadu/no-results
+ * Inserter: no
+ */
+?>
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
+<p style="margin-top:0px;margin-bottom:0px"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'xanadu' ); ?></p>
+<!-- /wp:paragraph -->
