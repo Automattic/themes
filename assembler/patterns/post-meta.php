591e5c1bbd2334f272ec74a697278a92b3f3a353
Ashar Fuadi <fushar@gmail.com>
2023-12-06 16:46:38 +0700

    Assembler: Add theme (#7528)

---
+++ assembler/patterns/post-meta.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Default post meta
+ * Slug: assembler/post-meta
+ * Categories: post meta
+ * Block Types: core/post-date, core/post-terms
+ */
+?>
+
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"border":{"radius":"4px"}},"backgroundColor":"tertiary","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="border-radius:4px"><!-- wp:post-terms {"term":"category","style":{"spacing":{"padding":{"top":"6px","right":"6px","bottom":"6px","left":"6px"}},"typography":{"fontSize":"0.8rem"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
