35cb6f8e2ab204dfe5493c4ac170434d1919ef04
Takashi Irie <takashi@automattic.com>
2023-12-07 04:26:38 +0100

    Beep: Add theme (#7514)
    * Beep: Add theme
    
    * Beep: Fix the colour of footer template parts
    
    * Beep: Cleanup style variation and improved search no result page.

---
+++ beep/patterns/404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: 404 page
+ * Slug: beep/404
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"textAlign":"left","level":1,"anchor":"oops-that-page-can-t-be-found"} -->
+<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Error: 404 >> Not Found.', 'beep' ); ?></h1>
+<!-- /wp:heading -->
---
+++ beep/patterns/footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Default Footer
+ * Slug: beep/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
+	<!-- wp:group {"templateLock":"insert","lock":{"move":false,"remove":true},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group">
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|inactive"}}}},"textColor":"inactive"} -->
+		<p class="has-inactive-color has-text-color has-link-color"><?php echo esc_html__( 'code_is_poetry', 'beep' ); ?>( '</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p><?php
+		/* Translators: WordPress link. */
+		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'beep' ) ) . '" rel="nofollow">WordPress</a>';
+		echo sprintf(
+			esc_html__( 'Designed with %1$s', 'beep' ),
+			$wordpress_link
+		);?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|inactive"}}}},"textColor":"inactive"} -->
+		<p class="has-inactive-color has-text-color has-link-color">' );</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
