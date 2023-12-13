9dc09dc1b91f229918acc30a9b377e56906f3ef8
Takashi Irie <takashi@automattic.com>
2023-12-13 13:05:13 +0100

    Ici: Add strict types to remaining php files (#7548)

--- ici/patterns/no-results.php
+++ ici/patterns/no-results.php
@@ -4,7 +4,10 @@
  * Slug: ici/no-results
  * Inserter: no
  */
+
+declare( strict_types = 1 );
 ?>
+
 <!-- wp:paragraph -->
 <p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'ici' ); ?></p>
 <!-- /wp:paragraph -->
