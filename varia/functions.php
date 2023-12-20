d0aaca54528e024662f136885746453463180db8
Jason Crist <jcrist@pbking.com>
2023-12-20 12:28:48 -0500

    Default featured images to false and leverage theme mod values that have already been set

--- varia/functions.php
+++ varia/functions.php
@@ -225,15 +225,16 @@ if ( ! function_exists( 'varia_setup' ) ) :
 				'enable_theme_default' => true,
 			)
 		);
-		
+
 		// Add support for Content Options.
 		add_theme_support( 'jetpack-content-options', array(
 			'blog-display' => 'content',
 			'featured-images' => array(
-				'archive'         => true,
-				'archive-default' => true,
-				'post'            => true,
-				'page'            => true,
+				'archive'         => false,
+				'archive-default' => false,
+				'post'            => false,
+				//featured images on pages boolean was previously managed by Varia rather than jetpack.  If that value has been set default to that.
+				'page'            => get_theme_mod( 'show_featured_image_on_pages', false ),
 			),
 		) );
 	}
