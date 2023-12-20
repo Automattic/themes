7de21f40b9eaf278c61415e5f837e17b230611f8
Takashi Irie <takashi@automattic.com>
2023-12-20 20:42:52 +0100

    Trellick: Submission Preps (#7570)

--- trellick/patterns/404.php
+++ trellick/patterns/404.php
@@ -4,7 +4,9 @@
  * Slug: trellick/404
  * Inserter: no
  */
+declare( strict_types = 1 );
 ?>
+
 <!-- wp:heading {"level":1,"fontSize":"medium","anchor":"oops-that-page-can-t-be-found"} -->
 <h1 class="wp-block-heading has-medium-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Nothing found. Maybe try a search?', 'trellick' ); ?></h1>
 <!-- /wp:heading -->
--- trellick/patterns/footer.php
+++ trellick/patterns/footer.php
@@ -6,7 +6,9 @@
  * Block Types: core/template-part/footer
  * Inserter: no
  */
+declare( strict_types = 1 );
 ?>
+
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"x-small"} -->
 <p class="has-x-small-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><?php
 		/* Translators: WordPress link. */
