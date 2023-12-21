a27fec4911458aff32c9d922bf73607345ad7598
Takashi Irie <takashi@automattic.com>
2023-12-21 18:41:59 +0000

    Azur: Initial commit

---
+++ azur/patterns/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Default header
+ * Slug: azur/header
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+
+declare( strict_types = 1 );
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"},"blockGap":"0","margin":{"bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained","contentSize":"800px","justifyContent":"left"}} -->
+<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
+	<!-- wp:site-title /-->
+</div>
+<!-- /wp:group -->
