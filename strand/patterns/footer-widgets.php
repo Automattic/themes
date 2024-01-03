311e9f46a6e7c7576994352f03fb274da5fbe0f7
alaczek <ola@bodera.com>
2024-01-03 19:51:44 +1100

    Update footer widgets
    Minor updates to the content of the footer widgets.

--- strand/patterns/footer-widgets.php
+++ strand/patterns/footer-widgets.php
@@ -10,7 +10,7 @@
 <div class="wp-block-group has-tertiary-background-color has-background" style="border-top-color:#1e1e1e26;border-top-width:1px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--60)"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase","letterSpacing":"1px"}},"textColor":"primary","fontSize":"small"} -->
-<h3 class="wp-block-heading has-primary-color has-text-color has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase"><?php echo  esc_html__( '— Navigation', 'strand' ); ?></h3>
+<h3 class="wp-block-heading has-primary-color has-text-color has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase"><?php echo  esc_html__( 'Navigation', 'strand' ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"blockGap":"0"},"typography":{"fontSize":"1rem","lineHeight":1.6}}} /--></div>
@@ -18,11 +18,11 @@
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase","letterSpacing":"1px"}},"textColor":"primary","fontSize":"small"} -->
-<h3 class="wp-block-heading has-primary-color has-text-color has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase"><?php echo  esc_html__( '— About the author', 'strand' ); ?></h3>
+<h3 class="wp-block-heading has-primary-color has-text-color has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:1px;text-transform:uppercase"><?php echo  esc_html__( 'About the author', 'strand' ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"1rem"}}} -->
-<p style="font-size:1rem"><?php echo  esc_html__( 'Meet Ava Montgomery, a passionate storyteller who started as a journalist and now writes freelance, uncovering unique tales from diverse cultures.', 'strand' ); ?></p>
+<p style="font-size:1rem"><?php echo  esc_html__( 'Meet Ava Montgomery, a passionate storyteller who started as a journalist and now writes freelance,  exploring the intersection of storytelling and productivity.', 'strand' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
