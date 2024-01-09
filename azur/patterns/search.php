4d9635886c9528bf28181adbad8c4c9a8b9c4957
Takashi Irie <takashi@automattic.com>
2024-01-09 20:54:13 +0000

    Azur: Initial commit (#7579)

---
+++ azur/patterns/search.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Search
+ * Slug: azur/search
+ * Insert: no
+ */
+
+declare( strict_types = 1 );
+?>
+
+<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'azur' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'azur' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"border":{"style":"solid","width":"2px"}},"borderColor":"primary"} /-->
