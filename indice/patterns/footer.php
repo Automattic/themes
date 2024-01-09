660b966c549ffab92bbd04adc30dfc1bc4656d4f
Takashi Irie <takashi@automattic.com>
2024-01-09 21:24:34 +0000

    Indice: Make the footer is always at the bottom of the viewport (#7594)

--- indice/patterns/footer.php
+++ indice/patterns/footer.php
@@ -9,37 +9,21 @@
 declare( strict_types = 1 );
 ?>
 
-<!-- wp:columns -->
-<div class="wp-block-columns">
-	<!-- wp:column {"width":"18%"} -->
-	<div class="wp-block-column" style="flex-basis:18%">
-		<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
-		<div class="wp-block-group" style="padding-left:var(--wp--preset--spacing--60)">
-			<!-- wp:paragraph -->
-			<p><?php echo esc_html__( 'Indice –––––––––', 'indice' ); ?></p>
-			<!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+	<!-- wp:paragraph -->
+	<p><?php echo esc_html__( 'Indice –––––––––', 'indice' ); ?></p>
+	<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph -->
-			<p><?php
-				/* Translators: WordPress link. */
-				$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'indice' ) ) . '" rel="nofollow">WordPress</a>';
-				echo sprintf(
-					esc_html__( 'Designed with %1$s', 'indice' ),
-					$wordpress_link
-				);
-			?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:group -->
-	</div>
-	<!-- /wp:column -->
-
-	<!-- wp:column {"width":"50%"} -->
-	<div class="wp-block-column" style="flex-basis:50%"></div>
-	<!-- /wp:column -->
-
-	<!-- wp:column {"width":"18%"} -->
-	<div class="wp-block-column" style="flex-basis:18%"></div>
-	<!-- /wp:column -->
+	<!-- wp:paragraph -->
+	<p><?php
+		/* Translators: WordPress link. */
+		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'indice' ) ) . '" rel="nofollow">WordPress</a>';
+		echo sprintf(
+			esc_html__( 'Designed with %1$s', 'indice' ),
+			$wordpress_link
+		);
+	?></p>
+	<!-- /wp:paragraph -->
 </div>
-<!-- /wp:columns -->
+<!-- /wp:group -->
