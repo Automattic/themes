f735086f905694a0ab3eba2ec5f3a3710566a3aa
Rich Tabor <hi@richtabor.com>
2024-01-12 17:57:29 -0500

    Translation functions

---
+++ assembler/patterns/hidden-no-search-results.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: 404
+ * Slug: assembler/hidden-no-search-results
+ * Inserter: no
+ */
+declare( strict_types = 1 );
+?>
+
+<!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center"><?php echo esc_html_x( 'No search results.', 'Text explaining that there are no results returned from a search', 'assembler' ); ?>.</p>
+<!-- /wp:paragraph -->
