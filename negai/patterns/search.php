78308eb86283916df72d21980f960299a00fc2e7
Takashi Irie <takashi@automattic.com>
2024-01-15 14:35:19 +0000

    Negai: Prepare the theme for the submission

--- negai/patterns/search.php
+++ negai/patterns/search.php
@@ -4,6 +4,7 @@
  * Slug: negai/search
  * Inserter: no
  */
+declare( strict_types = 1 );
 ?>
 
 <!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder text in a search field', 'negai' ); ?>","buttonText":"Search","buttonUseIcon":true} /-->
