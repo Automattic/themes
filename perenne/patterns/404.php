58ac1568f714468d58614e53848bb4d528bf0380
Takashi Irie <takashi@automattic.com>
2024-02-07 15:35:58 +0000

    Perenne: Add theme

---
+++ perenne/patterns/404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: A 404 page
+ * Slug: perenne/404
+ * Inserter: no
+ */
+
+declare( strict_types = 1 );
+?>
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.4"><?php echo esc_html__( 'Oops! That page can’t be found.', 'perenne' ); ?></p>
+<!-- /wp:paragraph -->
