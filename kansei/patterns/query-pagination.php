674c7f125589ec2568d5e72057f82ae7340df873
Takashi Irie <takashi@automattic.com>
2024-02-20 19:55:48 +0000

    Kansei: Add theme (#7638)

---
+++ kansei/patterns/query-pagination.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: A query pagination
+ * Slug: kansei/query-pagination
+ * Inserter: no
+ */
+
+declare( strict_types = 1 );
+?>
+
+<!-- wp:query-pagination {"paginationArrow":"arrow"} -->
+	<!-- wp:query-pagination-previous {"label":"<?php echo esc_html__( 'Prev', 'kansei' ); ?>"} /-->
+
+	<!-- wp:query-pagination-next {"label":"<?php echo esc_html__( 'Next', 'kansei' ); ?>"} /-->
+<!-- /wp:query-pagination -->
