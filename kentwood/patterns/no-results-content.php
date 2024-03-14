8ce803a8a765c2da4f34c7048844c7853a0d9e79
Takashi Irie <takashi@automattic.com>
2024-03-14 16:36:58 +0000

    Kentwood: Add theme

---
+++ kentwood/patterns/no-results-content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: kentwood/no-results-content
+ * Inserter: no
+ */
+?>
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
+<p style="margin-bottom:0px"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'kentwood' ); ?></p>
+<!-- /wp:paragraph -->
---
+++ kentwood/patterns/search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: kentwood/search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_html_x( 'Search...', 'This is a placeholder text in a search field', 'kentwood' ); ?>","buttonUseIcon":true,"style":{"border":{"radius":"999px"}}} /-->
