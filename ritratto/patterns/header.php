f827ded76bb01f7c3c4198de3d611a56ec7546c3
Sarah Norris <sarah@sekai.co.uk>
2024-04-04 18:36:27 +0100

    Updates from @henriqueiamarino
    Co-Authored-By: henriqueiamarino <103748085+henriqueiamarino@users.noreply.github.com>

--- ritratto/patterns/header.php
+++ ritratto/patterns/header.php
@@ -6,41 +6,41 @@
  * Inserter: no
  */
 ?>
-<!-- wp:cover {"customGradient":"linear-gradient(180deg,rgb(183,40,49) 1%,rgb(183,40,49) 82%,rgb(244,246,243) 82%)","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"4rem","bottom":"4rem","left":"0rem","right":"0rem"},"margin":{"top":"0px","bottom":"0px"}}},"textColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull is-light has-base-color has-text-color" style="margin-top:0px;margin-bottom:0px;padding-top:4rem;padding-right:0rem;padding-bottom:4rem;padding-left:0rem"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient" style="background:linear-gradient(180deg,rgb(183,40,49) 1%,rgb(183,40,49) 82%,rgb(244,246,243) 82%)"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"isUserOverlayColor":true,"gradient":"default-gradient","align":"full","style":{"spacing":{"padding":{"top":"4rem","bottom":"4rem","left":"0rem","right":"0rem"},"margin":{"top":"0px","bottom":"0px"}}},"textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-base-color has-text-color" style="margin-top:0px;margin-bottom:0px;padding-top:4rem;padding-right:0rem;padding-bottom:4rem;padding-left:0rem"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-default-gradient-gradient-background"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0rem","left":"0rem"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
 <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"2rem","margin":{"bottom":"3rem"},"padding":{"right":"1.5rem"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
 <div class="wp-block-group alignwide" style="margin-bottom:3rem;padding-right:1.5rem"><!-- wp:site-logo {"width":40,"shouldSyncIcon":false} /-->
 
-<!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"fontSize":"10rem","lineHeight":"1","letterSpacing":"-0.2rem","fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"instrument-serif"} -->
-<h1 class="wp-block-heading alignwide has-base-color has-text-color has-link-color has-instrument-serif-font-family" style="font-size:10rem;font-style:normal;font-weight:400;letter-spacing:-0.2rem;line-height:1"><a href="/"><?php echo esc_html__( 'Hemingway', 'ritratto' ); ?></a></h1>
+<!-- wp:heading {"level":1,"align":"wide","style":{"typography":{"fontSize":"8rem","lineHeight":"1","letterSpacing":"-0.2rem","fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"instrument-serif"} -->
+<h1 class="wp-block-heading alignwide has-base-color has-text-color has-link-color has-instrument-serif-font-family" style="font-size:8rem;font-style:normal;font-weight:400;letter-spacing:-0.2rem;line-height:1"><a href="/">Hemingway</a></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"1rem","margin":{"top":"2rem","bottom":"2rem"},"padding":{"right":"2.5rem"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"1rem","margin":{"top":"2rem","bottom":"2rem"},"padding":{"right":"2.5rem"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group" style="margin-top:2rem;margin-bottom:2rem;padding-right:2.5rem"><!-- wp:paragraph -->
-<p><?php echo esc_html__( 'Ernest Hemingway was an American novelist, short-story writer, journalist, and sportsman.', 'ritratto' ); ?></p>
+<p>Ernest Hemingway was an American novelist, short-story writer, journalist, and sportsman.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}},"typography":{"lineHeight":"1"}}} /-->
+<div class="wp-block-group"><!-- wp:navigation {"ref":456,"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}},"typography":{"lineHeight":"1"}}} /-->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"normal","fontWeight":"600"}}} -->
-<p style="font-style:normal;font-weight:600;line-height:1"><?php echo esc_html__( '/', 'ritratto' ); ?></p>
+<p style="font-style:normal;font-weight:600;line-height:1">/</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"normal","fontWeight":"600"}}} -->
-<p style="font-style:normal;font-weight:600;line-height:1"><?php echo esc_html__( 'Tumblr', 'ritratto' ); ?></p>
+<p style="font-style:normal;font-weight:600;line-height:1">Tumblr</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"normal","fontWeight":"600"}}} -->
-<p style="font-style:normal;font-weight:600;line-height:1"><?php echo esc_html__( '/', 'ritratto' ); ?></p>
+<p style="font-style:normal;font-weight:600;line-height:1">/</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"normal","fontWeight":"600"}}} -->
-<p style="font-style:normal;font-weight:600;line-height:1"><?php echo esc_html__( 'Linkedin', 'ritratto' ); ?></p>
+<p style="font-style:normal;font-weight:600;line-height:1">Linkedin</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
