2fed55764401f805547788e9ba8e1e4191d13f62
Vicente Canales <1157901+vcanales@users.noreply.github.com>
2024-04-11 14:01:02 -0500

    Jazzers: add theme (#7279)
    * Jazzers: add theme
    
    * remove table styles
    
    * Remove theme attribute from template part calls
    
    * Add updates from latest zip
    
    * update files
    
    * Add strict types
    
    ---------
    
    Co-authored-by: Sarah Norris <sarah@sekai.co.uk>

---
+++ the-jazzers/patterns/404.php
@@ -0,0 +1,17 @@
+<?php declare( strict_types = 1 ); ?>
+<?php
+/**
+ * Title: A 404 page
+ * Slug: jazzers/404
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
+<h1 class="has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'jazzers' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'jazzers' ); ?></p>
+<!-- /wp:paragraph -->
