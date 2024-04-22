968d79603f3ad06b32a71fee0b48f6dcf0c02fca
Jason Crist <jcrist@pbking.com>
2024-04-22 08:48:42 -0400

    Comitting provided changes to cookbook

--- cookbook/patterns/home.php
+++ cookbook/patterns/home.php
@@ -6,61 +6,63 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","bottom":"5rem","left":"0rem","right":"0rem"},"margin":{"top":"0rem","bottom":"0rem"},"blockGap":"2.5rem"},"color":{"gradient":"linear-gradient(180deg,rgb(242,73,36) 0%,rgb(224,54,17) 100%)"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"default"}} -->
-<div class="wp-block-group has-base-color has-text-color has-background has-link-color" style="background:linear-gradient(180deg,rgb(242,73,36) 0%,rgb(224,54,17) 100%);margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:5rem;padding-left:0rem"><!-- wp:template-part {"slug":"header","tagName":"header","align":"wide"} /-->
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:image {"align":"wide","id":35,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image alignwide size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Illustrations.png" alt="" class="wp-image-35"/></figure>
-<!-- /wp:image --></div>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"secondary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-secondary-background-color has-background" style="margin-top:0;margin-bottom:0"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"1px","bottom":"1px","left":"1px","right":"1px"}},"border":{"radius":{"topLeft":"4px","topRight":"4px","bottomLeft":"0px","bottomRight":"0px"}}},"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-contrast-background-color has-background" style="border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:1px;padding-right:1px;padding-bottom:1px;padding-left:1px"><!-- wp:query {"queryId":0,"query":{"perPage":8,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"tagName":"main","align":"wide","layout":{"type":"constrained"}} -->
+<main class="wp-block-query alignwide"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|50"}},"border":{"radius":{"bottomLeft":"2px","bottomRight":"2px"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="border-bottom-left-radius:2px;border-bottom-right-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:search {"label":"Search","showLabel":false,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true} /-->
+
+<!-- wp:query-pagination {"align":"wide"} -->
+<!-- wp:query-pagination-previous {"label":"↤"} /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next {"label":"↦"} /-->
+<!-- /wp:query-pagination --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"}}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-tertiary-background-color has-background" style="margin-top:0rem;margin-bottom:0rem"><!-- wp:group {"align":"wide","style":{"border":{"width":"1px"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-primary-background-color has-background" style="border-width:1px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:query {"queryId":0,"query":{"perPage":"8","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[9,6,7,5,12,11,10,8]},"parents":[]},"tagName":"main","align":"wide","layout":{"type":"constrained"}} -->
-<main class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"1px"}},"layout":{"type":"grid","columnCount":4}} -->
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"0px","left":"0rem","right":"0rem","top":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":""},"dimensions":{"minHeight":"100%"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-base-background-color has-background" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0rem;padding-bottom:0px;padding-left:0rem"><!-- wp:group {"style":{"spacing":{"padding":{"right":"1rem","left":"1.5rem","top":"1rem","bottom":"1rem"},"blockGap":"3rem"},"dimensions":{"minHeight":"100%"}},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between","flexWrap":"nowrap"}} -->
-<div class="wp-block-group has-background-background-color has-background" style="min-height:100%;padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1.5rem"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group"><!-- wp:post-featured-image {"isLink":true,"width":"","height":"120px","style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"border":{"radius":"5px"}}} /--></div>
-<!-- /wp:group -->
+<!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"1px"}},"layout":{"type":"grid","columnCount":"4","minimumColumnWidth":null}} -->
+<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1","style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem","left":"0rem","right":"0rem"}}}} /-->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"1.6rem"}}} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|30","bottom":"var:preset|spacing|60"},"blockGap":"0"},"dimensions":{"minHeight":""},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--40)"><!-- wp:post-terms {"term":"category","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary"} /-->
 
-<!-- wp:post-terms {"term":"category","style":{"typography":{"lineHeight":"1.2","fontSize":"0.8rem","textTransform":"uppercase"}}} /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
+<!-- wp:post-title {"isLink":true} /--></div>
 <!-- /wp:group -->
 <!-- /wp:post-template --></main>
 <!-- /wp:query --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:0rem;margin-bottom:0rem"><!-- wp:group {"align":"wide","style":{"border":{"right":{"width":"1px"},"bottom":{"width":"1px"},"left":{"width":"1px"}},"spacing":{"padding":{"top":"1.5rem","bottom":"1.5rem","right":"1.5rem","left":"1.5rem"},"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="border-right-width:1px;border-bottom-width:1px;border-left-width:1px;padding-top:1.5rem;padding-right:1.5rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:query {"queryId":0,"query":{"perPage":"1","pages":0,"offset":0,"postType":"page","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]},"tagName":"main","align":"wide","layout":{"type":"constrained"}} -->
-<main class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"1px"}},"layout":{"type":"grid","columnCount":1}} -->
-<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"3rem","left":"3rem"}}}} -->
-<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:image {"id":86,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"20px"}},"className":"is-style-default"} -->
-<figure class="wp-block-image size-full has-custom-border is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/pexels-yaroslav-shuraev-8845419.jpg" alt="" class="wp-image-86" style="border-radius:20px;aspect-ratio:1;object-fit:cover"/></figure>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","style":{"border":{"radius":"16px","style":"dotted","width":"1px","color":"#ee5b2b"},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-border-color" style="border-color:#ee5b2b;border-style:dotted;border-width:1px;border-radius:16px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"0"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":86,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/pexels-yaroslav-shuraev-8845419.jpg" alt="" class="wp-image-86" style="aspect-ratio:1;object-fit:cover"/></figure>
 <!-- /wp:image --></div>
-<!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"dimensions":{"minHeight":"7rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group" style="min-height:7rem"><!-- wp:post-title {"level":1,"isLink":true,"style":{"typography":{"letterSpacing":"-0.05rem"}},"fontFamily":"fraunces"} /-->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"var:preset|spacing|70","left":"var:preset|spacing|70"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group" style="min-height:100%;padding-right:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">The Chef</h1>
+<!-- /wp:heading -->
 
-<!-- wp:post-excerpt {"moreText":"About Martha Smith","excerptLength":40,"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}}} /--></div>
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Emma is a dedicated home chef who finds joy in cooking and sharing her favorite recipes with others. With a background in recipe development and food blogging, she enjoys exploring diverse cuisines and techniques to create mouthwatering dishes. Emma is eager to connect with fellow food lovers and create a community centered around delicious homemade meals.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"secondary"} -->
+<p class="has-text-align-center has-secondary-color has-text-color has-link-color" style="font-style:normal;font-weight:600">About Emma Johnson</p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns -->
-<!-- /wp:post-template --></main>
-<!-- /wp:query --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
--- cookbook/readme.txt
+++ cookbook/readme.txt
@@ -1,14 +1,14 @@
 === Cookbook ===
 Contributors: the WordPress team
 Requires at least: 6.0
-Tested up to: 6.3.1
+Tested up to: 6.4.3
 Requires PHP: 5.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Description ==
 
-Cookbook is a versatile theme designed to cater to a wide range of content creators. It explores the Query Loop element on a very particular way, with zero spacings and block strokes.
+Cookbook is a versatile theme designed to cater to various content creators. It explores the Query Loop element in a very particular way, with zero spacings and block strokes.
 
 == Changelog ==
 
@@ -17,9 +17,7 @@ Cookbook is a versatile theme designed to cater to a wide range of content creat
 
 == Copyright ==
 
-Cookbook is based on CB1 (), (C) , [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
-
-CB1 is based on Block Canvas (https://github.com/Automattic/themes/tree/trunk/block-canvas), (C) Automattic, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+Cookbook is based on Block Canvas (https://github.com/Automattic/themes/tree/trunk/block-canvas), (C) Automattic, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
 
 Block Canvas WordPress Theme, (C) 2022 Automattic
 Block Canvas is distributed under the terms of the GNU GPL.
@@ -34,25 +32,26 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
-Fraunces Font
-Copyright 2020 The Fraunces Project Authors (github.com/undercasetype/Fraunces) 
+Gabarito Font
+Copyright 2023 The Gabarito Project Authors (https://github.com/naipefoundry/gabarito) 
 This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL 
 License URL: https://scripts.sil.org/OFL 
--- End of Fraunces Font credits --
+Source: http://www.naipe.xyz/
+-- End of Gabarito Font credits --
 
+Bolota Font
+Copyright (c) 2024, Pang Brands www.pangbrands.com | contato@pangbrands.com,
+with Reserved Font Name Tonelada.
 
-Rubik Font
-Copyright 2015 The Rubik Project Authors (https://github.com/googlefonts/rubik) 
-This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL 
-License URL: https://scripts.sil.org/OFL 
-Source: http://www.google.com/fonts
--- End of Rubik Font credits --
+Copyright (c) 2024, Gabriel Pang pangbrands.com | gabriel@pangbrands.com,
+with Reserved Font Name Tonelada. 
+Copyright (c) 2024, Pang Brands www.pangbrands.com | contato@pangbrands.com, 
+License URL: https://1drv.ms/t/s!AlKtWKsMtIPDg-JgUB3N9cKiUdxoFw?e=GhLqE0 
+Source: pangbrands.com
+-- End of Bolota Font credits --
 
-Images on the Homepage
 https://www.rawpixel.com/image/2413765/free-illustration-image-cake-vintage-food
 https://www.rawpixel.com/image/2417579/free-illustration-image-food-victorian-russian
 https://www.rawpixel.com/image/2415907/free-illustration-image-meat-ham-victorian
 https://www.rawpixel.com/image/2415298/free-illustration-image-chicken-turkey-cooking
 https://www.rawpixel.com/image/2415301/free-illustration-image-cheese-food-vintage
-
-All recipe images are identified by their original titles from Foodiesfeed.com
