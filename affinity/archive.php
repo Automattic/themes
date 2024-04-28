6678422b8ed58ab46b91e599445975449ebdef70
Vicente Canales <vicente.canales@automattic.com>
2024-04-27 20:39:30 -0400

    test changes to preview  workflow

--- affinity/archive.php
+++ affinity/archive.php
@@ -30,6 +30,7 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
+				
 				get_template_part( 'components/post/content', get_post_format() );
 
 			endwhile;
