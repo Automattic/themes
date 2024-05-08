1dba09d90fe3c56a709f8a4f38e27059880c79e4
Matias Benedetto <matias.benedetto@gmail.com>
2024-05-08 10:30:34 -0300

    replace not needed esc_html_x by __

--- bibliophile/patterns/hidden-no-results-content.php
+++ bibliophile/patterns/hidden-no-results-content.php
@@ -8,5 +8,5 @@
 ?>
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'bibliophile' ); ?></p>
+<p><?php echo  __( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'bibliophile' ); ?></p>
 <!-- /wp:paragraph -->
