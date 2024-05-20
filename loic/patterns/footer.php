505b3167ad5f8147a79755db595edefb99800d2f
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-05-20 14:36:38 +0100

    Changes from WordPress Playground:
    - Replaces screenshot with one with the right size
    - Adjusts spacing between columns in the theme's templates
    - Adjusts pullquote and code block
    
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FAutomattic%2Fthemes%2Fplayground-changes-2024-05-20T13-35-25-016Z%2Floic%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FAutomattic%2Fthemes%2Ftrunk%2Floic%2Fplayground.zip)

--- loic/patterns/footer.php
+++ loic/patterns/footer.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * Title: Default footer
+ * Title: footer
  * Slug: loic/footer
- * Categories: footer
- * Block Types: core/template-part/footer
+ * Categories: hidden
+ * Inserter: no
  */
 ?>
-
 <!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|60","bottom":"var:preset|spacing|80","left":"var:preset|spacing|60"}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"default"}} -->
 <div class="wp-block-group has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--60)"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"44.45%"} -->
 <div class="wp-block-column" style="flex-basis:44.45%"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","right":"0px","left":"0px"}}},"layout":{"type":"flex","orientation":"vertical"},"fontSize":"small"} -->
-<div class="wp-block-group alignfull has-small-font-size" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo esc_html__( 'About Me', 'y' ); ?></h3>
+<div class="wp-block-group alignfull has-small-font-size" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} -->
+<h3 class="wp-block-heading has-background-color has-text-color has-link-color"><?php echo __('About Me', 'loic');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html__( 'I’m Loïc Azar, a French creative and product manager living in London. I’m passionate about creativity, technology, and productivity, and I write about these topics on my personal website.', 'y' ); ?></p>
+<p><?php echo __('I’m Loïc Azar, a French creative and product manager living in London. I’m passionate about creativity, technology, and productivity, and I write about these topics on my personal website.', 'loic');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -27,21 +26,21 @@
 
 <!-- wp:column {"width":"22.22%"} -->
 <div class="wp-block-column" style="flex-basis:22.22%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo esc_html__( 'Say Hello', 'y' ); ?></h3>
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} -->
+<h3 class="wp-block-heading has-background-color has-text-color has-link-color"><?php echo __('Say Hello', 'loic');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"small"} -->
 <div class="wp-block-group has-small-font-size"><!-- wp:paragraph -->
-<p><a href="/"><?php echo esc_html__( 'Email', 'y' ); ?></a></p>
+<p><?php echo __('<a href="/">Email</a>', 'loic');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="/"><?php echo esc_html__( 'Instagram', 'y' ); ?></a></p>
+<p><?php echo __('<a href="/">Instagram</a>', 'loic');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="/"><?php echo esc_html__( 'Twitter', 'y' ); ?></a></p>
+<p><?php echo __('<a href="/">Twitter</a>', 'loic');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
@@ -55,16 +54,7 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","right":"0px","left":"0px"}},"typography":{"fontSize":"0.8rem"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"bottom"}} -->
 <div class="wp-block-group alignfull" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;font-size:0.8rem"><!-- wp:paragraph {"align":"right"} -->
 <p class="has-text-align-right">
-    <?php
-
-    $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'loic' ) ) . '" rel="nofollow">WordPress</a>';
-    echo sprintf(
-        /* Translators: WordPress link. */
-        esc_html__( 'Designed with %1$s', 'loic' ),
-        $wordpress_link
-    );
-    ?>
-</p>
+    Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
