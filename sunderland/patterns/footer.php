f921be9c1007d64d20e605df8475c334afa6dab8
Sarah Norris <sarah@sekai.co.uk>
2024-06-17 00:09:30 +0200

    Use footer pattern in template part

--- sunderland/patterns/footer.php
+++ sunderland/patterns/footer.php
@@ -11,23 +11,19 @@
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center">
-            <?php
-                /* Translators: WordPress link. */
-                $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'sunderland' ) ) . '" rel="nofollow">WordPress</a>';
-                echo sprintf(
-                    esc_html__( 'Designed with %1$s', 'sunderland' ),
-                    $wordpress_link
-                );
-            ?>
-        </p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-right:5vw;padding-left:5vw"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"0.8rem","textTransform":"uppercase"}}} -->
+<p class="has-text-align-left" style="font-size:0.8rem;text-transform:uppercase">
+    <?php
+        /* Translators: WordPress link. */
+        $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'sunderland' ) ) . '" rel="nofollow">WordPress</a>';
+        echo sprintf(
+            esc_html__( 'Designed with %1$s', 'sunderland' ),
+            $wordpress_link
+        );
+    ?>
+</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
