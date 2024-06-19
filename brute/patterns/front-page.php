53287d41d5f2c5f6e3545b44501ddfc5b34dc45f
Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
2024-06-19 15:50:13 +0200

    Brute: add theme

---
+++ brute/patterns/front-page.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: brute/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","bottom":"8rem"},"blockGap":"8rem","margin":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-bottom:8rem"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0rem","left":"0rem"},"margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px"><!-- wp:column {"verticalAlignment":"top","width":"70%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:70%"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/businessman-sunglasses.jpg","dimRatio":0,"customOverlayColor":"#adacac","focalPoint":{"x":0.5,"y":0.5},"minHeight":40,"minHeightUnit":"rem","contentPosition":"bottom left","isDark":false,"style":{"color":[],"dimensions":{"aspectRatio":"4/3"},"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-left" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;min-height:40rem"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#adacac"></span><img class="wp-block-cover__image-background " alt="<?php echo __('', 'brute');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/businessman-sunglasses.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0rem","padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"},"blockGap":"0rem","padding":{"top":"0.25rem","right":"0.5rem","bottom":"0rem","left":"0rem"}}},"backgroundColor":"primary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="margin-top:0rem;margin-bottom:0rem;padding-top:0.25rem;padding-right:0.5rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"4rem","textTransform":"uppercase","lineHeight":"1"}}} -->
+<h1 class="wp-block-heading" style="font-size:4rem;line-height:1;text-transform:uppercase"><?php echo __('Strength', 'brute');?></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"},"blockGap":"0rem","padding":{"top":"0.25rem","right":"0.5rem","bottom":"0rem","left":"0rem"}}},"backgroundColor":"primary","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="margin-top:0rem;margin-bottom:0rem;padding-top:0.25rem;padding-right:0.5rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"4rem","textTransform":"uppercase","lineHeight":"1"}}} -->
+<h1 class="wp-block-heading" style="font-size:4rem;line-height:1;text-transform:uppercase"><?php echo __('In simplicity', 'brute');?></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"2.5%"} -->
+<div class="wp-block-column" style="flex-basis:2.5%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"2.5%"} -->
+<div class="wp-block-column" style="flex-basis:2.5%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"25%","style":{"spacing":{"padding":{"top":"2rem"}}}} -->
+<div class="wp-block-column" style="padding-top:2rem;flex-basis:25%"><!-- wp:heading {"level":1,"fontSize":"large"} -->
+<h1 class="wp-block-heading has-large-font-size"><?php echo __('An architectural style that emerged in the mid-20s is based in brute and unadorned use of concrete.', 'brute');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Named by the French term <em>béton brut</em>, or <em>brute concrete</em>, Brutalism became popular in the post-WWII era, when the need for affordable housing and public buildings led to the widespread use of concrete.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase"}}} -->
+<h1 class="wp-block-heading has-text-align-center" style="text-transform:uppercase"><?php echo __('Brutalists are celebrated FOR their <em>geometric forms</em> and the <em><em>use of concrete</em></em>', 'brute');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"4rem"} -->
+<div style="height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"right":"2rem","left":"0rem","top":"3rem","bottom":"1rem"}},"border":{"top":{"width":"1px"},"right":{"width":"1px"},"bottom":[],"left":[]}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="border-top-width:1px;border-right-width:1px;padding-top:3rem;padding-right:2rem;padding-bottom:1rem;padding-left:0rem"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Le Corbusier', 'brute');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Charles-Édouard Jeanneret, known as Le Corbusier, was a Swiss-French architect, designer, painter, urban planner, writer, and one of the pioneers of what is now regarded as modern architecture.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"right":"2rem","left":"0rem","top":"3rem","bottom":"1rem"}},"border":{"top":{"width":"1px"},"right":{"width":"1px"},"bottom":[],"left":[]}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="border-top-width:1px;border-right-width:1px;padding-top:3rem;padding-right:2rem;padding-bottom:1rem;padding-left:0rem"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Alison and Peter Smithson', 'brute');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Alison M. Smithson and Peter D. Smithson were English architects who formed an architectural partnership and are associated with Brutalism, especially in architectural and urban theory.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"right":"2rem","left":"0rem","top":"3rem","bottom":"1rem"}},"border":{"top":{"width":"1px"},"right":{"width":"1px"},"bottom":[],"left":[]}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="border-top-width:1px;border-right-width:1px;padding-top:3rem;padding-right:2rem;padding-bottom:1rem;padding-left:0rem"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Louis Kahn', 'brute');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Louis Isadore Kahn was an Estonian-born American architect based in Philadelphia. After working in various capacities for several firms in Philadelphia, he founded his own atelier in 1935.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"2rem"} -->
+<div style="height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:image {"width":"64px","height":"64px","sizeSlug":"full","linkDestination":"none","align":"center"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/btn_plus.png" alt="<?php echo __('', 'brute');?>" style="width:64px;height:64px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":""}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0rem","left":"0rem"}},"border":{"top":{"width":"1px"}}}} -->
+<div class="wp-block-columns alignwide" style="border-top-width:1px"><!-- wp:column {"width":"50%","style":{"spacing":{"padding":{"top":"2rem"}},"border":{"top":{"width":"0.3rem"}}}} -->
+<div class="wp-block-column" style="border-top-width:0.3rem;padding-top:2rem;flex-basis:50%"><!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide"><?php echo __('Six most appreciated Brutalist buildings', 'brute');?></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"12.5%"} -->
+<div class="wp-block-column" style="flex-basis:12.5%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"12.5%"} -->
+<div class="wp-block-column" style="flex-basis:12.5%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"25%","style":{"spacing":{"padding":{"top":"2.3rem"}}}} -->
+<div class="wp-block-column" style="padding-top:2.3rem;flex-basis:25%"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><?php echo __('Brutalism was also influenced by Bauhaus school, which emphasized functionalism and simplicity in design.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"2rem"} -->
+<div style="height:2rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:query {"queryId":10,"query":{"perPage":"4","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false,"taxQuery":null,"parents":[]}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"constrained"}} -->
+<!-- wp:columns {"verticalAlignment":"bottom","style":{"border":{"top":{"width":"1px"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-bottom" style="border-top-width:1px"><!-- wp:column {"verticalAlignment":"bottom","width":"25%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:25%"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3","style":{"color":[]}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom","width":"75%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:75%"><!-- wp:group {"style":{"spacing":{"blockGap":"1rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
+
+<!-- wp:post-excerpt {"showMoreOnNewLine":false} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:50%"><!-- wp:query {"queryId":10,"query":{"perPage":"1","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"only","inherit":false,"taxQuery":null,"parents":[]}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"isUserOverlayColor":true,"contentPosition":"bottom left","isDark":false,"style":{"dimensions":{"aspectRatio":"1"},"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light has-custom-content-position is-position-bottom-left" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"1rem","padding":{"right":"1rem","top":"1rem"},"margin":{"top":"0rem","bottom":"0rem"}}},"backgroundColor":"primary","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="margin-top:0rem;margin-bottom:0rem;padding-top:1rem;padding-right:1rem"><!-- wp:post-title {"level":1,"isLink":true,"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"fontSize":"large"} /-->
+
+<!-- wp:post-excerpt {"showMoreOnNewLine":false} /--></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"color":null,"style":null,"width":null,"top":{"width":"1px","style":"solid"},"right":[],"bottom":[],"left":[]},"spacing":{"margin":{"top":"0rem","bottom":"0rem"},"padding":{"top":"8rem","bottom":"8rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-top-style:solid;border-top-width:1px;margin-top:0rem;margin-bottom:0rem;padding-top:8rem;padding-bottom:8rem"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><?php echo __('Brutalism in Architecture expresses the essential qualities of a building’s materials and structure in an honest and direct way.<br>The architectural style emerged in the mid-20s, based on the brute and unadorned use of concrete.', 'brute');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><?php echo __('Brutalism, named by the French term béton brute, or brute concrete, became popular in the post-WWII era, when the need for affordable housing and public buildings led to widespread use of concrete.', 'brute');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:50%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
