9ae5c9b7532e8d655726349ee1f5784941379045
Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
2024-06-19 12:22:08 +0200

    JazzyGrid: add theme

---
+++ jazzygrid/patterns/header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: header
+ * Slug: boxedbio/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/beach-sea-coast-water-ocean-horizon.jpg","id":426,"dimRatio":0,"minHeight":15,"minHeightUnit":"vh","contentPosition":"bottom left","align":"full","style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"},"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-left" style="margin-top:0px;margin-bottom:0px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem;min-height:15vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-426" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/beach-sea-coast-water-ocean-horizon.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:site-logo {"width":120,"shouldSyncIcon":true,"align":"center","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /--></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"2rem","right":"2rem","bottom":"2rem","left":"2rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"},"dimensions":{"minHeight":"100%"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"tertiary","textColor":"background","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group alignfull has-background-color has-tertiary-background-color has-text-color has-background has-link-color" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:paragraph -->
+<p>The Jazz Messengers is a six-piece jazz ensemble known for their hard-bop style and virtuosic improvisation.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"1.5rem"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><a href="#" target="_blank" rel="noreferrer noopener">Website</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><a href="#" target="_blank" rel="noreferrer noopener">Albuns</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><a href="#" target="_blank" rel="noreferrer noopener">Tickets</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ jazzygrid/patterns/home.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Title: home
+ * Slug: boxedbio/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"dimRatio":0,"isUserOverlayColor":true,"minHeight":95,"minHeightUnit":"vh","isDark":false,"metadata":{"categories":["hidden"]},"style":{"spacing":{"padding":{"top":"1rem","right":"1rem","bottom":"1rem","left":"1rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="padding-top:1rem;padding-right:1rem;padding-bottom:1rem;padding-left:1rem;min-height:95vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"0px","left":"0px"},"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"25%","backgroundColor":"secondary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-secondary-background-color has-background" style="flex-basis:25%"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/beach-sea-coast-water-ocean-horizon.jpg","dimRatio":0,"minHeight":100,"minHeightUnit":"%","contentPosition":"bottom left","align":"full","style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"},"padding":{"top":"1.5rem","right":"3rem","bottom":"1.5rem","left":"1.5rem"}},"color":[]},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-left" style="margin-top:0px;margin-bottom:0px;padding-top:1.5rem;padding-right:3rem;padding-bottom:1.5rem;padding-left:1.5rem;min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background " alt="<?php echo __('', 'boxedbio');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/beach-sea-coast-water-ocean-horizon.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:site-logo {"width":120,"shouldSyncIcon":false,"align":"center"} /--></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"50%","backgroundColor":"tertiary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-tertiary-background-color has-background" style="flex-basis:50%"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"1.5rem","right":"3rem","bottom":"1.5rem","left":"1.5rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"dimensions":{"minHeight":"100%"}},"textColor":"background","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group alignfull has-background-color has-text-color has-link-color" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:1.5rem;padding-right:3rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:paragraph -->
+<p><?php echo __('The Jazz Messengers is a six-piece jazz ensemble known for their hard-bop style and virtuosic improvisation.', 'boxedbio');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"1.5rem"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Website</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Albuns</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Tickets</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"25%","backgroundColor":"primary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-primary-background-color has-background" style="flex-basis:25%"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/img_trumpet-detail.png","dimRatio":0,"customOverlayColor":"#505050","minHeight":100,"minHeightUnit":"%","contentPosition":"bottom left","align":"full","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"padding":{"top":"1.5rem","right":"3rem","bottom":"1.5rem","left":"1.5rem"},"blockGap":"0px"},"color":[]},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull has-custom-content-position is-position-bottom-left" style="margin-top:0px;margin-bottom:0px;padding-top:1.5rem;padding-right:3rem;padding-bottom:1.5rem;padding-left:1.5rem;min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#505050"></span><img class="wp-block-cover__image-background " alt="<?php echo __('', 'boxedbio');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/img_trumpet-detail.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0rem"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('<a rel="noreferrer noopener" href="https://www.wordpress.org/" target="_blank">Message</a><br><a rel="noreferrer noopener" href="https://www.wordpress.org/" target="_blank">in Jazz</a>', 'boxedbio');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Album launch →</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"0px","left":"0px"},"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"75%","backgroundColor":"primary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-primary-background-color has-background" style="flex-basis:75%"><!-- wp:cover {"url":"https://boxedbio.mystagingwebsite.com/wp-content/uploads/2024/06/cover_trumpeteer.png","hasParallax":true,"isRepeated":true,"dimRatio":0,"customOverlayColor":"#2d2d2d","focalPoint":{"x":0.4,"y":0.5},"minHeight":100,"minHeightUnit":"%","contentPosition":"top left","style":{"color":[],"spacing":{"padding":{"top":"5rem","right":"3rem","bottom":"5rem","left":"1.5rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-parallax is-repeated has-custom-content-position is-position-top-left" style="margin-top:0px;margin-bottom:0px;padding-top:5rem;padding-right:3rem;padding-bottom:5rem;padding-left:1.5rem;min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#2d2d2d"></span><div class="wp-block-cover__image-background  has-parallax is-repeated" style="background-position:40% 50%;background-image:url(https://boxedbio.mystagingwebsite.com/wp-content/uploads/2024/06/cover_trumpeteer.png)"></div><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"2rem","padding":{"right":"10%"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="padding-right:10%"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php echo __('<a rel="noreferrer noopener" href="https://www.wordpress.org/" target="_blank">Message in Jaz</a><a href="https://www.wordpress.org/" target="_blank" rel="noreferrer noopener">z</a>', 'boxedbio');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('TJM brings trumpeter Maxine Davis, saxophonist D. James, Tiana Alvarez on the piano, Derrick Hayes on bass, and Marcus Lee Jones on the drums. Together, they create an engaging experience to delight fans of classic and contemporary jazz.', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontSize":"0.76rem","letterSpacing":"0.1rem"}}} -->
+<p style="font-size:0.76rem;letter-spacing:0.1rem;text-transform:uppercase"><?php echo __('<a href="#">Coming Next</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Live at the Masonic Auditorium<br>San Francisco, July 22nd, 2023.', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"25%","backgroundColor":"secondary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-secondary-background-color has-background" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"},"padding":{"top":"4rem","right":"3rem","bottom":"4rem","left":"1.5rem"},"blockGap":"2.5rem"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group" style="min-height:100%;margin-top:0rem;margin-bottom:0rem;padding-top:4rem;padding-right:3rem;padding-bottom:4rem;padding-left:1.5rem"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"textColor":"foreground","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-foreground-color has-text-color has-link-color" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php echo __('<a href="https://en.wikipedia.org/wiki/Billie_Holiday" target="_blank" rel="noreferrer noopener"></a><a href="#" target="_blank" rel="noreferrer noopener">Jazzin\' Around Town</a>', 'boxedbio');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Join The Jazz Messengers for an unforgettable night of music. Tickets are selling fast, so save your spot!', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo __('Buy Tickets', 'boxedbio');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"0px","left":"0px"},"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:0px;margin-bottom:0px;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"50%","backgroundColor":"primary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-primary-background-color has-background" style="flex-basis:50%"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"1.5rem","right":"3rem","bottom":"1.5rem","left":"1.5rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignfull" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:1.5rem;padding-right:3rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Monthly list of new jazz music</a>', 'boxedbio');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Check out our YouTube channel to explore our monthly list of new jazz music, videos, and weekly live jam sessions.', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"25%","backgroundColor":"foreground"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-foreground-background-color has-background" style="flex-basis:25%"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"1.5rem","right":"3rem","bottom":"1.5rem","left":"1.5rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"1.5rem"},"dimensions":{"minHeight":"100%"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"top"}} -->
+<div class="wp-block-group alignfull has-background-color has-text-color has-link-color" style="min-height:100%;margin-top:0px;margin-bottom:0px;padding-top:1.5rem;padding-right:3rem;padding-bottom:1.5rem;padding-left:1.5rem"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Newsletter</a>', 'boxedbio');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Receive the latest updates about tickets &amp; events →', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"25%","backgroundColor":"custom-a2-dda5"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-custom-a-2-dda-5-background-color has-background" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"blockGap":"0rem","padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"}},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="min-height:100%;margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}},"color":{"background":"#ffffff40"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-background" style="background-color:#ffffff40;margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fill"}},"backgroundColor":"custom-a2-dda5","fontSize":"small"} -->
+<p class="has-text-align-center has-custom-a-2-dda-5-background-color has-background has-small-font-size"><?php echo __('<a href="#">Tumblr</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}},"color":{"background":"#ffffff33"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-background" style="background-color:#ffffff33;margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fill"}},"backgroundColor":"custom-a2-dda5","fontSize":"small"} -->
+<p class="has-text-align-center has-custom-a-2-dda-5-background-color has-background has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Fbook</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"}},"color":{"background":"#ffffff0d"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-background" style="background-color:#ffffff0d;margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fill"}},"backgroundColor":"custom-cce7-e2","fontSize":"small"} -->
+<p class="has-text-align-center has-custom-cce-7-e-2-background-color has-background has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Spotfy</a>', 'boxedbio');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer"} /--></div></div>
+<!-- /wp:cover -->
