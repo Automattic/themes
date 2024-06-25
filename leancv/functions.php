a27bbd1bda5f57072e04acf79540a5b7fe751617
Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
2024-06-25 11:11:01 +0200

    LeanCV: add theme

---
+++ leancv/functions.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * LeanCV functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package LeanCV
+ * @since LeanCV 1.0
+ */
+
+
+if ( ! function_exists( 'leancv_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since LeanCV 1.0
+	 *
+	 * @return void
+	 */
+	function leancv_support() {
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+		// Make theme available for translation.
+		load_theme_textdomain( 'leancv' );
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'leancv_support' );
+
+if ( ! function_exists( 'leancv_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since LeanCV 1.0
+	 *
+	 * @return void
+	 */
+	function leancv_styles() {
+
+		// Register theme stylesheet.
+		wp_register_style(
+			'leancv-style',
+			get_stylesheet_directory_uri() . '/style.css',
+			array(),
+			wp_get_theme()->get( 'Version' )
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'leancv-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'leancv_styles' );
---
+++ leancv/patterns/home.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Title: home
+ * Slug: leancv/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:columns {"metadata":{"categories":["hidden"]},"align":"full","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"},"blockGap":{"top":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns alignfull" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"65%"} -->
+<div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:65%"><!-- wp:group {"style":{"spacing":{"blockGap":"4rem","padding":{"right":"1.5rem","left":"1.5rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:1.5rem;padding-left:1.5rem"><!-- wp:template-part {"slug":"header"} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php echo __('Welcome to my Blog, the threshold of my imagination. Here, I write about my literary journey, which intertwines science fiction, editorial endeavors, and social advocacy.', 'leancv');?></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:query {"queryId":8,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"type":"constrained"}} -->
+<div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"4.5rem"}},"layout":{"type":"constrained"}} -->
+<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1","width":"120px","height":"120px"} /-->
+
+<!-- wp:post-title {"level":1,"isLink":true} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem","margin":{"top":"1rem"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="margin-top:1rem"><!-- wp:post-date /-->
+
+<!-- wp:post-excerpt {"moreText":"Read.","showMoreOnNewLine":false} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p><?php echo __('No results found.', 'leancv');?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query -->
+
+<!-- wp:template-part {"slug":"footer"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"35%","lock":{"move":false,"remove":false},"style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"blockGap":"0px"}}} -->
+<div class="wp-block-column is-vertically-aligned-stretch" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;flex-basis:35%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:template-part {"slug":"profile-pic"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
