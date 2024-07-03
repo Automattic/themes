28cbae854784604b22da73019e3052f4dd0d49c1
Rich Tabor <hi@richtabor.com>
2024-07-03 18:15:59 -0400

    Update front-page.php

--- leancv/patterns/front-page.php
+++ leancv/patterns/front-page.php
@@ -6,159 +6,159 @@
  * Inserter: no
  */
 ?>
-<!-- wp:columns {"metadata":{"categories":["hidden"]},"align":"full","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"},"blockGap":{"top":"0rem","left":"0rem"}}}} -->
-<div class="wp-block-columns alignfull" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"65%"} -->
+<!-- wp:columns {"metadata":{"categories":["hidden"],"name":"CV"},"align":"full","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"margin":{"top":"0rem","bottom":"0rem"},"blockGap":{"top":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns alignfull" style="margin-top:0rem;margin-bottom:0rem;padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem"><!-- wp:column {"verticalAlignment":"stretch","width":"65%","metadata":{"name":"Contents"}} -->
 <div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:65%"><!-- wp:group {"style":{"spacing":{"blockGap":"4rem","padding":{"right":"1.5rem","left":"1.5rem"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-right:1.5rem;padding-left:1.5rem"><!-- wp:template-part {"slug":"header"} /-->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading"><?php echo __('Hey there! I\'m Emily Jones, a results-driven marketing professional hailing from the Windy City of Chicago.', 'leancv');?></h1>
+<h1 class="wp-block-heading"><?php esc_html_e('Hey there! I\'m Emily Jones, a results-driven marketing professional hailing from the Windy City of Chicago.', 'leancv');?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading"><?php echo __('With a knack for blending creativity with strategic thinking, I specialize in crafting innovative marketing campaigns that resonate with audiences and drive tangible results.', 'leancv');?></h1>
+<h1 class="wp-block-heading"><?php esc_html_e('With a knack for blending creativity with strategic thinking, I specialize in crafting innovative marketing campaigns that resonate with audiences and drive tangible results.', 'leancv');?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo __('Experience', 'leancv');?></h2>
+<h2 class="wp-block-heading"><?php esc_html_e('Experience', 'leancv');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Marketing Coordinator, Big Apple Ventures NYC (2021-2023)', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Marketing Coordinator, Big Apple Ventures NYC (2021-2023)', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Managed marketing activities for a venture capital firm based in the bustling city of New York. Supported the development of marketing collateral, coordinated event sponsorships, and facilitated digital marketing campaigns to promote brand awareness and attract potential investors.', 'leancv');?></p>
+<p><?php esc_html_e('Managed marketing activities for a venture capital firm based in the bustling city of New York. Supported the development of marketing collateral, coordinated event sponsorships, and facilitated digital marketing campaigns to promote brand awareness and attract potential investors.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Content Marketing Specialist, ChiTownTech Solutions (2020-2021)', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Content Marketing Specialist, ChiTownTech Solutions (2020-2021)', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Led content marketing efforts for a tech startup in Chicago, creating engaging content across multiple platforms to enhance brand visibility and drive inbound traffic. Implemented SEO strategies and analyzed content performance metrics to optimize reach and engagement.', 'leancv');?></p>
+<p><?php esc_html_e('Led content marketing efforts for a tech startup in Chicago, creating engaging content across multiple platforms to enhance brand visibility and drive inbound traffic. Implemented SEO strategies and analyzed content performance metrics to optimize reach and engagement.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Senior Marketing Manager, Windy City Brands (2019-2020)', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Senior Marketing Manager, Windy City Brands (2019-2020)', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Spearheaded marketing initiatives for a Chicago-based company, overseeing the development and execution of comprehensive marketing strategies. Collaborated with cross-functional teams to drive brand awareness, increase customer engagement, and boost sales in the local market.', 'leancv');?></p>
+<p><?php esc_html_e('Spearheaded marketing initiatives for a Chicago-based company, overseeing the development and execution of comprehensive marketing strategies. Collaborated with cross-functional teams to drive brand awareness, increase customer engagement, and boost sales in the local market.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo __('Education', 'leancv');?></h2>
+<h2 class="wp-block-heading"><?php esc_html_e('Education', 'leancv');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Bachelor of Business Administration in Marketing', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Bachelor of Business Administration in Marketing', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('<strong>University of Illinois at Chicago</strong>', 'leancv');?></p>
+<p><?php esc_html_e('<strong>University of Illinois at Chicago</strong>', 'leancv');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Earned a solid foundation in marketing principles and strategic planning through coursework at a leading university in Chicago. Developed critical thinking skills and gained practical experience through hands-on projects and internships.', 'leancv');?></p>
+<p><?php esc_html_e('Earned a solid foundation in marketing principles and strategic planning through coursework at a leading university in Chicago. Developed critical thinking skills and gained practical experience through hands-on projects and internships.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Digital Marketing Certification Program', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Digital Marketing Certification Program', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('<strong>General Assembly (Chicago)</strong>', 'leancv');?></p>
+<p><?php esc_html_e('<strong>General Assembly (Chicago)</strong>', 'leancv');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Completed an intensive digital marketing certification program focused on the latest trends and strategies in the digital landscape. Acquired expertise in areas such as social media marketing, content creation, and data analytics to stay ahead in the competitive marketing field.', 'leancv');?></p>
+<p><?php esc_html_e('Completed an intensive digital marketing certification program focused on the latest trends and strategies in the digital landscape. Acquired expertise in areas such as social media marketing, content creation, and data analytics to stay ahead in the competitive marketing field.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo __('Skills', 'leancv');?></h2>
+<h2 class="wp-block-heading"><?php esc_html_e('Skills', 'leancv');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Local Marketing Expertise', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Local Marketing Expertise', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Deep understanding of local market dynamics and consumer behavior in Chicago, leveraging insights to develop targeted marketing campaigns that resonate with the city\'s diverse audience.', 'leancv');?></p>
+<p><?php esc_html_e('Deep understanding of local market dynamics and consumer behavior in Chicago, leveraging insights to develop targeted marketing campaigns that resonate with the city\'s diverse audience.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Creative Content Creation', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Creative Content Creation', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Proficient in crafting compelling content across various platforms, including social media, blogs, and email campaigns, to engage audiences and drive brand storytelling.', 'leancv');?></p>
+<p><?php esc_html_e('Proficient in crafting compelling content across various platforms, including social media, blogs, and email campaigns, to engage audiences and drive brand storytelling.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Data-driven Decision Making', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Data-driven Decision Making', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Skilled in analyzing marketing metrics and performance data to derive actionable insights and optimize marketing strategies for maximum impact and ROI.', 'leancv');?></p>
+<p><?php esc_html_e('Skilled in analyzing marketing metrics and performance data to derive actionable insights and optimize marketing strategies for maximum impact and ROI.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo __('Interests', 'leancv');?></h2>
+<h2 class="wp-block-heading"><?php esc_html_e('Interests', 'leancv');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Chicago Food', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Chicago Food', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Enthusiastic foodie passionate about exploring Chicago\'s vibrant culinary scene, from deep-dish pizza joints to trendy rooftop bars, and everything in between.', 'leancv');?></p>
+<p><?php esc_html_e('Enthusiastic foodie passionate about exploring Chicago\'s vibrant culinary scene, from deep-dish pizza joints to trendy rooftop bars, and everything in between.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Urban Exploration', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Urban Exploration', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Love immersing myself in the urban landscape, discovering hidden gems and unique neighborhoods while capturing the essence of city life through photography.', 'leancv');?></p>
+<p><?php esc_html_e('Love immersing myself in the urban landscape, discovering hidden gems and unique neighborhoods while capturing the essence of city life through photography.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo __('Community Involvement', 'leancv');?></h3>
+<h3 class="wp-block-heading"><?php esc_html_e('Community Involvement', 'leancv');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo __('Committed to giving back to the community through volunteer work and civic engagement, supporting local causes and initiatives that make a positive impact in Chicago and beyond.', 'leancv');?></p>
+<p><?php esc_html_e('Committed to giving back to the community through volunteer work and civic engagement, supporting local causes and initiatives that make a positive impact in Chicago and beyond.', 'leancv');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
@@ -167,8 +167,8 @@
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"stretch","width":"35%","lock":{"move":false,"remove":false},"style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"blockGap":"0px"}}} -->
-<div class="wp-block-column is-vertically-aligned-stretch" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;flex-basis:35%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+<!-- wp:column {"verticalAlignment":"stretch","width":"35%","lock":{"move":false,"remove":false},"metadata":{"name":"Image"},"style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"},"blockGap":"0px"}}} -->
+<div class="wp-block-column is-vertically-aligned-stretch" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;flex-basis:35%"><!-- wp:group {"metadata":{"name":"Pinned Image Container"},"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:template-part {"slug":"profile-pic"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
