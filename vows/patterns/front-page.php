1bd9ba823b24b28280504ac158d316fdac325b1b
Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
2024-07-03 16:11:55 +0200

    Vows: Fixing issues after review

--- vows/patterns/front-page.php
+++ vows/patterns/front-page.php
@@ -17,7 +17,7 @@
 <div class="wp-block-column" style="flex-basis:60%"><!-- wp:group {"style":{"spacing":{"blockGap":"0"},"dimensions":{"minHeight":"100vh"},"background":{"backgroundImage":{"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/groovepaper.png","source":"file","title":"groovepaper"},"backgroundSize":"300px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
 <div class="wp-block-group" style="min-height:100vh"><!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
 <main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading">The Wedding of Emily Johnson &amp; Michael Brown</h1>
+<h1 class="wp-block-heading">The Wedding of Emily Johnson &amp; Michael Brown</h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
