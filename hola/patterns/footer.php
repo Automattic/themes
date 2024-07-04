a8560237665cfbd7cc172cbd39d0d0c46a58ecda
Henrique Iamarino <103748085+henriqueiamarino@users.noreply.github.com>
2024-07-04 18:02:46 +0200

    Hola: add theme (#7927)
    * Hola: add theme
    
    * Update readme file.
    
    * Fix readme
    
    ---------
    
    Co-authored-by: Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
    Co-authored-by: jasmussen <joen@automattic.com>

---
+++ hola/patterns/footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: footer
+ * Slug: hola/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"bottom":"var:preset|spacing|70","right":"var:preset|spacing|50","left":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"var:preset|spacing|40"}},"border":{"top":{"width":"1px","style":"dotted"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top","justifyContent":"left"}} -->
+<div class="wp-block-group" style="border-top-style:dotted;border-top-width:1px;padding-top:var(--wp--preset--spacing--40)"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Tumblr</a>', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('·', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Behance</a>', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('·', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Mastodon</a>', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('·', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('<a href="#" target="_blank" rel="noreferrer noopener">Linkedin</a>', 'hola');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","style":{"layout":{"selfStretch":"fill","flexSize":null}},"fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('Get in <a href="#" data-type="mailto" data-id="mailto:">touch</a>.', 'hola');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size"><?php echo __('Designed with <a href="#" target="_blank" rel="noreferrer noopener nofollow">WordPress</a>', 'hola');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
