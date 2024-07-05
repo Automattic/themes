9d090e4f873327961fee9d5cfae61d0db972f164
Henrique 'Foca' Iamarino <henriquefoca@gmail.com>
2024-07-05 15:46:27 +0200

    Blissed: add theme

---
+++ blissed/patterns/front-page.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: blissed/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"0rem","left":"0rem"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"65%","style":{"spacing":{"padding":{"top":"5vh","bottom":"5vh"}}}} -->
+<div class="wp-block-column" style="padding-top:5vh;padding-bottom:5vh;flex-basis:65%"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|70","left":"0"},"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:column {"width":"33.3%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:33.3%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:template-part {"slug":"header","area":"header"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.6%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:66.6%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60","margin":{"top":"var:preset|spacing|30"}},"dimensions":{"minHeight":"30vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="min-height:30vh;margin-top:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php echo __('Anne Brown<br>&amp; Ashley Smith are set to wed', 'blissed');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Join us in celebrating this union with joy, love, laughter, and unforgettable memories. Your presence is requested and will be greatly appreciated.', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|70","left":"0"},"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:column {"width":"33.3%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:33.3%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:template-part {"slug":"footer","area":"footer"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.6%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:66.6%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|70"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"5vh"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<main class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div id="ceremony" class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Ceremony', 'blissed');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Date &amp; Time:</strong> September 15, 2024, 4:00 PM.<br><strong>Venue:</strong> Whispering Pines Resort, Lakeside Garden<br><strong>Address:</strong> 1234 Lakeview Road, Lake Tahoe, CA.<br><strong>Dress code:</strong> Semi-formal attire.', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div id="reception" class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Reception', 'blissed');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Date &amp; Time:</strong> September 15, 2024, 6:00 PM.<br><strong>Venue: </strong>Whispering Pines Resort, Grand Ballroom.<br><strong>Menu:</strong> Cocktail hour, followed by a three-course dinner with options for vegetarian, vegan, and gluten-free dishes.<br><strong>Entertainment: </strong>Live band, DJ, and photo booth.', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div id="accomodations" class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Accommodations', 'blissed');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('For your convenience, we have reserved a block of rooms at the following hotels:', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Whispering Pines Resort</strong><br>Special wedding rate available. <a href="http://localhost.local/accommodations/#">Book Here</a>.', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Lakeside Inn</strong><br>5-minute drive from the venue. <a href="http://localhost.local/accommodations/#">Book Here</a>.', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Mountain View Lodge</strong><br>Special wedding rate available. <a href="http://localhost.local/accommodations/#">Book Here</a>.', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div id="registry" class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading"><?php echo __('Wedding Registry', 'blissed');?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('Your presence at our wedding is the greatest gift of all. However, if you wish to honor us with a gift, we are registered at the following stores:', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<a href="http://localhost.local/wedding-registry/#">Amazon</a>&nbsp;|&nbsp;<a href="http://localhost.local/wedding-registry/#">Target</a>&nbsp;|&nbsp;<a href="http://localhost.local/wedding-registry/#">Crate &amp; Barrel</a>', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"className":"no-underline","fontSize":"small"} -->
+<p class="no-underline has-small-font-size"><?php echo __('<a href="#">Instagram</a> / <a href="#">TikTok</a> / <a href="#">X</a>', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"lock":{"move":false,"remove":true},"className":"no-underline","fontSize":"small"} -->
+<p class="no-underline has-small-font-size"><?php echo __('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"5%","style":{"spacing":{"padding":{"top":"5vh","bottom":"5vh"}}}} -->
+<div class="wp-block-column" style="padding-top:5vh;padding-bottom:5vh;flex-basis:5%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"30%"} -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"5vh"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:5vh"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Romantic-Middle-Aged-Women.jpg","dimRatio":10,"overlayColor":"base","isUserOverlayColor":true,"minHeight":87.5,"minHeightUnit":"vh","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="min-height:87.5vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-10 has-background-dim"></span><img class="wp-block-cover__image-background" alt="<?php echo __('', 'blissed');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Romantic-Middle-Aged-Women.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"><?php echo __('', 'blissed');?></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ blissed/patterns/header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: header
+ * Slug: blissed/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:site-title /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"className":"no-underline"} -->
+<p class="no-underline"><?php echo __('<a href="#ceremony" data-type="internal" data-id="#ceremony">Ceremony</a>', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"no-underline"} -->
+<p class="no-underline"><?php echo __('<a href="#ceremony" data-type="internal" data-id="#ceremony">Reception</a>', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"no-underline"} -->
+<p class="no-underline"><?php echo __('<a href="#ceremony" data-type="internal" data-id="#ceremony">Accommodations</a>', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"no-underline"} -->
+<p class="no-underline"><?php echo __('<a href="#ceremony" data-type="internal" data-id="#ceremony">Wedding Registry</a>', 'blissed');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"no-underline"} -->
+<p class="no-underline"><?php echo __('<a href="/blog" data-type="internal" data-id="#ceremony">Blog</a>', 'blissed');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
