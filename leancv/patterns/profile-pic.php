f8c28d0af0330fe542f6a05b44b4d1900b895f88
Rich Tabor <hi@richtabor.com>
2024-07-05 09:18:21 -0400

    LeanCV: Use shared color variations (#7923)
    * work
    
    * use assembler variations
    
    * use generic duotone slug, fix spacing sizes
    
    * Update front-page.php
    
    * Use generic duotone filter on cover

--- leancv/patterns/profile-pic.php
+++ leancv/patterns/profile-pic.php
@@ -7,8 +7,8 @@
  * Inserter: no
  */
 ?>
-<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/youngwoman_jobsearch_bw.jpg","dimRatio":0,"isUserOverlayColor":true,"focalPoint":{"x":0.2,"y":0.5},"minHeight":100,"minHeightUnit":"vh","contentPosition":"top left","isDark":false,"align":"full","style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"}},"color":[]}} -->
-<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-top-left" style="margin-top:0rem;margin-bottom:0rem;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background " alt="<?php echo __('', 'leancv');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/youngwoman_jobsearch_bw.jpg" style="object-position:20% 50%" data-object-fit="cover" data-object-position="20% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…"} -->
-<p class="has-text-align-center"><?php echo __('', 'leancv');?></p>
-<!-- /wp:paragraph --></div></div>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/youngwoman_jobsearch_bw.jpg","dimRatio":0,"isUserOverlayColor":true,"focalPoint":{"x":0.2,"y":0.5},"minHeight":100,"minHeightUnit":"vh","contentPosition":"top left","isDark":false,"align":"full","style":{"spacing":{"margin":{"top":"0rem","bottom":"0rem"}},"color":{"duotone":"var:preset|duotone|duotone-1"}}} -->
+<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-top-left" style="margin-top:0rem;margin-bottom:0rem;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="<?php esc_html_e('', 'leancv');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/youngwoman_jobsearch_bw.jpg" style="object-position:20% 50%" data-object-fit="cover" data-object-position="20% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div></div>
 <!-- /wp:cover -->
