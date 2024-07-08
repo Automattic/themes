Automattic/themes

commit 4bf4fb9af25afdce69293cf951ab1e9697a32735

Add strict types

seedlet/inc/wpcom-editor-colors.php
@@ -1,2 +1,3 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
-require_once 'wpcom-colors.php';
+require_once __DIR__ . 'wpcom-colors.php';

balasana/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors: Balasana

barnsbury/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors: Barnsbury

brompton/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors: Brompton

coutoire/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors

dalston/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors

exford/inc/wpcom-editor-colors.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /*
  * Custom Editor Colors

hever/inc/wpcom-editor-colors.php
@@ -1,2 +1,3 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 require_once get_template_directory() . '/inc/wpcom-colors-css-variables.php';

commit f945228b3b0d64fc2487cd66f8b9e08e76ce5d27

Seedlet: Fix a fatal error in Gutenberg
4bf4fb9a introduced a bug when including `wpcom-colors.php` by using the
editor. It was missing a directory separate and so  it fataled with an
error message like:
`PHP Fatal error:  Uncaught Error: Failed opening required '/xxx/seedlet/incwpcom-colors.php' (include_path='.') in /xxx/seedlet/inc/wpcom-editor-colors.php:3

seedlet/inc/wpcom-editor-colors.php
@@ -1,3 +1,3 @@
 <?php declare( strict_types = 1 ); ?>
 <?php
-require_once __DIR__ . 'wpcom-colors.php';
+require_once __DIR__ . '/wpcom-colors.php';