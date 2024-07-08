d8b3f8a681ba970cf65c7dd569ee3e3bea38ac8f
Joen A <1204802+jasmussen@users.noreply.github.com>
2024-07-08 14:48:45 +0200

    Changes from WordPress Playground, made some adjustments to the theme: (#7938)
    - Changed its main heading "Is this nice?" to "Is this cool?" after some feedback regarding the expression
    - Adjustments in the outer spacing
    - Adjustments in the templates wide widths
    - Added font license
    - Updated screenshot
    
    Co-authored-by: Beatriz Fialho <65220155+beafialho@users.noreply.github.com>

---
+++ muscat/patterns/header-landing-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: header-landing-page
+ * Slug: muscat/header-landing-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"metadata":{"name":"Header with cover"},"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|80","right":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignfull" style="min-height:100vh"><!-- wp:group {"metadata":{"name":"Top Nav"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"0","bottom":"var:preset|spacing|60","left":"0"},"blockGap":"var:preset|spacing|60"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"primary","textColor":"background","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignwide has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--60);padding-right:0;padding-bottom:var(--wp--preset--spacing--60);padding-left:0"><!-- wp:site-title {"textAlign":"left"} /-->
+
+<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"fontSize":"medium"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"100px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"align":"wide","style":{"typography":{"fontSize":"18rem","lineHeight":1,"fontStyle":"normal","fontWeight":"500","letterSpacing":"-0.04em"},"spacing":{"padding":{"top":"0rem","right":"0","bottom":"var:preset|spacing|70","left":"0"}}},"textColor":"background"} -->
+<h2 class="wp-block-heading alignwide has-background-color has-text-color" style="padding-top:0rem;padding-right:0;padding-bottom:var(--wp--preset--spacing--70);padding-left:0;font-size:18rem;font-style:normal;font-weight:500;letter-spacing:-0.04em;line-height:1"><?php echo __('Is this cool?', 'muscat');?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
--- muscat/readme.txt
+++ muscat/readme.txt
@@ -45,3 +45,13 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
+
+== Fonts ==
+
+Albert Sans Font
+Copyright 2023 The Albert Sans Project Authors (https://github.com/usted/Albert-Sans)
+This Font Software is licensed under the SIL Open Font License, Version 1.1.
+This license is copied below, and is also available with a FAQ at:
+http://scripts.sil.org/OFL
+License URL: http://scripts.sil.org/OFL 
+-- End of Albert Sans Font credits --
