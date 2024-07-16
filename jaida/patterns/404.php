fd2d50c5278ad165d85bf5a0b7a1ad95d9aa880a
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-07-16 11:52:15 +0100

    Fixes #7255:
    - Adds "inherit query template" on Archive, Seearch Results and Index templates
    
    Also exported as a zip file.
    
    * [Preview loaded from this PR – available **before** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FAutomattic%2Fthemes%2Fplayground-changes-2024-07-16T10-52-08-414Z%2Fjaida%2Fplayground.zip)
    * [Preview loaded from the main branch – available **after** this PR is merged](https://playground.wordpress.net/?import-site=https%3A%2F%2Fraw.githubusercontent.com%2FAutomattic%2Fthemes%2Ftrunk%2Fjaida%2Fplayground.zip)

--- jaida/patterns/404.php
+++ jaida/patterns/404.php
@@ -1,21 +1,23 @@
 <?php
 /**
- * Title: A 404 page
+ * Title: 404
  * Slug: jaida/404
+ * Categories: hidden
  * Inserter: no
  */
-
 ?>
+<!-- wp:template-part {"slug":"secondary-header"} /-->
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"top":"16vh","bottom":"16vh"}}},"layout":{"type":"constrained","wideSize":"1400px"}} -->
-<main class="wp-block-group" style="margin-top:16vh;margin-bottom:16vh"><!-- wp:heading {"textAlign":"left","level":1,"align":"wide","className":"wp-block-heading","anchor":"oops-that-page-can-t-be-found"} -->
-<h1 class="wp-block-heading alignwide has-text-align-left" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can’t be found.', 'jaida' ); ?></h1>
+<main class="wp-block-group" style="margin-top:16vh;margin-bottom:16vh"><!-- wp:heading {"textAlign":"left","level":1,"align":"wide","className":"wp-block-heading"} -->
+<h1 class="wp-block-heading alignwide has-text-align-left" id="oops-that-page-can-t-be-found"><?php esc_html_e('Oops! That page can’t be found.', 'jaida');?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'jaida' ); ?></p>
+<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'jaida');?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:search {"label":"","showLabel":false} /--></main>
+<!-- wp:search {"label":"","showLabel":false,"placeholder":"Search..."} /--></main>
 <!-- /wp:group -->
 
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
