4c3fecca8a2eb85b0681bd673be4dc73fd52f346
Henrique Iamarino <103748085+henriqueiamarino@users.noreply.github.com>
2024-07-16 23:59:04 +0200

    Specials: add theme (#7920)
    * Specials: add theme
    
    * Specials: Fixing after review
    
    * Specials: Fixing Hero Image
    
    ---------
    
    Co-authored-by: Henrique 'Foca' Iamarino <henriquefoca@gmail.com>

---
+++ specials/patterns/footer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Title: footer
+ * Slug: specials/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"5vh","bottom":"10vh"},"margin":{"top":"0vh","bottom":"0vh"},"blockGap":"0vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0vh;margin-bottom:0vh;padding-top:5vh;padding-bottom:10vh"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"2px","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"border":{"bottom":{"width":"2px"},"top":[],"right":[],"left":[]}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="border-bottom-width:2px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:2px;padding-left:0"><!-- wp:columns {"isStackedOnMobile":false,"align":"wide","style":{"spacing":{"blockGap":{"top":"0","left":"0px"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"border":{"width":"1px"}}} -->
+<div class="wp-block-columns alignwide is-not-stacked-on-mobile" style="border-width:1px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"stretch","width":"1%","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}},"backgroundColor":"tertiary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-tertiary-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:1%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"98%","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"0"},"border":{"left":{"width":"1px"},"top":[],"right":{"width":"1px"},"bottom":[]}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="border-right-width:1px;border-left-width:1px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--50);flex-basis:98%"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"2vh","bottom":"4vh","right":"0","left":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:2vh;padding-right:0;padding-bottom:4vh;padding-left:0"><!-- wp:site-title {"textAlign":"left","style":{"typography":{"fontSize":"6.5vw","letterSpacing":"-0.3rem"}},"fontFamily":"rubik-mono-one"} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><?php echo __('Discover authentic Chinese cuisine, crafted for food lovers.', 'specials');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo __('<strong>Book via Phone</strong> +(39) 1111-123456.<br>785 15th St, New York, NY 01234', 'specials');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100%"},"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="min-height:100%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"><?php echo __('↑ <a href="#" target="_blank" rel="noreferrer noopener">Back to top</a>', 'specials');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"><?php echo __('·', 'specials');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"><?php echo __('Designed with <a href="#" rel="nofollow">WordPress</a>.', 'specials');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:social-links {"iconColor":"background","iconColorValue":"#ffcab3","iconBackgroundColor":"primary","iconBackgroundColorValue":"#681d2a","showLabels":true,"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<ul class="wp-block-social-links has-visible-labels has-icon-color has-icon-background-color"><!-- wp:social-link {"url":"#","service":"tumblr","label":"Tumblr"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram","label":"Instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"stretch","width":"1%","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"}},"backgroundColor":"tertiary"} -->
+<div class="wp-block-column is-vertically-aligned-stretch has-tertiary-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:1%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
