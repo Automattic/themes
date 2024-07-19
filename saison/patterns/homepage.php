b55875569c7ea62e96c0174d66c8adaf86c82ac8
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-07-19 15:22:10 +0100

    Saison is a clean, modern theme for restaurants, bakeries and other food businesses. It comes with page patterns and style variations that allow you to quickly design and launch your website.

---
+++ saison/patterns/homepage.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Title: Homepage
+ * Slug: saison/homepage
+ * Categories: featured, page
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"6vw","left":"6vw"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-right:6vw;padding-left:6vw"><!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"80%"} -->
+<div class="wp-block-column" style="flex-basis:80%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Welcome to Saison, your favorite local and seasonal food place in Norwich.</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"10px"} -->
+<div style="height:10px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":398,"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://saison.mystagingwebsite.com/wp-content/themes/saison/assets/images/home-image-1-scaled.jpg" alt="" class="wp-image-398" style="aspect-ratio:16/9;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:columns {"style":{"spacing":{"padding":{"top":"6vh","bottom":"6vh"}}}} -->
+<div class="wp-block-columns" style="padding-top:6vh;padding-bottom:6vh"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">At Saison, we embrace the bounty of the sea and the richness of local, seasonal ingredients to create a culinary experience that reflects the heart of our community.</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"60.2%"} -->
+<div class="wp-block-column" style="flex-basis:60.2%"><!-- wp:image {"id":399,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://saison.mystagingwebsite.com/wp-content/themes/saison/assets/images/home-image-2-1024x556.jpg" alt="" class="wp-image-399"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":400,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://saison.mystagingwebsite.com/wp-content/themes/saison/assets/images/home-image-3-1024x891.jpg" alt="" class="wp-image-400"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://saison.mystagingwebsite.com/reservations/">Book a Table</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Home Delivery</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|60"} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.9rem","letterSpacing":"0.06em"}}} -->
+<p class="has-text-align-center" style="font-size:0.9rem;letter-spacing:0.06em">92 St. Faiths Lane,<br>Norwich NR1 1NE</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"0.9rem","letterSpacing":"0.06em"}}} -->
+<p class="has-text-align-center" style="font-size:0.9rem;letter-spacing:0.06em"><a href="#">email@example.com</a><br>+00 123 456 789</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
