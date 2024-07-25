96939b1e78b1b9fdf826b0582f28c8ac66827912
Beatriz Fialho <65220155+beafialho@users.noreply.github.com>
2024-07-25 11:39:49 +0100

    Adds Goodskin theme:
    Goodskin is a blogging theme designed for skincare blogs.

---
+++ goodskin/patterns/two-column-gallery.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Two Column Gallery
+ * Slug: goodskin/two-column-gallery
+ * Categories: gallery
+ * Inserter: yes
+ */
+declare( strict_types = 1 );
+?>
+
+<!-- wp:columns {"verticalAlignment":"center","metadata":{"name":"Gallery"},"style":{"spacing":{"blockGap":{"left":"0"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":246,"scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="https://skinblog.mystagingwebsite.com/wp-content/themes/goodskin/assets/images/skin-blog-home-image-2-2.jpg" alt="" class="wp-image-246" style="object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"45%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:45%"><!-- wp:image {"id":236,"scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"8vw"}},"borderColor":"base"} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="https://skinblog.mystagingwebsite.com/wp-content/themes/goodskin/assets/images/skin-blog-home-image-3-1.jpg" alt="" class="has-border-color has-base-border-color wp-image-236" style="border-width:8vw;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
