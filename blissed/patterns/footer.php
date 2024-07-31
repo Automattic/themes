e1ebaafbe3db8d6e5bf34a100066946a4e080174
henriqueiamarino <henrique.iamarino@a8c.com>
2024-07-31 15:27:22 +0200

    Blissed: Fix the footer

--- blissed/patterns/footer.php
+++ blissed/patterns/footer.php
@@ -1,4 +1,3 @@
-<?php declare( strict_types = 1 ); ?>
 <?php
 /**
  * Title: footer
@@ -10,21 +9,21 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"10vh"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading"><?php echo __('Save the date', 'blissed');?></h2>
+<h2 class="wp-block-heading"><?php esc_html_e('Save the date', 'blissed');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left"><?php echo __('September 15, 2024 — 4:00 PM<br>Whispering Pines, Lake Tahoe, CA<br>Reception to follow ceremony', 'blissed');?></p>
+<p class="has-text-align-left"><?php esc_html_e('September 15, 2024 — 4:00 PM<br>Whispering Pines, Lake Tahoe, CA<br>Reception to follow ceremony', 'blissed');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20","margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":1,"className":"no-underline","fontSize":"large"} -->
-<h1 class="wp-block-heading no-underline has-large-font-size"><?php echo __('<a href="/our-story" data-type="page" data-id="391">Our story</a>', 'blissed');?></h1>
+<h1 class="wp-block-heading no-underline has-large-font-size"><?php esc_html_e('<a href="#" data-type="page" data-id="391">Our story</a>', 'blissed');?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:heading {"level":1,"className":"no-underline","fontSize":"large"} -->
-<h1 class="wp-block-heading no-underline has-large-font-size"><?php echo __('<a href="/rsvp" data-type="page" data-id="391">RSVP</a>', 'blissed');?></h1>
+<h1 class="wp-block-heading no-underline has-large-font-size"><?php esc_html_e('<a href="#" data-type="page" data-id="391">RSVP</a>', 'blissed');?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
