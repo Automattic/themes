bb95faf3e73cca14967558ed02209786ad359454
alaczek <ola@bodera.com>
2024-07-31 14:32:51 +1000

    Update two column gallery pattern
    Fix image paths.

--- goodskin/patterns/two-column-gallery.php
+++ goodskin/patterns/two-column-gallery.php
@@ -10,14 +10,14 @@ declare( strict_types = 1 );
 
 <!-- wp:columns {"verticalAlignment":"center","metadata":{"name":"Gallery"},"style":{"spacing":{"blockGap":{"left":"0"}}}} -->
 <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":246,"scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-style-default"><img src="https://skinblog.mystagingwebsite.com/wp-content/themes/goodskin/assets/images/skin-blog-home-image-2-2.jpg" alt="" class="wp-image-246" style="object-fit:cover"/></figure>
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/skin-blog-home-image-2-2.jpg" alt="<?php esc_html_e('', 'goodskin');?>" style="object-fit:cover"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"45%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:45%"><!-- wp:image {"id":236,"scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"8vw"}},"borderColor":"base"} -->
-<figure class="wp-block-image size-full has-custom-border"><img src="https://skinblog.mystagingwebsite.com/wp-content/themes/goodskin/assets/images/skin-blog-home-image-3-1.jpg" alt="" class="has-border-color has-base-border-color wp-image-236" style="border-width:8vw;object-fit:cover"/></figure>
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:45%"><!-- wp:image {"scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"width":"8vw"}},"borderColor":"base"} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/skin-blog-home-image-3-1.jpg" alt="<?php esc_html_e('', 'goodskin');?>" class="has-border-color has-base-border-color" style="border-width:8vw;object-fit:cover"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
