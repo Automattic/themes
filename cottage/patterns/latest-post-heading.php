abd3a98a70b69583d89a4076a713ad4c0cec1bef
alaczek <ola@bodera.com>
2024-08-07 16:07:29 +1000

    Remove references to font family not used in the theme

--- cottage/patterns/latest-post-heading.php
+++ cottage/patterns/latest-post-heading.php
@@ -10,8 +10,8 @@ declare( strict_types = 1 );
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0.5rem","margin":{"bottom":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="margin-bottom:var(--wp--preset--spacing--60)">
-	<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"0px"}},"fontSize":"small","fontFamily":"source-sans-3"} -->
-	<h2 class="wp-block-heading has-source-sans-3-font-family has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:0px"><?php echo esc_html__( 'Latest Posts', 'cottage' ); ?></h2>
+	<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","letterSpacing":"0px"}},"fontSize":"small"} -->
+	<h2 class="wp-block-heading has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:0px"><?php echo esc_html__( 'Latest Posts', 'cottage' ); ?></h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:separator -->
