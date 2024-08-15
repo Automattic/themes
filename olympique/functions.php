2ba4dd2aa9e29d6ad566564befb974f18479c674
Miguel <miguel.san.segundo@automattic.com>
2024-08-15 13:30:19 +0700

    Version Bump

--- olympique/functions.php
+++ olympique/functions.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /**
  * Olympique functions and definitions
--- olympique/patterns/posts-list.php
+++ olympique/patterns/posts-list.php
@@ -1,3 +1,4 @@
+<?php declare( strict_types = 1 ); ?>
 <?php
 /**
  * Title: List of posts without images, 1 column
