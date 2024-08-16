5b18a6582f151cb795fab2dbe8c13e124324e91b
alaczek <ola@bodera.com>
2024-08-16 10:43:56 +1000

    Update footer.php (#8021)
    * Update footer.php
    
    Swap static links to a nav menu.
    
    * Update footer.php
    
    Remove nav ref id.

--- blissed/patterns/footer.php
+++ blissed/patterns/footer.php
@@ -18,12 +18,12 @@
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20","margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":2,"className":"no-underline","fontSize":"large"} -->
-<h2 class="wp-block-heading no-underline has-large-font-size"><a href="#" alt=""><?php esc_html_e('Our story', 'blissed');?></a></h2>
-<!-- /wp:heading -->
+<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50)">
 
-<!-- wp:heading {"level":2,"className":"no-underline","fontSize":"large"} -->
-<h2 class="wp-block-heading no-underline has-large-font-size"><a href="#" alt=""><?php esc_html_e('RSVP', 'blissed');?></a></h2>
-<!-- /wp:heading --></div>
+<!-- wp:navigation {"layout":{"type":"flex","orientation":"vertical"}} /-->
+
+</div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
+
+
