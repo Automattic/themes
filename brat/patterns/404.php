095487182ca2f394b55fb1938a15308269fc0436
Sarah Norris <sarah@sekai.co.uk>
2024-08-22 12:05:20 +0100

    Use 404 pattern

--- brat/patterns/404.php
+++ brat/patterns/404.php
@@ -3,15 +3,12 @@
 /**
  * Title: A 404 page
  * Slug: brat/404
- * Inserter: no
  */
-
 ?>
-
-<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
-<h1 class="has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Oops! That page can&rsquo;t be found.', 'brat' ); ?></h1>
+<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--60)"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
+<h1 class="wp-block-heading has-text-align-center has-x-large-font-size" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'what the hell?! nothing was found', 'brat' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php echo  esc_html__( 'It looks like nothing was found at this location. Maybe try a search?', 'brat' ); ?></p>
-<!-- /wp:paragraph -->
+<!-- wp:search {"label":"","showLabel":false,"placeholder":"<?php echo esc_html__( 'type something...', 'brat' ); ?>","width":100,"widthUnit":"%","buttonText":"<?php echo esc_html__( 'show me the good stuff', 'brat' ); ?>","align":"center","style":{"border":{"radius":"0px","width":"0px","style":"none"},"typography":{"textTransform":"lowercase"}}} /--></main>
+<!-- /wp:group -->
