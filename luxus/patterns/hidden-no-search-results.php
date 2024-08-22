92bd08d71a5f2929a03920a8b5b423175b84022b
alaczek <ola@bodera.com>
2024-08-22 13:26:52 +1000

    Luxus: Update the index and archive templates.

--- luxus/patterns/hidden-no-search-results.php
+++ luxus/patterns/hidden-no-search-results.php
@@ -8,5 +8,5 @@ declare( strict_types = 1 );
 ?>
 
 <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><?php echo esc_html_x( 'No search results.', 'Text explaining that there are no results returned from a search', 'luxus' ); ?>.</p>
+    <p class="has-text-align-center"><?php echo esc_html_x( 'No search results.', 'Text explaining that there are no results returned from a search', 'luxus' ); ?></p>
 <!-- /wp:paragraph -->
