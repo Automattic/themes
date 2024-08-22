98e390fae1fa15bdb874cf858bc52d2ffcbf4f5e
Henrique Iamarino <103748085+henriqueiamarino@users.noreply.github.com>
2024-08-22 14:50:21 +0200

    Layover: fixing after Dotorg review (#8030)
    * Layover: Fixing after Dotorg review
    
    * Layover: fixing content links
    
    * Layover: Fixing palette to fix color links
    
    * Layover: Updating version number

--- layover/patterns/404.php
+++ layover/patterns/404.php
@@ -6,10 +6,11 @@
  * Inserter: no
  */
 ?>
-<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+<!-- wp:group {"style":{"dimensions":{"minHeight":"100vh"}},"backgroundColor":"primary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="min-height:100vh"><!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"4rem","top":"4rem"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:group -->
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"bottom":"4rem","top":"4rem"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:group -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
 <h1 class="wp-block-heading alignwide has-text-align-center" id="oops-that-page-can-t-be-found"><?php echo __('Oops!', 'layover');?></h1>
@@ -25,7 +26,8 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:search {"showLabel":false,"placeholder":"SEARCH","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"border":{"width":"0px","style":"none"}}} /--></div>
-<!-- /wp:group --></div>
+<!-- /wp:group --></main>
 <!-- /wp:group -->
 
-<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /--></div>
+<!-- /wp:group -->
--- layover/patterns/footer.php
+++ layover/patterns/footer.php
@@ -7,7 +7,7 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"4rem","top":"4rem"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php echo __('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a>.', 'layover');?></p>
+<div class="wp-block-group" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"textColor":"foreground"} -->
+<p class="has-text-align-center has-foreground-color has-text-color has-link-color"><?php echo __('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a>.', 'layover');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
