748ac71d0f6688d7e0972b2ce45693e7081f7a1e
MaggieCabrera <maggie.cabrera@automattic.com>
2024-09-03 10:43:06 +0200

    added a test theme

---
+++ test-theme/patterns/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: footer
+ * Slug: test-theme/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
+	<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px">
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php esc_html_e('Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'test-theme');?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
