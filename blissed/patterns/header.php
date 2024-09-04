ad6a162c89bbc8fa71cf574494ac75085de2f9ff
alaczek <ola@bodera.com>
2024-09-04 16:25:59 +1000

    Blissed: Update header.php
    Fix page jumps to always point to homepage sections.

--- blissed/patterns/header.php
+++ blissed/patterns/header.php
@@ -1,4 +1,10 @@
 <?php declare( strict_types = 1 ); ?>
+
+<?php
+// Get the homepage URL to create page jumps.
+$url = esc_url(home_url( '/')); 
+?>
+
 <?php
 /**
  * Title: header
@@ -12,19 +18,19 @@
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"className":"no-underline"} -->
-<p class="no-underline"><a href="#ceremony" data-type="internal" data-id="#ceremony"><?php esc_html_e('Ceremony', 'blissed');?></a></p>
+<p class="no-underline"><a href="<?php echo $url; ?>#ceremony" data-type="internal" data-id="#ceremony"><?php esc_html_e('Ceremony', 'blissed');?></a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"className":"no-underline"} -->
-<p class="no-underline"><a href="#ceremony" data-type="internal" data-id="#ceremony"><?php esc_html_e('Reception', 'blissed');?></a></p>
+<p class="no-underline"><a href="<?php echo $url; ?>#reception" data-type="internal" data-id="#reception"><?php esc_html_e('Reception', 'blissed');?></a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"className":"no-underline"} -->
-<p class="no-underline"><a href="#ceremony" data-type="internal" data-id="#ceremony"><?php esc_html_e('Accommodations', 'blissed');?></a></p>
+<p class="no-underline"><a href="<?php echo $url; ?>#accomodations" data-type="internal" data-id="#accomodations"><?php esc_html_e('Accommodations', 'blissed');?></a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"className":"no-underline"} -->
-<p class="no-underline"><a href="#ceremony" data-type="internal" data-id="#ceremony"><?php esc_html_e('Wedding Registry', 'blissed');?></a></p>
+<p class="no-underline"><a href="<?php echo $url; ?>#registry" data-type="internal" data-id="#registry"><?php esc_html_e('Wedding Registry', 'blissed');?></a></p>
 <!-- /wp:paragraph -->
 
 </div>
