590860a7e17bd9a08b680b36b609e6316eb5abe0
henriqueiamarino <henrique.iamarino@a8c.com>
2024-09-04 12:35:08 +0200

    Titleless: add theme

---
+++ titleless/patterns/footer-home.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Title: footer-home
+ * Slug: titleless/footer-home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|80"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"full"} -->
+<div class="wp-block-group alignfull"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"fontSize":"large"} /-->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><?php esc_html_e('A design for writers who want their posts displayed as a direct conversation, with no images or post titles. Titleless also has diverse style variations to spark your creativity. Enjoy!', 'titleless');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"medium"} -->
+<p class="has-text-align-left has-medium-font-size"><?php esc_html_e('Designed with WordPress', 'titleless');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"50%","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:50%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-3856482.jpg","dimRatio":0,"isUserOverlayColor":true,"minHeight":70,"minHeightUnit":"vh","contentPosition":"top center","style":{"spacing":{"padding":{"top":"0rem","right":"0rem","bottom":"0rem","left":"0rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-center" style="padding-top:0rem;padding-right:0rem;padding-bottom:0rem;padding-left:0rem;min-height:70vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="<?php esc_attr_e('', 'titleless');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/image-from-rawpixel-id-3856482.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"75%","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"bottom":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"backgroundColor":"primary","textColor":"background"} -->
+<div class="wp-block-column is-vertically-aligned-top has-background-color has-primary-background-color has-text-color has-background has-link-color" style="padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);flex-basis:75%"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<p><?php esc_html_e('Month Discover: Henri Rousseau\'s Virgin Forest with Sunset (1910). Original from the Kunstmuseum Basel Museum.', 'titleless');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"25%","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:25%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
---
+++ titleless/patterns/sidebar.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Title: sidebar
+ * Slug: titleless/sidebar
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|70"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignfull"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php esc_html_e('Related Articles', 'titleless');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:query {"queryId":21,"query":{"perPage":"4","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null},"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-query alignfull"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"default","columnCount":3}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"isLink":true} /-->
+
+<!-- wp:post-date /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php esc_html_e('Get updates', 'titleless');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Spam-free subscription, we guarantee. This is just a friendly ping when new content is out.', 'titleless');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:jetpack/contact-form {"subject":"[Texty] Posts","to":"henrique.iamarino@a8c.com","className":"is-style-animated","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<div class="wp-block-jetpack-contact-form is-style-animated" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:jetpack/field-name {"required":true,"requiredText":"(required)","borderRadius":4,"borderWidth":1,"labelFontSize":"1rem","fieldFontSize":"1rem","lineHeight":1,"labelLineHeight":1,"inputColor":"#111111","labelColor":"#111111","borderColor":"#111111"} /-->
+
+<!-- wp:jetpack/field-email {"required":true,"requiredText":"(required)","borderRadius":4,"borderWidth":1,"labelFontSize":"1rem","fieldFontSize":"1rem","lineHeight":1,"labelLineHeight":1,"inputColor":"#111111","labelColor":"#111111","borderColor":"#111111"} /-->
+
+<!-- wp:jetpack/button {"element":"button","text":"Subscribe","textColor":"background","backgroundColor":"primary","borderRadius":4,"width":"100%","lock":{"remove":true},"fontSize":"small"} /--></div>
+<!-- /wp:jetpack/contact-form --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
