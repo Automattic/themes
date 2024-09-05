151b92d177b4a07def0128c527f580e6e7432458
henriqueiamarino <henrique.iamarino@a8c.com>
2024-09-05 14:29:13 +0200

    Promoter: fixing patterns issue

--- promoter/patterns/search.php
+++ promoter/patterns/search.php
@@ -56,7 +56,7 @@
 
 <!-- wp:query-no-results {"align":"wide"} -->
 <!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p><?php esc_html_e('No posts were found.', 'promoter');?></p>
+<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'promoter' ); ?></p>
 <!-- /wp:paragraph -->
 <!-- /wp:query-no-results --></div>
 <!-- /wp:group --></div>
