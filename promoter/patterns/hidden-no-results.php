c63a5389e98d6138111f3148280d440bb89407c9
henriqueiamarino <henrique.iamarino@a8c.com>
2024-09-05 12:40:09 +0200

    Promoter: fixing issues after dotorg review

--- promoter/patterns/hidden-no-results.php
+++ promoter/patterns/hidden-no-results.php
@@ -6,5 +6,5 @@
  */
 ?>
 <!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-<p><?php echo esc_html_x('No posts were found.', 'promoter');?></p>
+<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'promoter' ); ?></p>
 <!-- /wp:paragraph -->
