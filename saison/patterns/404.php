7776d7530bc0ead881a72cda40913f5c0b432351
MaggieCabrera <maggie.cabrera@automattic.com>
2024-09-05 12:16:56 +0200

    fix typo

--- saison/patterns/404.php
+++ saison/patterns/404.php
@@ -10,7 +10,7 @@
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|60"},"padding":{"right":"6vw","left":"6vw"}}},"layout":{"type":"default"}} -->
 <main class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--60);padding-right:6vw;padding-left:6vw"><!-- wp:heading {"textAlign":"left","level":1,"fontSize":"large"} -->
-<h1 class="wp-block-heading has-text-align-left has-large-font-size" id="oops-that-page-can-t-be-found"><?php esc_html_e('Oops! That page can’t be foundd.', 'saison');?></h1>
+<h1 class="wp-block-heading has-text-align-left has-large-font-size" id="oops-that-page-can-t-be-found"><?php esc_html_e('Oops! That page can’t be found.', 'saison');?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"left"} -->
