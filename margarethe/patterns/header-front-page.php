f0d456fc6db3c3bbabd300ff876d54ad35c23b38
MaggieCabrera <maggie.cabrera@automattic.com>
2024-09-06 12:16:05 +0200

    alt texts

--- margarethe/patterns/header-front-page.php
+++ margarethe/patterns/header-front-page.php
@@ -2,13 +2,12 @@
 /**
  * Title: header-front-page
  * Slug: margarethe/header-front-page
- * Categories: header
- * Block Types: core/template-part/header
+ * Categories: hidden
+ * Inserter: no
  */
 ?>
-
-<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/margarethe-img-1-scaled.jpg","id":24,"dimRatio":50,"minHeight":100,"minHeightUnit":"vh","customGradient":"linear-gradient(180deg,rgb(0,0,0) 0%,rgba(0,0,0,0) 50%,rgb(0,0,0) 96%)","style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(180deg,rgb(0,0,0) 0%,rgba(0,0,0,0) 50%,rgb(0,0,0) 96%)"></span><img class="wp-block-cover__image-background wp-image-24" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/margarethe-img-1-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/margarethe-img-1-scaled.jpg","dimRatio":50,"minHeight":100,"minHeightUnit":"vh","customGradient":"linear-gradient(180deg,rgb(0,0,0) 0%,rgba(0,0,0,0) 50%,rgb(0,0,0) 96%)","style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(180deg,rgb(0,0,0) 0%,rgba(0,0,0,0) 50%,rgb(0,0,0) 96%)"></span><img class="wp-block-cover__image-background " alt="<?php esc_html_e('Elegant & cozy home interior.', 'margarethe');?>" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/margarethe-img-1-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group alignfull" style="min-height:100vh"><!-- wp:group {"align":"full","style":{"border":{"top":{"width":"0px","style":"none"},"right":[],"bottom":[],"left":[]}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="border-top-style:none;border-top-width:0px"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:column {"verticalAlignment":"center","width":"35%"} -->
@@ -17,11 +16,11 @@
 
 <!-- wp:column {"verticalAlignment":"center","width":"65%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:65%"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right","verticalAlignment":"center"}} -->
-<div class="wp-block-group alignwide"><!-- wp:navigation {"ref":4,"overlayBackgroundColor":"base","overlayTextColor":"primary","align":"wide","layout":{"type":"flex","justifyContent":"right"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"}}} /-->
+<div class="wp-block-group alignwide"><!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"primary","align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","justifyContent":"right"}} /-->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Work with me', 'margarethe' ); ?></a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Work with me', 'margarethe');?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -36,13 +35,13 @@
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":""} -->
 <div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","letterSpacing":"0.04em"},"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="margin-top:var(--wp--preset--spacing--30);letter-spacing:0.04em;text-transform:uppercase"><?php esc_html_e( 'What I do', 'margarethe' ); ?></p>
+<p class="has-small-font-size" style="margin-top:var(--wp--preset--spacing--30);letter-spacing:0.04em;text-transform:uppercase"><?php esc_html_e('What I do', 'margarethe');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"65.16%"} -->
-<div class="wp-block-column" style="flex-basis:65.16%"><!-- wp:heading {"style":{"typography":{"lineHeight":"1.1"}},"fontSize":"xxxx-large"} -->
-<h2 class="wp-block-heading has-xxxx-large-font-size" style="line-height:1.1"><?php esc_html_e( 'I’m an interior designer based in Antwerp.', 'margarethe' ); ?></h2>
+<div class="wp-block-column" style="flex-basis:65.16%"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.1"}},"fontSize":"xxxx-large"} -->
+<h1 class="wp-block-heading has-xxxx-large-font-size" style="line-height:1.1"><?php esc_html_e('I’m an interior designer based in Antwerp.', 'margarethe');?></h1>
 <!-- /wp:heading --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
