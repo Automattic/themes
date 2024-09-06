4832943f54c861f22fa539b196dd20ecd749b0ce
MaggieCabrera <maggie.cabrera@automattic.com>
2024-09-06 10:34:07 +0200

    fixed main tags

--- feature/patterns/404.php
+++ feature/patterns/404.php
@@ -9,8 +9,8 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"2rem","left":"2rem"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:0px;padding-right:2rem;padding-left:2rem"><!-- wp:template-part {"slug":"header-pages"} /-->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"35vh"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group" style="min-height:35vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|70"}}} -->
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"35vh"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<main class="wp-block-group" style="min-height:35vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|70"}}} -->
 <div class="wp-block-group alignfull"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"bottom","width":"70%"} -->
 <div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:70%"><!-- wp:group {"align":"full","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
@@ -32,7 +32,7 @@
 <div class="wp-block-column is-vertically-aligned-bottom" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:30%"></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
-<!-- /wp:group --></div>
+<!-- /wp:group --></main>
 <!-- /wp:group -->
 
 <!-- wp:template-part {"slug":"footer-pages"} /--></div>
