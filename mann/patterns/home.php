f6e49e1d0bffad1b39dbef0fd26b79ed483415ba
Ben Dwyer <ben@scruffian.com>
2024-09-13 12:47:37 +0100

    Add Mann

---
+++ mann/patterns/home.php
@@ -0,0 +1,283 @@
+<?php
+/**
+ * Title: home
+ * Slug: mann/home
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"backgroundColor":"contrast","layout":{"type":"default"}} -->
+<div class="wp-block-group has-contrast-background-color has-background"><!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"1.6rem","bottom":"1.6rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:1.6rem;padding-right:1.6rem;padding-bottom:1.6rem;padding-left:1.6rem"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"left":"12rem"}}}} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"bottom","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontSize":"4rem"}}} -->
+<p style="font-size:4rem;line-height:1">I’m a certified piano teacher from North London, teaching private in-person piano lessons or online.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-2.jpg","dimRatio":0,"customOverlayColor":"#b9aea8","isUserOverlayColor":true,"focalPoint":{"x":0.64,"y":0.34},"minHeight":620,"isDark":false,"style":{"dimensions":{"aspectRatio":"3/4"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="min-height:620px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#b9aea8"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-2.jpg" style="object-position:64% 34%" data-object-fit="cover" data-object-position="64% 34%"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"12.6rem","bottom":"3.6rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:12.6rem;padding-right:1.6rem;padding-bottom:3.6rem;padding-left:1.6rem"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"left":"2.6rem"}}}} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"bottom","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:50%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-1.jpg" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:heading -->
+<h2 class="wp-block-heading">Background</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Upon earning my BA Honours in Music from university, I quickly decided to embark on a career in music education. Over time, I have expanded my roster of students, developing a packed teaching schedule that I find immensely rewarding. Teaching music to both children and adults has been a fulfilling and enjoyable journey.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn more</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"12.6rem","bottom":"3.6rem"}}},"backgroundColor":"tertiary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="padding-top:12.6rem;padding-right:1.6rem;padding-bottom:3.6rem;padding-left:1.6rem"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"left":"2.6rem"}}}} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">Where I teach</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.1"}},"fontSize":"x-large"} -->
+<p class="has-x-large-font-size" style="line-height:1.1">I teach lessons at my studio in London, a cozy space where we have all the privacy and sound conditions for a focused practice.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Get directions</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-3.jpg" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"3.6rem","bottom":"12.6rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:3.6rem;padding-right:1.6rem;padding-bottom:12.6rem;padding-left:1.6rem"><!-- wp:columns {"verticalAlignment":"top","align":"full","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","width":"35%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:35%"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">Lesson Fees</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"1rem","bottom":"0px"}},"border":{"top":{"width":"0.04em"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:1rem;padding-bottom:0px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">In-person lessons</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>£50 per hour</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>These lessons happen at my studio in North London. They are suitable for beginners or advanced.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button">Book lesson</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"1rem","bottom":"0px"}},"border":{"top":{"width":"0.04em"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:1rem;padding-bottom:0px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Online lessons</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>£50 per hour</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Online lessons, available no matter where you are located in the UK. For beginners or advanced.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button">Book lesson</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"1rem","bottom":"0px"}},"border":{"top":{"width":"0.04em"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:1rem;padding-bottom:0px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Group lessons</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>£35 per hour</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Groups of 3/4 students, suitable for advanced students looking forward to perfect their practice.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button">Book lesson</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"8.6rem","bottom":"8.6rem"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:8.6rem;padding-right:1.6rem;padding-bottom:8.6rem;padding-left:1.6rem"><!-- wp:pullquote {"style":{"spacing":{"padding":{"right":"16vw","left":"16vw"}}}} -->
+<figure class="wp-block-pullquote" style="padding-right:16vw;padding-left:16vw"><blockquote><p>“I wish I had started my piano lessons with Heidi sooner, she is a great teacher and very patient with me.”</p><cite>Christine Q.</cite></blockquote></figure>
+<!-- /wp:pullquote --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"1.6rem","left":"1.6rem","top":"3.6rem","bottom":"12.6rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:3.6rem;padding-right:1.6rem;padding-bottom:12.6rem;padding-left:1.6rem"><!-- wp:heading {"fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-x-large-font-size">Upcoming Performances</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>These are some of the upcoming performances you can watch, either from me or my students.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"width":"0.04em"},"right":[],"left":[]}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>June Lansdowne Club Piano Recital</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Tuesday 11 June 2024 — <a href="#">The Lansdowne Club, London</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Tickets</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"width":"0.04em"},"right":[],"left":[]}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>End of Year, Student Summer Piano Recital</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Saturday 14 June 2024 — <a href="#">Pianist Club, London</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Tickets</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"width":"0.04em"},"right":[],"left":[]}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Heidi Mann Plays Chopin</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Friday 12 July 2024 —<a href="#"> The Hut, 44 Glenhurst Ave</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Tickets</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"border":{"top":{"width":"0.04em"},"right":[],"left":[]}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-width:0.04em;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Tuesday 1 August 2024 — <a href="#">Landon House, London</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Tickets</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-6-scaled.jpg","dimRatio":20,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":600,"minHeightUnit":"px","contentPosition":"top left","style":{"spacing":{"padding":{"top":"2.6rem"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-top-left" style="padding-top:2.6rem;min-height:600px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-20 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/music-image-6-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","fontSize":"x-large"} -->
+<p class="has-text-align-left has-x-large-font-size">Want to get started today, or just see how I can help you out?</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contact me</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /--></div>
+<!-- /wp:group -->
