582efc9c641fccef815caa526147499741d44cda
Ben Dwyer <ben@scruffian.com>
2024-09-13 12:33:03 +0100

    Add Dropp

---
+++ dropp/patterns/product-grid.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Title: Product Grid
+ * Slug: dropp/product-grid
+ * Categories: featured
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Product Grid"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.34%"} -->
+<div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":181,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.34%"} -->
+<div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":178,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-5.webp" alt="" class="wp-image-178" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Techwear+ Backpack</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":186,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-11.webp" alt="" class="wp-image-186" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Ghask Beanie</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|50"} -->
+<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.34%"} -->
+<div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":180,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-9.webp" alt="" class="wp-image-180" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Rainno Waterproof Shorts</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.34%"} -->
+<div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":185,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-12.webp" alt="" class="wp-image-185" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">AIRS® Burgundy Hat</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.34%"} -->
+<div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":181,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
+<!-- /wp:image -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ dropp/readme.txt
@@ -0,0 +1,35 @@
+=== Dropp ===
+Contributors: Automattic
+Requires at least: 6.0
+Tested up to: 6.4.3
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Dropp is a blogging theme that appeals to the sneakerhead. Its urban styles with bold typography and vibrant accent color make it ideal to the streetwear enthusiasts looking to express themselves.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Dropp is based on Block Canvas (https://github.com/Automattic/themes/tree/trunk/block-canvas), (C) Automattic, [GPLv2 or later](http://www.gnu.org/licenses/gpl-2.0.html)
+
+Block Canvas WordPress Theme, (C) 2022 Automattic
+Block Canvas is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+All images used in the theme have been generated with AI in Midjourney.
