cab89c406e749e690540098788a37c4eeba04726
Takashi Irie <takashi@automattic.com>
2024-09-17 16:41:32 +0100

    Foam: Add home pattern

--- foam/patterns/home.php
+++ foam/patterns/home.php
@@ -7,34 +7,26 @@
  * Inserter: yes
  */
 ?>
-<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/summertime_image.png","id":22,"dimRatio":10,"overlayColor":"base","minHeight":100,"minHeightUnit":"vh","style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"textColor":"contrast","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover has-contrast-color has-text-color" style="padding-right:5vw;padding-left:5vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-10 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-22" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/summertime_image.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"right":"0vw","left":"0vw"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
-<div class="wp-block-group alignfull" style="min-height:100vh;padding-right:0vw;padding-left:0vw"><!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"0px","selfStretch":"fixed"}}} -->
-<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"139px","selfStretch":"fixed"}}} -->
-<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0.2em"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:site-title /-->
-
-<!-- wp:site-tagline /--></div>
-<!-- /wp:group -->
-
-<!-- wp:spacer {"height":"19px","style":{"layout":{"flexSize":"19px","selfStretch":"fixed"}}} -->
-<div style="height:19px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
+<!-- wp:cover {"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"textColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light has-contrast-color has-text-color" style="padding-right:5vw;padding-left:5vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"0vw","left":"0vw"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group alignfull" style="min-height:100%;padding-right:0vw;padding-left:0vw"><!-- wp:group {"style":{"spacing":{"margin":{"top":"14rem","bottom":"14rem"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:14rem;margin-bottom:14rem"><!-- wp:heading {"textAlign":"center","align":"wide"} -->
+<h2 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Sign up for FOAM_ Digital and guarantee your attendance.', 'foam');?></h2>
+<!-- /wp:heading -->
 
 <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontSize":"0.8rem"}}} -->
-<p style="font-size:0.8rem;text-transform:uppercase"><strong>in stores June 26th<br></strong><br>Release Party and exclusive pre-show cocktail reception<br>Eden Garden Amphitheater on June 25th<br><a href="#">more info here</a><br></p>
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('The Future of Online Apprenticeship and Mentorship groundbreaking digital conference that is set to redefine the way we connect, learn, and collaborate in the ever-evolving digital landscape is back in 2023.', 'foam');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:spacer {"height":"60px","style":{"layout":{"flexSize":"60px","selfStretch":"fixed"}}} -->
-<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer --></div>
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Sign Up', 'foam');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer"} /--></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
