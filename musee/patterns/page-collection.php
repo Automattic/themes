b0e03cb6f8f3ec861cd5b722c59cba83ed1b5dc9
alaczek <ola@bodera.com>
2024-09-18 11:04:13 +1000

    Musee: Turn page templates into patterns (#8182)
    * Add sidebar
    
    Create sidebar template part and pattern, use it in page template.
    
    * Remove patterns
    
    Remove unused patterns and template parts.
    
    * Add patterns
    
    Add collections and exhibitions patterns (these used to be page templates).
    
    * Create page-without-sidebar.html

--- musee/patterns/page-collection.php
+++ musee/patterns/page-collection.php
@@ -1,35 +1,23 @@
 <?php
 /**
- * Title: page-collection
+ * Title: Collections
  * Slug: musee/page-collection
- * Categories: hidden
- * Inserter: no
+ * Categories: featured, posts
  */
+
+declare( strict_types = 1 );
 ?>
-<!-- wp:template-part {"slug":"header"} /-->
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
-<main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
-<div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-<div class="wp-block-group"><!-- wp:post-title /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
+<main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
 
 <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide"><!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"parents":[]},"tagName":"main","align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:query {"query":{"perPage":10,"pages":0,"offset":"0","postType":"post","order":"asc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]},"tagName":"main","align":"wide","layout":{"type":"default"}} -->
 <main class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":2}} -->
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","margin":{"bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"customOverlayColor":"#c7ae89","isUserOverlayColor":true,"minHeight":320,"contentPosition":"top right","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px"},"dimensions":{"aspectRatio":"4/3"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover has-custom-content-position is-position-top-right" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;min-height:320px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim" style="background-color:#c7ae89"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"base","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group has-base-background-color has-background" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:read-more {"content":"ver colleción ","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} /--></div>
+<div class="wp-block-group has-base-background-color has-background" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:read-more {"content":"see collection ","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
 
@@ -77,5 +65,3 @@
 <!-- /wp:query --></div>
 <!-- /wp:group --></main>
 <!-- /wp:group -->
-
-<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
--- musee/patterns/sidebar.php
+++ musee/patterns/sidebar.php
@@ -1,24 +1,18 @@
 <?php
 /**
- * Title: page
- * Slug: musee/page
+ * Title: sidebar
+ * Slug: musee/sidebar
  * Categories: hidden
  * Inserter: no
  */
+
+declare( strict_types = 1 );
 ?>
-<!-- wp:template-part {"slug":"header"} /-->
 
-<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"5%"}}}} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"70%"} -->
-<div class="wp-block-column" style="flex-basis:70%"><!-- wp:post-content {"lock":{"move":false,"remove":false},"layout":{"type":"constrained"}} /--></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<!-- wp:group {"metadata":{"categories":["hidden"],"patternName":"musee/sidebar","name":"sidebar"},"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"},"blockGap":"0px"},"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"1"}},"fontSize":"small"} -->
-<h2 class="wp-block-heading has-small-font-size" style="margin-top:0;font-style:normal;font-weight:700;line-height:1;text-transform:uppercase"><?php echo __('Archive', 'musee');?></h2>
+<h2 class="wp-block-heading has-small-font-size" style="margin-top:0;font-style:normal;font-weight:700;line-height:1;text-transform:uppercase">Archive</h2>
 <!-- /wp:heading -->
 
 <!-- wp:archives {"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /--></div>
@@ -26,7 +20,7 @@
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"},"blockGap":"0px"},"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","lineHeight":"1"}},"fontSize":"small"} -->
-<h2 class="wp-block-heading has-small-font-size" style="margin-top:0;font-style:normal;font-weight:700;line-height:1;text-transform:uppercase"><?php echo __('Categories', 'musee');?></h2>
+<h2 class="wp-block-heading has-small-font-size" style="margin-top:0;font-style:normal;font-weight:700;line-height:1;text-transform:uppercase">Categories</h2>
 <!-- /wp:heading -->
 
 <!-- wp:categories {"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /--></div>
@@ -41,9 +35,4 @@
 <p><?php echo __('Would you like to subscribe to our newsletter? <a href="#">Sign up</a>.', 'musee');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></main>
 <!-- /wp:group -->
-
-<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
