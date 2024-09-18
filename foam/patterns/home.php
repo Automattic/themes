f46348eb6368d7bfa79745fa461ab4ba1f7a4d3c
Takashi Irie <takashi@automattic.com>
2024-09-18 15:06:31 +0100

    Foam: Add main tag to home template

--- foam/patterns/home.php
+++ foam/patterns/home.php
@@ -9,8 +9,8 @@
 ?>
 <!-- wp:cover {"dimRatio":0,"minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","isDark":false,"style":{"spacing":{"padding":{"right":"5vw","left":"5vw"}}},"textColor":"contrast","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover is-light has-contrast-color has-text-color" style="padding-right:5vw;padding-left:5vw;min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"dimensions":{"minHeight":"100%"},"spacing":{"padding":{"right":"0vw","left":"0vw"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
-<div class="wp-block-group alignfull" style="min-height:100%;padding-right:0vw;padding-left:0vw"><!-- wp:group {"style":{"spacing":{"margin":{"top":"14rem","bottom":"14rem"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-group" style="margin-top:14rem;margin-bottom:14rem"><!-- wp:heading {"textAlign":"center","align":"wide"} -->
+<div class="wp-block-group alignfull" style="min-height:100%;padding-right:0vw;padding-left:0vw"><!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"14rem","bottom":"14rem"}},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<main class="wp-block-group" style="margin-top:14rem;margin-bottom:14rem"><!-- wp:heading {"textAlign":"center","align":"wide"} -->
 <h2 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Sign up for FOAM_ Digital and guarantee your attendance.', 'foam');?></h2>
 <!-- /wp:heading -->
 
@@ -24,7 +24,7 @@
 <div class="wp-block-buttons"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Sign Up', 'foam');?></a></div>
 <!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
+<!-- /wp:buttons --></main>
 <!-- /wp:group -->
 
 <!-- wp:template-part {"slug":"footer","area":"footer"} /--></div>
