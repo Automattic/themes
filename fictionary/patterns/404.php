e79a4a177bb6db14c25ecd9e78ae427b70926633
Maggie <maggie.cabrera@automattic.com>
2024-09-20 14:10:40 +0100

    Fictionary: Add new theme (#8081)
    * initial commit
    
    * Fictionary: Address feedback and cleanup
    
    ---------
    
    Co-authored-by: Takashi Irie <takashi@automattic.com>

---
+++ fictionary/patterns/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: 404
+ * Slug: fictionary/404
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontSize":"70px"}}} -->
+<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found" style="font-size:70px"><?php esc_html_e('Oops! That page could not be found.', 'fictionary');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'fictionary');?></p>
+<!-- /wp:paragraph -->
