f580a3dedf41aed6dba2f8ff5809d0239ba96d28
Ben Dwyer <ben@scruffian.com>
2024-09-23 19:53:30 +0100

    Add Alley-Oop Theme (#8124)
    * Add: Alley Oop
    
    * update with CBT
    
    * compressed images
    
    * removed .com code, pattern escaping
    
    * compressed screenshot
    
    * fixed meta info
    
    * missing skip links
    
    * removed static links
    
    * AlleyOop: Cleanup to launch
    
    ---------
    
    Co-authored-by: MaggieCabrera <maggie.cabrera@automattic.com>
    Co-authored-by: Takashi Irie <takashi@automattic.com>

---
+++ alleyoop/patterns/search.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Title: search
+ * Slug: alleyoop/search
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"tagName":"main","align":"wide","style":{"spacing":{"blockGap":"1vh","margin":{"top":"10vh","bottom":"10vh"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignwide" style="margin-top:10vh;margin-bottom:10vh"><!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"bottom":"2.5vh"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="margin-bottom:2.5vh"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search...","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0rem","bottom":"0rem"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"bottom"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0rem;padding-bottom:0rem"><!-- wp:query-title {"type":"search","style":{"typography":{"fontSize":"0.88rem","letterSpacing":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontFamily":"hezaedrus"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":true},"enhancedPagination":true,"align":"wide"} -->
+<div class="wp-block-query alignwide"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"default","columnCount":3}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"5vh","top":"5vh"}},"border":{"top":{"width":"2px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="border-top-width:2px;padding-top:5vh;padding-bottom:5vh"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:post-title {"isLink":true,"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"xxx-large"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"30%"} -->
+<div class="wp-block-column" style="flex-basis:30%"><!-- wp:group {"style":{"spacing":{"blockGap":"1.5rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:post-excerpt {"moreText":"","showMoreOnNewLine":false,"excerptLength":40} /-->
+
+<!-- wp:post-date {"format":"M j, Y"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"20%"} -->
+<div class="wp-block-column" style="flex-basis:20%"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/4"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Sorry, but nothing found. Please try again with some different keywords.', 'alleyoop');?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"fontSize":"x-large","layout":{"type":"default"}} -->
+<div class="wp-block-group has-x-large-font-size" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"left"}} -->
+<!-- wp:query-pagination-previous {"label":"Prev"} /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next {"label":"Next"} /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:group --></div>
+<!-- /wp:query --></main>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px"><!-- wp:template-part {"slug":"footer","tagName":"footer"} /--></div>
+<!-- /wp:group -->
---
+++ alleyoop/readme.txt
@@ -0,0 +1,56 @@
+=== AlleyOop ===
+Contributors: the WordPress team
+Requires at least: 6.0
+Tested up to: 6.7
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+AlleyOop is a theme site created for fans (and practitioners) who want to blog about their sport.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+AlleyOop WordPress Theme (C) 2023 Automattic
+AlleyOop is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+This theme bundles the following third-party resources:
+
+Included in theme screenshot:
+https://unsplash.com/photos/woman-in-black-tank-top-and-black-shorts-playing-basketball--TJUZDO3sJ8
+https://unsplash.com/photos/white-and-black-heart-print-textile-N_7Kb4hpaoU
+
+Included in a footer pattern:
+https://unsplash.com/photos/red-and-white-abstract-painting-O2kqxbix4Mw
+
+Included in a header pattern:
+https://unsplash.com/photos/white-and-black-heart-print-textile-N_7Kb4hpaoU
+
+Tektur Tight Font
+Copyright 2020 The Tektur Project Authors (https://www.github.com/hyvyys/Tektur) 
+This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL 
+License URL: https://scripts.sil.org/OFL 
+-- End of Tektur Tight Font credits --
+
+Hezaedrus Font
+Copyright (c) 2015 by Hubert & Fischer. All rights reserved. 
+This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: http://scripts.sil.org/OFL 
+License URL: http://scripts.sil.org/OFL 
+Source: www.google.com/fonts
+-- End of Hezaedrus Font credits --
