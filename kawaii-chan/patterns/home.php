61516845f6ee5149e420eed4280f807db414addd
henriqueiamarino <henrique.iamarino@a8c.com>
2024-10-02 11:39:37 +0200

    Kawaii-chan: add theme

---
+++ kawaii-chan/patterns/home.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Title: home
+ * Slug: kawaii-chan/home
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"10vh","padding":{"top":"5vh","bottom":"10vh"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:5vh;padding-bottom:10vh"><!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.05rem"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:700;letter-spacing:0.05rem;text-transform:uppercase"><?php esc_html_e('Konnichiwa!', 'kawaii-chan');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","level":1,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center has-primary-color has-text-color has-link-color"><?php esc_html_e('I\'m Sakura-chan, your guide to all things cute and colorful.', 'kawaii-chan');?></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('As a Tokyo native with a heart full of love for Kawaii culture, I\'m thrilled to welcome you to my whimsical corner of the internet. Here, you\'ll find me sharing my adventures, my latest manga obsessions, and my never-ending quest for the most adorable cafes in town.', 'kawaii-chan');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('From fashion to food, travel to DIY crafts, I\'m passionate about spreading joy and positivity through my love for all things Kawaii.', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":"1","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"tagName":"main","align":"wide","layout":{"type":"constrained"}} -->
+<main class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"10vh"}},"layout":{"type":"default","columnCount":null,"minimumColumnWidth":"1rem"}} -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"overlayColor":"base","isUserOverlayColor":true,"minHeight":45,"minHeightUnit":"vh","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:45vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"45vh"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group" style="min-height:45vh"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|purplepink"}}} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/sub-elipses_1_top.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|purplepink"}}} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/sub-elipses_1_bot.png" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-title {"textAlign":"center","level":1,"isLink":true} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:post-date {"textAlign":"center","isLink":true} /-->
+
+<!-- wp:post-excerpt {"textAlign":"center","moreText":"Read more."} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></main>
+<!-- /wp:query -->
+
+<!-- wp:query {"queryId":0,"query":{"perPage":"6","pages":0,"offset":"1","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"10vh"}},"layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":10,"overlayColor":"base","isUserOverlayColor":true,"minHeight":25,"minHeightUnit":"rem","contentPosition":"center center","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:25rem"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"dimensions":{"minHeight":"25rem"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group" style="min-height:25rem"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|purplepink"}}} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/sub-elipses_2_top.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"width":"319px","height":"auto","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":"var:preset|duotone|purplepink"}}} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/sub-elipses_2_bot.png" alt="" style="width:319px;height:auto"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-title {"textAlign":"center","isLink":true} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:post-date {"textAlign":"center","isLink":true,"fontSize":"small"} /-->
+
+<!-- wp:post-excerpt {"textAlign":"center","moreText":"Read more."} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"5vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Calling all Kawaii enthusiasts', 'kawaii-chan');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('Get ready to be charmed! Follow me into the world of Kawaii, where the cutest items are waiting to amuse you at the Sakura-chan Store.', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"align":"wide","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"20px","width":"2px","color":"#faf9fa"}}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Pin_Thumbs-up-Enamel.jpg" alt="" class="has-border-color" style="border-color:#faf9fa;border-width:2px;border-radius:20px;aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Store Article XPTO ', 'kawaii-chan');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.05rem"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:700;letter-spacing:0.05rem;text-transform:uppercase"><?php esc_html_e('$12.99', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"align":"wide","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"20px","width":"2px","color":"#faf9fa"}}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Pin_Dog-Paw-Enamel-Icon.jpg" alt="" class="has-border-color" style="border-color:#faf9fa;border-width:2px;border-radius:20px;aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Store Article XPTO ', 'kawaii-chan');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.05rem"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:700;letter-spacing:0.05rem;text-transform:uppercase"><?php esc_html_e('$12.99', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"align":"wide","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"20px","width":"2px","color":"#faf9fa"}}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/Pin_Flame-Icon-Enamel.jpg" alt="" class="has-border-color" style="border-color:#faf9fa;border-width:2px;border-radius:20px;aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"center","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Store Article XPTO ', 'kawaii-chan');?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"0.05rem"}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="font-style:normal;font-weight:700;letter-spacing:0.05rem;text-transform:uppercase"><?php esc_html_e('$12.99', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"5vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center"><?php esc_html_e('Feeling cute and curious?', 'kawaii-chan');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('Discover the magic of Kawaii with Sakura-chan. Let\'s create happiness!', 'kawaii-chan');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"10%","left":"10%"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:10%;padding-left:10%"><!-- wp:jetpack/contact-form {"className":"is-style-animated"} -->
+<div class="wp-block-jetpack-contact-form is-style-animated">
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group">
+<!-- wp:jetpack/field-email {"required":true,"borderRadius":10,"labelFontSize":"1rem","fieldFontSize":"1rem","borderColor":"#ff4cb4"} /-->
+<!-- wp:jetpack/button {"element":"button","text":"Subscribe","borderRadius":10,"width":"100%","lock":{"move":false,"remove":false}} /-->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:jetpack/contact-form --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
---
+++ kawaii-chan/readme.txt
@@ -0,0 +1,98 @@
+=== Kawaii-Chan ===
+Contributors: Automattic
+Requires at least: 6.1
+Tested up to: 6.6
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Kawaii-Chan is a simple theme that supports full-site editing. It comes with a set of minimal templates and design settings that can be manipulated through Global Styles. Use it to build something beautiful.
+
+== Changelog ==
+
+= 0.0.30 =
+* Kawaii-Chan: Update header alignment (#7575)
+
+= 0.0.29 =
+* Remove auto-loading-homepage tag. (#7300)
+
+= 0.0.28 =
+* Remove Rubik font from heading definition (#7267)
+
+= 0.0.27 =
+* Add link styles for post-terms (#7058)
+
+= 0.0.26 =
+* Remove table CSS from block canvas theme as it has been addressed in Gutenberg (#6954)
+
+= 0.0.24 =
+* Kawaii-Chan: Add no results content to search template (#6860)
+
+= 0.0.23 =
+* Remove font-smoothing CSS (#6816)
+
+= 0.0.22 =
+* Add home.html template to Kawaii-Chan (#6784)
+
+= 0.0.21 =
+* Kawaii-Chan: Add self-closing tag to comments pattern in page template (#6764)
+
+= 0.0.20 =
+* Kawaii-Chan: small updates (#6705)
+
+= 0.0.19 =
+* Make theme available for translation as much as possible (#6484)
+
+= 0.0.18 =
+* Update block canvas styles (#6640)
+
+= 0.0.17 =
+* Block canvas: clean up templates (#6620)
+
+== Copyright ==
+
+Kawaii-Chan WordPress Theme, (C) 2022 Automattic
+Kawaii-Chan is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+
+== Fonts ==
+
+Modak
+Copyright: Designed by Ek Type (https://github.com/girish-dalvi/Modak)
+License: SIL Open Font License (OFL) version 1.1 - https://openfontlicense.org/
+Source: https://fonts.google.com/specimen/Modak
+
+Gabarito
+Copyright: Designed by Naipe Foundry, Leandro Assis, Álvaro Franca, Felipe Casaprima (https://github.com/naipefoundry/gabarito)
+License: SIL Open Font License (OFL) version 1.1 - https://openfontlicense.org/
+Source: https://fonts.google.com/specimen/Gabarito
+
+Rubik Bubbles
+Copyright: Designed by NaN, Luke Prowse (https://github.com/NaN-xyz/Rubik-Filtered)
+License: SIL Open Font License (OFL) version 1.1 - https://openfontlicense.org/
+Source: https://fonts.google.com/specimen/Rubik+Bubbles
+
+Bellota Text
+Copyright: Designed by Kemie Guaida (https://github.com/kemie/Bellota-Font)
+License: SIL Open Font License (OFL) version 1.1 - https://openfontlicense.org/
+Source: https://fonts.google.com/specimen/Bellota
+
+
+== Images ==
+
+Graphics on the screenshot by the theme author Henrique Iamarino.
+License: CC0 1.0 Universal
+License URL: https://creativecommons.org/publicdomain/zero/1.0/
+
