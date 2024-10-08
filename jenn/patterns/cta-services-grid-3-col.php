070a4c23aec942bf89117beff8386dd0404a4248
Takashi Irie <takashi@automattic.com>
2024-10-08 17:30:30 +0100

    Jenn: Rename a pattern and fixed missing images

--- jenn/patterns/cta-services-grid-3-col.php
+++ jenn/patterns/cta-services-grid-3-col.php
@@ -6,12 +6,11 @@
  * Viewport width: 1400
  */
 ?>
-
 <!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} -->
 <div class="wp-block-columns alignwide" style="padding-top:0;padding-bottom:0"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
 <div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":6,"aspectRatio":"3/2","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://jenn.mystagingwebsite.com/wp-content/uploads/2023/12/hero-image-scaled.jpg" alt="" class="wp-image-6" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/hero-image-scaled.jpg" alt="" class="wp-image-6" style="aspect-ratio:3/2;object-fit:cover"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"left","level":3,"className":"is-style-default","fontSize":"x-large","fontFamily":"body"} -->
@@ -37,7 +36,7 @@
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
 <div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":23,"aspectRatio":"3/2","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://jenn.mystagingwebsite.com/wp-content/uploads/2023/12/post-2-image-scaled.jpg" alt="" class="wp-image-23" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/jenn-post-2.jpg" alt="" class="wp-image-23" style="aspect-ratio:3/2;object-fit:cover"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"left","level":3,"className":"is-style-default","fontSize":"x-large","fontFamily":"body"} -->
@@ -45,16 +44,16 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><strong>60 minutes</strong><br>Minimum 1 pax/can be individual<br>Tue, Thu, Fri and Sat, 8 AM</p>
+<p class="has-small-font-size"><?php echo wp_kses_post( __ ( '<strong>60 minutes</strong><br>Minimum 1 pax/can be individual<br>Tue, Thu, Fri and Sat, 8 AM', 'jenn' ) ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>Accessible to everyone lasting 1 hour, where we practice and build a conscious practice around different themes related to yoga and life.</p>
+<p><?php esc_html_e( 'Accessible to everyone lasting 1 hour, where we practice and build a conscious practice around different themes related to yoga and life.', 'jenn' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Book Class</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Book Class', 'jenn' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -63,7 +62,7 @@
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
 <div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":22,"aspectRatio":"3/2","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://jenn.mystagingwebsite.com/wp-content/uploads/2023/12/post-1-image-scaled.jpg" alt="" class="wp-image-22" style="aspect-ratio:3/2;object-fit:cover"/></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/jenn-post-1.jpg" alt="" class="wp-image-22" style="aspect-ratio:3/2;object-fit:cover"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:heading {"textAlign":"left","level":3,"className":"is-style-default","fontSize":"x-large","fontFamily":"body"} -->
@@ -71,16 +70,16 @@
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><strong>60 minutes</strong><br>Minimum 2 pax<br>At LA Yoga Studio or your home</p>
+<p class="has-small-font-size"><?php echo wp_kses_post( __ ( '<strong>60 minutes</strong><br>Minimum 2 pax<br>At LA Yoga Studio or your home', 'jenn' ) ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>If you would like to have a more personalized practice completely adapted to you, your needs and your pace then this modality is for you.</p>
+<p><?php esc_html_e( 'If you would like to have a more personalized practice completely adapted to you, your needs and your pace then this modality is for you.', 'jenn' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contact Me</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Contact Me', 'jenn' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
