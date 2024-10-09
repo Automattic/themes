14ad7cd7ab703836a16a488ecb4eb82d1b08eac9
Rich Tabor <hi@richtabor.com>
2024-10-09 11:24:52 +0200

    more services patterns

---
+++ assembler/patterns/services-5.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Title: Services 5
+ * Slug: assembler/services-5
+ * Categories: services
+ */
+declare( strict_types = 1 );
+?>
+
+<!-- wp:group {"metadata":{"name":"Services"},"align":"full","className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0"><!-- wp:spacer {"height":"var:preset|spacing|60"} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"left","align":"full"} -->
+<h2 class="wp-block-heading alignfull has-text-align-left">Services</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Sections"},"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"metadata":{"name":"Section"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"var:preset|spacing|20"} -->
+<div style="height:var(--wp--preset--spacing--20)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"metadata":{"name":"Items"},"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|40","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"metadata":{"name":"Item"}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Service 1</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Adding services to your website helps provide value by addressing specific needs or challenges that your clients face. Offer expertise, streamline processes, and create tailored solutions that enhance customer satisfaction.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">Learn more</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"metadata":{"name":"Item"}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Service 2</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Adding services to your website helps provide value by addressing specific needs or challenges that your clients face. Offer expertise, streamline processes, and create tailored solutions that enhance customer satisfaction.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">Learn more</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"metadata":{"name":"Item"}} -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Service 3</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Adding services to your website helps provide value by addressing specific needs or challenges that your clients face. Offer expertise, streamline processes, and create tailored solutions that enhance customer satisfaction.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">Learn more</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|60"} -->
+<div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
