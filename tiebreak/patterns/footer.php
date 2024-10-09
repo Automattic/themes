2a7708fbdcb7c181156034f4436e08146972b573
Takashi Irie <takashi@automattic.com>
2024-10-09 11:29:08 +0100

    TieBreak: Re save the theme in CBT

--- tiebreak/patterns/footer.php
+++ tiebreak/patterns/footer.php
@@ -6,28 +6,74 @@
  * Block Types: core/template-part/footer
  */
 ?>
-
-<!-- wp:spacer {"height":"var(--wp--preset--spacing--80)"} -->
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center">
-            <?php
-                /* Translators: WordPress link. */
-                $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'tiebreak' ) ) . '" rel="nofollow">WordPress</a>';
-                echo sprintf(
-                    esc_html__( 'Designed with %1$s', 'tiebreak' ),
-                    $wordpress_link
-                );
-            ?>
-        </p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"right":"6vw","left":"6vw"}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-right:6vw;padding-left:6vw"><!-- wp:spacer {"height":"6vh"} -->
+<div style="height:6vh" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"bottom":"2vh"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-bottom:2vh"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-title {"align":"wide","fontSize":"medium"} /-->
+
+<!-- wp:social-links {"iconColor":"base","iconColorValue":"#f8f8f8","size":"has-normal-icon-size","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|60"}}},"className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-normal-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"x"} /-->
+
+<!-- wp:social-link {"url":"#","service":"youtube"} /-->
+
+<!-- wp:social-link {"url":"#","service":"feed"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","letterSpacing":"0.06em"}},"fontFamily":"metropolis"} -->
+<p class="has-metropolis-font-family" style="font-size:12px;letter-spacing:0.06em;text-transform:uppercase"><strong>More from TieBreak</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.1rem"},"typography":{"fontSize":"12px"}},"layout":{"type":"flex","orientation":"vertical"},"fontFamily":"metropolis"} -->
+<div class="wp-block-group has-metropolis-font-family" style="font-size:12px"><!-- wp:paragraph -->
+<p><a href="#">Newsletter</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">Subscribe</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px","textTransform":"uppercase","letterSpacing":"0.06em"}},"fontFamily":"metropolis"} -->
+<p class="has-metropolis-font-family" style="font-size:12px;letter-spacing:0.06em;text-transform:uppercase"><strong>Contacts</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.1rem"},"typography":{"fontSize":"12px"}},"layout":{"type":"flex","orientation":"vertical"},"fontFamily":"metropolis"} -->
+<div class="wp-block-group has-metropolis-font-family" style="font-size:12px"><!-- wp:paragraph -->
+<p><a href="#">Contact Us</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">Questions</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"3vh","bottom":"6vh"}},"border":{"top":{"color":"var:preset|color|custom-dark","width":"4px"},"right":[],"bottom":[],"left":[]}}} -->
+<div class="wp-block-group alignwide" style="border-top-color:var(--wp--preset--color--custom-dark);border-top-width:4px;padding-top:3vh;padding-bottom:6vh"><!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"0.7rem"}},"fontFamily":"metropolis"} -->
+<p class="has-text-align-left has-metropolis-font-family" style="font-size:0.7rem">Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
