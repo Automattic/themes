69a40967e34d562b8314939b268d17070b376042
Takashi Irie <takashi@automattic.com>
2024-10-10 17:35:24 +0100

    Dropp: fix footer pattern

--- dropp/patterns/footer.php
+++ dropp/patterns/footer.php
@@ -5,7 +5,7 @@
  * Inserter: no
  */
 ?>
-<!-- wp:spacer {"height":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d80)"} -->
+<!-- wp:spacer {height":"var:preset|spacing|80} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
@@ -24,7 +24,7 @@
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|70"}}}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
-<p class="has-text-align-center has-small-font-size"><?php esc_html_e('Crafted with that <a href="https://wordpress.org" rel="nofollow">WordPress</a> heat 🔥', 'dropp');?></p>
+<p class="has-text-align-center has-small-font-size"><?php echo wp_kses_post( __ ( 'Crafted with that <a href="https://wordpress.org" rel="nofollow">WordPress</a> heat 🔥 ', 'dropp' ) );?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
