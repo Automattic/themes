5d4c6edcd716bfd4722f142f763f545ae8f3e62c
Takashi Irie <takashi@automattic.com>
2024-10-10 20:10:10 +0100

    Dropp: Improve product grid pattern

--- dropp/patterns/product-grid.php
+++ dropp/patterns/product-grid.php
@@ -12,12 +12,12 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.34%"} -->
 <div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":181,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('A-Navy Varsity Jacket', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -26,12 +26,12 @@
 <!-- wp:column {"width":"33.34%"} -->
 <div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":178,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-style-default"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-5.webp" alt="" class="wp-image-178" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Techwear+ Backpack</figcaption></figure>
+<figure class="wp-block-image size-full is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-5.webp" alt="" class="wp-image-178" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Techwear+ Backpack', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -40,12 +40,12 @@
 <!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":186,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-11.webp" alt="" class="wp-image-186" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Ghask Beanie</figcaption></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-11.webp" alt="" class="wp-image-186" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Ghask Beanie', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -60,12 +60,12 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"33.34%"} -->
 <div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":180,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-9.webp" alt="" class="wp-image-180" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Rainno Waterproof Shorts</figcaption></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-9.webp" alt="" class="wp-image-180" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Rainno Waterproof Shorts', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -74,12 +74,12 @@
 <!-- wp:column {"width":"33.34%"} -->
 <div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":185,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-12.webp" alt="" class="wp-image-185" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">AIRS® Burgundy Hat</figcaption></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-12.webp" alt="" class="wp-image-185" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('AIRS® Burgundy Hat', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -88,12 +88,12 @@
 <!-- wp:column {"width":"33.34%"} -->
 <div class="wp-block-column" style="flex-basis:33.34%"><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:image {"id":181,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('A-Navy Varsity Jacket', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Buy</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Buy', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
