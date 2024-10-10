93fdd9b8307bf1a25455ac0a477278a3604e5401
Takashi Irie <takashi@automattic.com>
2024-10-10 16:22:53 +0100

    Adonay: Prepare for submission (#8254)
    * Adonay: Re-run the CBT
    
    * Adonay: Fix Header and Footer patterns
    
    * Adonay: Remove comment pattern
    
    * Adonay: Add query-no-results block to archive, index, and search templates
    
    * Adonay: Fix home template
    
    * Adonay: Improve the two homepage patterns
    
    * Adonay: Adjust the font size for the credit
    
    * Adonay: Optimised image and cleanup the css and the readme.
    
    * Adonay: Cleanup the readme.

--- adonay/patterns/footer.php
+++ adonay/patterns/footer.php
@@ -1,33 +1,18 @@
 <?php
 /**
- * Title: Default footer
+ * Title: footer
  * Slug: adonay/footer
- * Categories: footer
- * Block Types: core/template-part/footer
+ * Inserter: no
  */
 ?>
-
-<!-- wp:spacer {"height":"var(--wp--preset--spacing--80)"} -->
+<!-- wp:spacer {"height":"var:preset|spacing|80"} -->
 <div style="height:var(--wp--preset--spacing--80)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var(--wp--preset--spacing--60)","bottom":"var(--wp--preset--spacing--60)"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:paragraph {"align":"center"} -->
-		<p class="has-text-align-center">
-            <?php
-                /* Translators: WordPress link. */
-                $wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'adonay' ) ) . '" rel="nofollow">WordPress</a>';
-                echo sprintf(
-                    esc_html__( 'Designed with %1$s', 'adonay' ),
-                    $wordpress_link
-                );
-            ?>
-        </p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}}} -->
+<p style="font-size:14px"><?php echo wp_kses_post( __ ('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a> ', 'adonay') );?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
