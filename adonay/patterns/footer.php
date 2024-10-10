33125dc1e97b0c87d883a6f9a3562783679e7c38
Takashi Irie <takashi@automattic.com>
2024-10-10 15:29:46 +0100

    Adonay: Fix home template

--- adonay/patterns/footer.php
+++ adonay/patterns/footer.php
@@ -12,12 +12,7 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}}} -->
-<p style="font-size:14px"><?php
-	printf(
-		/* Translators: Designed with WordPress. %s: WordPress link. */
-		esc_html__( 'Designed with %s', 'adonay' ),
-		'<a href="' . esc_url( __( 'https://wordpress.org', 'adonay' ) ) . '" rel="nofollow">WordPress</a>'
-	);?></p>
+<p style="font-size:14px"><?php echo wp_kses_post( __ ('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
--- adonay/patterns/homepage-with-full-height-image.php
+++ adonay/patterns/homepage-with-full-height-image.php
@@ -16,35 +16,37 @@
 <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:site-title /-->
 
-<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"},"spacing":{"margin":{"right":"var:preset|spacing|80"}}}} -->
-<p style="margin-right:var(--wp--preset--spacing--80);line-height:1.3">I’m a photographer and art director based in Manchester, United Kingdom. When I’m not shooting weddings and portraits, you can find me posting on Instagram. Currently available for hire.</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:group {"tagName":"main","layout":{"type":"default"}} -->
+<main class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.3"},"spacing":{"margin":{"right":"var:preset|spacing|80"}}}} -->
+<p style="margin-right:var(--wp--preset--spacing--80);line-height:1.3"><?php esc_html_e('I’m a photographer and art director based in Manchester, United Kingdom. When I’m not shooting weddings and portraits, you can find me posting on Instagram. Currently available for hire.', 'adonay');?></p>
+<!-- /wp:paragraph --></main>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:paragraph -->
-<p><a href="#">Get in Touch</a></p>
+<p><?php echo wp_kses_post( __ ('<a href="#">Get in Touch</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="#">Instagram</a></p>
+<p><?php echo wp_kses_post( __ ('<a href="#">Instagram</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="#">X</a></p>
+<p><?php echo wp_kses_post( __ ('<a href="#">X</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="#">Youtube</a></p>
+<p><?php echo wp_kses_post( __ ('<a href="#">Youtube</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:cover {"url":"https://linkinbioone.mystagingwebsite.com/wp-content/uploads/2023/11/adonay_image-1-scaled.jpg","id":149,"dimRatio":0,"overlayColor":"base","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"bottom right","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"color":{}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover has-custom-content-position is-position-bottom-right" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-149" alt="" src="https://linkinbioone.mystagingwebsite.com/wp-content/uploads/2023/11/adonay_image-1-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","placeholder":"Write title…","style":{"typography":{"fontSize":"14px"}}} -->
-<p class="has-text-align-right" style="font-size:14px">Designed with WordPress</p>
+<div class="wp-block-column"><!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adonay_image-1-scaled.jpg","dimRatio":0,"overlayColor":"base","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","contentPosition":"bottom right","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"color":[]},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-right" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/adonay_image-1-scaled.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","placeholder":"Write title…","style":{"typography":{"fontSize":"14px"}}} -->
+<p class="has-text-align-right" style="font-size:14px"><?php echo wp_kses_post( __ ('Designed with <a href="https://wordpress.org" rel="nofollow">WordPress</a> ', 'adonay') );?></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:column --></div>
