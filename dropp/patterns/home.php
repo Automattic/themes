93dc71cda3d41dfcb7ddc83ccf7f07edf3ca53cf
Takashi Irie <takashi@automattic.com>
2024-10-10 19:48:17 +0100

    Dropp: Improve shop section pattern

--- dropp/patterns/home.php
+++ dropp/patterns/home.php
@@ -87,38 +87,6 @@
 <!-- /wp:query --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"metadata":{"name":"Products"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"15rem","fontStyle":"italic","fontWeight":"900","letterSpacing":"-0.12em","textTransform":"uppercase"}}} -->
-<h2 class="wp-block-heading has-text-align-center" style="font-size:15rem;font-style:italic;font-weight:900;letter-spacing:-0.12em;text-transform:uppercase"><?php esc_html_e('Shopp', 'dropp');?></h2>
-<!-- /wp:heading -->
-
-<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-7-1.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
-<!-- /wp:image -->
-
-<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-5.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Techwear+ Backpack</figcaption></figure>
-<!-- /wp:image -->
-
-<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-11.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Ghask Beanie</figcaption></figure>
-<!-- /wp:image -->
-
-<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-9.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Rainno Waterproof Shorts</figcaption></figure>
-<!-- /wp:image -->
-
-<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-12.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">AIRS® Burgundy Hat</figcaption></figure>
-<!-- /wp:image --></div>
-<!-- /wp:group -->
-
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e('Peep the rest', 'dropp');?></a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group -->
+<!-- wp:pattern {"slug":"dropp/shop-section"} /-->
 
 <!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
--- dropp/patterns/shop-section.php
+++ dropp/patterns/shop-section.php
@@ -6,37 +6,36 @@
  * Inserter: yes
  */
 ?>
-
 <!-- wp:group {"metadata":{"name":"Products"},"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"15rem","fontStyle":"italic","fontWeight":"900","letterSpacing":"-0.12em","textTransform":"uppercase"}}} -->
-<h2 class="wp-block-heading has-text-align-center" style="font-size:15rem;font-style:italic;font-weight:900;letter-spacing:-0.12em;text-transform:uppercase">Shopp</h2>
+<h2 class="wp-block-heading has-text-align-center" style="font-size:15rem;font-style:italic;font-weight:900;letter-spacing:-0.12em;text-transform:uppercase"><?php esc_html_e('Shopp', 'dropp');?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":181,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-7-1.webp" alt="" class="wp-image-181" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">A-Navy Varsity Jacket</figcaption></figure>
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-7-1.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('A-Navy Varsity Jacket', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {"id":178,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
-<figure class="wp-block-image size-full is-style-default"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-5.webp" alt="" class="wp-image-178" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Techwear+ Backpack</figcaption></figure>
+<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default"} -->
+<figure class="wp-block-image size-full is-style-default"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-5.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Techwear+ Backpack', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {"id":186,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-11.webp" alt="" class="wp-image-186" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Ghask Beanie</figcaption></figure>
+<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-11.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Ghask Beanie', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {"id":180,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-9.webp" alt="" class="wp-image-180" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">Rainno Waterproof Shorts</figcaption></figure>
+<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-9.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('Rainno Waterproof Shorts', 'dropp');?></figcaption></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {"id":185,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://streetwear.mystagingwebsite.com/wp-content/themes/dropp/assets/images/post-12.webp" alt="" class="wp-image-185" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption">AIRS® Burgundy Hat</figcaption></figure>
+<!-- wp:image {"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/post-12.webp" alt="" class="" style="aspect-ratio:1;object-fit:cover"/><figcaption class="wp-element-caption"><?php esc_html_e('AIRS® Burgundy Hat', 'dropp');?></figcaption></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Peep the rest</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_attr_x( 'Peep the rest', 'Button text. Verb.', 'dropp' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
