4a710daace78a763fc0386f6a5a2c9350b32b995
Takashi Irie <takashi@automattic.com>
2024-10-31 12:29:23 +0000

    Coachben: Added no result block and pagination to archive, index, and search results templates

---
+++ coachben/patterns/archive.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: archive
+ * Slug: coachben/archive
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+
+<!-- wp:group {"tagName":"main","metadata":{"name":"Content"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:query {"queryId":14,"query":{"perPage":10,"pages":"0","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":true,"parents":[]},"enhancedPagination":true,"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-query alignwide"><!-- wp:query-title {"type":"archive","align":"wide","style":{"spacing":{"margin":{"bottom":"2.5rem"}}}} /-->
+
+<!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"4/3","style":{"border":{"radius":"3px"},"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+
+<!-- wp:group {"metadata":{"name":"Title and meta"},"style":{"spacing":{"blockGap":"0.5rem","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:post-title {"level":3,"isLink":true} /-->
+
+<!-- wp:post-date {"format":"M j, Y"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+<!-- wp:pattern {"slug":"coachben/pagination"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:query-no-results {"align":"wide"} -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'coachben');?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query -->
+
+<!-- wp:template-part {"slug":"cta","align":"wide"} /--></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer"} /-->
