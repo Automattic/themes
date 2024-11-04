2eb81a9b4c93f0fda709fa4711c16ae429b66308
Takashi Irie <takashi@automattic.com>
2024-11-04 14:20:33 +0000

    Update Footer

--- indice/patterns/footer.php
+++ indice/patterns/footer.php
@@ -1,29 +1,17 @@
 <?php
 /**
- * Title: Footer
+ * Title: footer
  * Slug: indice/footer
- * Categories: footer
- * Block Types: core/template-part/footer
+ * Inserter: no
  */
-
-declare( strict_types = 1 );
 ?>
-
 <!-- wp:group {"style":{"spacing":{"blockGap":"0px","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
-	<!-- wp:paragraph -->
-	<p><?php echo esc_html__( 'Indice –––––––––', 'indice' ); ?></p>
-	<!-- /wp:paragraph -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph -->
+<p><?php esc_html_e('Indice –––––––––', 'indice');?></p>
+<!-- /wp:paragraph -->
 
-	<!-- wp:paragraph -->
-	<p><?php
-		/* Translators: WordPress link. */
-		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'indice' ) ) . '" rel="nofollow">WordPress</a>';
-		echo sprintf(
-			esc_html__( 'Designed with %1$s', 'indice' ),
-			$wordpress_link
-		);
-	?></p>
-	<!-- /wp:paragraph -->
-</div>
+<!-- wp:paragraph -->
+<p><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
+echo sprintf( esc_html__( 'Designed with %1$sWordPress%2$s', 'indice' ), '<a href="' . esc_url( 'https://wordpress.org' ) . '" rel="nofollow">', '</a>' ); ?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
