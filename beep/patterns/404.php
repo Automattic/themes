b9bf1d4f6d2e53023da35efc606cb511b58427cd
Takashi Irie <takashi@automattic.com>
2024-11-05 16:10:18 +0000

    Simplify the 404 template

--- beep/patterns/404.php
+++ beep/patterns/404.php
@@ -1,13 +1,18 @@
 <?php
 /**
- * Title: 404 page
+ * Title: 404
  * Slug: beep/404
  * Inserter: no
  */
-
-declare( strict_types = 1 );
 ?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:heading {"textAlign":"left","level":1,"anchor":"oops-that-page-can-t-be-found"} -->
-<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found"><?php echo esc_html__( 'Error: 404 >> Not Found.', 'beep' ); ?></h1>
-<!-- /wp:heading -->
+<!-- wp:template-part {"slug":"navigation"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","layout":{"type":"default"}} -->
+<main class="wp-block-group has-white-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"left","level":1} -->
+<h1 class="wp-block-heading has-text-align-left" id="oops-that-page-can-t-be-found"><?php esc_html_e('Error: 404 >> Not Found.', 'beep');?></h1>
+<!-- /wp:heading --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
