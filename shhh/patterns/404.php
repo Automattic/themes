4e71d9c1472392e39f7a50f5fefefc2cd331b005
Takashi Irie <takashi@automattic.com>
2024-11-06 11:33:48 +0000

    Update 404 template

--- shhh/patterns/404.php
+++ shhh/patterns/404.php
@@ -1,13 +1,20 @@
 <?php
 /**
- * Title: A 404 page
+ * Title: 404
  * Slug: shhh/404
  * Inserter: no
  */
-
-declare( strict_types = 1 );
 ?>
+<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
 
-<!-- wp:paragraph -->
-<p><?php echo esc_html__( '⌇⌇ Oops, this page cannot be found.', 'shhh' ); ?></p>
+<!-- wp:group {"tagName":"main","layout":{"type":"constrained"}} -->
+<main class="wp-block-group"><!-- wp:paragraph -->
+<p><?php esc_html_e('⌇⌇ Oops, this page cannot be found..', 'shhh');?></p>
 <!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"4rem"} -->
+<div style="height:4rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
