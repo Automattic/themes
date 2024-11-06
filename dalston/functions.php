d98036f6b5a609ae61e8b2b3499a6a29049ccbae
Renatho De Carli Rosa <renatho@gmail.com>
2024-11-06 12:18:39 -0300

    Fix warning if index does not exist

--- dalston/functions.php
+++ dalston/functions.php
@@ -206,7 +206,7 @@ function dalston_editor_styles() {
 
 	// Hide duplicate palette colors
 	$colors_array = get_theme_mod( 'colors_manager', array( 'colors' => true ) ); // color annotations array()
-	if ( ! empty( $colors_array ) && $colors_array['colors']['txt'] != '#1E1E1E' ) { // $config-global--color-foreground-light-default;
+	if ( ! empty( $colors_array ) && ! empty( $colors_array['colors'] ) && ! empty( $colors_array['colors']['txt'] ) && $colors_array['colors']['txt'] != '#1E1E1E' ) { // $config-global--color-foreground-light-default;
 		$inline_palette_css = '.components-circular-option-picker__option-wrapper:nth-child(5),
 			.components-circular-option-picker__option-wrapper:nth-child(6) {
 				display: none;
