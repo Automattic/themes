92eb856f2f8aadd876972595bf8379014311c2c7
Takashi Irie <takashi@automattic.com>
2024-11-07 20:34:52 +0000

    Simplified 404 template

---
+++ archivo/patterns/404.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: 404
+ * Slug: archivo/404
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"25%"} -->
+<div class="wp-block-column" style="flex-basis:25%"><!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:template-part {"slug":"header","tagName":"header"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"75%"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"34.0137%"} -->
+<div class="wp-block-column" style="flex-basis:34.0137%"><!-- wp:group {"tagName":"main","layout":{"type":"default"}} -->
+<main class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":1,"fontSize":"medium"} -->
+<h1 class="wp-block-heading has-text-align-left has-medium-font-size" id="oops-that-page-can-t-be-found"><?php esc_html_e('Oops! That page can’t be found.', 'archivo');?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'archivo');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:search {"showLabel":false,"placeholder":"<?php esc_html_e('Search...', 'archivo');?>","buttonText":"<?php esc_html_e('Search', 'archivo');?>"} /--></main>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"65.9863%"} -->
+<div class="wp-block-column" style="flex-basis:65.9863%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></main>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","align":"wide"} /--></div>
+<!-- /wp:group -->
