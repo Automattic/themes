989b18463957c2c7d5769779d2a6c8f6763d16d6
Takashi Irie <takashi@automattic.com>
2024-11-07 19:24:08 +0000

    Cleanup footer template part

--- archivo/patterns/footer.php
+++ archivo/patterns/footer.php
@@ -1,39 +1,32 @@
 <?php
 /**
- * Title: Default footer
+ * Title: footer
  * Slug: archivo/footer
- * Categories: footer
- * Block Types: core/template-part/footer
+ * Inserter: no
  */
 ?>
-
-<!-- wp:spacer {"height":"192px"} -->
-<div style="height:192px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- wp:spacer {"height":"10vw"} -->
+<div style="height:10vw" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
 <!-- wp:site-logo {"width":96,"shouldSyncIcon":false,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} /-->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-<div class="wp-block-group" style="padding-top:0;padding-bottom:var(--wp--preset--spacing--50)">
-	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-	<div class="wp-block-group">
-		<!-- wp:site-title {"level":0,"style":{"typography":{"lineHeight":1.388888889}}} /-->
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:site-title {"level":0} /-->
 
-		<!-- wp:paragraph -->
-		<p><a href="#"><?php echo esc_html__( 'Tumblr', 'archivo' ); ?></a> / <a href="#"><?php echo esc_html__( 'Instagram', 'archivo' ); ?></a> / <a href="#"><?php echo esc_html__( 'Email', 'archivo' ); ?></a></p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
+<!-- wp:paragraph -->
+<p><a href="#"><?php echo esc_html__( 'Tumblr', 'archivo' ); ?></a> / <a href="#"><?php echo esc_html__( 'Instagram', 'archivo' ); ?></a> / <a href="#"><?php echo esc_html__( 'Email', 'archivo' ); ?></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-	<!-- wp:paragraph -->
-	<p><?php
-		/* Translators: WordPress link. */
-		$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'archivo' ) ) . '" rel="nofollow">WordPress</a>';
-		echo sprintf(
-			esc_html__( 'Designed with %1$s', 'archivo' ),
-			$wordpress_link
-		);
-	?></p>
-	<!-- /wp:paragraph -->
-</div>
+<!-- wp:paragraph -->
+<p><?php
+	$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'archivo' ) ) . '" rel="nofollow">WordPress</a>';
+	echo sprintf(
+		esc_html__( 'Designed with %1$s', 'archivo' ),
+		$wordpress_link
+	);
+?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
