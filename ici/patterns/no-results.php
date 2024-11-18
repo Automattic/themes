37c8a4d1d481f723548571f36483c81cdccd6b99
Takashi Irie <takashi@automattic.com>
2024-11-18 15:54:49 +0000

    Update Search Template

--- ici/patterns/no-results.php
+++ ici/patterns/no-results.php
@@ -8,6 +8,6 @@
 declare( strict_types = 1 );
 ?>
 
-<!-- wp:paragraph -->
-<p><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'ici' ); ?></p>
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p style="margin-top:0;margin-bottom:0"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'ici' ); ?></p>
 <!-- /wp:paragraph -->
