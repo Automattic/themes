683cf7e4c3a18a7dcc3594c5c76dd5841c33f8e4
Takashi Irie <takashi@automattic.com>
2024-11-19 11:57:37 +0000

    Widen the header

--- azur/patterns/header.php
+++ azur/patterns/header.php
@@ -9,8 +9,8 @@
 declare( strict_types = 1 );
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"},"blockGap":"0","margin":{"bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained","contentSize":"800px","justifyContent":"left"}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|70"},"blockGap":"0","margin":{"bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--70);padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
-	<!-- wp:site-title /-->
+	<!-- wp:site-title {"align":"wide"} /-->
 </div>
 <!-- /wp:group -->
