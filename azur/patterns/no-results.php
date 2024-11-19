e88a66f1fd77a3aae100b68aa086dafc3274538e
Takashi Irie <takashi@automattic.com>
2024-11-19 15:38:09 +0000

    Fixed text domain error

--- azur/patterns/no-results.php
+++ azur/patterns/no-results.php
@@ -9,5 +9,5 @@ declare( strict_types = 1 );
 ?>
 
 <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<p style="margin-top:0;margin-bottom:0"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'ici' ); ?></p>
+<p style="margin-top:0;margin-bottom:0"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'azur' ); ?></p>
 <!-- /wp:paragraph -->
