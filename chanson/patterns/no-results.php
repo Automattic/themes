503dbcaf190da36e2b68c71bae97f0e496d8b1f6
Takashi Irie <takashi@automattic.com>
2024-11-21 16:56:00 +0000

    Added no results block to templates with QL

---
+++ chanson/patterns/no-results.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: No Results Content
+ * Slug: chanson/no-results
+ * Inserter: no
+ */
+declare( strict_types = 1 );
+?>
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p style="margin-top:0;margin-bottom:0"><?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'chanson' ); ?></p>
+<!-- /wp:paragraph -->
