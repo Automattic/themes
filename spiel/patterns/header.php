acffff2c1198d2decae2fb9dc8accabde081950c
Takashi Irie <takashi@automattic.com>
2024-11-25 23:04:31 +0000

    Added paddings to header template part

--- spiel/patterns/header.php
+++ spiel/patterns/header.php
@@ -9,14 +9,14 @@
 declare( strict_types = 1 );
 ?>
 
-<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|40"}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"default"}} -->
-<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40)">
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|40","left":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|40"}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
 	<!-- wp:group {"layout":{"type":"default"}} -->
 	<div class="wp-block-group">
 		<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
 		<div class="wp-block-columns are-vertically-aligned-center">
-			<!-- wp:column {"verticalAlignment":"center","width":"300px","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
-			<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:300px">
+			<!-- wp:column {"verticalAlignment":"center","width":"300px"} -->
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:300px">
 				<!-- wp:social-links {"iconColor":"base","iconColorValue":"#ffffff","size":"has-small-icon-size","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
 				<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only">
 					<!-- wp:social-link {"url":"#","service":"facebook"} /-->
@@ -35,8 +35,8 @@ declare( strict_types = 1 );
 			<div class="wp-block-column is-vertically-aligned-center"></div>
 			<!-- /wp:column -->
 
-			<!-- wp:column {"verticalAlignment":"center","width":"300px","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
-			<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:300px">
+			<!-- wp:column {"verticalAlignment":"center","width":"300px"} -->
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:300px">
 				<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"align":"center","style":{"border":{"top":{"color":"#4a4a4a","style":"solid","width":"1px"},"right":{"color":"#4a4a4a","style":"solid","width":"1px"},"bottom":{"color":"#4a4a4a","style":"solid","width":"1px"},"left":{"color":"#4a4a4a","style":"solid","width":"1px"},"radius":"0px"}},"backgroundColor":"contrast","textColor":"base","fontSize":"x-small"} /-->
 			</div>
 			<!-- /wp:column -->
@@ -49,8 +49,8 @@ declare( strict_types = 1 );
 
 	<!-- wp:site-title {"level":0,"textAlign":"center","align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"},":hover":{"color":{"text":"var:preset|color|base"}}}}}} /-->
 
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+	<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--50);">
 		<!-- wp:navigation {"align":"wide","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}},"typography":[]},"fontSize":"small"} /-->
 	</div>
 	<!-- /wp:group -->
