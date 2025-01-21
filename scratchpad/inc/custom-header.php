405a1841aa28b4ea8c9bba6fa64d7156c90f53cc
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-01-21 15:31:25 +0100

    Fix theme-check list

--- scratchpad/inc/custom-header.php
+++ scratchpad/inc/custom-header.php
@@ -43,9 +43,9 @@ function scratchpad_header_style() {
 
 	/*
 	 * If no custom options for text are set, let's bail.
-	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
+	 * get_header_textcolor() options: add_theme_support( 'custom-header' ) is default, hide text (returns 'blank') or any hex value.
 	 */
-	if ( HEADER_TEXTCOLOR === $header_text_color ) {
+	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
 		return;
 	}
 
