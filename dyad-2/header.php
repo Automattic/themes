a3c69bbff7f4e91383f86533f5a2d7372bad5ab6
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-01-21 17:35:48 +0100

    Fix theme check list

--- dyad-2/header.php
+++ dyad-2/header.php
@@ -19,6 +19,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+}
+?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'dyad-2' ); ?></a>
 
