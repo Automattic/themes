ad62e348e34a1d0649ac119ca9e875d3dbafa2c0
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2025-01-21 17:43:01 +0100

    Fix theme check list

--- dara/header.php
+++ dara/header.php
@@ -20,6 +20,11 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+}
+?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'dara' ); ?></a>
 
